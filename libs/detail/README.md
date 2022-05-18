ad93fd87b37a1ccb06a46f5839cd8dc503565f89
shibainuudon <shibainuudon@gmail.com>
2022-05-19 01:04:18 +0900

    [Detail]decay_copy関数を追加 refs #9

---
+++ libs/detail/README.md
@@ -0,0 +1,6 @@
+﻿# Hamon.Detail
+
+ユーザーへ提供される関数ではなく、HamonCore内部の実装のための関数たち
+
+## 依存ライブラリ
+* Hamon.Config
---
+++ libs/detail/include/hamon/detail/decay_copy.hpp
@@ -0,0 +1,37 @@
+﻿/**
+ *	@file	decay_copy.hpp
+ *
+ *	@brief	decay_copy の定義
+ */
+
+#ifndef HAMON_DETAIL_DECAY_COPY_HPP
+#define HAMON_DETAIL_DECAY_COPY_HPP
+
+#include <hamon/config.hpp>
+#include <type_traits>
+#include <utility>
+
+namespace hamon
+{
+
+namespace ranges
+{
+
+namespace detail
+{
+
+template <typename T>
+inline HAMON_CONSTEXPR typename std::decay<T>::type
+decay_copy(T&& t)
+HAMON_NOEXCEPT_IF((std::is_nothrow_convertible<T, typename std::decay<T>::type>::value))
+{
+	return std::forward<T>(t);
+}
+
+}	// namespace detail
+
+}	// namespace ranges
+
+}	// namespace hamon
+
+#endif // HAMON_DETAIL_DECAY_COPY_HPP
