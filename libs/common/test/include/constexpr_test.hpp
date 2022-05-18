b1ed2970974006395309d0f877f868bd74c7a761
shibainuudon <shibainuudon@gmail.com>
2022-05-19 00:13:34 +0900

    HAMON_CXX20_CONSTEXPR_EXPECT_EQマクロを追加

--- libs/common/test/include/constexpr_test.hpp
+++ libs/common/test/include/constexpr_test.hpp
@@ -99,4 +99,32 @@
 
 #endif
 
+
+#if defined(HAMON_HAS_CXX20_CONSTEXPR)
+
+#  define HAMON_CXX20_CONSTEXPR_EXPECT_EQ(x, y)	\
+	static_assert((x) == (y), "");	\
+	EXPECT_EQ(x, y)
+
+#  define HAMON_CXX20_CONSTEXPR_EXPECT_TRUE(x)	\
+	static_assert((x), #x);	\
+	EXPECT_TRUE(x)
+
+#  define HAMON_CXX20_CONSTEXPR_EXPECT_FALSE(x)	\
+	static_assert(!(x), #x);	\
+	EXPECT_FALSE(x)
+
+#else
+
+#  define HAMON_CXX20_CONSTEXPR_EXPECT_EQ(x, y)	\
+	EXPECT_EQ(x, y)
+
+#  define HAMON_CXX20_CONSTEXPR_EXPECT_TRUE(x)	\
+	EXPECT_TRUE(x)
+
+#  define HAMON_CXX20_CONSTEXPR_EXPECT_FALSE(x)	\
+	EXPECT_FALSE(x)
+
+#endif
+
 #endif // UNIT_TEST_FND_TEST_COMMON_CONSTEXPR_TEST_HPP
