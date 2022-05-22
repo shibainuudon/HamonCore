fa549ce118186012d987cc866e9676adf6b0b84f
shibainuudon <shibainuudon@gmail.com>
2022-05-23 00:06:48 +0900

    [Detail]decay_copy修正

--- libs/detail/include/hamon/detail/decay_copy.hpp
+++ libs/detail/include/hamon/detail/decay_copy.hpp
@@ -7,14 +7,12 @@
 #ifndef HAMON_DETAIL_DECAY_COPY_HPP
 #define HAMON_DETAIL_DECAY_COPY_HPP
 
+#include <hamon/type_traits/is_nothrow_convertible.hpp>
 #include <hamon/config.hpp>
 #include <type_traits>
 #include <utility>
 
 namespace hamon
-{
-
-namespace ranges
 {
 
 namespace detail
@@ -23,14 +21,12 @@ namespace detail
 template <typename T>
 inline HAMON_CONSTEXPR typename std::decay<T>::type
 decay_copy(T&& t)
-HAMON_NOEXCEPT_IF((std::is_nothrow_convertible<T, typename std::decay<T>::type>::value))
+HAMON_NOEXCEPT_IF((hamon::is_nothrow_convertible<T, typename std::decay<T>::type>::value))
 {
 	return std::forward<T>(t);
 }
 
 }	// namespace detail
-
-}	// namespace ranges
 
 }	// namespace hamon
 
