7118251bffb9d6829c5f400ddacba425fec0b5cd
shibainuudon <shibainuudon@gmail.com>
2022-05-25 16:57:24 +0900

    [TypeTraits]remove_all_extents_tを追加 #47

---
+++ libs/type_traits/include/hamon/type_traits/remove_all_extents.hpp
@@ -0,0 +1,30 @@
+﻿/**
+ *	@file	remove_all_extents.hpp
+ *
+ *	@brief	remove_all_extents の定義
+ */
+
+#ifndef HAMON_TYPE_TRAITS_REMOVE_ALL_EXTENTS_HPP
+#define HAMON_TYPE_TRAITS_REMOVE_ALL_EXTENTS_HPP
+
+#include <type_traits>
+
+namespace hamon
+{
+
+/**
+ *	@brief		配列型Tから全ての次元を除去する。
+ *
+ *	@tparam		T
+ *
+ *	remove_all_extentsは、型Tが、何らかの型Uの多次元配列型である場合はU型を、
+ *	そうでなければ型Tをメンバ型typeとして定義する。
+ */
+using std::remove_all_extents;
+
+template <typename T>
+using remove_all_extents_t = typename remove_all_extents<T>::type;
+
+}	// namespace hamon
+
+#endif // HAMON_TYPE_TRAITS_REMOVE_ALL_EXTENTS_HPP
---
+++ libs/type_traits/test/src/unit_test_type_traits_remove_all_extents.cpp
@@ -0,0 +1,57 @@
+﻿/**
+ *	@file	unit_test_type_traits_remove_all_extents.cpp
+ *
+ *	@brief	remove_all_extents のテスト
+ */
+
+#include <hamon/type_traits/remove_all_extents.hpp>
+#include <gtest/gtest.h>
+#include "type_traits_test_utility.hpp"
+
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, /**/          , /**/);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const         , const);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,       volatile,       volatile);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const volatile, const volatile);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,                &,                &);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const          &, const          &);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,       volatile &,       volatile &);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const volatile &, const volatile &);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, *               , *               );
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, * const         , * const         );
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, *       volatile, *       volatile);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, * const volatile, * const volatile);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const          *, const          *);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,       volatile *,       volatile *);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const volatile *, const volatile *);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,                [2],               );
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const          [2], const         );
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,       volatile [2],       volatile);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const volatile [2], const volatile);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,                [2][3],               );
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const          [2][3], const         );
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,       volatile [2][3],       volatile);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const volatile [2][3], const volatile);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,                [5][10][20],               );
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const          [5][10][20], const         );
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,       volatile [5][10][20],       volatile);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const volatile [5][10][20], const volatile);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,                [],               );
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const          [], const         );
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,       volatile [],       volatile);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const volatile [], const volatile);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,                [][3],               );
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const          [][3], const         );
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,       volatile [][3],       volatile);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const volatile [][3], const volatile);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,                (&)[2],                (&)[2]);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const          (&)[2], const          (&)[2]);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,       volatile (&)[2],       volatile (&)[2]);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const volatile (&)[2], const volatile (&)[2]);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,                &&,                &&);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const          &&, const          &&);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,       volatile &&,       volatile &&);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const volatile &&, const volatile &&);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,                (&&)[2],                (&&)[2]);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const          (&&)[2], const          (&&)[2]);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents,       volatile (&&)[2],       volatile (&&)[2]);
+HAMON_TRANSFORM_TEST(hamon::remove_all_extents, const volatile (&&)[2], const volatile (&&)[2]);
