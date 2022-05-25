a61c25fc836f54266438471e42540779cf6117fe
shibainuudon <shibainuudon@gmail.com>
2022-05-25 10:54:39 +0900

    [Concepts]copyableを追加 refs #30

---
+++ libs/concepts/include/hamon/concepts/copyable.hpp
@@ -0,0 +1,98 @@
+﻿/**
+ *	@file	copyable.hpp
+ *
+ *	@brief	copyable の定義
+ */
+
+#ifndef HAMON_CONCEPTS_COPYABLE_HPP
+#define HAMON_CONCEPTS_COPYABLE_HPP
+
+#include <hamon/concepts/config.hpp>
+
+#if defined(HAMON_USE_STD_CONCEPTS)
+
+namespace hamon
+{
+
+using std::copyable;
+
+}	// namespace hamon
+
+#elif defined(HAMON_HAS_CXX20_CONCEPTS)
+
+#include <hamon/concepts/copy_constructible.hpp>
+#include <hamon/concepts/movable.hpp>
+#include <hamon/concepts/assignable_from.hpp>
+
+namespace hamon
+{
+
+template <typename T>
+concept copyable =
+	hamon::copy_constructible<T> &&
+	hamon::movable<T> &&
+	hamon::assignable_from<T&, T&> &&
+	hamon::assignable_from<T&, const T&> &&
+	hamon::assignable_from<T&, const T>;
+
+}	// namespace hamon
+
+#else
+
+#include <hamon/concepts/copy_constructible.hpp>
+#include <hamon/concepts/movable.hpp>
+#include <hamon/concepts/assignable_from.hpp>
+#include <hamon/type_traits/conjunction.hpp>
+#include <type_traits>
+
+namespace hamon
+{
+
+namespace detail
+{
+
+template <typename T>
+struct copyable_impl
+{
+private:
+	template <typename U>
+	static auto test(int) -> hamon::conjunction<
+		hamon::copy_constructible<U>,
+		hamon::movable<U>,
+		hamon::assignable_from<U&, U&>,
+		hamon::assignable_from<U&, const U&>,
+		hamon::assignable_from<U&, const U>>;
+
+	template <typename U>
+	static auto test(...) -> std::false_type;
+
+public:
+	using type = decltype(test<T>(0));
+};
+
+}	// namespace detail
+
+template <typename T>
+using copyable =
+	typename detail::copyable_impl<T>::type;
+
+}	// namespace hamon
+
+#endif
+
+#include <hamon/type_traits/bool_constant.hpp>
+
+namespace hamon
+{
+
+template <typename T>
+using copyable_t =
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+	hamon::bool_constant<hamon::copyable<T>>;
+#else
+	hamon::copyable<T>;
+#endif
+
+}	// namespace hamon
+
+#endif // HAMON_CONCEPTS_COPYABLE_HPP
---
+++ libs/concepts/test/src/unit_test_concepts_copyable.cpp
@@ -0,0 +1,117 @@
+﻿/**
+ *	@file	unit_test_fnd_concepts_copyable.cpp
+ *
+ *	@brief	copyable のテスト
+ */
+
+#include <hamon/concepts/copyable.hpp>
+#include <hamon/config.hpp>
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+#  define HAMON_COPYABLE_TEST(B, T)	\
+	static_assert(B == hamon::copyable<T>, "");	\
+	static_assert(B == hamon::copyable_t<T>::value, "")
+#else
+#  define HAMON_COPYABLE_TEST(B, T)	\
+	static_assert(B == hamon::copyable<T>::value, "");	\
+	static_assert(B == hamon::copyable_t<T>::value, "")
+#endif
+
+namespace hamon_concepts_test
+{
+
+namespace copyable_test
+{
+
+HAMON_COPYABLE_TEST(true,  int);
+HAMON_COPYABLE_TEST(true,  int*);
+HAMON_COPYABLE_TEST(false, int&);
+HAMON_COPYABLE_TEST(false, void);
+HAMON_COPYABLE_TEST(false, void());
+HAMON_COPYABLE_TEST(false, void() noexcept);
+HAMON_COPYABLE_TEST(false, void() const);
+
+struct Trivial { };
+HAMON_COPYABLE_TEST(true,  Trivial);
+
+struct NotTrivial
+{
+	NotTrivial(int) { }
+	NotTrivial(const NotTrivial&) { }
+	NotTrivial& operator=(const NotTrivial&) { return *this; }
+	~NotTrivial() { }
+};
+HAMON_COPYABLE_TEST(true,  NotTrivial);
+
+namespace N1
+{
+struct Immovable
+{
+	Immovable() = default;
+	Immovable(Immovable&&) = delete;
+};
+}
+HAMON_COPYABLE_TEST(false, N1::Immovable);
+
+struct Movable
+{
+	Movable() = default;
+	Movable(Movable&&) = default;
+	Movable& operator=(Movable&&) = default;
+};
+HAMON_COPYABLE_TEST(false, Movable);
+
+struct MovableAndCopyAssignable
+{
+	MovableAndCopyAssignable() = default;
+	MovableAndCopyAssignable(MovableAndCopyAssignable&&) = default;
+	MovableAndCopyAssignable& operator=(MovableAndCopyAssignable&&) = default;
+	MovableAndCopyAssignable& operator=(const MovableAndCopyAssignable&) = default;
+};
+HAMON_COPYABLE_TEST(false, MovableAndCopyAssignable);
+
+struct MovableAndCopyConstructible
+{
+	MovableAndCopyConstructible() = default;
+	MovableAndCopyConstructible(MovableAndCopyConstructible&&) = default;
+	MovableAndCopyConstructible(const MovableAndCopyConstructible&) = default;
+	MovableAndCopyConstructible& operator=(MovableAndCopyConstructible&&) = default;
+};
+HAMON_COPYABLE_TEST(false, MovableAndCopyConstructible);
+
+namespace N2
+{
+struct Swappable
+{
+	Swappable() = default;
+	Swappable(Swappable&&) = default;
+	friend void swap(Swappable&, Swappable&) { }
+};
+}
+HAMON_COPYABLE_TEST(false, N2::Swappable);
+
+struct NotAssignable
+{
+	NotAssignable() = default;
+	NotAssignable(NotAssignable&&) = default;
+	NotAssignable& operator=(NotAssignable&&) = default;
+	NotAssignable(const NotAssignable&) = default;
+	NotAssignable& operator=(const NotAssignable&) = delete;
+	friend void swap(NotAssignable&, NotAssignable&) { }
+};
+HAMON_COPYABLE_TEST(false, NotAssignable);
+
+struct NotSwappable
+{
+	NotSwappable() = default;
+	NotSwappable(const NotSwappable&) = default;
+	NotSwappable& operator=(const NotSwappable&) = default;
+};
+void swap(NotSwappable&, NotSwappable&) = delete;
+HAMON_COPYABLE_TEST(true,  NotSwappable); // ranges::swap still works!
+
+}	// namespace copyable_test
+
+}	// namespace hamon_concepts_test
+
+#undef HAMON_COPYABLE_TEST
