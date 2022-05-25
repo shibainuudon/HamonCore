a7c93597d9f854a423dd204cd848cca0f70269a3
shibainuudon <shibainuudon@gmail.com>
2022-05-25 10:31:54 +0900

    [Detail]overload_priorityを追加

---
+++ libs/detail/include/hamon/detail/overload_priority.hpp
@@ -0,0 +1,30 @@
+﻿/**
+ *	@file	overload_priority.hpp
+ *
+ *	@brief	overload_priority の定義
+ */
+
+#ifndef HAMON_DETAIL_OVERLOAD_PRIORITY_HPP
+#define HAMON_DETAIL_OVERLOAD_PRIORITY_HPP
+
+#include <cstddef>
+
+namespace hamon
+{
+
+namespace detail
+{
+
+template <std::size_t N>
+struct overload_priority : public overload_priority<N - 1>
+{};
+
+template <>
+struct overload_priority<0>
+{};
+
+}	// namespace detail
+
+}	// namespace hamon
+
+#endif // HAMON_DETAIL_OVERLOAD_PRIORITY_HPP
