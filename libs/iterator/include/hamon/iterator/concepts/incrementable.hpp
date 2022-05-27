d2704c8ce891c9a446d48be501ddef857924f8ee
shibainuudon <shibainuudon@gmail.com>
2022-05-28 00:26:20 +0900

    [Iterator]incrementableコンセプトを追加 refs #63

---
+++ libs/iterator/include/hamon/iterator/concepts/incrementable.hpp
@@ -0,0 +1,93 @@
+﻿/**
+ *	@file	incrementable.hpp
+ *
+ *	@brief	incrementable の定義
+ */
+
+#ifndef HAMON_ITERATOR_CONCEPTS_INCREMENTABLE_HPP
+#define HAMON_ITERATOR_CONCEPTS_INCREMENTABLE_HPP
+
+#include <hamon/iterator/config.hpp>
+
+#if defined(HAMON_USE_STD_RANGES_ITERATOR)
+
+namespace hamon
+{
+
+using std::incrementable;
+
+}	// namespace hamon
+
+#else
+
+#include <hamon/iterator/concepts/weakly_incrementable.hpp>
+#include <hamon/concepts/regular.hpp>
+#include <hamon/concepts/same_as.hpp>
+#include <hamon/type_traits/conjunction.hpp>
+#include <hamon/type_traits/enable_if.hpp>
+#include <hamon/config.hpp>
+#include <type_traits>
+#include <utility>
+
+namespace hamon
+{
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+
+template <typename Iter>
+concept incrementable =
+	hamon::regular<Iter> &&
+	hamon::weakly_incrementable<Iter> &&
+	requires(Iter i) { { i++ } -> hamon::same_as<Iter>; };
+
+#else
+
+namespace detail
+{
+
+template <typename Iter>
+struct incrementable_impl
+{
+private:
+	template <typename I2,
+		typename = hamon::enable_if_t<hamon::regular<I2>::value>,
+		typename = hamon::enable_if_t<hamon::weakly_incrementable<I2>::value>,
+		typename T = decltype(std::declval<I2&>()++)
+	>
+	static auto test(int) -> hamon::same_as<T, I2>;
+
+	template <typename I2>
+	static auto test(...) -> std::false_type;
+
+public:
+	using type = decltype(test<Iter>(0));
+};
+
+}	// namespace detail
+
+template <typename Iter>
+using incrementable =
+	typename detail::incrementable_impl<Iter>::type;
+
+#endif
+
+}	// namespace hamon
+
+#endif
+
+#include <hamon/type_traits/bool_constant.hpp>
+
+namespace hamon
+{
+
+template <typename Iter>
+using incrementable_t =
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+	hamon::bool_constant<hamon::incrementable<Iter>>;
+#else
+	hamon::incrementable<Iter>;
+#endif
+
+}	// namespace hamon
+
+#endif // HAMON_ITERATOR_CONCEPTS_INCREMENTABLE_HPP
---
+++ libs/iterator/test/src/concepts/unit_test_iterator_concepts_incrementable.cpp
@@ -0,0 +1,102 @@
+﻿/**
+ *	@file	unit_test_iterator_concepts_incrementable.cpp
+ *
+ *	@brief	incrementable のテスト
+ */
+
+#include <hamon/iterator/concepts/incrementable.hpp>
+#include <hamon/config.hpp>
+#include <memory>
+#include <vector>
+#include <iterator>
+#include "iterator_test.hpp"
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+#  define HAMON_INCREMENTABLE_TEST(B, ...)	\
+	static_assert(B == hamon::incrementable<__VA_ARGS__>, "");	\
+	static_assert(B == hamon::incrementable_t<__VA_ARGS__>::value, "")
+#else
+#  define HAMON_INCREMENTABLE_TEST(B, ...)	\
+	static_assert(B == hamon::incrementable<__VA_ARGS__>::value, "");	\
+	static_assert(B == hamon::incrementable_t<__VA_ARGS__>::value, "")
+#endif
+
+namespace hamon_iterator_test
+{
+
+namespace incrementable_test
+{
+
+HAMON_INCREMENTABLE_TEST(true,  int               *);
+HAMON_INCREMENTABLE_TEST(true,  int const         *);
+HAMON_INCREMENTABLE_TEST(true,  int       volatile*);
+HAMON_INCREMENTABLE_TEST(true,  int const volatile*);
+HAMON_INCREMENTABLE_TEST(false, int               * const);
+HAMON_INCREMENTABLE_TEST(false, int const         * const);
+HAMON_INCREMENTABLE_TEST(false, int       volatile* const);
+HAMON_INCREMENTABLE_TEST(false, int const volatile* const);
+
+HAMON_INCREMENTABLE_TEST(false, void               *);
+HAMON_INCREMENTABLE_TEST(false, void const         *);
+HAMON_INCREMENTABLE_TEST(false, void       volatile*);
+HAMON_INCREMENTABLE_TEST(false, void const volatile*);
+HAMON_INCREMENTABLE_TEST(false, void               * const);
+HAMON_INCREMENTABLE_TEST(false, void const         * const);
+HAMON_INCREMENTABLE_TEST(false, void       volatile* const);
+HAMON_INCREMENTABLE_TEST(false, void const volatile* const);
+
+HAMON_INCREMENTABLE_TEST(false, std::unique_ptr<int>);
+HAMON_INCREMENTABLE_TEST(false, std::shared_ptr<int>);
+HAMON_INCREMENTABLE_TEST(false, std::vector<int>);
+HAMON_INCREMENTABLE_TEST(true,  std::vector<int>::iterator);
+HAMON_INCREMENTABLE_TEST(true,  std::vector<int>::const_iterator);
+HAMON_INCREMENTABLE_TEST(true,  std::istream_iterator<int>);
+HAMON_INCREMENTABLE_TEST(false, std::ostream_iterator<int>);
+
+HAMON_INCREMENTABLE_TEST(false, int[]);
+HAMON_INCREMENTABLE_TEST(false, int[2]);
+HAMON_INCREMENTABLE_TEST(false, int&);
+HAMON_INCREMENTABLE_TEST(false, int&&);
+
+struct X
+{
+	friend auto operator++(X&)->X&;
+	friend auto operator++(X&, int)->X&;
+	using difference_type = int;
+};
+
+struct Y
+{
+	friend auto operator++(Y&)->Y&;
+	friend auto operator++(Y&, int)->Y; // prvalueを返す必要がある
+	friend bool operator==(const Y&, const Y&);
+	friend bool operator!=(const Y&, const Y&);
+	using difference_type = int;
+};
+
+HAMON_INCREMENTABLE_TEST(false, X);
+HAMON_INCREMENTABLE_TEST(false, X&);
+HAMON_INCREMENTABLE_TEST(false, X const);
+HAMON_INCREMENTABLE_TEST(false, X const&);
+HAMON_INCREMENTABLE_TEST(true,  Y);
+HAMON_INCREMENTABLE_TEST(false, Y&);
+HAMON_INCREMENTABLE_TEST(false, Y const);
+HAMON_INCREMENTABLE_TEST(false, Y const&);
+
+HAMON_INCREMENTABLE_TEST(false, weakly_incrementable_wrapper<int>);
+HAMON_INCREMENTABLE_TEST(true,  incrementable_wrapper<int>);
+HAMON_INCREMENTABLE_TEST(false, indirectly_readable_wrapper<int>);
+HAMON_INCREMENTABLE_TEST(false, indirectly_writable_wrapper<int>);
+HAMON_INCREMENTABLE_TEST(false, input_or_output_iterator_wrapper<int>);
+HAMON_INCREMENTABLE_TEST(false, input_iterator_wrapper<int>);
+HAMON_INCREMENTABLE_TEST(false, output_iterator_wrapper<int>);
+HAMON_INCREMENTABLE_TEST(true,  forward_iterator_wrapper<int>);
+HAMON_INCREMENTABLE_TEST(true,  bidirectional_iterator_wrapper<int>);
+HAMON_INCREMENTABLE_TEST(true,  random_access_iterator_wrapper<int>);
+HAMON_INCREMENTABLE_TEST(true,  contiguous_iterator_wrapper<int>);
+
+}	// namespace incrementable_test
+
+}	// namespace hamon_iterator_test
+
+#undef HAMON_INCREMENTABLE_TEST
