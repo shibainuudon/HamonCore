50fd33876835862ca01fdf913b63a91913a3f280
shibainuudon <shibainuudon@gmail.com>
2022-05-28 01:02:28 +0900

    [TypeTraits]add_cvを追加 refs #70

---
+++ libs/type_traits/include/hamon/type_traits/add_cv.hpp
@@ -0,0 +1,33 @@
+﻿/**
+ *	@file	add_cv.hpp
+ *
+ *	@brief	add_cv の定義
+ */
+
+#ifndef HAMON_TYPE_TRAITS_ADD_CV_HPP
+#define HAMON_TYPE_TRAITS_ADD_CV_HPP
+
+#include <type_traits>
+
+namespace hamon
+{
+
+/**
+ *	@brief		型をconst-volatile修飾する
+ *
+ *	@tparam		T
+ *
+ *	add_cvは、型Tにconst修飾およびvolatile修飾両方を付加した型を、メンバ型typeとして定義する。
+ *	型Tが参照、関数、すでに最上位がconst-volatile修飾された型である場合は、型Tをそのままメンバ型typeとして定義する。
+ */
+using std::add_cv;
+
+/**
+ *	@brief	add_cvのエイリアステンプレート
+ */
+template <typename T>
+using add_cv_t = typename add_cv<T>::type;
+
+}	// namespace hamon
+
+#endif // HAMON_TYPE_TRAITS_ADD_CV_HPP
---
+++ libs/type_traits/test/src/unit_test_type_traits_add_cv.cpp
@@ -0,0 +1,66 @@
+﻿/**
+ *	@file	unit_test_type_traits_add_cv.cpp
+ *
+ *	@brief	add_cv のテスト
+ */
+
+#include <hamon/type_traits/add_cv.hpp>
+#include <gtest/gtest.h>
+#include <type_traits>
+#include "type_traits_test_utility.hpp"
+
+HAMON_TRANSFORM_TEST(hamon::add_cv, /**/,           const volatile);
+HAMON_TRANSFORM_TEST(hamon::add_cv, const,          const volatile);
+HAMON_TRANSFORM_TEST(hamon::add_cv,       volatile, const volatile);
+HAMON_TRANSFORM_TEST(hamon::add_cv, const volatile, const volatile);
+HAMON_TRANSFORM_TEST(hamon::add_cv,                &,                &);
+HAMON_TRANSFORM_TEST(hamon::add_cv, const          &, const          &);
+HAMON_TRANSFORM_TEST(hamon::add_cv,       volatile &,       volatile &);
+HAMON_TRANSFORM_TEST(hamon::add_cv, const volatile &, const volatile &);
+HAMON_TRANSFORM_TEST(hamon::add_cv, *,               *const volatile);
+HAMON_TRANSFORM_TEST(hamon::add_cv, *const,          *const volatile);
+HAMON_TRANSFORM_TEST(hamon::add_cv, *      volatile, *const volatile);
+HAMON_TRANSFORM_TEST(hamon::add_cv, *const volatile, *const volatile);
+HAMON_TRANSFORM_TEST(hamon::add_cv, const          *, const          * const volatile);
+HAMON_TRANSFORM_TEST(hamon::add_cv,       volatile *,       volatile * const volatile);
+HAMON_TRANSFORM_TEST(hamon::add_cv, const volatile *, const volatile * const volatile);
+HAMON_TRANSFORM_TEST(hamon::add_cv,               [2], const volatile[2]);
+HAMON_TRANSFORM_TEST(hamon::add_cv, const         [2], const volatile[2]);
+HAMON_TRANSFORM_TEST(hamon::add_cv,       volatile[2], const volatile[2]);
+HAMON_TRANSFORM_TEST(hamon::add_cv, const volatile[2], const volatile[2]);
+HAMON_TRANSFORM_TEST(hamon::add_cv,                (&)[2],                (&)[2]);
+HAMON_TRANSFORM_TEST(hamon::add_cv, const          (&)[2], const          (&)[2]);
+HAMON_TRANSFORM_TEST(hamon::add_cv,       volatile (&)[2],       volatile (&)[2]);
+HAMON_TRANSFORM_TEST(hamon::add_cv, const volatile (&)[2], const volatile (&)[2]);
+HAMON_TRANSFORM_TEST(hamon::add_cv,                &&,                &&);
+HAMON_TRANSFORM_TEST(hamon::add_cv, const          &&, const          &&);
+HAMON_TRANSFORM_TEST(hamon::add_cv,       volatile &&,       volatile &&);
+HAMON_TRANSFORM_TEST(hamon::add_cv, const volatile &&, const volatile &&);
+HAMON_TRANSFORM_TEST(hamon::add_cv,                (&&)[2],                (&&)[2]);
+HAMON_TRANSFORM_TEST(hamon::add_cv, const          (&&)[2], const          (&&)[2]);
+HAMON_TRANSFORM_TEST(hamon::add_cv,       volatile (&&)[2],       volatile (&&)[2]);
+HAMON_TRANSFORM_TEST(hamon::add_cv, const volatile (&&)[2], const volatile (&&)[2]);
+
+#define HAMON_ADD_CV_TEST(T1, T2)	\
+	static_assert(std::is_same<hamon::add_cv_t<T1>, T2>::value, "");	\
+	static_assert(std::is_same<hamon::add_cv<T1>::type, T2>::value, "")
+
+HAMON_ADD_CV_TEST(               void, const volatile void);
+HAMON_ADD_CV_TEST(const          void, const volatile void);
+HAMON_ADD_CV_TEST(      volatile void, const volatile void);
+HAMON_ADD_CV_TEST(const volatile void, const volatile void);
+HAMON_ADD_CV_TEST(               void*,                void* const volatile);
+HAMON_ADD_CV_TEST(const          void*, const          void* const volatile);
+HAMON_ADD_CV_TEST(      volatile void*,       volatile void* const volatile);
+HAMON_ADD_CV_TEST(const volatile void*, const volatile void* const volatile);
+HAMON_ADD_CV_TEST(int(int), int(int));
+HAMON_ADD_CV_TEST(void (UDT::*)(void), void (UDT::* const volatile)(void));
+HAMON_ADD_CV_TEST(int (UDT::*)(int), int (UDT::* const volatile)(int));
+HAMON_ADD_CV_TEST(int (UDT::*)(int, float), int (UDT::* const volatile)(int, float));
+HAMON_ADD_CV_TEST(int (UDT::*)(int) const, int (UDT::* const volatile)(int) const);
+HAMON_ADD_CV_TEST(int (UDT::*)(int) volatile, int (UDT::* const volatile)(int) volatile);
+HAMON_ADD_CV_TEST(int (UDT::*)(int) const volatile, int (UDT::* const volatile)(int) const volatile);
+HAMON_ADD_CV_TEST(int (UDT::*), int (UDT::* const volatile));
+HAMON_ADD_CV_TEST(const float (UDT::*), const float (UDT::* const volatile));
+
+#undef HAMON_ADD_CV_TEST
