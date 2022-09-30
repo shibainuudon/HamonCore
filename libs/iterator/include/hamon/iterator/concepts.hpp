8e7de65c5ee1b9dce08394a940172bbe7fdeafd5
shibainuudon <shibainuudon@gmail.com>
2022-10-01 01:05:26 +0900

    [Iterator]indirect_equivalence_relationを追加

--- libs/iterator/include/hamon/iterator/concepts.hpp
+++ libs/iterator/include/hamon/iterator/concepts.hpp
@@ -13,14 +13,28 @@
 #include <hamon/iterator/concepts/forward_iterator.hpp>
 #include <hamon/iterator/concepts/incrementable.hpp>
 #include <hamon/iterator/concepts/indirect_binary_predicate.hpp>
+#include <hamon/iterator/concepts/indirect_equivalence_relation.hpp>
+//#include <hamon/iterator/concepts/indirect_strict_weak_order.hpp>
+//#include <hamon/iterator/concepts/indirect_unary_predicate.hpp>
+//#include <hamon/iterator/concepts/indirectly_comparable.hpp>
 #include <hamon/iterator/concepts/indirectly_copyable.hpp>
+//#include <hamon/iterator/concepts/indirectly_copyable_storable.hpp>
+//#include <hamon/iterator/concepts/indirectly_movable.hpp>
+//#include <hamon/iterator/concepts/indirectly_movable_storable.hpp>
 #include <hamon/iterator/concepts/indirectly_readable.hpp>
+//#include <hamon/iterator/concepts/indirectly_regular_unary_invocable.hpp>
+//#include <hamon/iterator/concepts/indirectly_swappable.hpp>
+//#include <hamon/iterator/concepts/indirectly_unary_invocable.hpp>
 #include <hamon/iterator/concepts/indirectly_writable.hpp>
 #include <hamon/iterator/concepts/input_iterator.hpp>
 #include <hamon/iterator/concepts/input_or_output_iterator.hpp>
+//#include <hamon/iterator/concepts/mergeable.hpp>
+//#include <hamon/iterator/concepts/output_iterator.hpp>
+//#include <hamon/iterator/concepts/permutable.hpp>
 #include <hamon/iterator/concepts/random_access_iterator.hpp>
 #include <hamon/iterator/concepts/sentinel_for.hpp>
 #include <hamon/iterator/concepts/sized_sentinel_for.hpp>
+//#include <hamon/iterator/concepts/sortable.hpp>
 #include <hamon/iterator/concepts/weakly_incrementable.hpp>
 
 #endif // HAMON_ITERATOR_CONCEPTS_HPP
---
+++ libs/iterator/test/src/concepts/unit_test_iterator_concepts_indirect_equivalence_relation.cpp
@@ -0,0 +1,54 @@
+﻿/**
+ *	@file	unit_test_iterator_concepts_indirect_equivalence_relation.cpp
+ *
+ *	@brief	indirect_equivalence_relation のテスト
+ */
+
+#include <hamon/iterator/concepts/indirect_equivalence_relation.hpp>
+#include <hamon/config.hpp>
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+#  define HAMON_INDIRECT_EQUIVALENCE_RELATION_TEST(B, ...)	\
+	static_assert(B == hamon::indirect_equivalence_relation<__VA_ARGS__>, "");	\
+	static_assert(B == hamon::indirect_equivalence_relation_t<__VA_ARGS__>::value, "")
+#else
+#  define HAMON_INDIRECT_EQUIVALENCE_RELATION_TEST(B, ...)	\
+	static_assert(B == hamon::indirect_equivalence_relation<__VA_ARGS__>::value, "");	\
+	static_assert(B == hamon::indirect_equivalence_relation_t<__VA_ARGS__>::value, "")
+#endif
+
+namespace hamon_iterator_test
+{
+
+namespace indirect_equivalence_relation_test
+{
+
+struct Pred1
+{
+	bool operator()(int, int) const;
+};
+struct Pred2
+{
+	bool operator()(int) const;
+	void operator()(int, int) const;
+};
+struct Pred3
+{
+	Pred3(Pred3 const&) = delete;
+	bool operator()(int, int) const;
+};
+
+HAMON_INDIRECT_EQUIVALENCE_RELATION_TEST(true,  bool(*)(int, int), int*, int*);
+HAMON_INDIRECT_EQUIVALENCE_RELATION_TEST(false, bool(*)(int, int), int, int);
+HAMON_INDIRECT_EQUIVALENCE_RELATION_TEST(false, void(*)(int, int), int*, int*);
+HAMON_INDIRECT_EQUIVALENCE_RELATION_TEST(false, void(*)(int, int), int, int);
+HAMON_INDIRECT_EQUIVALENCE_RELATION_TEST(true,  bool(*)(int, int), short*, char*);
+HAMON_INDIRECT_EQUIVALENCE_RELATION_TEST(true,  Pred1, int*, int*);
+HAMON_INDIRECT_EQUIVALENCE_RELATION_TEST(false, Pred2, int*, int*);
+HAMON_INDIRECT_EQUIVALENCE_RELATION_TEST(false, Pred3, int*, int*);
+
+}	// namespace indirect_equivalence_relation_test
+
+}	// namespace hamon_iterator_test
+
+#undef HAMON_INDIRECT_EQUIVALENCE_RELATION_TEST
