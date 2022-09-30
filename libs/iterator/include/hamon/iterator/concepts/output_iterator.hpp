bfb949d7c7b34367c00c73c79cc846cf041cca30
shibainuudon <shibainuudon@gmail.com>
2022-10-01 02:46:28 +0900

    [Iterator]output_iteratorを追加

---
+++ libs/iterator/include/hamon/iterator/concepts/output_iterator.hpp
@@ -0,0 +1,92 @@
+﻿/**
+ *	@file	output_iterator.hpp
+ *
+ *	@brief	output_iterator の定義
+ */
+
+#ifndef HAMON_ITERATOR_CONCEPTS_OUTPUT_ITERATOR_HPP
+#define HAMON_ITERATOR_CONCEPTS_OUTPUT_ITERATOR_HPP
+
+#include <hamon/iterator/config.hpp>
+
+#if defined(HAMON_USE_STD_RANGES_ITERATOR)
+
+namespace hamon
+{
+
+using std::output_iterator;
+
+}	// namespace hamon
+
+#else
+
+#include <hamon/iterator/concepts/input_or_output_iterator.hpp>
+#include <hamon/iterator/concepts/indirectly_writable.hpp>
+#include <hamon/type_traits/bool_constant.hpp>
+#include <hamon/config.hpp>
+#include <type_traits>
+#include <utility>
+
+namespace hamon
+{
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+
+template <typename Iter, typename T>
+concept output_iterator =
+	hamon::input_or_output_iterator<Iter> &&
+	hamon::indirectly_writable<Iter, T> &&
+	requires(Iter i, T&& t) { *i++ = std::forward<T>(t); };
+
+#else
+
+namespace detail
+{
+
+template <typename Iter, typename T>
+struct output_iterator_impl
+{
+private:
+	template <typename I2, typename T2,
+		typename = hamon::enable_if_t<hamon::input_or_output_iterator<I2>::value>,
+		typename = hamon::enable_if_t<hamon::indirectly_writable<I2, T2>::value>,
+		typename = decltype(*std::declval<I2&>()++ = std::declval<T2&&>())
+	>
+	static auto test(int) -> std::true_type;
+
+	template <typename I2, typename T2>
+	static auto test(...) -> std::false_type;
+
+public:
+	using type = decltype(test<Iter, T>(0));
+};
+
+}	// namespace detail
+
+template <typename Iter, typename T>
+using output_iterator =
+	typename detail::output_iterator_impl<Iter, T>::type;
+
+#endif
+
+}	// namespace hamon
+
+#endif
+
+#include <hamon/type_traits/bool_constant.hpp>
+
+namespace hamon
+{
+
+template <typename Iter, typename T>
+using output_iterator_t =
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+	hamon::bool_constant<hamon::output_iterator<Iter, T>>;
+#else
+	hamon::output_iterator<Iter, T>;
+#endif
+
+}	// namespace hamon
+
+
+#endif // HAMON_ITERATOR_CONCEPTS_OUTPUT_ITERATOR_HPP
---
+++ libs/iterator/test/src/concepts/unit_test_iterator_concepts_output_iterator.cpp
@@ -0,0 +1,116 @@
+﻿/**
+ *	@file	unit_test_iterator_concepts_output_iterator.cpp
+ *
+ *	@brief	output_iterator のテスト
+ */
+
+#include <hamon/iterator/concepts/output_iterator.hpp>
+#include <hamon/config.hpp>
+#include <array>
+#include <vector>
+#include <list>
+#include <forward_list>
+#include <deque>
+#include <set>
+#include <string>
+//#include <string_view>
+#include "iterator_test.hpp"
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+#  define HAMON_OUTPUT_ITERATOR_TEST(B, ...)	\
+	static_assert(B == hamon::output_iterator<__VA_ARGS__>, "");	\
+	static_assert(B == hamon::output_iterator_t<__VA_ARGS__>::value, "")
+#else
+#  define HAMON_OUTPUT_ITERATOR_TEST(B, ...)	\
+	static_assert(B == hamon::output_iterator<__VA_ARGS__>::value, "");	\
+	static_assert(B == hamon::output_iterator_t<__VA_ARGS__>::value, "")
+#endif
+
+namespace hamon_iterator_test
+{
+
+namespace output_iterator_test
+{
+
+struct A {};
+
+HAMON_OUTPUT_ITERATOR_TEST(true,  int*, int);
+HAMON_OUTPUT_ITERATOR_TEST(true,  int*, const int);
+HAMON_OUTPUT_ITERATOR_TEST(true,  int*, long);
+HAMON_OUTPUT_ITERATOR_TEST(true,  void**, void*);
+HAMON_OUTPUT_ITERATOR_TEST(true,  void**, long*);
+HAMON_OUTPUT_ITERATOR_TEST(false, const int*, int);
+
+HAMON_OUTPUT_ITERATOR_TEST(false, int* const, int);
+HAMON_OUTPUT_ITERATOR_TEST(false, const int* const, int);
+HAMON_OUTPUT_ITERATOR_TEST(false, void** const, void*);
+
+HAMON_OUTPUT_ITERATOR_TEST(false, void*, void);
+HAMON_OUTPUT_ITERATOR_TEST(false, const void*, void);
+HAMON_OUTPUT_ITERATOR_TEST(false, const void*, void*);
+HAMON_OUTPUT_ITERATOR_TEST(false, volatile void*, void);
+HAMON_OUTPUT_ITERATOR_TEST(false, volatile void*, void*);
+
+HAMON_OUTPUT_ITERATOR_TEST(false, void(*)(), void(&)());
+HAMON_OUTPUT_ITERATOR_TEST(false, void(&)(), void(&)());
+HAMON_OUTPUT_ITERATOR_TEST(true,  void(**)(), void(*)());
+HAMON_OUTPUT_ITERATOR_TEST(true,  void(**)(), void(&)());
+
+// Avoid gcc 7 internal compiler error
+#if !(defined(HAMON_GCC_VERSION) && (HAMON_GCC_VERSION < 80000))
+HAMON_OUTPUT_ITERATOR_TEST(false, void(A::*)(), A*);
+HAMON_OUTPUT_ITERATOR_TEST(false, void(A::*)(), void(A::*)());
+#endif
+HAMON_OUTPUT_ITERATOR_TEST(false, int A::*, int);
+HAMON_OUTPUT_ITERATOR_TEST(false, int A::*, int A::*);
+
+HAMON_OUTPUT_ITERATOR_TEST(true,  std::array<int, 1>::iterator, int);
+HAMON_OUTPUT_ITERATOR_TEST(true,  std::array<A, 1>::iterator, A);
+HAMON_OUTPUT_ITERATOR_TEST(false, std::array<int, 1>::const_iterator, int);
+HAMON_OUTPUT_ITERATOR_TEST(false, std::array<A, 1>::const_iterator, A);
+HAMON_OUTPUT_ITERATOR_TEST(true,  std::deque<int>::iterator, int);
+HAMON_OUTPUT_ITERATOR_TEST(true,  std::deque<A>::iterator, A);
+HAMON_OUTPUT_ITERATOR_TEST(false, std::deque<int>::const_iterator, int);
+HAMON_OUTPUT_ITERATOR_TEST(false, std::deque<A>::const_iterator, A);
+HAMON_OUTPUT_ITERATOR_TEST(true,  std::forward_list<int>::iterator, int);
+HAMON_OUTPUT_ITERATOR_TEST(true,  std::forward_list<A>::iterator, A);
+HAMON_OUTPUT_ITERATOR_TEST(false, std::forward_list<int>::const_iterator, int);
+HAMON_OUTPUT_ITERATOR_TEST(false, std::forward_list<A>::const_iterator, A);
+HAMON_OUTPUT_ITERATOR_TEST(true,  std::list<int>::iterator, int);
+HAMON_OUTPUT_ITERATOR_TEST(true,  std::list<A>::iterator, A);
+HAMON_OUTPUT_ITERATOR_TEST(false, std::list<int>::const_iterator, int);
+HAMON_OUTPUT_ITERATOR_TEST(false, std::list<A>::const_iterator, A);
+HAMON_OUTPUT_ITERATOR_TEST(false, std::set<int>::iterator, int);
+HAMON_OUTPUT_ITERATOR_TEST(false, std::set<A>::iterator, A);
+HAMON_OUTPUT_ITERATOR_TEST(false, std::set<int>::const_iterator, int);
+HAMON_OUTPUT_ITERATOR_TEST(false, std::set<A>::const_iterator, A);
+HAMON_OUTPUT_ITERATOR_TEST(true,  std::string::iterator, char);
+HAMON_OUTPUT_ITERATOR_TEST(true,  std::string::iterator, int);
+HAMON_OUTPUT_ITERATOR_TEST(false, std::string::const_iterator, char);
+HAMON_OUTPUT_ITERATOR_TEST(false, std::string::const_iterator, int);
+//HAMON_OUTPUT_ITERATOR_TEST(false, std::string_view::iterator, char);
+//HAMON_OUTPUT_ITERATOR_TEST(false, std::string_view::iterator, int);
+//HAMON_OUTPUT_ITERATOR_TEST(false, std::string_view::const_iterator, char);
+//HAMON_OUTPUT_ITERATOR_TEST(false, std::string_view::const_iterator, int);
+HAMON_OUTPUT_ITERATOR_TEST(true,  std::vector<int>::iterator, int);
+HAMON_OUTPUT_ITERATOR_TEST(true,  std::vector<A>::iterator, A);
+HAMON_OUTPUT_ITERATOR_TEST(false, std::vector<int>::const_iterator, int);
+HAMON_OUTPUT_ITERATOR_TEST(false, std::vector<A>::const_iterator, A);
+
+HAMON_OUTPUT_ITERATOR_TEST(false, weakly_incrementable_wrapper<int>,     int);
+HAMON_OUTPUT_ITERATOR_TEST(false, incrementable_wrapper<int>,            int);
+HAMON_OUTPUT_ITERATOR_TEST(false, indirectly_readable_wrapper<int>,      int);
+HAMON_OUTPUT_ITERATOR_TEST(false, indirectly_writable_wrapper<int>,      int);
+HAMON_OUTPUT_ITERATOR_TEST(false, input_or_output_iterator_wrapper<int>, int);
+HAMON_OUTPUT_ITERATOR_TEST(false, input_iterator_wrapper<int>,           int);
+HAMON_OUTPUT_ITERATOR_TEST(true,  output_iterator_wrapper<int>,          int);
+HAMON_OUTPUT_ITERATOR_TEST(true,  forward_iterator_wrapper<int>,         int);
+HAMON_OUTPUT_ITERATOR_TEST(true,  bidirectional_iterator_wrapper<int>,   int);
+HAMON_OUTPUT_ITERATOR_TEST(true,  random_access_iterator_wrapper<int>,   int);
+HAMON_OUTPUT_ITERATOR_TEST(true,  contiguous_iterator_wrapper<int>,      int);
+
+}	// namespace output_iterator_test
+
+}	// namespace hamon_iterator_test
+
+#undef HAMON_OUTPUT_ITERATOR_TEST
