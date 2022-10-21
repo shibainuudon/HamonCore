6685388fcd6bf606e1ed9384ee14ec5ba7a4b778
shibainuudon <shibainuudon@gmail.com>
2022-10-21 15:34:58 +0900

    [Algorithm]equal関数を追加

---
+++ libs/algorithm/include/hamon/algorithm/equal.hpp
@@ -0,0 +1,231 @@
+﻿/**
+ *	@file	equal.hpp
+ *
+ *	@brief	equal の定義
+ */
+
+#ifndef HAMON_ALGORITHM_EQUAL_HPP
+#define HAMON_ALGORITHM_EQUAL_HPP
+
+#include <hamon/algorithm/config.hpp>
+
+#if defined(HAMON_USE_STD_ALGORITHM)
+
+#include <algorithm>
+
+namespace hamon
+{
+
+using std::equal;
+
+}	// namespace hamon
+
+#else
+
+#include <hamon/functional/equal_to.hpp>
+#include <hamon/iterator/iterator_category.hpp>
+#include <hamon/iterator/distance.hpp>
+#include <hamon/config.hpp>
+
+namespace hamon
+{
+
+namespace detail
+{
+
+template <
+	typename InputIterator1,
+	typename InputIterator2,
+	typename BinaryPredicate
+>
+inline HAMON_CXX14_CONSTEXPR bool
+equal(
+	InputIterator1 first1, InputIterator1 last1,
+	InputIterator2 first2,
+	BinaryPredicate pred)
+{
+	for (; first1 != last1; ++first1, ++first2)
+	{
+		if (!pred(*first1, *first2))
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+template <
+	typename InputIterator1,
+	typename InputIterator2,
+	typename BinaryPredicate
+>
+inline HAMON_CXX14_CONSTEXPR bool
+equal(
+	InputIterator1 first1, InputIterator1 last1,
+	InputIterator2 first2, InputIterator2 last2,
+	BinaryPredicate pred,
+	std::input_iterator_tag*,
+	std::input_iterator_tag*)
+{
+	for (; first1 != last1 && first2 != last2; ++first1, ++first2)
+	{
+		if (!pred(*first1, *first2))
+		{
+			return false;
+		}
+	}
+
+	return first1 == last1 && first2 == last2;
+}
+
+template <
+	typename RandomAccessIterator1,
+	typename RandomAccessIterator2,
+	typename BinaryPredicate
+>
+inline HAMON_CXX14_CONSTEXPR bool
+equal(
+	RandomAccessIterator1 first1, RandomAccessIterator1 last1,
+	RandomAccessIterator2 first2, RandomAccessIterator2 last2,
+	BinaryPredicate pred,
+	std::random_access_iterator_tag*,
+	std::random_access_iterator_tag*)
+{
+	if (hamon::distance(first1, last1) != hamon::distance(first2, last2))
+	{
+		return false;
+	}
+
+	return hamon::detail::equal(first1, last1, first2, pred);
+}
+
+}	// namespace detail
+
+/**
+ *	@brief	2つの範囲を等値比較する
+ *
+ *	@tparam		InputIterator1
+ *	@tparam		InputIterator2
+ *	@tparam		BinaryPredicate
+ *
+ *	@param		first1
+ *	@param		last1
+ *	@param		first2
+ *	@param		pred
+ */
+template <
+	typename InputIterator1,
+	typename InputIterator2,
+	typename BinaryPredicate
+>
+inline HAMON_CXX14_CONSTEXPR bool
+equal(
+	InputIterator1 first1, InputIterator1 last1,
+	InputIterator2 first2,
+	BinaryPredicate pred)
+{
+	return hamon::detail::equal(
+		first1, last1, first2, pred);
+}
+
+/**
+ *	@brief	2つの範囲を等値比較する
+ *
+ *	@tparam		InputIterator1
+ *	@tparam		InputIterator2
+ *
+ *	@param		first1
+ *	@param		last1
+ *	@param		first2
+ */
+template <
+	typename InputIterator1,
+	typename InputIterator2
+>
+inline HAMON_CXX14_CONSTEXPR bool
+equal(
+	InputIterator1 first1, InputIterator1 last1,
+	InputIterator2 first2)
+{
+	return hamon::equal(
+		first1, last1, first2, hamon::equal_to<>());
+}
+
+/**
+ *	@brief	2つの範囲を等値比較する
+ *
+ *	@tparam		InputIterator1
+ *	@tparam		InputIterator2
+ *	@tparam		BinaryPredicate
+ *
+ *	@param		first1
+ *	@param		last1
+ *	@param		first2
+ *	@param		last2
+ *	@param		pred
+ *
+ *	@return		もしlast1 - first1 != last2 - first2 であれば、falseを返す。
+ *				そうでない場合、[first1, last1)のイテレータi、[first2, last2)のイテレータjについて、
+ *				pred(*i, *j) が全て満たされるのであればtrueを返す。
+ *				そうでない場合はfalseを返す。
+ *
+ *	@complexity	もし InputIterator1 と InputIterator2 が共にランダムアクセスイテレータの要件を満たす場合で、
+ *				かつ、last1 - first1 != last2 - first2 の場合、1 度も述語は適用されない。
+ *				そうでない場合、最大で min(last1 - first1, last2 - first2) 回の述語が適用される。
+ */
+template <
+	typename InputIterator1,
+	typename InputIterator2,
+	typename BinaryPredicate
+>
+inline HAMON_CXX14_CONSTEXPR bool
+equal(
+	InputIterator1 first1, InputIterator1 last1,
+	InputIterator2 first2, InputIterator2 last2,
+	BinaryPredicate pred)
+{
+	using Category1 = hamon::iterator_category<InputIterator1>*;
+	using Category2 = hamon::iterator_category<InputIterator2>*;
+	return hamon::detail::equal(
+		first1, last1, first2, last2, pred, Category1(), Category2());
+}
+
+/**
+ *	@brief		2つの範囲を等値比較する
+ *
+ *	@tparam		InputIterator1
+ *	@tparam		InputIterator2
+ *
+ *	@param		first1
+ *	@param		last1
+ *	@param		first2
+ *	@param		last2
+ *
+ *	@return		もしlast1 - first1 != last2 - first2 であれば、falseを返す。
+ *				そうでない場合、[first1, last1)のイテレータi、[first2, last2)のイテレータjについて、
+ *				*i == *j が全て満たされるのであればtrueを返す。
+ *				そうでない場合はfalseを返す。
+ *
+ *	@complexity	もし InputIterator1 と InputIterator2 が共にランダムアクセスイテレータの要件を満たす場合で、
+ *				かつ、last1 - first1 != last2 - first2 の場合、1 度も比較は適用されない。
+ *				そうでない場合、最大で min(last1 - first1, last2 - first2) 回の比較が適用される。
+ */
+template <
+	typename InputIterator1,
+	typename InputIterator2
+>
+inline HAMON_CXX14_CONSTEXPR bool
+equal(
+	InputIterator1 first1, InputIterator1 last1,
+	InputIterator2 first2, InputIterator2 last2)
+{
+	return hamon::equal(
+		first1, last1, first2, last2, hamon::equal_to<>());
+}
+
+}	// namespace hamon
+
+#endif
+
+#endif // HAMON_ALGORITHM_EQUAL_HPP
---
+++ libs/algorithm/test/src/unit_test_algorithm_equal.cpp
@@ -0,0 +1,97 @@
+﻿/**
+ *	@file	unit_test_algorithm_equal.cpp
+ *
+ *	@brief	equal のテスト
+ */
+
+#include <hamon/algorithm/equal.hpp>
+#include <hamon/iterator/begin.hpp>
+#include <hamon/iterator/end.hpp>
+#include <gtest/gtest.h>
+#include <array>
+#include <vector>
+#include <list>
+#include "constexpr_test.hpp"
+
+namespace hamon_algorithm_test
+{
+
+namespace equal_test
+{
+
+GTEST_TEST(AlgorithmTest, EqualTest)
+{
+	HAMON_CONSTEXPR_OR_CONST int a1[] {0,1,2};
+	HAMON_CONSTEXPR_OR_CONST int a2[] {0,1,2};
+	HAMON_CONSTEXPR_OR_CONST int a3[] {0,1,3};
+	HAMON_CONSTEXPR_OR_CONST int a4[] {2,1,2};
+	HAMON_CONSTEXPR_OR_CONST int a5[] {0,1};
+	HAMON_CONSTEXPR_OR_CONST int a6[] {0,1,2,3};
+
+	HAMON_CXX17_CONSTEXPR std::array<float, 3> a7{{0, 1, 2}};
+	HAMON_CXX17_CONSTEXPR std::array<float, 3> a8{{1, 1, 2}};
+	HAMON_CXX17_CONSTEXPR std::array<float, 2> a9{{0, 1}};
+	HAMON_CXX17_CONSTEXPR std::array<float, 4> a10{{0, 1, 2, 3}};
+
+	const std::vector<int> v1 {0,1,2};
+	const std::vector<int> v2 {0,1,2,3};
+	const std::vector<int> v3 {0,1,2,3};
+
+	const std::list<int> l1 {0,1,2};
+	const std::list<int> l2 {0,1,2,3};
+	const std::list<int> l3 {0,1,4,3};
+
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE (hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a1), hamon::end(a1)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE (hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a2), hamon::end(a2)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_FALSE(hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a3), hamon::end(a3)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_FALSE(hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a4), hamon::end(a4)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_FALSE(hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a5), hamon::end(a5)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_FALSE(hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a6), hamon::end(a6)));
+	HAMON_CXX17_CONSTEXPR_EXPECT_TRUE (hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a7), hamon::end(a7)));
+	HAMON_CXX17_CONSTEXPR_EXPECT_FALSE(hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a8), hamon::end(a8)));
+	HAMON_CXX17_CONSTEXPR_EXPECT_FALSE(hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a9), hamon::end(a9)));
+	HAMON_CXX17_CONSTEXPR_EXPECT_FALSE(hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a10), hamon::end(a10)));
+
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE (hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a1)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE (hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a2)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_FALSE(hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a3)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_FALSE(hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a4)));
+//	HAMON_CXX14_CONSTEXPR_EXPECT_FALSE(hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a5)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE (hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a6)));
+	HAMON_CXX17_CONSTEXPR_EXPECT_TRUE (hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a7)));
+	HAMON_CXX17_CONSTEXPR_EXPECT_FALSE(hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a8)));
+//	HAMON_CXX17_CONSTEXPR_EXPECT_FALSE(hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a9)));
+	HAMON_CXX17_CONSTEXPR_EXPECT_TRUE (hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(a10)));
+
+	EXPECT_TRUE (hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(v1), hamon::end(v1)));
+	EXPECT_FALSE(hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(v2), hamon::end(v2)));
+	EXPECT_TRUE (hamon::equal(hamon::begin(v2), hamon::end(v2), hamon::begin(v3), hamon::end(v3)));
+	EXPECT_TRUE (hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(l1), hamon::end(l1)));
+	EXPECT_FALSE(hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(l2), hamon::end(l2)));
+	EXPECT_TRUE (hamon::equal(hamon::begin(l1), hamon::end(l1), hamon::begin(l1), hamon::end(l1)));
+	EXPECT_FALSE(hamon::equal(hamon::begin(l1), hamon::end(l1), hamon::begin(l2), hamon::end(l2)));
+	EXPECT_FALSE(hamon::equal(hamon::begin(l2), hamon::end(l2), hamon::begin(l1), hamon::end(l1)));
+	EXPECT_FALSE(hamon::equal(hamon::begin(l2), hamon::end(l2), hamon::begin(l3), hamon::end(l3)));
+	EXPECT_TRUE (hamon::equal(hamon::begin(v1), hamon::end(v1), hamon::begin(l1), hamon::end(l1)));
+	EXPECT_FALSE(hamon::equal(hamon::begin(v1), hamon::end(v1), hamon::begin(l2), hamon::end(l2)));
+	EXPECT_FALSE(hamon::equal(hamon::begin(v2), hamon::end(v2), hamon::begin(l1), hamon::end(l1)));
+	EXPECT_FALSE(hamon::equal(hamon::begin(v3), hamon::end(v3), hamon::begin(l3), hamon::end(l3)));
+
+	EXPECT_TRUE (hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(v1)));
+	EXPECT_TRUE (hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(v2)));
+	EXPECT_TRUE (hamon::equal(hamon::begin(v2), hamon::end(v2), hamon::begin(v3)));
+	EXPECT_TRUE (hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(l1)));
+	EXPECT_TRUE (hamon::equal(hamon::begin(a1), hamon::end(a1), hamon::begin(l2)));
+	EXPECT_TRUE (hamon::equal(hamon::begin(l1), hamon::end(l1), hamon::begin(l1)));
+	EXPECT_TRUE (hamon::equal(hamon::begin(l1), hamon::end(l1), hamon::begin(l2)));
+//	EXPECT_TRUE (hamon::equal(hamon::begin(l2), hamon::end(l2), hamon::begin(l1)));
+	EXPECT_FALSE(hamon::equal(hamon::begin(l2), hamon::end(l2), hamon::begin(l3)));
+	EXPECT_TRUE (hamon::equal(hamon::begin(v1), hamon::end(v1), hamon::begin(l1)));
+	EXPECT_TRUE (hamon::equal(hamon::begin(v1), hamon::end(v1), hamon::begin(l2)));
+//	EXPECT_TRUE (hamon::equal(hamon::begin(v2), hamon::end(v2), hamon::begin(l1)));
+	EXPECT_FALSE(hamon::equal(hamon::begin(v3), hamon::end(v3), hamon::begin(l3)));
+}
+
+}	// namespace equal_test
+
+}	// namespace hamon_algorithm_test
