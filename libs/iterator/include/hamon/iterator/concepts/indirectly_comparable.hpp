8cfbf88bfb7a6fe4966073718b8d6e575897d93f
shibainuudon <shibainuudon@gmail.com>
2022-10-21 15:52:46 +0900

    [Iterator]indirectly_comparableを追加

---
+++ libs/iterator/include/hamon/iterator/concepts/indirectly_comparable.hpp
@@ -0,0 +1,104 @@
+﻿/**
+ *	@file	indirectly_comparable.hpp
+ *
+ *	@brief	indirectly_comparable の定義
+ */
+
+#ifndef HAMON_ITERATOR_CONCEPTS_INDIRECTLY_COMPARABLE_HPP
+#define HAMON_ITERATOR_CONCEPTS_INDIRECTLY_COMPARABLE_HPP
+
+#include <hamon/iterator/config.hpp>
+
+#if defined(HAMON_USE_STD_RANGES_ITERATOR)
+
+namespace hamon
+{
+
+using std::indirectly_comparable;
+
+}	// namespace hamon
+
+#else
+
+#include <hamon/iterator/concepts/indirect_binary_predicate.hpp>
+#include <hamon/iterator/projected.hpp>
+#include <hamon/functional/identity.hpp>
+#include <hamon/config.hpp>
+#include <type_traits>
+
+namespace hamon
+{
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+
+template <
+	typename I1,
+	typename I2,
+	typename Rel,
+	typename P1 = hamon::identity,
+	typename P2 = hamon::identity
+>
+concept indirectly_comparable =
+	hamon::indirect_binary_predicate<Rel, hamon::projected<I1, P1>, hamon::projected<I2, P2>>;
+
+#else
+
+namespace detail
+{
+
+template <typename I1, typename I2, typename Rel, typename P1, typename P2>
+struct indirectly_comparable_impl
+{
+private:
+	template <typename J1, typename J2, typename R2, typename Q1, typename Q2>
+	static auto test(int) -> hamon::indirect_binary_predicate<
+		R2, hamon::projected<J1, Q1>, hamon::projected<J2, Q2>>;
+
+	template <typename J1, typename J2, typename R2, typename Q1, typename Q2>
+	static auto test(...) -> std::false_type;
+
+public:
+	using type = decltype(test<I1, I2, Rel, P1, P2>(0));
+};
+
+}	// namespace detail
+
+template <
+	typename I1,
+	typename I2,
+	typename Rel,
+	typename P1 = hamon::identity,
+	typename P2 = hamon::identity
+>
+using indirectly_comparable =
+	typename detail::indirectly_comparable_impl<I1, I2, Rel, P1, P2>::type;
+
+#endif
+
+}	// namespace hamon
+
+#endif
+
+#include <hamon/functional/identity.hpp>
+#include <hamon/type_traits/bool_constant.hpp>
+
+namespace hamon
+{
+
+template <
+	typename I1,
+	typename I2,
+	typename Rel,
+	typename P1 = hamon::identity,
+	typename P2 = hamon::identity
+>
+using indirectly_comparable_t =
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+	hamon::bool_constant<hamon::indirectly_comparable<I1, I2, Rel, P1, P2>>;
+#else
+	hamon::indirectly_comparable<I1, I2, Rel, P1, P2>;
+#endif
+
+}	// namespace hamon
+
+#endif // HAMON_ITERATOR_CONCEPTS_INDIRECTLY_COMPARABLE_HPP
---
+++ libs/iterator/test/src/concepts/unit_test_iterator_concepts_indirectly_comparable.cpp
@@ -0,0 +1,69 @@
+﻿/**
+ *	@file	unit_test_iterator_concepts_indirectly_comparable.cpp
+ *
+ *	@brief	indirectly_comparable のテスト
+ */
+
+#include <hamon/iterator/concepts/indirectly_comparable.hpp>
+#include <hamon/config.hpp>
+#include <memory>
+#include <vector>
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+#  define HAMON_INDIRECTLY_COMPARABLE_TEST(B, ...)	\
+	static_assert(B == hamon::indirectly_comparable<__VA_ARGS__>, "");	\
+	static_assert(B == hamon::indirectly_comparable_t<__VA_ARGS__>::value, "")
+#else
+#  define HAMON_INDIRECTLY_COMPARABLE_TEST(B, ...)	\
+	static_assert(B == hamon::indirectly_comparable<__VA_ARGS__>::value, "");	\
+	static_assert(B == hamon::indirectly_comparable_t<__VA_ARGS__>::value, "")
+#endif
+
+namespace hamon_iterator_test
+{
+
+namespace indirectly_comparable_test
+{
+
+struct Pred1
+{
+	bool operator()(int, int) const;
+};
+struct Pred2
+{
+	bool operator()(int) const;
+	void operator()(int, int) const;
+};
+struct Pred3
+{
+	Pred3(Pred3 const&) = delete;
+	bool operator()(int, int) const;
+};
+
+HAMON_INDIRECTLY_COMPARABLE_TEST(true,  int      *, int      *,                 bool(*)(int, int));
+HAMON_INDIRECTLY_COMPARABLE_TEST(true,  int const*, int      *,                 bool(*)(int, int));
+HAMON_INDIRECTLY_COMPARABLE_TEST(true,  int      *, int const*,                 bool(*)(int, int));
+HAMON_INDIRECTLY_COMPARABLE_TEST(true,  int const*, int const*,                 bool(*)(int, int));
+HAMON_INDIRECTLY_COMPARABLE_TEST(true,  int*, float*,                           bool(*)(int, int));
+HAMON_INDIRECTLY_COMPARABLE_TEST(true,  int*, char*,                            bool(*)(int, int));
+HAMON_INDIRECTLY_COMPARABLE_TEST(false, int*, void*,                            bool(*)(int, int));
+HAMON_INDIRECTLY_COMPARABLE_TEST(false, int*, int**,                            bool(*)(int, int));
+HAMON_INDIRECTLY_COMPARABLE_TEST(false, int*, int,                              bool(*)(int, int));
+HAMON_INDIRECTLY_COMPARABLE_TEST(false, int*, int&,                             bool(*)(int, int));
+HAMON_INDIRECTLY_COMPARABLE_TEST(false, int*, int&&,                            bool(*)(int, int));
+HAMON_INDIRECTLY_COMPARABLE_TEST(false, int*, int[],                            bool(*)(int, int));
+HAMON_INDIRECTLY_COMPARABLE_TEST(false, int*, int[2],                           bool(*)(int, int));
+HAMON_INDIRECTLY_COMPARABLE_TEST(true,  int*, std::unique_ptr<int>,             bool(*)(int, int));
+HAMON_INDIRECTLY_COMPARABLE_TEST(true,  int*, std::shared_ptr<int>,             bool(*)(int, int));
+HAMON_INDIRECTLY_COMPARABLE_TEST(true,  int*, std::vector<int>::iterator,       bool(*)(int, int));
+HAMON_INDIRECTLY_COMPARABLE_TEST(true,  int*, std::vector<int>::const_iterator, bool(*)(int, int));
+
+HAMON_INDIRECTLY_COMPARABLE_TEST(true,  int*, int*, Pred1);
+HAMON_INDIRECTLY_COMPARABLE_TEST(false, int*, int*, Pred2);
+HAMON_INDIRECTLY_COMPARABLE_TEST(false, int*, int*, Pred3);
+
+}	// namespace indirectly_comparable_test
+
+}	// namespace hamon_iterator_test
+
+#undef HAMON_INDIRECTLY_COMPARABLE_TEST
