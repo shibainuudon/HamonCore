ebd062dd63ad9c81a38232f150160555a44a5dbf
shibainuudon <shibainuudon@gmail.com>
2022-12-08 02:49:42 +0900

    [QVM]Visual Studio 2022 でのビルドエラーを修正

--- libs/qvm/include/hamon/qvm/detail/transform.hpp
+++ libs/qvm/include/hamon/qvm/detail/transform.hpp
@@ -25,57 +25,57 @@ namespace detail
 {
 
 template <
-	template <typename, std::size_t...> class GenType,
+	template <typename, std::size_t> class Vector,
 	typename T1, std::size_t N0,
 	typename F,
 	typename T2 = decltype(std::declval<F>()(std::declval<T1>())),
-	typename Result = GenType<T2, N0>,
+	typename Result = Vector<T2, N0>,
 	std::size_t... Is
 >
 HAMON_NODISCARD inline HAMON_CONSTEXPR Result
-transform_impl(GenType<T1, N0> const& v, F unary_op, hamon::index_sequence<Is...>) HAMON_NOEXCEPT
+transform_impl(Vector<T1, N0> const& v, F unary_op, hamon::index_sequence<Is...>) HAMON_NOEXCEPT
 {
 	return Result { unary_op(v[Is])... };
 }
 
 template <
-	template <typename, std::size_t...> class GenType,
-	typename T1, std::size_t N0, std::size_t N1, std::size_t... Ns,
+	template <typename, std::size_t, std::size_t> class Matrix,
+	typename T1, std::size_t N0, std::size_t N1,
 	typename F,
 	typename T2 = decltype(std::declval<F>()(std::declval<T1>())),
-	typename Result = GenType<T2, N0, N1, Ns...>,
+	typename Result = Matrix<T2, N0, N1>,
 	std::size_t... Is
 >
 HAMON_NODISCARD inline HAMON_CONSTEXPR Result
-transform_impl(GenType<T1, N0, N1, Ns...> const& m, F unary_op, hamon::index_sequence<Is...>) HAMON_NOEXCEPT
+transform_impl(Matrix<T1, N0, N1> const& m, F unary_op, hamon::index_sequence<Is...>) HAMON_NOEXCEPT
 {
 	return Result { transform_impl(m[Is], unary_op, hamon::make_index_sequence<N1>{})... };
 }
 
 template <
-	template <typename, std::size_t...> class GenType,
+	template <typename, std::size_t> class Vector,
 	typename T1, typename T2, std::size_t N0,
 	typename F,
 	typename T3 = decltype(std::declval<F>()(std::declval<T1>(), std::declval<T2>())),
-	typename Result = GenType<T3, N0>,
+	typename Result = Vector<T3, N0>,
 	std::size_t... Is
 >
 HAMON_NODISCARD inline HAMON_CONSTEXPR Result
-transform_impl(GenType<T1, N0> const& lhs, GenType<T2, N0> const& rhs, F binary_op, hamon::index_sequence<Is...>) HAMON_NOEXCEPT
+transform_impl(Vector<T1, N0> const& lhs, Vector<T2, N0> const& rhs, F binary_op, hamon::index_sequence<Is...>) HAMON_NOEXCEPT
 {
 	return Result { binary_op(lhs[Is], rhs[Is])... };
 }
 
 template <
-	template <typename, std::size_t...> class GenType,
-	typename T1, typename T2, std::size_t N0, std::size_t N1, std::size_t... Ns,
+	template <typename, std::size_t, std::size_t> class Matrix,
+	typename T1, typename T2, std::size_t N0, std::size_t N1,
 	typename F,
 	typename T3 = decltype(std::declval<F>()(std::declval<T1>(), std::declval<T2>())),
-	typename Result = GenType<T3, N0, N1, Ns...>,
+	typename Result = Matrix<T3, N0, N1>,
 	std::size_t... Is
 >
 HAMON_NODISCARD inline HAMON_CONSTEXPR Result
-transform_impl(GenType<T1, N0, N1, Ns...> const& lhs, GenType<T2, N0, N1, Ns...> const& rhs, F binary_op, hamon::index_sequence<Is...>) HAMON_NOEXCEPT
+transform_impl(Matrix<T1, N0, N1> const& lhs, Matrix<T2, N0, N1> const& rhs, F binary_op, hamon::index_sequence<Is...>) HAMON_NOEXCEPT
 {
 	return Result{ transform_impl(lhs[Is], rhs[Is], binary_op, hamon::make_index_sequence<N1>{})... };
 }
@@ -84,12 +84,24 @@ transform_impl(GenType<T1, N0, N1, Ns...> const& lhs, GenType<T2, N0, N1, Ns...>
  *	@brief	transform (Unary operation)
  */
 template <
-	template <typename, std::size_t...> class GenType,
-	typename T, std::size_t N0, std::size_t... Ns,
+	template <typename, std::size_t> class Vector,
+	typename T, std::size_t N0,
 	typename F
 >
 HAMON_NODISCARD inline HAMON_CONSTEXPR auto
-transform(GenType<T, N0, Ns...> const& v, F unary_op) HAMON_NOEXCEPT
+transform(Vector<T, N0> const& v, F unary_op) HAMON_NOEXCEPT
+->decltype(detail::transform_impl(v, unary_op, hamon::make_index_sequence<N0>{}))
+{
+	return detail::transform_impl(v, unary_op, hamon::make_index_sequence<N0>{});
+}
+
+template <
+	template <typename, std::size_t, std::size_t> class Matrix,
+	typename T, std::size_t N0, std::size_t N1,
+	typename F
+>
+HAMON_NODISCARD inline HAMON_CONSTEXPR auto
+transform(Matrix<T, N0, N1> const& v, F unary_op) HAMON_NOEXCEPT
 ->decltype(detail::transform_impl(v, unary_op, hamon::make_index_sequence<N0>{}))
 {
 	return detail::transform_impl(v, unary_op, hamon::make_index_sequence<N0>{});
@@ -99,12 +111,24 @@ transform(GenType<T, N0, Ns...> const& v, F unary_op) HAMON_NOEXCEPT
  *	@brief	transform (Binary operation)
  */
 template <
-	template <typename, std::size_t...> class GenType,
-	typename T1, typename T2, std::size_t N0, std::size_t... Ns,
+	template <typename, std::size_t> class Vector,
+	typename T1, typename T2, std::size_t N0,
 	typename F
 >
 HAMON_NODISCARD inline HAMON_CONSTEXPR auto
-transform(GenType<T1, N0, Ns...> const& lhs, GenType<T2, N0, Ns...> const& rhs, F binary_op) HAMON_NOEXCEPT
+transform(Vector<T1, N0> const& lhs, Vector<T2, N0> const& rhs, F binary_op) HAMON_NOEXCEPT
+->decltype(detail::transform_impl(lhs, rhs, binary_op, hamon::make_index_sequence<N0>{}))
+{
+	return detail::transform_impl(lhs, rhs, binary_op, hamon::make_index_sequence<N0>{});
+}
+
+template <
+	template <typename, std::size_t, std::size_t> class Matrix,
+	typename T1, typename T2, std::size_t N0, std::size_t N1,
+	typename F
+>
+HAMON_NODISCARD inline HAMON_CONSTEXPR auto
+transform(Matrix<T1, N0, N1> const& lhs, Matrix<T2, N0, N1> const& rhs, F binary_op) HAMON_NOEXCEPT
 ->decltype(detail::transform_impl(lhs, rhs, binary_op, hamon::make_index_sequence<N0>{}))
 {
 	return detail::transform_impl(lhs, rhs, binary_op, hamon::make_index_sequence<N0>{});
