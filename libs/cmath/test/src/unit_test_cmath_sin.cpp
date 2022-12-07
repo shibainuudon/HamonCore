292adc5ab2c2dd238811d5db44e00bdfbed16cd3
shibainuudon <shibainuudon@gmail.com>
2022-12-07 13:22:41 +0900

    [CMath]sinをconstexprに対応

--- libs/cmath/test/src/unit_test_cmath_sin.cpp
+++ libs/cmath/test/src/unit_test_cmath_sin.cpp
@@ -6,6 +6,7 @@
 
 #include <hamon/cmath/sin.hpp>
 #include <hamon/cmath/isnan.hpp>
+#include <hamon/cmath/fabs.hpp>
 #include <hamon/numbers.hpp>
 #include <hamon/config.hpp>
 #include <gtest/gtest.h>
@@ -27,29 +28,52 @@ static_assert(std::is_same<long double, decltype(hamon::sin(0.0l))>::value, "");
 static_assert(std::is_same<long double, decltype(hamon::sinl(0.0l))>::value, "");
 
 template <typename T>
-void SinTestFloat(double error)
+double sin_error();
+
+template <>
+inline HAMON_CXX11_CONSTEXPR double sin_error<float>()
 {
-	HAMON_CONSTEXPR auto nan = std::numeric_limits<T>::quiet_NaN();
-	HAMON_CONSTEXPR auto inf = std::numeric_limits<T>::infinity();
+	return 0.00001;
+}
 
-	EXPECT_NEAR( 0.35078322768961985, (double)hamon::sin(T(-3.5)), error);
-	EXPECT_NEAR(-0.14112000805986722, (double)hamon::sin(T(-3.0)), error);
-	EXPECT_NEAR(-0.59847214410395649, (double)hamon::sin(T(-2.5)), error);
-	EXPECT_NEAR(-0.90929742682568169, (double)hamon::sin(T(-2.0)), error);
-	EXPECT_NEAR(-0.99749498660405443, (double)hamon::sin(T(-1.5)), error);
-	EXPECT_NEAR(-0.84147098480789650, (double)hamon::sin(T(-1.0)), error);
-	EXPECT_NEAR(-0.47942553860420300, (double)hamon::sin(T(-0.5)), error);
-	EXPECT_NEAR( 0.00000000000000000, (double)hamon::sin(T( 0.0)), error);
-	EXPECT_NEAR( 0.47942553860420300, (double)hamon::sin(T( 0.5)), error);
-	EXPECT_NEAR( 0.84147098480789650, (double)hamon::sin(T( 1.0)), error);
-	EXPECT_NEAR( 0.99749498660405443, (double)hamon::sin(T( 1.5)), error);
-	EXPECT_NEAR( 0.90929742682568169, (double)hamon::sin(T( 2.0)), error);
-	EXPECT_NEAR( 0.59847214410395649, (double)hamon::sin(T( 2.5)), error);
-	EXPECT_NEAR( 0.14112000805986722, (double)hamon::sin(T( 3.0)), error);
-	EXPECT_NEAR(-0.35078322768961985, (double)hamon::sin(T( 3.5)), error);
+template <>
+inline HAMON_CXX11_CONSTEXPR double sin_error<double>()
+{
+	return 0.00000000001;
+}
 
-	EXPECT_NEAR(0.0, (double)hamon::sin( hamon::numbers::pi_fn<T>()), error);
-	EXPECT_NEAR(0.0, (double)hamon::sin(-hamon::numbers::pi_fn<T>()), error);
+template <>
+inline HAMON_CXX11_CONSTEXPR double sin_error<long double>()
+{
+	return 0.00000000001;
+}
+
+template <typename T>
+void SinTestFloat()
+{
+	HAMON_CXX11_CONSTEXPR auto nan = std::numeric_limits<T>::quiet_NaN();
+	HAMON_CXX11_CONSTEXPR auto inf = std::numeric_limits<T>::infinity();
+
+	HAMON_CXX11_CONSTEXPR double error = sin_error<T>();
+
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR( 0.35078322768961985, (double)hamon::sin(T(-3.5)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(-0.14112000805986722, (double)hamon::sin(T(-3.0)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(-0.59847214410395649, (double)hamon::sin(T(-2.5)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(-0.90929742682568169, (double)hamon::sin(T(-2.0)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(-0.99749498660405443, (double)hamon::sin(T(-1.5)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(-0.84147098480789650, (double)hamon::sin(T(-1.0)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(-0.47942553860420300, (double)hamon::sin(T(-0.5)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR( 0.00000000000000000, (double)hamon::sin(T( 0.0)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR( 0.47942553860420300, (double)hamon::sin(T( 0.5)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR( 0.84147098480789650, (double)hamon::sin(T( 1.0)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR( 0.99749498660405443, (double)hamon::sin(T( 1.5)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR( 0.90929742682568169, (double)hamon::sin(T( 2.0)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR( 0.59847214410395649, (double)hamon::sin(T( 2.5)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR( 0.14112000805986722, (double)hamon::sin(T( 3.0)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(-0.35078322768961985, (double)hamon::sin(T( 3.5)), error);
+
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(0.0, (double)hamon::sin( hamon::numbers::pi_fn<T>()), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(0.0, (double)hamon::sin(-hamon::numbers::pi_fn<T>()), error);
 
 	HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0.0, hamon::sin(T(+0.0)));
 	HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0.0, hamon::sin(T(-0.0)));
@@ -62,33 +86,33 @@ void SinTestFloat(double error)
 template <typename T>
 void SinTestSignedInt(void)
 {
-	const double error = 0.00000000001;
+	HAMON_CXX11_CONSTEXPR double error = 0.00000000001;
 
-	EXPECT_NEAR( 0.00000000000000000, hamon::sin(T( 0)), error);
-	EXPECT_NEAR( 0.84147098480789650, hamon::sin(T( 1)), error);
-	EXPECT_NEAR(-0.84147098480789650, hamon::sin(T(-1)), error);
-	EXPECT_NEAR( 0.90929742682568169, hamon::sin(T( 2)), error);
-	EXPECT_NEAR(-0.90929742682568169, hamon::sin(T(-2)), error);
-	EXPECT_NEAR( 0.14112000805986722, hamon::sin(T( 3)), error);
-	EXPECT_NEAR(-0.14112000805986722, hamon::sin(T(-3)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR( 0.00000000000000000, hamon::sin(T( 0)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR( 0.84147098480789650, hamon::sin(T( 1)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(-0.84147098480789650, hamon::sin(T(-1)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR( 0.90929742682568169, hamon::sin(T( 2)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(-0.90929742682568169, hamon::sin(T(-2)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR( 0.14112000805986722, hamon::sin(T( 3)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(-0.14112000805986722, hamon::sin(T(-3)), error);
 }
 
 template <typename T>
 void SinTestUnsignedInt(void)
 {
-	const double error = 0.00000000001;
+	HAMON_CXX11_CONSTEXPR double error = 0.00000000001;
 
-	EXPECT_NEAR(0.00000000000000000, hamon::sin(T( 0)), error);
-	EXPECT_NEAR(0.84147098480789650, hamon::sin(T( 1)), error);
-	EXPECT_NEAR(0.90929742682568169, hamon::sin(T( 2)), error);
-	EXPECT_NEAR(0.14112000805986722, hamon::sin(T( 3)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(0.00000000000000000, hamon::sin(T( 0)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(0.84147098480789650, hamon::sin(T( 1)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(0.90929742682568169, hamon::sin(T( 2)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(0.14112000805986722, hamon::sin(T( 3)), error);
 }
 
 GTEST_TEST(CMathTest, SinTest)
 {
-	SinTestFloat<float>(0.0000001);
-	SinTestFloat<double>(0.00000000001);
-	SinTestFloat<long double>(0.00000000001);
+	SinTestFloat<float>();
+	SinTestFloat<double>();
+	SinTestFloat<long double>();
 
 	SinTestSignedInt<int>();
 //	SinTestSignedInt<signed char>();
