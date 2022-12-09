139676056e5e3fc9d16c199d4815e956e058dc7a
shibainuudon <shibainuudon@gmail.com>
2022-12-09 16:39:07 +0900

    ユニットテストのビルドでプリコンパイルヘッダを使う

---
+++ libs/common/test/include/pch.hpp
@@ -0,0 +1,37 @@
+﻿/**
+ *	@file	pch.hpp
+ *
+ *	@brief
+ */
+
+#ifndef HAMON_PCH_HPP
+#define HAMON_PCH_HPP
+
+#include <algorithm>
+#include <array>
+#include <deque>
+#include <forward_list>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iterator>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <ostream>
+#include <random>
+#include <ratio>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <streambuf>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#endif // HAMON_PCH_HPP
