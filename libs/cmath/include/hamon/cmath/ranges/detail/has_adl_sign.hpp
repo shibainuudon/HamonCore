e664ce9056d7da978e1e4e74ccec4b9b38b65d0f
shibainuudon <shibainuudon@gmail.com>
2023-01-05 16:56:06 +0900

    [CMath]ranges::sign を追加

---
+++ libs/cmath/include/hamon/cmath/ranges/detail/has_adl_sign.hpp
@@ -0,0 +1,71 @@
+﻿/**
+ *	@file	has_adl_sign.hpp
+ *
+ *	@brief	has_adl_sign の定義
+ */
+
+#ifndef HAMON_CMATH_RANGES_DETAIL_HAS_ADL_SIGN_HPP
+#define HAMON_CMATH_RANGES_DETAIL_HAS_ADL_SIGN_HPP
+
+#include <hamon/concepts/detail/class_or_enum.hpp>
+#include <hamon/concepts/detail/can_reference.hpp>
+#include <type_traits>
+#include <utility>
+
+namespace hamon
+{
+
+namespace ranges
+{
+
+namespace sign_detail
+{
+
+#if 0
+void sign(const auto&) = delete;
+#else
+template <typename T> void sign(const T&) = delete;
+#endif
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+
+template <typename T>
+concept has_adl_sign =
+	hamon::detail::class_or_enum<T> &&
+	requires(T const& t)
+	{
+		{ sign(t) } -> hamon::detail::can_reference;
+	};
+
+#else
+
+template <typename T>
+struct has_adl_sign_impl
+{
+private:
+	template <typename U,
+		typename = hamon::enable_if_t<
+			hamon::detail::class_or_enum<U>::value>,
+		typename S = decltype(sign(std::declval<U const&>()))
+	>
+	static auto test(int) -> hamon::detail::can_reference<S>;
+
+	template <typename U>
+	static auto test(...) -> std::false_type;
+
+public:
+	using type = decltype(test<T>(0));
+};
+
+template <typename T>
+using has_adl_sign = typename has_adl_sign_impl<T>::type;
+
+#endif
+
+}	// namespace sign_detail
+
+}	// namespace ranges
+
+}	// namespace hamon
+
+#endif // HAMON_CMATH_RANGES_DETAIL_HAS_ADL_SIGN_HPP
---
+++ libs/cmath/test/src/ranges/unit_test_cmath_ranges_sign.cpp
@@ -0,0 +1,104 @@
+﻿/**
+ *	@file	unit_test_cmath_ranges_sign.cpp
+ *
+ *	@brief	ranges::sign のテスト
+ */
+
+#include <hamon/cmath/ranges/sign.hpp>
+#include <gtest/gtest.h>
+#include "constexpr_test.hpp"
+
+namespace hamon_cmath_test
+{
+
+namespace ranges_sign_test
+{
+
+template <typename T>
+struct Real
+{
+	T value;
+
+	HAMON_CXX11_CONSTEXPR bool operator==(Real const& rhs) const
+	{
+		return value == rhs.value;
+	}
+};
+
+template <typename T>
+HAMON_CXX11_CONSTEXPR Real<T> sign(Real<T> const& r)
+{
+	return Real<T>{ hamon::ranges::sign(r.value) };
+}
+
+template <typename T>
+struct Vector3
+{
+	T x;
+	T y;
+	T z;
+
+	friend HAMON_CXX11_CONSTEXPR Vector3<T>
+	sign(Vector3<T> const& v)
+	{
+		return Vector3<T>
+		{
+			hamon::ranges::sign(v.x),
+			hamon::ranges::sign(v.y),
+			hamon::ranges::sign(v.z),
+		};
+	}
+};
+
+template <typename T>
+void RangesSignTest(void)
+{
+	HAMON_CXX11_CONSTEXPR_EXPECT_EQ(T{ 0}, hamon::ranges::sign(T{ 0.0}));
+	HAMON_CXX11_CONSTEXPR_EXPECT_EQ(T{ 0}, hamon::ranges::sign(T{-0.0}));
+	HAMON_CXX11_CONSTEXPR_EXPECT_EQ(T{ 1}, hamon::ranges::sign(T{ 1.0}));
+	HAMON_CXX11_CONSTEXPR_EXPECT_EQ(T{-1}, hamon::ranges::sign(T{-1.0}));
+	HAMON_CXX11_CONSTEXPR_EXPECT_EQ(T{ 1}, hamon::ranges::sign(T{ 2.0}));
+	HAMON_CXX11_CONSTEXPR_EXPECT_EQ(T{-1}, hamon::ranges::sign(T{-2.0}));
+	HAMON_CXX11_CONSTEXPR_EXPECT_EQ(T{ 1}, hamon::ranges::sign(T{ 3.0}));
+	HAMON_CXX11_CONSTEXPR_EXPECT_EQ(T{-1}, hamon::ranges::sign(T{-3.0}));
+	HAMON_CXX11_CONSTEXPR_EXPECT_EQ(T{ 1}, hamon::ranges::sign(T{ 0.5}));
+	HAMON_CXX11_CONSTEXPR_EXPECT_EQ(T{-1}, hamon::ranges::sign(T{-0.5}));
+}
+
+template <typename T>
+void RangesSignTest2(void)
+{
+	{
+		HAMON_CXX11_CONSTEXPR Vector3<T> v1 { -1, 0, 0.5 };
+		HAMON_CXX11_CONSTEXPR Vector3<T> v2 = hamon::ranges::sign(v1);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(T(-1), v2.x);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(T( 0), v2.y);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(T( 1), v2.z);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR Vector3<T> v1 { 0, 2, -3 };
+		HAMON_CXX11_CONSTEXPR Vector3<T> v2 = hamon::ranges::sign(v1);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(T( 0), v2.x);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(T( 1), v2.y);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(T(-1), v2.z);
+	}
+}
+
+GTEST_TEST(CMathTest, RangesSignTest)
+{
+	RangesSignTest<float>();
+	RangesSignTest<double>();
+	RangesSignTest<long double>();
+
+	RangesSignTest<Real<float>>();
+	RangesSignTest<Real<double>>();
+	RangesSignTest<Real<long double>>();
+
+	RangesSignTest2<float>();
+	RangesSignTest2<double>();
+	RangesSignTest2<long double>();
+}
+
+}	// namespace ranges_sign_test
+
+}	// namespace hamon_cmath_test
