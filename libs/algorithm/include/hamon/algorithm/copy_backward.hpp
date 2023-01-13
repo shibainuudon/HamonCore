442300bd936dce9a33af8a354d87acfb28868ae5
shibainuudon <shibainuudon@gmail.com>
2023-01-13 11:13:11 +0900

    [Algorithm]copy_backward関数を追加

---
+++ libs/algorithm/include/hamon/algorithm/copy_backward.hpp
@@ -0,0 +1,130 @@
+﻿/**
+ *	@file	copy_backward.hpp
+ *
+ *	@brief	copy_backward の定義
+ */
+
+#ifndef HAMON_ALGORITHM_COPY_BACKWARD_HPP
+#define HAMON_ALGORITHM_COPY_BACKWARD_HPP
+
+#include <hamon/algorithm/config.hpp>
+
+#if defined(HAMON_USE_STD_ALGORITHM) &&	\
+	!(defined(HAMON_MSVC) && (HAMON_MSVC == 1930) && (HAMON_MSVC_FULL_VER <= 193030705))
+
+// 以下のコード
+// char a[] { 1, 2, 3 };
+// hamon::copy_backward(a, a+2, a+3);
+// で、
+// Visual Studio 2022 Version 17.0.(0-1) かつ、
+// Releaseビルドかつ、
+// 実行時(非constexpr)
+// のときのみ、a[2]の値が想定通りにならない。
+// Visual Studio 2022 のバグと思われる。
+// (配列の型を変えると大丈夫な時があったりする。int型は大丈夫なときが多い)
+
+#include <algorithm>
+
+namespace hamon
+{
+
+using std::copy_backward;
+
+}	// namespace hamon
+
+#else
+
+//#include <hamon/type_traits/enable_if.hpp>
+//#include <hamon/type_traits/remove_const.hpp>
+//#include <hamon/type_traits/is_same.hpp>
+//#include <hamon/type_traits/is_trivially_copy_assignable.hpp>
+#include <hamon/config.hpp>
+//#include <cstddef>
+//#include <cstring>	// memmove
+
+namespace hamon
+{
+
+namespace detail
+{
+
+template <typename BidirectionalIterator, typename OutputIterator>
+inline HAMON_CXX14_CONSTEXPR OutputIterator
+copy_backward_impl(
+	BidirectionalIterator first,
+	BidirectionalIterator last,
+	OutputIterator result)
+{
+	while (first != last)
+	{
+		*--result = *--last;
+	}
+
+	return result;
+}
+
+#if 0
+template <typename T, typename U>
+inline HAMON_CXX14_CONSTEXPR
+hamon::enable_if_t<
+	hamon::is_same<hamon::remove_const_t<T>, U>::value &&
+	hamon::is_trivially_copy_assignable<U>::value,
+	U*
+>
+copy_backward_impl(T* first, T* last, U* result)
+{
+	auto const n = static_cast<std::size_t>(last - first);
+
+	if (n > 0)
+	{
+		result -= n;
+		std::memmove(result, first, n * sizeof(U));
+	}
+
+	return result;
+}
+#endif
+
+}	// namespace detail
+
+/**
+ *	@brief		指定された範囲の要素を後ろからコピーする
+ *
+ *	@tparam		BidirectionalIterator1
+ *	@tparam		BidirectionalIterator2
+ *
+ *	@param		first
+ *	@param		last
+ *	@param		result
+ *
+ *	@require	result は (first,last] の範囲に含まれてはならない
+ *
+ *	@return		result + (last - first)
+ *
+ *	@effect		[first,last) 内にある要素を、それぞれ [result - (last-first),result) へコピーする。
+ *				コピーは last - 1 から順番に行い、1 以上 last - first 以下であるそれぞれの n について、
+ *				*(result - n) = *(last - n) を行う。
+ *
+ *	@complexity	正確に last - first 回代入が行われる。
+ *
+ *	@note		last が [result - (last-first), result) の範囲内にあるときには、
+ *				copy() の代わりに copy_backward() を使うべきである。
+ */
+template <
+	typename BidirectionalIterator1,
+	typename BidirectionalIterator2
+>
+inline HAMON_CXX14_CONSTEXPR BidirectionalIterator2
+copy_backward(
+	BidirectionalIterator1 first,
+	BidirectionalIterator1 last,
+	BidirectionalIterator2 result)
+{
+	return hamon::detail::copy_backward_impl(first, last, result);
+}
+
+}	// namespace hamon
+
+#endif
+
+#endif // HAMON_ALGORITHM_COPY_BACKWARD_HPP
---
+++ libs/algorithm/test/src/unit_test_algorithm_copy_backward.cpp
@@ -0,0 +1,131 @@
+﻿/**
+ *	@file	unit_test_algorithm_copy_backward.cpp
+ *
+ *	@brief	copy_backward のテスト
+ */
+
+#include <hamon/algorithm/copy_backward.hpp>
+#include <hamon/iterator/begin.hpp>
+#include <hamon/iterator/end.hpp>
+#include <hamon/iterator/next.hpp>
+#include <gtest/gtest.h>
+#include <array>
+#include <vector>
+#include <list>
+#include "constexpr_test.hpp"
+
+namespace hamon_algorithm_test
+{
+
+namespace copy_backward_test
+{
+
+#define VERIFY(...)	if (!(__VA_ARGS__)) { return false; }
+
+inline HAMON_CXX14_CONSTEXPR bool CopyBackwardArrayTest()
+{
+	{
+		const int a1[] { 1, 2, 3 };
+		int a2[3]{};
+
+		auto it = hamon::copy_backward(hamon::begin(a1), hamon::end(a1), hamon::end(a2));
+
+		VERIFY(1 == a2[0]);
+		VERIFY(2 == a2[1]);
+		VERIFY(3 == a2[2]);
+		VERIFY(it == hamon::begin(a2));
+	}
+	{
+		int a1[] { 1, 2, 3 };
+
+		auto it = hamon::copy_backward(a1, a1+2, a1+3);
+
+		VERIFY(1 == a1[0]);
+		VERIFY(1 == a1[1]);
+		VERIFY(2 == a1[2]);
+		VERIFY(it == hamon::begin(a1)+1);
+	}
+	{
+		short a1[] { 1, 2, 3 };
+
+		auto it = hamon::copy_backward(a1, a1+2, a1+3);
+
+		VERIFY(1 == a1[0]);
+		VERIFY(1 == a1[1]);
+		VERIFY(2 == a1[2]);
+		VERIFY(it == hamon::begin(a1)+1);
+	}
+	{
+		char a1[] { 1, 2, 3 };
+
+		auto it = hamon::copy_backward(a1, a1+2, a1+3);
+
+		VERIFY(1 == a1[0]);
+		VERIFY(1 == a1[1]);
+		VERIFY(2 == a1[2]);
+		VERIFY(it == hamon::begin(a1)+1);
+	}
+
+	return true;
+}
+
+inline HAMON_CXX17_CONSTEXPR bool CopyBackwardStdArrayTest()
+{
+	{
+		const int a1[] { 1, 2, 3 };
+		std::array<int, 5> a2{{}};
+
+		auto it = hamon::copy_backward(hamon::begin(a1), hamon::end(a1), hamon::end(a2));
+
+		VERIFY(0 == a2[0]);
+		VERIFY(0 == a2[1]);
+		VERIFY(1 == a2[2]);
+		VERIFY(2 == a2[3]);
+		VERIFY(3 == a2[4]);
+		VERIFY(it == hamon::begin(a2) + 2);
+	}
+
+	return true;
+}
+
+GTEST_TEST(AlgorithmTest, CopyBackwardTest)
+{
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(CopyBackwardArrayTest());
+	HAMON_CXX17_CONSTEXPR_EXPECT_TRUE(CopyBackwardStdArrayTest());
+
+	{
+		std::vector<int> v{ 1, 2, 3, 4, 5 };
+
+		auto it = hamon::copy_backward(hamon::begin(v), hamon::begin(v) + 3, hamon::end(v));
+
+		EXPECT_EQ(1, v[0]);
+		EXPECT_EQ(2, v[1]);
+		EXPECT_EQ(1, v[2]);
+		EXPECT_EQ(2, v[3]);
+		EXPECT_EQ(3, v[4]);
+		EXPECT_TRUE(it == hamon::begin(v) + 2);
+	}
+	{
+		const std::vector<int> v{ 1, 2, 3, 4, 5 };
+		std::list<int> l{ 6, 7, 8, 9 };
+
+		{
+			auto it = hamon::copy_backward(hamon::begin(v), hamon::begin(v) + 2, hamon::end(l));
+			EXPECT_TRUE(it == hamon::next(l.begin(), 2));
+		}
+		{
+			auto it = l.begin();
+			EXPECT_EQ(6, *it++);
+			EXPECT_EQ(7, *it++);
+			EXPECT_EQ(1, *it++);
+			EXPECT_EQ(2, *it++);
+			EXPECT_TRUE(it == l.end());
+		}
+	}
+}
+
+#undef VERIFY
+
+}	// namespace copy_backward_test
+
+}	// namespace hamon_algorithm_test
