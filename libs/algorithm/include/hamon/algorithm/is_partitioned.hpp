059695fa7a7ebf6cbfdfd8b639dfbe6e5efb7222
shibainuudon <shibainuudon@gmail.com>
2023-01-19 10:29:19 +0900

    [Algorithm]is_partitioned関数を追加

---
+++ libs/algorithm/include/hamon/algorithm/is_partitioned.hpp
@@ -0,0 +1,86 @@
+﻿/**
+ *	@file	is_partitioned.hpp
+ *
+ *	@brief	is_partitioned の定義
+ */
+
+#ifndef HAMON_ALGORITHM_IS_PARTITIONED_HPP
+#define HAMON_ALGORITHM_IS_PARTITIONED_HPP
+
+#include <hamon/algorithm/config.hpp>
+
+#if defined(HAMON_USE_STD_ALGORITHM)
+
+#include <algorithm>
+
+namespace hamon
+{
+
+using std::is_partitioned;
+
+}	// namespace hamon
+
+#else
+
+#include <hamon/config.hpp>
+
+namespace hamon
+{
+
+/**
+ *	@brief		与えられた範囲が条件によって区分化されているか判定する。
+ *
+ *	@tparam		InputIterator
+ *	@tparam		Predicate
+ *
+ *	@param		first
+ *	@param		last
+ *	@param		pred
+ *
+ *	@require	InputIteratorのvalue typeは Predicate の引数型へ変換可能でなければならない。
+ *				つまり pred(*first) という式が有効でなければならない。
+ *
+ *	@return		[first,last) が空、 または [first,last) が pred によって区分化されているなら true 、
+ *				そうでなければ false を返す。
+ *				つまり、pred を満たす全ての要素が、pred を満たさない全ての要素より前に出現するなら true を返す。
+ *
+ *	@complexity	線形時間。最大で last - first 回 pred が適用される。
+ */
+template <typename InputIterator, typename Predicate>
+inline HAMON_CXX14_CONSTEXPR bool
+is_partitioned(
+	InputIterator first,
+	InputIterator last,
+	Predicate pred)
+{
+	for (; first != last; ++first)
+	{
+		if (!pred(*first))
+		{
+			break;
+		}
+	}
+
+	if (first == last)
+	{
+		return true;
+	}
+
+	++first;
+	
+	for (; first != last; ++first)
+	{
+		if (pred(*first))
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+}	// namespace hamon
+
+#endif
+
+#endif // HAMON_ALGORITHM_IS_PARTITIONED_HPP
---
+++ libs/algorithm/test/src/unit_test_algorithm_is_partitioned.cpp
@@ -0,0 +1,101 @@
+﻿/**
+ *	@file	unit_test_algorithm_is_partitioned.cpp
+ *
+ *	@brief	is_partitioned のテスト
+ */
+
+#include <hamon/algorithm/is_partitioned.hpp>
+#include <hamon/iterator/begin.hpp>
+#include <hamon/iterator/end.hpp>
+#include <gtest/gtest.h>
+#include <array>
+#include <vector>
+#include <list>
+#include "constexpr_test.hpp"
+
+namespace hamon_algorithm_test
+{
+
+namespace is_partitioned_test
+{
+
+HAMON_CONSTEXPR bool pred1(int x)
+{
+	return (x % 2) == 0;
+}
+
+struct pred2
+{
+	HAMON_CONSTEXPR bool operator()(int x) const
+	{
+		return x > 3;
+	}
+};
+
+GTEST_TEST(AlgorithmTest, IsPartitionedTest)
+{
+	{
+		HAMON_STATIC_CONSTEXPR int a[] {1,2,3,4,5,6,7,8,9};
+		HAMON_CXX14_CONSTEXPR_EXPECT_FALSE(hamon::is_partitioned(hamon::begin(a), hamon::end(a), pred1));
+	}
+	{
+		HAMON_STATIC_CONSTEXPR int a[] {2,4,6,8,1,3,5,7,9};
+		HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(hamon::is_partitioned(hamon::begin(a), hamon::end(a), pred1));
+	}
+	{
+		HAMON_STATIC_CONSTEXPR int a[] {3,3,3};
+		HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(hamon::is_partitioned(hamon::begin(a), hamon::end(a), pred2()));
+	}
+	{
+		HAMON_STATIC_CONSTEXPR int a[] {4,4,4};
+		HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(hamon::is_partitioned(hamon::begin(a), hamon::end(a), pred2()));
+	}
+	{
+		HAMON_STATIC_CONSTEXPR std::array<int, 5> a {{4,2,3,1,5}};
+		HAMON_CXX17_CONSTEXPR_EXPECT_TRUE(hamon::is_partitioned(hamon::begin(a), hamon::end(a), pred1));
+	}
+	{
+		HAMON_STATIC_CONSTEXPR std::array<int, 5> a {{4,2,3,1,5}};
+		HAMON_CXX17_CONSTEXPR_EXPECT_FALSE(hamon::is_partitioned(hamon::begin(a), hamon::end(a), pred2()));
+	}
+	{
+		HAMON_STATIC_CONSTEXPR std::array<int, 3> a {{1,2,3}};
+		HAMON_CXX17_CONSTEXPR_EXPECT_TRUE(hamon::is_partitioned(hamon::begin(a), hamon::end(a), pred2()));
+	}
+	{
+		const std::vector<int> a {1,2,3};
+		EXPECT_FALSE(hamon::is_partitioned(hamon::begin(a), hamon::end(a), pred1));
+	}
+	{
+		const std::vector<int> a {1,3,2};
+		EXPECT_TRUE(hamon::is_partitioned(hamon::begin(a), hamon::end(a), [](int x) { return (x % 2) == 1; }));
+	}
+	{
+		const std::vector<int> a {1,2,3};
+		EXPECT_TRUE(hamon::is_partitioned(hamon::begin(a), hamon::end(a), pred2()));
+	}
+	{
+		const std::vector<int> a {1,3,2};
+		EXPECT_FALSE(hamon::is_partitioned(hamon::begin(a), hamon::end(a), [](int x) { return x > 2; }));
+	}
+	{
+		const std::list<int> a {1,2,3};
+		EXPECT_FALSE(hamon::is_partitioned(hamon::begin(a), hamon::end(a), pred1));
+	}
+	{
+		const std::list<int> a {1,3,2};
+		EXPECT_TRUE(hamon::is_partitioned(hamon::begin(a), hamon::end(a), [](int x) { return (x % 2) == 1; }));
+	}
+	{
+		const std::list<int> a {1,2,3};
+		EXPECT_TRUE(hamon::is_partitioned(hamon::begin(a), hamon::end(a), pred2()));
+	}
+	{
+		const std::list<int> a {1,3,2};
+		EXPECT_FALSE(hamon::is_partitioned(hamon::begin(a), hamon::end(a), [](int x) { return x > 2; }));
+	}
+}
+
+}	// namespace is_partitioned_test
+
+}	// namespace hamon_algorithm_test
