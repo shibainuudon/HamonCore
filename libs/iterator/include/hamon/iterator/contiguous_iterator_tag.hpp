e399b918a1456c18b28fde0aa7971546bdb5a34e
shibainuudon <shibainuudon@gmail.com>
2023-01-27 10:11:06 +0900

    [Iterator]contiguous_iterator_tagの定義を修正

--- libs/iterator/include/hamon/iterator/contiguous_iterator_tag.hpp
+++ libs/iterator/include/hamon/iterator/contiguous_iterator_tag.hpp
@@ -7,13 +7,12 @@
 #ifndef HAMON_ITERATOR_CONTIGUOUS_ITERATOR_TAG_HPP
 #define HAMON_ITERATOR_CONTIGUOUS_ITERATOR_TAG_HPP
 
-#include <hamon/iterator/config.hpp>
 #include <iterator>
 
 namespace hamon
 {
 
-#if defined(HAMON_USE_STD_RANGES_ITERATOR)
+#if defined(__cpp_lib_ranges) && (__cpp_lib_ranges >= 201911)
 using std::contiguous_iterator_tag;
 #else
 struct contiguous_iterator_tag : public std::random_access_iterator_tag {};
