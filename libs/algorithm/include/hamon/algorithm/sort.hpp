9fd1c66b213550397a8e0f63e980934a0a3a3f84
shibainuudon <shibainuudon@gmail.com>
2023-02-13 11:07:44 +0900

    [Algorithm]sort 関数を追加

---
+++ libs/algorithm/include/hamon/algorithm/sort.hpp
@@ -0,0 +1,36 @@
+﻿/**
+ *	@file	sort.hpp
+ *
+ *	@brief	sort の定義
+ */
+
+#ifndef HAMON_ALGORITHM_SORT_HPP
+#define HAMON_ALGORITHM_SORT_HPP
+
+#include <algorithm>
+
+namespace hamon
+{
+
+/**
+ *	@brief		範囲を並べ替える
+ *
+ *	@tparam		RandomAccessIterator
+ *	@tparam		Compare
+ *
+ *	@param		first
+ *	@param		last
+ *	@param		comp
+ *
+ *	@require	RandomAccessIterator は ValueSwappable の要求を満たしている必要がある。
+ *				*first の型は MoveConstructible と MoveAssignable の要件を満たしている必要がある。
+ *
+ *	@effect		[first,last) の範囲をソートする
+ *
+ *	@complexity	N log N (N == last - first) 回の比較
+ */
+using std::sort;
+
+}	// namespace hamon
+
+#endif // HAMON_ALGORITHM_SORT_HPP
---
+++ libs/algorithm/test/src/unit_test_algorithm_sort.cpp
@@ -0,0 +1,84 @@
+﻿/**
+ *	@file	unit_test_algorithm_sort.cpp
+ *
+ *	@brief	sort のテスト
+ */
+
+#include <hamon/algorithm/sort.hpp>
+#include <hamon/functional/greater.hpp>
+#include <hamon/iterator/begin.hpp>
+#include <hamon/iterator/end.hpp>
+#include <gtest/gtest.h>
+#include <array>
+#include <vector>
+
+namespace hamon_algorithm_test
+{
+
+namespace sort_test
+{
+
+GTEST_TEST(AlgorithmTest, SortTest)
+{
+	{
+		int a[] { 3, 1, 4 };
+		hamon::sort(hamon::begin(a), hamon::end(a));
+		EXPECT_EQ(1, a[0]);
+		EXPECT_EQ(3, a[1]);
+		EXPECT_EQ(4, a[2]);
+	}
+	{
+		int a[] { 3, 1, 4 };
+		hamon::sort(hamon::begin(a), hamon::end(a), hamon::greater<>());
+		EXPECT_EQ(4, a[0]);
+		EXPECT_EQ(3, a[1]);
+		EXPECT_EQ(1, a[2]);
+	}
+	{
+		std::array<int, 6> a {{ 3, 1, 4, 1, 5, 9 }};
+		hamon::sort(hamon::begin(a), hamon::end(a));
+		EXPECT_EQ(1, a[0]);
+		EXPECT_EQ(1, a[1]);
+		EXPECT_EQ(3, a[2]);
+		EXPECT_EQ(4, a[3]);
+		EXPECT_EQ(5, a[4]);
+		EXPECT_EQ(9, a[5]);
+	}
+	{
+		std::array<int, 6> a {{ 3, 1, 4, 1, 5, 9 }};
+		hamon::sort(hamon::begin(a), hamon::end(a), hamon::greater<>());
+		EXPECT_EQ(9, a[0]);
+		EXPECT_EQ(5, a[1]);
+		EXPECT_EQ(4, a[2]);
+		EXPECT_EQ(3, a[3]);
+		EXPECT_EQ(1, a[4]);
+		EXPECT_EQ(1, a[5]);
+	}
+	{
+		std::vector<int> a { 5, 4, 1, 1, 3 };
+		hamon::sort(hamon::begin(a), hamon::end(a));
+		EXPECT_EQ(1, a[0]);
+		EXPECT_EQ(1, a[1]);
+		EXPECT_EQ(3, a[2]);
+		EXPECT_EQ(4, a[3]);
+		EXPECT_EQ(5, a[4]);
+	}
+	{
+		std::vector<int> a { 5, 4, 1, 1, 3 };
+		hamon::sort(hamon::begin(a), hamon::end(a), hamon::greater<>());
+		EXPECT_EQ(5, a[0]);
+		EXPECT_EQ(4, a[1]);
+		EXPECT_EQ(3, a[2]);
+		EXPECT_EQ(1, a[3]);
+		EXPECT_EQ(1, a[4]);
+	}
+	{
+		std::vector<int> a;
+		hamon::sort(hamon::begin(a), hamon::end(a));
+		EXPECT_TRUE(a.empty());
+	}
+}
+
+}	// namespace sort_test
+
+}	// namespace hamon_algorithm_test
