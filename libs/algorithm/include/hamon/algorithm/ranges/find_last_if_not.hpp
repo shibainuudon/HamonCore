c8d82963dfe9a31e63d817d3bfc0120804101c9c
shibainuudon <shibainuudon@gmail.com>
2023-02-15 02:09:19 +0900

    [Algorithm]ranges::find_last_if_not を追加

---
+++ libs/algorithm/include/hamon/algorithm/ranges/find_last_if_not.hpp
@@ -0,0 +1,164 @@
+﻿/**
+ *	@file	find_last_if_not.hpp
+ *
+ *	@brief	ranges::find_last_if_not の定義
+ */
+
+#ifndef HAMON_ALGORITHM_RANGES_FIND_LAST_IF_NOT_HPP
+#define HAMON_ALGORITHM_RANGES_FIND_LAST_IF_NOT_HPP
+
+#include <hamon/algorithm/config.hpp>
+
+#if defined(HAMON_USE_STD_RANGES_ALGORITHM) &&	\
+	defined(__cpp_lib_find_last) && (__cpp_lib_find_last >= 202207L)
+
+#include <algorithm>
+
+namespace hamon
+{
+
+namespace ranges
+{
+
+using std::ranges::find_last_if_not;
+
+}	// namespace ranges
+
+}	// namespace hamon
+
+#else
+
+#include <hamon/algorithm/ranges/find_if_not.hpp>
+#include <hamon/concepts/detail/constrained_param.hpp>
+#include <hamon/concepts/same_as.hpp>
+#include <hamon/detail/overload_priority.hpp>
+#include <hamon/functional/identity.hpp>
+#include <hamon/iterator/concepts/bidirectional_iterator.hpp>
+#include <hamon/iterator/concepts/forward_iterator.hpp>
+#include <hamon/iterator/concepts/sentinel_for.hpp>
+#include <hamon/iterator/concepts/indirect_unary_predicate.hpp>
+#include <hamon/iterator/ranges/prev.hpp>
+#include <hamon/iterator/ranges/next.hpp>
+#include <hamon/iterator/projected.hpp>
+#include <hamon/iterator/reverse_iterator.hpp>
+#include <hamon/preprocessor/punctuation/comma.hpp>
+#include <hamon/ranges/concepts/forward_range.hpp>
+#include <hamon/ranges/views/subrange.hpp>
+#include <hamon/ranges/borrowed_subrange_t.hpp>
+#include <hamon/ranges/iterator_t.hpp>
+#include <hamon/ranges/begin.hpp>
+#include <hamon/ranges/end.hpp>
+#include <hamon/config.hpp>
+#include <utility>
+
+namespace hamon
+{
+
+namespace ranges
+{
+
+struct find_last_if_not_fn
+{
+private:
+	template <
+		HAMON_CONSTRAINED_PARAM(hamon::bidirectional_iterator, Iter),
+		HAMON_CONSTRAINED_PARAM(hamon::same_as, Iter, Sent),
+		typename Proj,
+		typename Pred
+	>
+	HAMON_CXX14_CONSTEXPR ranges::subrange<Iter> impl(
+		Iter first, Sent last, Pred pred, Proj proj,
+		hamon::detail::overload_priority<1>) const
+	{
+		Iter found = ranges::find_if_not(
+			hamon::reverse_iterator<Iter>{last},
+			hamon::reverse_iterator<Iter>{first},
+			std::move(pred), std::move(proj)).base();
+
+		if (found == first)
+		{
+			return { last, last };
+		}
+		else
+		{
+			return { ranges::prev(found), last };
+		}
+	}
+
+	template <typename Iter, typename Sent, typename Proj, typename Pred>
+	HAMON_CXX14_CONSTEXPR ranges::subrange<Iter> impl(
+		Iter first, Sent last, Pred pred, Proj proj,
+		hamon::detail::overload_priority<0>) const
+	{
+		Iter found = ranges::find_if_not(first, last, pred, proj);
+		if (found == last)
+		{
+			return { found, found };
+		}
+
+		first = found;
+
+		for (;;)
+		{
+			first = ranges::find_if_not(ranges::next(first), last, pred, proj);
+			if (first == last)
+			{
+				return { found, first };
+			}
+
+			found = first;
+		}
+	}
+
+public:
+	template <
+		HAMON_CONSTRAINED_PARAM(hamon::forward_iterator, Iter),
+		HAMON_CONSTRAINED_PARAM(hamon::sentinel_for, Iter, Sent),
+		typename Proj = hamon::identity,
+		HAMON_CONSTRAINED_PARAM(
+			hamon::indirect_unary_predicate,
+			hamon::projected<Iter HAMON_PP_COMMA() Proj>,
+			Pred)
+	>
+	HAMON_CXX14_CONSTEXPR ranges::subrange<Iter>
+	operator()(Iter first, Sent last, Pred pred, Proj proj = {}) const
+	{
+		return impl(
+			std::move(first),
+			std::move(last),
+			std::move(pred),
+			std::move(proj),
+			hamon::detail::overload_priority<1>{});
+	}
+
+	template <
+		HAMON_CONSTRAINED_PARAM(ranges::forward_range, Range),
+		typename Proj = hamon::identity,
+		HAMON_CONSTRAINED_PARAM(
+			hamon::indirect_unary_predicate,
+			hamon::projected<ranges::iterator_t<Range> HAMON_PP_COMMA() Proj>,
+			Pred)
+	>
+	HAMON_CXX14_CONSTEXPR ranges::borrowed_subrange_t<Range>
+	operator()(Range&& r, Pred pred, Proj proj = {}) const
+	{
+		return (*this)(
+			ranges::begin(r), ranges::end(r),
+			std::move(pred), std::move(proj));
+	}
+};
+
+inline namespace cpo
+{
+
+HAMON_INLINE_VAR HAMON_CONSTEXPR find_last_if_not_fn find_last_if_not{};
+
+}	// inline namespace cpo
+
+}	// namespace ranges
+
+}	// namespace hamon
+
+#endif
+
+#endif // HAMON_ALGORITHM_RANGES_FIND_LAST_IF_NOT_HPP
---
+++ libs/algorithm/test/src/ranges/unit_test_algorithm_ranges_find_last_if_not.cpp
@@ -0,0 +1,151 @@
+﻿/**
+ *	@file	unit_test_algorithm_ranges_find_last_if_not.cpp
+ *
+ *	@brief	ranges::find_last_if_not のテスト
+ */
+
+#include <hamon/algorithm/ranges/find_last_if_not.hpp>
+#include <hamon/algorithm/ranges/equal.hpp>
+#include <hamon/ranges/empty.hpp>
+#include <hamon/ranges/end.hpp>
+#include <gtest/gtest.h>
+#include "constexpr_test.hpp"
+#include "ranges_test.hpp"
+
+namespace hamon_algorithm_test
+{
+
+namespace ranges_find_last_if_not_test
+{
+
+#define VERIFY(...)	if (!(__VA_ARGS__)) { return false; }
+
+template <int N>
+struct ne
+{
+	constexpr bool operator()(int m) const { return m != N; }
+};
+
+struct plus3
+{
+	constexpr int operator()(int n) const { return n + 3; }
+};
+
+struct plus4
+{
+	constexpr int operator()(int n) const { return n + 4; }
+};
+
+inline HAMON_CXX14_CONSTEXPR bool test01()
+{
+	namespace ranges = hamon::ranges;
+
+	int x[] = { 1, 2, 1, 2, 1, 2, 1, 2 };
+	{
+		auto sr = ranges::find_last_if_not(x, ne<0>{});
+		VERIFY(ranges::empty(sr));
+		VERIFY(sr.begin() == ranges::end(x));
+	}
+	{
+		auto sr = ranges::find_last_if_not(x, ne<1>{});
+		int const y[] = { 1, 2 };
+		VERIFY(ranges::equal(sr, y));
+		VERIFY(sr.begin() == &x[6]);
+	}
+	{
+		auto sr = ranges::find_last_if_not(x, ne<2>{});
+		int const y[] = { 2 };
+		VERIFY(ranges::equal(sr, y));
+		VERIFY(sr.begin() == &x[7]);
+	}
+
+	{
+		auto sr = ranges::find_last_if_not(x, ne<3>{}, plus3{});
+		VERIFY(ranges::empty(sr));
+		VERIFY(sr.begin() == ranges::end(x));
+	}
+	{
+		auto sr = ranges::find_last_if_not(x, ne<4>{}, plus3{});
+		int const y[] = { 1, 2 };
+		VERIFY(ranges::equal(sr, y));
+		VERIFY(sr.begin() == &x[6]);
+	}
+	{
+		auto sr = ranges::find_last_if_not(x, ne<5>{}, plus3{});
+		int const y[] = { 2 };
+		VERIFY(ranges::equal(sr, y));
+		VERIFY(sr.begin() == &x[7]);
+	}
+
+	return true;
+}
+
+inline HAMON_CXX14_CONSTEXPR bool test02()
+{
+	namespace ranges = hamon::ranges;
+
+	int x[] = { 1, 2, 3, 1, 2, 3, 1, 2, 3 };
+	test_forward_range<int> rx(x);
+
+	{
+		auto sr = ranges::find_last_if_not(rx, ne<0>{});
+		VERIFY(ranges::empty(sr));
+		VERIFY(sr.begin() == ranges::end(rx));
+	}
+	{
+		auto sr = ranges::find_last_if_not(rx, ne<1>{});
+		int const y[] = { 1, 2, 3 };
+		VERIFY(ranges::equal(sr, y));
+		VERIFY(sr.begin().m_ptr == &x[6]);
+	}
+	{
+		auto sr = ranges::find_last_if_not(rx, ne<2>{});
+		int const y[] = { 2, 3 };
+		VERIFY(ranges::equal(sr, y));
+		VERIFY(sr.begin().m_ptr == &x[7]);
+	}
+	{
+		auto sr = ranges::find_last_if_not(rx, ne<3>{});
+		int const y[] = { 3 };
+		VERIFY(ranges::equal(sr, y));
+		VERIFY(sr.begin().m_ptr == &x[8]);
+	}
+
+	{
+		auto sr = ranges::find_last_if_not(rx, ne<4>{}, plus4{});
+		VERIFY(ranges::empty(sr));
+		VERIFY(sr.begin() == ranges::end(rx));
+	}
+	{
+		auto sr = ranges::find_last_if_not(rx, ne<5>{}, plus4{});
+		int const y[] = { 1, 2, 3 };
+		VERIFY(ranges::equal(sr, y));
+		VERIFY(sr.begin().m_ptr == &x[6]);
+	}
+	{
+		auto sr = ranges::find_last_if_not(rx, ne<6>{}, plus4{});
+		int const y[] = { 2, 3 };
+		VERIFY(ranges::equal(sr, y));
+		VERIFY(sr.begin().m_ptr == &x[7]);
+	}
+	{
+		auto sr = ranges::find_last_if_not(rx, ne<7>{}, plus4{});
+		int const y[] = { 3 };
+		VERIFY(ranges::equal(sr, y));
+		VERIFY(sr.begin().m_ptr == &x[8]);
+	}
+
+	return true;
+}
+
+#undef VERIFY
+
+GTEST_TEST(AlgorithmTest, RangesFindLastIfNotTest)
+{
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test01());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test02());
+}
+
+}	// namespace ranges_find_last_if_not_test
+
+}	// namespace hamon_algorithm_test
