22fd6eb8a4d7e277de71e26a19dc54ad83411809
shibainuudon <shibainuudon@gmail.com>
2023-02-22 11:34:37 +0900

    [Concepts]detail::cpp17_destructible コンセプトを追加

---
+++ libs/concepts/include/hamon/concepts/detail/cpp17_destructible.hpp
@@ -0,0 +1,54 @@
+﻿/**
+ *	@file	cpp17_destructible.hpp
+ *
+ *	@brief	cpp17_destructible の定義
+ */
+
+#ifndef HAMON_CONCEPTS_DETAIL_CPP17_DESTRUCTIBLE_HPP
+#define HAMON_CONCEPTS_DETAIL_CPP17_DESTRUCTIBLE_HPP
+
+#include <hamon/type_traits/conjunction.hpp>
+#include <hamon/type_traits/negation.hpp>
+#include <hamon/type_traits/bool_constant.hpp>
+#include <hamon/config.hpp>
+#include <type_traits>
+
+namespace hamon
+{
+
+namespace detail
+{
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+
+template <typename T>
+concept cpp17_destructible =
+	std::is_object_v<T> &&
+	std::is_destructible_v<T> &&
+	!std::is_array_v<T>;
+
+#else
+
+template <typename T>
+using cpp17_destructible =
+	hamon::conjunction<
+		std::is_object<T>,
+		std::is_destructible<T>,
+		hamon::negation<std::is_array<T>>
+	>;
+
+#endif
+
+template <typename T>
+using cpp17_destructible_t =
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+	hamon::bool_constant<hamon::detail::cpp17_destructible<T>>;
+#else
+	hamon::detail::cpp17_destructible<T>;
+#endif
+
+}	// namespace detail
+
+}	// namespace hamon
+
+#endif // HAMON_CONCEPTS_DETAIL_CPP17_DESTRUCTIBLE_HPP
---
+++ libs/concepts/test/src/detail/unit_test_concepts_cpp17_destructible.cpp
@@ -0,0 +1,72 @@
+﻿/**
+ *	@file	unit_test_concepts_cpp17_destructible.cpp
+ *
+ *	@brief	cpp17_destructible のテスト
+ */
+
+#include <hamon/concepts/detail/cpp17_destructible.hpp>
+#include <hamon/config.hpp>
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+#  define HAMON_CPP17_DESTRUCTIBLE_TEST(B, ...)	\
+	static_assert(B == hamon::detail::cpp17_destructible<__VA_ARGS__>, "");	\
+	static_assert(B == hamon::detail::cpp17_destructible_t<__VA_ARGS__>::value, "")
+#else
+#  define HAMON_CPP17_DESTRUCTIBLE_TEST(B, ...)	\
+	static_assert(B == hamon::detail::cpp17_destructible<__VA_ARGS__>::value, "");	\
+	static_assert(B == hamon::detail::cpp17_destructible_t<__VA_ARGS__>::value, "")
+#endif
+
+namespace hamon_concepts_test
+{
+
+namespace cpp17_destructible_test
+{
+
+HAMON_CPP17_DESTRUCTIBLE_TEST(false, void);
+HAMON_CPP17_DESTRUCTIBLE_TEST(true,  void*);
+HAMON_CPP17_DESTRUCTIBLE_TEST(true,  char);
+HAMON_CPP17_DESTRUCTIBLE_TEST(true,  float);
+HAMON_CPP17_DESTRUCTIBLE_TEST(true,  int);
+HAMON_CPP17_DESTRUCTIBLE_TEST(true,  int const);
+HAMON_CPP17_DESTRUCTIBLE_TEST(true,  int volatile);
+HAMON_CPP17_DESTRUCTIBLE_TEST(true,  int const volatile);
+HAMON_CPP17_DESTRUCTIBLE_TEST(true,  int*);
+HAMON_CPP17_DESTRUCTIBLE_TEST(false, int&);			// different from `destructible`
+HAMON_CPP17_DESTRUCTIBLE_TEST(false, int&&);		// different from `destructible`
+HAMON_CPP17_DESTRUCTIBLE_TEST(false, int const&);	// different from `destructible`
+HAMON_CPP17_DESTRUCTIBLE_TEST(false, int[]);
+HAMON_CPP17_DESTRUCTIBLE_TEST(false, int[2]);		// different from `destructible`
+HAMON_CPP17_DESTRUCTIBLE_TEST(false, int());
+HAMON_CPP17_DESTRUCTIBLE_TEST(true,  int(*)());
+HAMON_CPP17_DESTRUCTIBLE_TEST(false, int(&)());		// different from `destructible`
+
+enum E { };
+HAMON_CPP17_DESTRUCTIBLE_TEST(true,  E);
+enum class CE { };
+HAMON_CPP17_DESTRUCTIBLE_TEST(true,  CE);
+struct A { };
+HAMON_CPP17_DESTRUCTIBLE_TEST(true,  A);
+union B { };
+HAMON_CPP17_DESTRUCTIBLE_TEST(true,  B);
+
+struct C
+{
+	~C() noexcept(false) { }
+};
+HAMON_CPP17_DESTRUCTIBLE_TEST(true, C);				// different from `destructible`
+
+class D
+{
+public:
+	D() { }
+private:
+	~D() { }
+};
+HAMON_CPP17_DESTRUCTIBLE_TEST(false, D);
+
+}	// namespace cpp17_destructible_test
+
+}	// namespace hamon_concepts_test
+
+#undef HAMON_CPP17_DESTRUCTIBLE_TEST
