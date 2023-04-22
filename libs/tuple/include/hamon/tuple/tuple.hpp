872dd8a8fa7461b6ff729ae005e24ad4b327f88c
shibainuudon <shibainuudon@gmail.com>
2023-04-22 22:11:02 +0900

    [Tuple]common_typeの特殊化を追加

--- libs/tuple/include/hamon/tuple/tuple.hpp
+++ libs/tuple/include/hamon/tuple/tuple.hpp
@@ -1950,17 +1950,6 @@ operator>=(tuple<TTypes...> const& t, UTuple const& u)
 
 #endif
 
-
-#if 0	// TODO
-// common_reference related specializations	[tuple.common.ref]
-
-template <tuple-like TTuple, tuple-like UTuple>
-struct common_type<TTuple, UTuple>
-{
-	using type = see below;
-};
-#endif
-
 #if 0	// TODO
 // Tuple traits	[tuple.traits]
 template <typename... Types, typename Alloc>
@@ -2016,18 +2005,16 @@ struct is_specialization_of_tuple<hamon::tuple<Types...>>
 
 // basic_common_referenceの特殊化
 
-namespace HAMON_BASIC_COMMON_REFERENCE_NAMESPACE
-{
-
-namespace detail
-{
+namespace hamon {
+namespace detail {
 
 template <
 	typename TTuple,
 	typename UTuple,
 	template <typename> class TQual,
 	template <typename> class UQual,
-    typename Indices = hamon::make_index_sequence<std::tuple_size<TTuple>::value>>
+    typename Indices = hamon::make_index_sequence<std::tuple_size<TTuple>::value>,
+	typename = void>
 struct tuple_like_common_reference;
 
 template <
@@ -2036,7 +2023,13 @@ template <
 	template <typename> class TQual,
 	template <typename> class UQual,
     hamon::size_t... I>
-struct tuple_like_common_reference<TTuple, UTuple, TQual, UQual, hamon::index_sequence<I...>>
+struct tuple_like_common_reference<TTuple, UTuple, TQual, UQual,
+	hamon::index_sequence<I...>,
+	hamon::void_t<hamon::tuple<hamon::common_reference_t<
+		TQual<hamon::tuple_element_t<I, TTuple>>,
+		UQual<hamon::tuple_element_t<I, UTuple>>
+	>...>>
+>
 {
     using type = hamon::tuple<hamon::common_reference_t<
 		TQual<hamon::tuple_element_t<I, TTuple>>,
@@ -2045,11 +2038,14 @@ struct tuple_like_common_reference<TTuple, UTuple, TQual, UQual, hamon::index_se
 };
 
 }	// namespace detail
+}	// namespace hamon
 
-// common_reference related specializations	[tuple.common.ref]
+namespace HAMON_BASIC_COMMON_REFERENCE_NAMESPACE
+{
 
 #if defined(HAMON_HAS_CXX20_CONCEPTS)
 
+// common_reference related specializations	[tuple.common.ref]
 template <
 	hamon::tuple_like TTuple,
 	hamon::tuple_like UTuple,
@@ -2061,10 +2057,10 @@ requires
 	hamon::is_same_v<TTuple, hamon::decay_t<TTuple>> &&		// [tuple.common.ref]2.2
 	hamon::is_same_v<UTuple, hamon::decay_t<UTuple>> &&		// [tuple.common.ref]2.3
 	(std::tuple_size_v<TTuple> == std::tuple_size_v<UTuple>) &&	// [tuple.common.ref]2.4
-	requires { typename detail::tuple_like_common_reference<TTuple, UTuple, TQual, UQual>::type; }	// [tuple.common.ref]2.5
+	requires { typename hamon::detail::tuple_like_common_reference<TTuple, UTuple, TQual, UQual>::type; }	// [tuple.common.ref]2.5
 struct basic_common_reference<TTuple, UTuple, TQual, UQual>
 {
-	using type = typename detail::tuple_like_common_reference<TTuple, UTuple, TQual, UQual>::type;
+	using type = typename hamon::detail::tuple_like_common_reference<TTuple, UTuple, TQual, UQual>::type;
 };
 
 #else
@@ -2095,10 +2091,10 @@ struct tuple_basic_common_reference_impl<TTuple, UTuple, TQual, UQual,
 		hamon::is_same<UTuple, hamon::decay_t<UTuple>>::value &&		// [tuple.common.ref]2.3
 		(std::tuple_size<TTuple>::value == std::tuple_size<UTuple>::value)	// [tuple.common.ref]2.4
 	>,
-	hamon::void_t<typename tuple_like_common_reference<TTuple, UTuple, TQual, UQual>::type>	// [tuple.common.ref]2.5
+	hamon::void_t<typename hamon::detail::tuple_like_common_reference<TTuple, UTuple, TQual, UQual>::type>	// [tuple.common.ref]2.5
 >
 {
-	using type = typename tuple_like_common_reference<TTuple, UTuple, TQual, UQual>::type;
+	using type = typename hamon::detail::tuple_like_common_reference<TTuple, UTuple, TQual, UQual>::type;
 };
 
 template <
@@ -2158,6 +2154,129 @@ struct basic_common_reference<hamon::tuple<TTypes...>, hamon::tuple<UTypes...>, 
 #endif
 
 }	// namespace HAMON_BASIC_COMMON_REFERENCE_NAMESPACE
+
+#include <hamon/type_traits/common_type.hpp>
+#include <hamon/utility/make_index_sequence.hpp>
+
+namespace hamon
+{
+
+namespace detail
+{
+
+template <typename TTuple, typename UTuple,
+    typename Indices = hamon::make_index_sequence<std::tuple_size<TTuple>::value>,
+	typename = void>
+struct tuple_like_common_type;
+
+template <typename TTuple, typename UTuple, hamon::size_t... I>
+struct tuple_like_common_type<TTuple, UTuple,
+	hamon::index_sequence<I...>,
+	hamon::void_t<hamon::tuple<hamon::common_type_t<
+		hamon::tuple_element_t<I, TTuple>,
+		hamon::tuple_element_t<I, UTuple>
+	>...>>>
+{
+    using type = hamon::tuple<hamon::common_type_t<
+		hamon::tuple_element_t<I, TTuple>,
+		hamon::tuple_element_t<I, UTuple>
+	>...>;
+};
+
+}	// namespace detail
+
+}	// namespace hamon
+
+namespace HAMON_COMMON_TYPE_NAMESPACE
+{
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+
+// common_reference related specializations	[tuple.common.ref]
+template <hamon::tuple_like TTuple, hamon::tuple_like UTuple>
+requires
+	(hamon::detail::is_specialization_of_tuple<TTuple>::value ||	// [tuple.common.ref]/3.1
+	 hamon::detail::is_specialization_of_tuple<UTuple>::value) &&
+	hamon::is_same_v<TTuple, hamon::decay_t<TTuple>> &&				// [tuple.common.ref]3.2
+	hamon::is_same_v<UTuple, hamon::decay_t<UTuple>> &&				// [tuple.common.ref]3.3
+	(std::tuple_size_v<TTuple> == std::tuple_size_v<UTuple>) &&		// [tuple.common.ref]3.4
+	requires { typename hamon::detail::tuple_like_common_type<TTuple, UTuple>::type; }	// [tuple.common.ref]3.5
+struct common_type<TTuple, UTuple>
+{
+	using type = typename hamon::detail::tuple_like_common_type<TTuple, UTuple>::type;
+};
+
+#else
+
+namespace detail
+{
+
+template <
+	typename TTuple,
+	typename UTuple,
+	typename = void,
+	typename = void
+>
+struct tuple_common_type_impl
+{};
+
+template <
+	typename TTuple,
+	typename UTuple
+>
+struct tuple_common_type_impl<TTuple, UTuple,
+	hamon::enable_if_t<
+		hamon::is_same<TTuple, hamon::decay_t<TTuple>>::value &&			// [tuple.common.ref]3.2
+		hamon::is_same<UTuple, hamon::decay_t<UTuple>>::value &&			// [tuple.common.ref]3.3
+		(std::tuple_size<TTuple>::value == std::tuple_size<UTuple>::value)	// [tuple.common.ref]3.4
+	>,
+	hamon::void_t<typename hamon::detail::tuple_like_common_type<TTuple, UTuple>::type>	// [tuple.common.ref]3.5
+>
+{
+	using type = typename hamon::detail::tuple_like_common_type<TTuple, UTuple>::type;
+};
+
+template <
+	typename TTuple,
+	typename UTuple,
+	typename = void
+>
+struct tuple_common_type
+{};
+
+template <
+	typename TTuple,
+	typename UTuple
+>
+struct tuple_common_type<TTuple, UTuple,
+	hamon::enable_if_t<hamon::conjunction<
+		hamon::tuple_like_t<hamon::decay_t<TTuple>>,
+		hamon::tuple_like_t<hamon::decay_t<UTuple>>
+	>::value>
+>
+	: public tuple_common_type_impl<hamon::decay_t<TTuple>, hamon::decay_t<UTuple>>
+{};
+
+}	// namespace detail
+
+template <typename... TTypes, typename UTuple>
+struct common_type<hamon::tuple<TTypes...>, UTuple>
+	: public detail::tuple_common_type<hamon::tuple<TTypes...>, UTuple>
+{};
+
+template <typename TTuple, typename... UTypes>
+struct common_type<TTuple, hamon::tuple<UTypes...>>
+	: public detail::tuple_common_type<TTuple, hamon::tuple<UTypes...>>
+{};
+
+template <typename... TTypes, typename... UTypes>
+struct common_type<hamon::tuple<TTypes...>, hamon::tuple<UTypes...>>
+	: public detail::tuple_common_type<hamon::tuple<TTypes...>, hamon::tuple<UTypes...>>
+{};
+
+#endif
+
+}	// namespace HAMON_COMMON_TYPE_NAMESPACE
 
 #endif
 
--- libs/tuple/test/src/tuple/unit_test_tuple_tuple_common_type.cpp
+++ libs/tuple/test/src/tuple/unit_test_tuple_tuple_common_type.cpp
@@ -10,7 +10,11 @@
  */
 
 #include <hamon/tuple/tuple.hpp>
+#include <hamon/array.hpp>
+#include <hamon/pair.hpp>
+#include <hamon/ranges/views/subrange.hpp>
 #include <hamon/type_traits.hpp>
+#include <hamon/concepts.hpp>
 #include <hamon/config.hpp>
 
 namespace hamon_tuple_test
@@ -19,7 +23,87 @@ namespace hamon_tuple_test
 namespace common_type_test
 {
 
-// TODO
+template <typename T, typename = void>
+struct has_type : hamon::false_type { };
+
+template <typename T>
+struct has_type<T, hamon::void_t<typename T::type>>: hamon::true_type { };
+
+template <typename... T>
+constexpr bool
+has_common_type()
+{
+	return has_type<hamon::common_type<T...>>::value;
+}
+
+static_assert( has_common_type<hamon::tuple<int>, hamon::tuple<int>>(), "");
+static_assert(!has_common_type<hamon::tuple<int>, hamon::tuple<int*>>(), "");
+static_assert( has_common_type<hamon::tuple<int>, hamon::tuple<int&>>(), "");
+static_assert(!has_common_type<hamon::tuple<int>, hamon::tuple<int, int>>(), "");
+static_assert( has_common_type<hamon::tuple<int, int>, hamon::tuple<int, int>>(), "");
+static_assert( has_common_type<hamon::tuple<int>, hamon::array<int, 1>>(), "");
+static_assert(!has_common_type<hamon::tuple<int>, hamon::array<int, 2>>(), "");
+static_assert(!has_common_type<hamon::tuple<int*>, hamon::array<int, 1>>(), "");
+static_assert(!has_common_type<hamon::tuple<int, int>, hamon::array<int, 1>>(), "");
+static_assert( has_common_type<hamon::tuple<int, int>, hamon::array<int, 2>>(), "");
+static_assert(!has_common_type<hamon::tuple<int>, hamon::pair<int, int>>(), "");
+static_assert( has_common_type<hamon::tuple<int, int>, hamon::pair<int, int>>(), "");
+static_assert(!has_common_type<hamon::tuple<int, int, int>, hamon::pair<int, int>>(), "");
+static_assert( has_common_type<hamon::tuple<int*, int*>, hamon::ranges::subrange<int*, int*>>(), "");
+static_assert(!has_common_type<hamon::tuple<int, int>, hamon::ranges::subrange<int*, int*>>(), "");
+static_assert(!has_common_type<hamon::tuple<int*>, hamon::ranges::subrange<int*, int*>>(), "");
+
+template <typename T, typename U, typename Expected>
+constexpr bool test_common_type()
+{
+	return
+		hamon::same_as_t<hamon::common_type_t<T, U>, Expected>::value &&
+		hamon::same_as_t<hamon::common_type_t<U, T>, Expected>::value;
+}
+
+// Test common_type<tuple, tuple>
+static_assert(test_common_type<hamon::tuple<>, hamon::tuple<>, hamon::tuple<>>(), "");
+static_assert(test_common_type<hamon::tuple<char>, hamon::tuple<int>, hamon::tuple<int>>(), "");
+static_assert(test_common_type<hamon::tuple<int&>, hamon::tuple<const int&>, hamon::tuple<int>>(), "");
+static_assert(test_common_type<hamon::tuple<int&>, hamon::tuple<int&&>, hamon::tuple<int>>(), "");
+static_assert(test_common_type<hamon::tuple<short, long, long>, hamon::tuple<long, int, short>, hamon::tuple<long, long, long>>(), "");
+
+// Test common_reference<cvref tuple, cvref tuple>
+static_assert(test_common_type<const hamon::tuple<>&, hamon::tuple<>&&, hamon::tuple<>>(), "");
+static_assert(test_common_type<hamon::tuple<char>&, const hamon::tuple<int>&&, hamon::tuple<int>>(), "");
+static_assert(test_common_type<hamon::tuple<int&>&, hamon::tuple<const int&>, hamon::tuple<int>>(), "");
+static_assert(test_common_type<volatile hamon::tuple<int&>&, hamon::tuple<int&&>, hamon::tuple<int>>(), "");
+
+// Test common_type<tuple, tuple-like>, tuple-like is pair
+static_assert(test_common_type<hamon::tuple<int, int>, hamon::pair<int, int>, hamon::tuple<int, int>>(), "");
+static_assert(test_common_type<hamon::tuple<int, long>, hamon::pair<short, long long>, hamon::tuple<int, long long>>(), "");
+static_assert(test_common_type<hamon::tuple<int&, long&&>, hamon::pair<const int&, const long&&>, hamon::tuple<int, long>>(), "");
+static_assert(test_common_type<hamon::tuple<short&, const volatile long&&>, hamon::pair<volatile int, long>, hamon::tuple<int, long>>(), "");
+
+// Test common_type<cvref tuple, cvref tuple-like>, tuple-like is pair
+static_assert(test_common_type<const hamon::tuple<int, int>&&, volatile hamon::pair<int, int>&, hamon::tuple<int, int>>(), "");
+static_assert(test_common_type<volatile hamon::tuple<int, long>&&, const hamon::pair<short, long long>&&, hamon::tuple<int, long long>>(), "");
+
+// Test common_type<tuple, tuple-like>, tuple-like is array
+static_assert(test_common_type<hamon::tuple<>, hamon::array<int, 0>, hamon::tuple<>>(), "");
+static_assert(test_common_type<hamon::tuple<int>, hamon::array<int, 1>, hamon::tuple<int>>(), "");
+static_assert(test_common_type<hamon::tuple<int, long>, hamon::array<int, 2>, hamon::tuple<int, long>>(), "");
+static_assert(test_common_type<hamon::tuple<short, int, long>, hamon::array<int, 3>, hamon::tuple<int, int, long>>(), "");
+static_assert(test_common_type<hamon::tuple<short&, int&&, const volatile long>, hamon::array<int, 3>, hamon::tuple<int, int, long>>(), "");
+
+// Test common_type<cvref tuple, cvref tuple-like>, tuple-like is array
+static_assert(test_common_type<const hamon::tuple<>&&, volatile hamon::array<int, 0>&, hamon::tuple<>>(), "");
+static_assert(test_common_type<const hamon::tuple<int>&, volatile hamon::array<int, 1>&&, hamon::tuple<int>>(), "");
+
+// Test common_type<tuple, tuple-like>, tuple-like is ranges::subrange
+static_assert(test_common_type<hamon::tuple<int*, int*>, hamon::ranges::subrange<int*, int*>, hamon::tuple<int*, int*>>(), "");
+static_assert(test_common_type<hamon::tuple<int*, const int*>, hamon::ranges::subrange<const int*, int*>, hamon::tuple<const int*, const int*>>(), "");
+//static_assert(test_common_type<hamon::tuple<int*, Sentinel>, hamon::ranges::subrange<int*, unreachable_sentinel_t>, hamon::tuple<int*, unreachable_sentinel_t>>(), "");
+//static_assert(test_common_type<hamon::tuple<const volatile int*, Sentinel>, hamon::ranges::subrange<int*, unreachable_sentinel_t>, hamon::tuple<const volatile int*, unreachable_sentinel_t>>(), "");
+
+// Test common_type<cvref tuple, cvref tuple-like>, tuple-like is ranges::subrange
+static_assert(test_common_type<const hamon::tuple<int*, int*>&, volatile hamon::ranges::subrange<int*, int*>&&, hamon::tuple<int*, int*>>(), "");
+//static_assert(test_common_type<volatile hamon::tuple<int*, Sentinel>&, const hamon::ranges::subrange<int*, unreachable_sentinel_t>&&, hamon::tuple<int*, unreachable_sentinel_t>>(), "");
 
 }	// namespace common_type_test
 
