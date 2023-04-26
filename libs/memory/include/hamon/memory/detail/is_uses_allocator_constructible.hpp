087a750c90a1630e1415de04b791fd82afc30143
shibainuudon <shibainuudon@gmail.com>
2023-04-26 17:25:49 +0900

    [Memory]is_uses_allocator_constructible を追加

---
+++ libs/memory/include/hamon/memory/detail/is_uses_allocator_constructible.hpp
@@ -0,0 +1,31 @@
+﻿/**
+ *	@file	is_uses_allocator_constructible.hpp
+ *
+ *	@brief	is_uses_allocator_constructible を定義
+ */
+
+#ifndef HAMON_MEMORY_DETAIL_IS_USES_ALLOCATOR_CONSTRUCTIBLE_HPP
+#define HAMON_MEMORY_DETAIL_IS_USES_ALLOCATOR_CONSTRUCTIBLE_HPP
+
+#include <hamon/memory/detail/uses_allocator_construction_type.hpp>
+#include <hamon/type_traits/negation.hpp>
+#include <hamon/type_traits/is_same.hpp>
+
+namespace hamon
+{
+
+namespace detail
+{
+
+template <typename T, typename Alloc, typename... Args>
+using is_uses_allocator_constructible =
+	hamon::negation<hamon::is_same<
+		hamon::detail::uses_allocator_construction_type_t<T, Alloc, Args...>,
+		hamon::detail::uses_allocator_construction_type::Invalid
+	>>;
+
+}	// namespace detail
+
+}	// namespace hamon
+
+#endif // HAMON_MEMORY_DETAIL_IS_USES_ALLOCATOR_CONSTRUCTIBLE_HPP
---
+++ libs/memory/test/src/detail/unit_test_memory_detail_is_uses_allocator_constructible.cpp
@@ -0,0 +1,80 @@
+﻿/**
+ *	@file	unit_test_memory_detail_is_uses_allocator_constructible.cpp
+ *
+ *	@brief	is_uses_allocator_constructible のテスト
+ */
+
+#include <hamon/memory/detail/is_uses_allocator_constructible.hpp>
+#include <hamon/memory/allocator_arg_t.hpp>
+
+namespace hamon_memory_test
+{
+
+namespace is_uses_allocator_constructible_test
+{
+
+struct MyAlloc {};
+
+struct FirstAlloc
+{
+	using allocator_type = MyAlloc;
+	FirstAlloc(hamon::allocator_arg_t, MyAlloc const&, int);
+	FirstAlloc(hamon::allocator_arg_t, MyAlloc const&, char, float);
+};
+
+struct LastAlloc
+{
+	using allocator_type = MyAlloc;
+	LastAlloc(MyAlloc const&);
+	LastAlloc(int, MyAlloc const&);
+};
+
+struct NoAlloc
+{
+	NoAlloc(int);
+};
+
+struct NoAlloc2
+{
+	NoAlloc2(char, float);
+};
+
+struct Invalid
+{
+	using allocator_type = MyAlloc;
+	Invalid(int);
+};
+
+struct Invalid2
+{
+	using allocator_type = MyAlloc;
+	Invalid2(char, float);
+};
+
+static_assert( hamon::detail::is_uses_allocator_constructible<int,        MyAlloc>::value, "");
+static_assert(!hamon::detail::is_uses_allocator_constructible<FirstAlloc, MyAlloc>::value, "");
+static_assert( hamon::detail::is_uses_allocator_constructible<LastAlloc,  MyAlloc>::value, "");
+static_assert(!hamon::detail::is_uses_allocator_constructible<NoAlloc,    MyAlloc>::value, "");
+static_assert(!hamon::detail::is_uses_allocator_constructible<NoAlloc2,   MyAlloc>::value, "");
+static_assert(!hamon::detail::is_uses_allocator_constructible<Invalid,    MyAlloc>::value, "");
+static_assert(!hamon::detail::is_uses_allocator_constructible<Invalid2,   MyAlloc>::value, "");
+
+static_assert( hamon::detail::is_uses_allocator_constructible<int,        MyAlloc, int>::value, "");
+static_assert( hamon::detail::is_uses_allocator_constructible<FirstAlloc, MyAlloc, int>::value, "");
+static_assert( hamon::detail::is_uses_allocator_constructible<LastAlloc,  MyAlloc, int>::value, "");
+static_assert( hamon::detail::is_uses_allocator_constructible<NoAlloc,    MyAlloc, int>::value, "");
+static_assert(!hamon::detail::is_uses_allocator_constructible<NoAlloc2,   MyAlloc, int>::value, "");
+static_assert(!hamon::detail::is_uses_allocator_constructible<Invalid,    MyAlloc, int>::value, "");
+static_assert(!hamon::detail::is_uses_allocator_constructible<Invalid2,   MyAlloc, int>::value, "");
+
+static_assert(!hamon::detail::is_uses_allocator_constructible<int,        MyAlloc, char, float>::value, "");
+static_assert( hamon::detail::is_uses_allocator_constructible<FirstAlloc, MyAlloc, char, float>::value, "");
+static_assert(!hamon::detail::is_uses_allocator_constructible<LastAlloc,  MyAlloc, char, float>::value, "");
+static_assert(!hamon::detail::is_uses_allocator_constructible<NoAlloc,    MyAlloc, char, float>::value, "");
+static_assert( hamon::detail::is_uses_allocator_constructible<NoAlloc2,   MyAlloc, char, float>::value, "");
+static_assert(!hamon::detail::is_uses_allocator_constructible<Invalid,    MyAlloc, char, float>::value, "");
+static_assert(!hamon::detail::is_uses_allocator_constructible<Invalid2,   MyAlloc, char, float>::value, "");
+
+}	// namespace is_uses_allocator_constructible_test
+
+}	// namespace hamon_memory_test
