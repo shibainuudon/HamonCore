632fe7161488f77634e6adf54bf8ff583bf893c6
shibainuudon <shibainuudon@gmail.com>
2023-04-26 16:22:41 +0900

    [Memory]uses_allocator_construction_typeを追加
    uses-allocator構築で、どの種類になるか取得できるようにした

---
+++ libs/memory/include/hamon/memory/detail/uses_allocator_construction_type.hpp
@@ -0,0 +1,69 @@
+﻿/**
+ *	@file	uses_allocator_construction_type.hpp
+ *
+ *	@brief	uses_allocator_construction_type を定義
+ */
+
+#ifndef HAMON_MEMORY_DETAIL_USES_ALLOCATOR_CONSTRUCTION_TYPE_HPP
+#define HAMON_MEMORY_DETAIL_USES_ALLOCATOR_CONSTRUCTION_TYPE_HPP
+
+#include <hamon/memory/allocator_arg_t.hpp>
+#include <hamon/type_traits/is_constructible.hpp>
+#include <hamon/type_traits/remove_cv.hpp>
+#include <hamon/type_traits/conditional.hpp>
+#include <hamon/config.hpp>
+#include <memory>	// uses_allocator
+
+namespace hamon
+{
+
+namespace detail
+{
+
+// Uses-allocator construction
+// [allocator.uses.construction]/2 の仕様に従って、
+// uses-allocator構築のための引数の渡し方の種類を取得する。
+struct uses_allocator_construction_type
+{
+	// Allocator を使わない
+	struct NoAlloc{};
+
+	// allocator_arg_t, Allocator を最初に渡す
+	struct FirstAlloc{};
+
+	// Allocator を最後に渡す
+	struct LastAlloc{};
+
+	// uses-allocator 構築できない
+	struct Invalid{};
+
+	template <typename T, typename Alloc, typename... Args>
+	using type =
+		hamon::conditional_t<
+			!std::uses_allocator<hamon::remove_cv_t<T>, Alloc>::value,
+			hamon::conditional_t<
+				hamon::is_constructible<T, Args...>::value,
+				NoAlloc,		// [allocator.uses.construction]/2.1
+				Invalid
+			>,
+			hamon::conditional_t<
+				hamon::is_constructible<T, hamon::allocator_arg_t, Alloc const&, Args...>::value,
+				FirstAlloc,		// [allocator.uses.construction]/2.2
+				hamon::conditional_t<
+					hamon::is_constructible<T, Args..., Alloc const&>::value,
+					LastAlloc,	// [allocator.uses.construction]/2.3
+					Invalid
+				>
+			>
+		>;
+};
+
+template <typename T, typename Alloc, typename... Args>
+using uses_allocator_construction_type_t =
+	typename uses_allocator_construction_type::template type<T, Alloc, Args...>;
+
+}	// namespace detail
+
+}	// namespace hamon
+
+#endif // HAMON_MEMORY_DETAIL_USES_ALLOCATOR_CONSTRUCTION_TYPE_HPP
--- libs/memory/include/hamon/memory/uses_allocator_construction_args.hpp
+++ libs/memory/include/hamon/memory/uses_allocator_construction_args.hpp
@@ -21,17 +21,18 @@ using std::uses_allocator_construction_args;
 #else
 
 #include <hamon/memory/allocator_arg_t.hpp>
+#include <hamon/memory/detail/uses_allocator_construction_type.hpp>
 #include <hamon/concepts/detail/is_specialization_of_pair.hpp>
-#include <hamon/detail/overload_priority.hpp>
 #include <hamon/pair/pair.hpp>
 #include <hamon/tuple/adl_get.hpp>
 #include <hamon/tuple/apply.hpp>
 #include <hamon/tuple/forward_as_tuple.hpp>
 #include <hamon/tuple/make_tuple.hpp>
+#include <hamon/tuple/tuple.hpp>
 #include <hamon/type_traits/enable_if.hpp>
 #include <hamon/type_traits/remove_cv.hpp>
-#include <hamon/type_traits/is_constructible.hpp>
 #include <hamon/utility/forward.hpp>
+#include <hamon/config.hpp>
 
 #define HAMON_DECLTYPE_RETURN(...)	\
 	-> decltype(__VA_ARGS__)		\
@@ -44,41 +45,47 @@ namespace detail
 {
 
 template <typename T>
+struct uses_allocator_construction_args_impl
+{
+private:
+	// [allocator.uses.construction]/5.1
+	template <typename Alloc, typename... Args>
+	static HAMON_CXX11_CONSTEXPR auto
+	impl(uses_allocator_construction_type::NoAlloc, Alloc const&, Args&&... args) HAMON_NOEXCEPT
+	HAMON_DECLTYPE_RETURN(
+		hamon::forward_as_tuple(hamon::forward<Args>(args)...))
+
+	// [allocator.uses.construction]/5.2
+	template <typename Alloc, typename... Args>
+	static HAMON_CXX11_CONSTEXPR auto
+	impl(uses_allocator_construction_type::FirstAlloc, Alloc const& alloc, Args&&... args) HAMON_NOEXCEPT
+	HAMON_DECLTYPE_RETURN(
+		hamon::tuple<hamon::allocator_arg_t, Alloc const&, Args&&...>(
+			hamon::allocator_arg, alloc, hamon::forward<Args>(args)...))
+
+	// [allocator.uses.construction]/5.3
+	template <typename Alloc, typename... Args>
+	static HAMON_CXX11_CONSTEXPR auto
+	impl(uses_allocator_construction_type::LastAlloc, Alloc const& alloc, Args&&... args) HAMON_NOEXCEPT
+	HAMON_DECLTYPE_RETURN(
+		hamon::forward_as_tuple(hamon::forward<Args>(args)..., alloc))
+
+	// [allocator.uses.construction]/5.4
+	// Otherwise, the program is ill-formed.
+
+public:
+	template <typename Alloc, typename... Args>
+	static HAMON_CXX11_CONSTEXPR auto
+	invoke(Alloc const& alloc, Args&&... args) HAMON_NOEXCEPT
+	HAMON_DECLTYPE_RETURN(impl(
+		hamon::detail::uses_allocator_construction_type_t<T, Alloc, Args...>{},
+		alloc, hamon::forward<Args>(args)...))
+};
+
+// is_cv_pair
+template <typename T>
 using is_cv_pair =
 	hamon::detail::is_specialization_of_pair<hamon::remove_cv_t<T>>;
-
-// [allocator.uses.construction]/5.1
-template <typename T, typename Alloc, typename... Args,
-	typename = hamon::enable_if_t<
-		!std::uses_allocator<hamon::remove_cv_t<T>, Alloc>::value &&
-		hamon::is_constructible<T, Args...>::value>>
-constexpr auto
-uses_allocator_construction_args_impl(hamon::detail::overload_priority<2>, Alloc const&, Args&&... args) noexcept
-HAMON_DECLTYPE_RETURN(
-	hamon::forward_as_tuple(hamon::forward<Args>(args)...))
-
-// [allocator.uses.construction]/5.2
-template <typename T, typename Alloc, typename... Args,
-	typename = hamon::enable_if_t<
-		std::uses_allocator<hamon::remove_cv_t<T>, Alloc>::value &&
-		hamon::is_constructible<T, hamon::allocator_arg_t, Alloc const&, Args...>::value>>
-constexpr auto
-uses_allocator_construction_args_impl(hamon::detail::overload_priority<1>, Alloc const& alloc, Args&&... args) noexcept
-HAMON_DECLTYPE_RETURN(
-	hamon::tuple<hamon::allocator_arg_t, Alloc const&, Args&&...>(
-		hamon::allocator_arg, alloc, hamon::forward<Args>(args)...))
-
-// [allocator.uses.construction]/5.3
-template <typename T, typename Alloc, typename... Args,
-	typename = hamon::enable_if_t<
-		std::uses_allocator<hamon::remove_cv_t<T>, Alloc>::value &&
-		hamon::is_constructible<T, Args..., Alloc const&>::value>>
-constexpr auto
-uses_allocator_construction_args_impl(hamon::detail::overload_priority<0>, Alloc const& alloc, Args&&... args) noexcept
-HAMON_DECLTYPE_RETURN(
-	hamon::forward_as_tuple(hamon::forward<Args>(args)..., alloc))
-
-// [allocator.uses.construction]/5.4
 
 }	// namespace detail
 
@@ -86,11 +93,10 @@ HAMON_DECLTYPE_RETURN(
 template <typename T, typename Alloc, typename... Args,
 	typename = hamon::enable_if_t<
 		!hamon::detail::is_cv_pair<T>::value>>		// [allocator.uses.construction]/4
-constexpr auto
-uses_allocator_construction_args(Alloc const& alloc, Args&&... args) noexcept
+HAMON_CXX11_CONSTEXPR auto
+uses_allocator_construction_args(Alloc const& alloc, Args&&... args) HAMON_NOEXCEPT
 HAMON_DECLTYPE_RETURN(
-	hamon::detail::uses_allocator_construction_args_impl<T>(
-		hamon::detail::overload_priority<2>{},
+	hamon::detail::uses_allocator_construction_args_impl<T>::invoke(
 		alloc, hamon::forward<Args>(args)...))
 
 namespace detail
@@ -102,7 +108,7 @@ struct UsesAllocatorConstructionArgs
 	Alloc const& m_alloc;
 
 	template <typename... Args>
-	constexpr auto operator()(Args&&... args) const noexcept
+	HAMON_CXX11_CONSTEXPR auto operator()(Args&&... args) const HAMON_NOEXCEPT
 	HAMON_DECLTYPE_RETURN(
 		hamon::uses_allocator_construction_args<T>(
 			m_alloc, hamon::forward<Args>(args)...))
@@ -113,8 +119,8 @@ struct UsesAllocatorConstructionArgs
 template <typename T, typename Alloc, typename Tuple1, typename Tuple2,
 	typename = hamon::enable_if_t<
 		hamon::detail::is_cv_pair<T>::value>>		// [allocator.uses.construction]/7
-constexpr auto
-uses_allocator_construction_args(Alloc const& alloc, hamon::piecewise_construct_t, Tuple1&& x, Tuple2&& y) noexcept
+HAMON_CXX11_CONSTEXPR auto
+uses_allocator_construction_args(Alloc const& alloc, hamon::piecewise_construct_t, Tuple1&& x, Tuple2&& y) HAMON_NOEXCEPT
 HAMON_DECLTYPE_RETURN(
 	// [allocator.uses.construction]/8
 	hamon::make_tuple(
@@ -130,8 +136,8 @@ HAMON_DECLTYPE_RETURN(
 template <typename T, typename Alloc,
 	typename = hamon::enable_if_t<
 		hamon::detail::is_cv_pair<T>::value>>		// [allocator.uses.construction]/9
-constexpr auto
-uses_allocator_construction_args(Alloc const& alloc) noexcept
+HAMON_CXX11_CONSTEXPR auto
+uses_allocator_construction_args(Alloc const& alloc) HAMON_NOEXCEPT
 HAMON_DECLTYPE_RETURN(
 	// [allocator.uses.construction]/10
 	hamon::uses_allocator_construction_args<T>(
@@ -143,8 +149,8 @@ HAMON_DECLTYPE_RETURN(
 template <typename T, typename Alloc, typename U, typename V,
 	typename = hamon::enable_if_t<
 		hamon::detail::is_cv_pair<T>::value>>		// [allocator.uses.construction]/11
-constexpr auto
-uses_allocator_construction_args(Alloc const& alloc, U&& u, V&& v) noexcept
+HAMON_CXX11_CONSTEXPR auto
+uses_allocator_construction_args(Alloc const& alloc, U&& u, V&& v) HAMON_NOEXCEPT
 HAMON_DECLTYPE_RETURN(
 	// [allocator.uses.construction]/12
 	hamon::uses_allocator_construction_args<T>(
@@ -156,8 +162,8 @@ HAMON_DECLTYPE_RETURN(
 template <typename T, typename Alloc, typename U, typename V,
 	typename = hamon::enable_if_t<
 		hamon::detail::is_cv_pair<T>::value>>		// [allocator.uses.construction]/13
-constexpr auto
-uses_allocator_construction_args(Alloc const& alloc, hamon::pair<U, V>& pr) noexcept
+HAMON_CXX11_CONSTEXPR auto
+uses_allocator_construction_args(Alloc const& alloc, hamon::pair<U, V>& pr) HAMON_NOEXCEPT
 HAMON_DECLTYPE_RETURN(
 	// [allocator.uses.construction]/14
 	hamon::uses_allocator_construction_args<T>(
@@ -169,8 +175,8 @@ HAMON_DECLTYPE_RETURN(
 template <typename T, typename Alloc, typename U, typename V,
 	typename = hamon::enable_if_t<
 		hamon::detail::is_cv_pair<T>::value>>		// [allocator.uses.construction]/13
-constexpr auto
-uses_allocator_construction_args(Alloc const& alloc, hamon::pair<U, V> const& pr) noexcept
+HAMON_CXX11_CONSTEXPR auto
+uses_allocator_construction_args(Alloc const& alloc, hamon::pair<U, V> const& pr) HAMON_NOEXCEPT
 HAMON_DECLTYPE_RETURN(
 	// [allocator.uses.construction]/14
 	hamon::uses_allocator_construction_args<T>(
@@ -182,8 +188,8 @@ HAMON_DECLTYPE_RETURN(
 template <typename T, typename Alloc, typename U, typename V,
 	typename = hamon::enable_if_t<
 		hamon::detail::is_cv_pair<T>::value>>		// [allocator.uses.construction]/15
-constexpr auto
-uses_allocator_construction_args(Alloc const& alloc, hamon::pair<U, V>&& pr) noexcept
+HAMON_CXX11_CONSTEXPR auto
+uses_allocator_construction_args(Alloc const& alloc, hamon::pair<U, V>&& pr) HAMON_NOEXCEPT
 HAMON_DECLTYPE_RETURN(
 	// [allocator.uses.construction]/16
 	hamon::uses_allocator_construction_args<T>(
@@ -195,8 +201,8 @@ HAMON_DECLTYPE_RETURN(
 template <typename T, typename Alloc, typename U, typename V,
 	typename = hamon::enable_if_t<
 		hamon::detail::is_cv_pair<T>::value>>		// [allocator.uses.construction]/15
-constexpr auto
-uses_allocator_construction_args(Alloc const& alloc, hamon::pair<U, V> const&& pr) noexcept
+HAMON_CXX11_CONSTEXPR auto
+uses_allocator_construction_args(Alloc const& alloc, hamon::pair<U, V> const&& pr) HAMON_NOEXCEPT
 HAMON_DECLTYPE_RETURN(
 	// [allocator.uses.construction]/16
 	hamon::uses_allocator_construction_args<T>(
@@ -207,9 +213,9 @@ HAMON_DECLTYPE_RETURN(
 
 #if 0	// TODO
 template <typename T, typename Alloc, pair-like P>
-constexpr auto uses_allocator_construction_args(Alloc const& alloc, P&& p) noexcept;
+HAMON_CXX11_CONSTEXPR auto uses_allocator_construction_args(Alloc const& alloc, P&& p) HAMON_NOEXCEPT;
 template <typename T, typename Alloc, typename U>
-constexpr auto uses_allocator_construction_args(Alloc const& alloc, U&& u) noexcept;
+HAMON_CXX11_CONSTEXPR auto uses_allocator_construction_args(Alloc const& alloc, U&& u) HAMON_NOEXCEPT;
 #endif
 
 }	// namespace hamon
