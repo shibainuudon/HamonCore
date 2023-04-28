190588411a8276636e1e50dc537b1b8764c3eda8
shibainuudon <shibainuudon@gmail.com>
2023-04-28 23:10:24 +0900

    [Config]msvc.hppを更新
    * Visual Studio 2022 Preview Version 17.6.0 Preview 5.0

--- libs/config/include/hamon/config/compiler/msvc.hpp
+++ libs/config/include/hamon/config/compiler/msvc.hpp
@@ -805,6 +805,10 @@
 #if (_MSC_FULL_VER >= 193632522)
 #endif
 
+// Visual Studio 2022 Preview Version 17.6.0 Preview 5.0
+#if (_MSC_FULL_VER >= 193632530)
+#endif
+
 // Visual Studio 2022 Version 17.0.(0-1)
 #if (_MSC_FULL_VER >= 193030705)
 #endif
