67abe59e76144d36285f61b5278734ca0d6717d7
shibainuudon <shibainuudon@gmail.com>
2023-06-01 01:05:39 +0900

    [Concepts]cpp17_default_constructible を追加 refs #136

---
+++ libs/concepts/include/hamon/concepts/detail/cpp17_default_constructible.hpp
@@ -0,0 +1,86 @@
+﻿/**
+ *	@file	cpp17_default_constructible.hpp
+ *
+ *	@brief	cpp17_default_constructible の定義
+ */
+
+#ifndef HAMON_CONCEPTS_DETAIL_CPP17_DEFAULT_CONSTRUCTIBLE_HPP
+#define HAMON_CONCEPTS_DETAIL_CPP17_DEFAULT_CONSTRUCTIBLE_HPP
+
+#include <hamon/type_traits/bool_constant.hpp>
+#include <hamon/type_traits/enable_if.hpp>
+#include <hamon/type_traits/is_void.hpp>
+#include <hamon/type_traits/is_object.hpp>
+#include <hamon/type_traits/is_array.hpp>
+#include <hamon/config.hpp>
+
+namespace hamon
+{
+
+namespace detail
+{
+
+template <typename T>
+struct cpp17_default_constructible_helper
+{
+	T u;
+
+	~cpp17_default_constructible_helper();
+};
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+
+template <typename T>
+concept cpp17_default_constructible =
+	hamon::is_object_v<T> &&
+	!hamon::is_void_v<T> &&
+	!hamon::is_array_v<T> &&
+	requires
+	{
+		{ cpp17_default_constructible_helper<T>() };
+		{ T() };
+		{ T{} };
+	};
+
+#else
+
+template <typename T>
+struct cpp17_default_constructible_impl
+{
+private:
+	template <typename U,
+		typename = hamon::enable_if_t<hamon::is_object<U>::value>,
+		typename = hamon::enable_if_t<!hamon::is_void<U>::value>,
+		typename = hamon::enable_if_t<!hamon::is_array<U>::value>,
+		typename = decltype(cpp17_default_constructible_helper<U>()),
+		typename = decltype(U()),
+		typename = decltype(U{})
+	>
+	static auto test(int) -> hamon::true_type;
+
+	template <typename U>
+	static auto test(...) -> hamon::false_type;
+
+public:
+	using type = decltype(test<T>(0));
+};
+
+template <typename T>
+using cpp17_default_constructible =
+	typename cpp17_default_constructible_impl<T>::type;
+
+#endif
+
+template <typename T>
+using cpp17_default_constructible_t =
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+	hamon::bool_constant<hamon::detail::cpp17_default_constructible<T>>;
+#else
+	hamon::detail::cpp17_default_constructible<T>;
+#endif
+
+}	// namespace detail
+
+}	// namespace hamon
+
+#endif // HAMON_CONCEPTS_DETAIL_CPP17_DEFAULT_CONSTRUCTIBLE_HPP
---
+++ libs/concepts/test/src/detail/unit_test_concepts_cpp17_default_constructible.cpp
@@ -0,0 +1,165 @@
+﻿/**
+ *	@file	unit_test_concepts_cpp17_default_constructible.cpp
+ *
+ *	@brief	cpp17_default_constructible のテスト
+ */
+
+#include <hamon/concepts/detail/cpp17_default_constructible.hpp>
+#include <hamon/config.hpp>
+
+HAMON_WARNING_PUSH()
+HAMON_WARNING_DISABLE_CLANG("-Wunused-private-field")
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+#  define HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(B, ...)	\
+	static_assert(B == hamon::detail::cpp17_default_constructible<__VA_ARGS__>, "");	\
+	static_assert(B == hamon::detail::cpp17_default_constructible_t<__VA_ARGS__>::value, "")
+#else
+#  define HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(B, ...)	\
+	static_assert(B == hamon::detail::cpp17_default_constructible<__VA_ARGS__>::value, "");	\
+	static_assert(B == hamon::detail::cpp17_default_constructible_t<__VA_ARGS__>::value, "")
+#endif
+
+namespace hamon_concepts_test
+{
+
+namespace cpp17_default_constructible_test
+{
+
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, void);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, void const);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, void       volatile);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, void const volatile);
+
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  void               *);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  void const         *);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  void       volatile*);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  void const volatile*);
+
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  int);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int const);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  int       volatile);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int const volatile);
+
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  int               *);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  int const         *);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  int       volatile*);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  int const volatile*);
+
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int               &);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int const         &);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int       volatile&);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int const volatile&);
+
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int               &&);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int const         &&);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int       volatile&&);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int const volatile&&);
+
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int               []);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int const         []);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int       volatile[]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int const volatile[]);
+
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  int               (*)[]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  int const         (*)[]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  int       volatile(*)[]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  int const volatile(*)[]);
+
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int               (&)[]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int const         (&)[]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int       volatile(&)[]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int const volatile(&)[]);
+
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int               (&&)[]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int const         (&&)[]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int       volatile(&&)[]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int const volatile(&&)[]);
+
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int               [2]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int const         [2]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int       volatile[2]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int const volatile[2]);
+
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  int               (*)[2]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  int const         (*)[2]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  int       volatile(*)[2]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  int const volatile(*)[2]);
+
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int               (&)[2]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int const         (&)[2]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int       volatile(&)[2]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int const volatile(&)[2]);
+
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int               (&&)[2]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int const         (&&)[2]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int       volatile(&&)[2]);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int const volatile(&&)[2]);
+
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int());
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  int(*)());
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, int(&)());
+
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  char);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  long);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  float);
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  double);
+
+enum E { };
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  E);
+enum class CE { };
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  CE);
+struct A { };
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  A);
+union B { };
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  B);
+
+struct C
+{
+	C(void* = nullptr) { }
+	~C() noexcept(false) { }
+};
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  C);	// different from `default_initializable_t`
+
+class D
+{
+public:
+	D() { }
+};
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  D);
+
+class S1
+{
+public:
+	S1() { }
+
+private:
+	int n;
+};
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  S1);
+
+class S2
+{
+public:
+	S2(int) { }
+
+private:
+	int n;
+};
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(false, S2);
+
+class S3
+{
+	int n;
+
+	virtual int f() { return n; }
+};
+HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST(true,  S3);
+
+}	// namespace cpp17_default_constructible_test
+
+}	// namespace hamon_concepts_test
+
+#undef HAMON_CPP17_DEFAULT_CONSTRUCTIBLE_TEST
+
+HAMON_WARNING_POP()
