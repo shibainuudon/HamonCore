6e1ed2475394fea22660554ec1d6d54a02a637a2
shibainuudon <shibainuudon@gmail.com>
2023-06-02 11:00:29 +0900

    [Concepts]cpp17_function_object を追加 refs #142

---
+++ libs/concepts/include/hamon/concepts/detail/cpp17_function_object.hpp
@@ -0,0 +1,70 @@
+﻿/**
+ *	@file	cpp17_function_object.hpp
+ *
+ *	@brief	cpp17_function_object の定義
+ */
+
+#ifndef HAMON_CONCEPTS_DETAIL_CPP17_FUNCTION_OBJECT_HPP
+#define HAMON_CONCEPTS_DETAIL_CPP17_FUNCTION_OBJECT_HPP
+
+#include <hamon/type_traits/bool_constant.hpp>
+#include <hamon/type_traits/enable_if.hpp>
+#include <hamon/type_traits/is_object.hpp>
+#include <hamon/utility/declval.hpp>
+#include <hamon/utility/forward.hpp>
+#include <hamon/config.hpp>
+
+namespace hamon
+{
+
+namespace detail
+{
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+
+template <typename T, typename... Args>
+concept cpp17_function_object =
+	hamon::is_object_v<T> &&
+	requires(T f, Args&&... args)
+	{
+		{ f(hamon::forward<Args>(args)...) };
+	};
+
+#else
+
+template <typename T, typename... Args>
+struct cpp17_function_object_impl
+{
+private:
+	template <typename U, typename... UArgs,
+		typename = hamon::enable_if_t<hamon::is_object<U>::value>,
+		typename = decltype(hamon::declval<U>()(hamon::declval<UArgs>()...))
+	>
+	static auto test(int) -> hamon::true_type;
+
+	template <typename U, typename... UArgs>
+	static auto test(...) -> hamon::false_type;
+
+public:
+	using type = decltype(test<T, Args...>(0));
+};
+
+template <typename T, typename... Args>
+using cpp17_function_object =
+	typename cpp17_function_object_impl<T, Args...>::type;
+
+#endif
+
+template <typename T, typename... Args>
+using cpp17_function_object_t =
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+	hamon::bool_constant<hamon::detail::cpp17_function_object<T, Args...>>;
+#else
+	hamon::detail::cpp17_function_object<T, Args...>;
+#endif
+
+}	// namespace detail
+
+}	// namespace hamon
+
+#endif // HAMON_CONCEPTS_DETAIL_CPP17_FUNCTION_OBJECT_HPP
---
+++ libs/concepts/test/src/detail/unit_test_concepts_cpp17_function_object.cpp
@@ -0,0 +1,103 @@
+﻿/**
+ *	@file	unit_test_concepts_cpp17_function_object.cpp
+ *
+ *	@brief	cpp17_function_object のテスト
+ */
+
+#include <hamon/concepts/detail/cpp17_function_object.hpp>
+#include <hamon/config.hpp>
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+#  define HAMON_CPP17_FUNCTION_OBJECT_TEST(B, ...)	\
+	static_assert(B == hamon::detail::cpp17_function_object<__VA_ARGS__>, "");	\
+	static_assert(B == hamon::detail::cpp17_function_object_t<__VA_ARGS__>::value, "")
+#else
+#  define HAMON_CPP17_FUNCTION_OBJECT_TEST(B, ...)	\
+	static_assert(B == hamon::detail::cpp17_function_object<__VA_ARGS__>::value, "");	\
+	static_assert(B == hamon::detail::cpp17_function_object_t<__VA_ARGS__>::value, "")
+#endif
+
+namespace hamon_concepts_test
+{
+
+namespace cpp17_function_object_test
+{
+
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, void);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, int);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, int*);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, int&);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, int&&);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, int[]);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, int(*)[]);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, int(&)[]);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, int(&&)[]);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, int[2]);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, int(*)[2]);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, int(&)[2]);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, int(&&)[2]);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, int());
+HAMON_CPP17_FUNCTION_OBJECT_TEST(true,  int(*)());
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, int(&)());
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, int(*)(int));
+HAMON_CPP17_FUNCTION_OBJECT_TEST(true,  int(*)(int), int);
+
+enum E { };
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, E);
+enum class CE { };
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, CE);
+struct A { };
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, A);
+union B { };
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, B);
+
+struct F1
+{
+	void operator()();
+};
+HAMON_CPP17_FUNCTION_OBJECT_TEST(true,  F1);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, F1, int);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, F1, int, float);
+
+struct F2
+{
+	bool operator()(int) const;
+};
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, F2);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(true,  F2, int);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, F2, int, float);
+
+struct F3
+{
+	bool operator()() const;
+	void operator()(int, float);
+};
+HAMON_CPP17_FUNCTION_OBJECT_TEST(true,  F3);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, F3, int);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(true,  F3, int, float);
+
+struct F4
+{
+	void operator()(int) = delete;
+	void operator()(int) const;
+};
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, F4, int);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(true,  F4 const, int);
+
+struct F5
+{
+	void operator()(int&) = delete;
+	void operator()(int&&);
+	void operator()(int const&);
+	void operator()(int const&&) = delete;
+};
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, F5, int&);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(true,  F5, int&&);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(true,  F5, int const&);
+HAMON_CPP17_FUNCTION_OBJECT_TEST(false, F5, int const&&);
+
+}	// namespace cpp17_function_object_test
+
+}	// namespace hamon_concepts_test
+
+#undef HAMON_CPP17_FUNCTION_OBJECT_TEST
