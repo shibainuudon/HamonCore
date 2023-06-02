be2bae6b4d8efa6b6c33177d962419eeb4620faa
shibainuudon <shibainuudon@gmail.com>
2023-06-02 10:26:02 +0900

    [Concepts]cpp17_swappable を追加 refs #138

---
+++ libs/concepts/include/hamon/concepts/detail/cpp17_swappable.hpp
@@ -0,0 +1,44 @@
+﻿/**
+ *	@file	cpp17_swappable.hpp
+ *
+ *	@brief	cpp17_swappable の定義
+ */
+
+#ifndef HAMON_CONCEPTS_DETAIL_CPP17_SWAPPABLE_HPP
+#define HAMON_CONCEPTS_DETAIL_CPP17_SWAPPABLE_HPP
+
+#include <hamon/type_traits/bool_constant.hpp>
+#include <hamon/type_traits/is_swappable.hpp>
+#include <hamon/config.hpp>
+
+namespace hamon
+{
+
+namespace detail
+{
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+
+template <typename T>
+concept cpp17_swappable = hamon::is_swappable_v<T>;
+
+#else
+
+template <typename T>
+using cpp17_swappable = hamon::is_swappable<T>;
+
+#endif
+
+template <typename T>
+using cpp17_swappable_t =
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+	hamon::bool_constant<hamon::detail::cpp17_swappable<T>>;
+#else
+	hamon::detail::cpp17_swappable<T>;
+#endif
+
+}	// namespace detail
+
+}	// namespace hamon
+
+#endif // HAMON_CONCEPTS_DETAIL_CPP17_SWAPPABLE_HPP
---
+++ libs/concepts/test/src/detail/unit_test_concepts_cpp17_swappable.cpp
@@ -0,0 +1,126 @@
+﻿/**
+ *	@file	unit_test_concepts_cpp17_swappable.cpp
+ *
+ *	@brief	cpp17_swappable のテスト
+ */
+
+#include <hamon/concepts/detail/cpp17_swappable.hpp>
+#include <hamon/config.hpp>
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+#  define HAMON_CPP17_SWAPPABLE_TEST(B, ...)	\
+	static_assert(B == hamon::detail::cpp17_swappable<__VA_ARGS__>, "");	\
+	static_assert(B == hamon::detail::cpp17_swappable_t<__VA_ARGS__>::value, "")
+#else
+#  define HAMON_CPP17_SWAPPABLE_TEST(B, ...)	\
+	static_assert(B == hamon::detail::cpp17_swappable<__VA_ARGS__>::value, "");	\
+	static_assert(B == hamon::detail::cpp17_swappable_t<__VA_ARGS__>::value, "")
+#endif
+
+namespace hamon_concepts_test
+{
+
+namespace cpp17_swappable_test
+{
+
+HAMON_CPP17_SWAPPABLE_TEST(false, void);
+HAMON_CPP17_SWAPPABLE_TEST(false, void const);
+HAMON_CPP17_SWAPPABLE_TEST(false, void       volatile);
+HAMON_CPP17_SWAPPABLE_TEST(false, void const volatile);
+
+HAMON_CPP17_SWAPPABLE_TEST(true,  void               *);
+HAMON_CPP17_SWAPPABLE_TEST(true,  void const         *);
+HAMON_CPP17_SWAPPABLE_TEST(true,  void       volatile*);
+HAMON_CPP17_SWAPPABLE_TEST(true,  void const volatile*);
+
+HAMON_CPP17_SWAPPABLE_TEST(true,  int);
+HAMON_CPP17_SWAPPABLE_TEST(false, int const);
+HAMON_CPP17_SWAPPABLE_TEST(true,  int       volatile);
+HAMON_CPP17_SWAPPABLE_TEST(false,  int const volatile);
+
+HAMON_CPP17_SWAPPABLE_TEST(true,  int               *);
+HAMON_CPP17_SWAPPABLE_TEST(true,  int const         *);
+HAMON_CPP17_SWAPPABLE_TEST(true,  int       volatile*);
+HAMON_CPP17_SWAPPABLE_TEST(true,  int const volatile*);
+
+HAMON_CPP17_SWAPPABLE_TEST(true,  int               &);
+HAMON_CPP17_SWAPPABLE_TEST(false, int const         &);
+HAMON_CPP17_SWAPPABLE_TEST(true,  int       volatile&);
+HAMON_CPP17_SWAPPABLE_TEST(false, int const volatile&);
+
+HAMON_CPP17_SWAPPABLE_TEST(true,  int               &&);
+HAMON_CPP17_SWAPPABLE_TEST(false, int const         &&);
+HAMON_CPP17_SWAPPABLE_TEST(true,  int       volatile&&);
+HAMON_CPP17_SWAPPABLE_TEST(false, int const volatile&&);
+
+HAMON_CPP17_SWAPPABLE_TEST(false, int               []);
+HAMON_CPP17_SWAPPABLE_TEST(false, int const         []);
+HAMON_CPP17_SWAPPABLE_TEST(false, int       volatile[]);
+HAMON_CPP17_SWAPPABLE_TEST(false, int const volatile[]);
+
+HAMON_CPP17_SWAPPABLE_TEST(true,  int               (*)[]);
+HAMON_CPP17_SWAPPABLE_TEST(true,  int const         (*)[]);
+HAMON_CPP17_SWAPPABLE_TEST(true,  int       volatile(*)[]);
+HAMON_CPP17_SWAPPABLE_TEST(true,  int const volatile(*)[]);
+
+HAMON_CPP17_SWAPPABLE_TEST(false, int               (&)[]);
+HAMON_CPP17_SWAPPABLE_TEST(false, int const         (&)[]);
+HAMON_CPP17_SWAPPABLE_TEST(false, int       volatile(&)[]);
+HAMON_CPP17_SWAPPABLE_TEST(false, int const volatile(&)[]);
+
+HAMON_CPP17_SWAPPABLE_TEST(false, int               (&&)[]);
+HAMON_CPP17_SWAPPABLE_TEST(false, int const         (&&)[]);
+HAMON_CPP17_SWAPPABLE_TEST(false, int       volatile(&&)[]);
+HAMON_CPP17_SWAPPABLE_TEST(false, int const volatile(&&)[]);
+
+HAMON_CPP17_SWAPPABLE_TEST(true,  int               [2]);
+HAMON_CPP17_SWAPPABLE_TEST(false, int const         [2]);
+HAMON_CPP17_SWAPPABLE_TEST(true,  int       volatile[2]);
+HAMON_CPP17_SWAPPABLE_TEST(false, int const volatile[2]);
+
+HAMON_CPP17_SWAPPABLE_TEST(true,  int               (*)[2]);
+HAMON_CPP17_SWAPPABLE_TEST(true,  int const         (*)[2]);
+HAMON_CPP17_SWAPPABLE_TEST(true,  int       volatile(*)[2]);
+HAMON_CPP17_SWAPPABLE_TEST(true,  int const volatile(*)[2]);
+
+HAMON_CPP17_SWAPPABLE_TEST(true,  int               (&)[2]);
+HAMON_CPP17_SWAPPABLE_TEST(false, int const         (&)[2]);
+HAMON_CPP17_SWAPPABLE_TEST(true,  int       volatile(&)[2]);
+HAMON_CPP17_SWAPPABLE_TEST(false, int const volatile(&)[2]);
+
+HAMON_CPP17_SWAPPABLE_TEST(true,  int               (&&)[2]);
+HAMON_CPP17_SWAPPABLE_TEST(false, int const         (&&)[2]);
+HAMON_CPP17_SWAPPABLE_TEST(true,  int       volatile(&&)[2]);
+HAMON_CPP17_SWAPPABLE_TEST(false, int const volatile(&&)[2]);
+
+HAMON_CPP17_SWAPPABLE_TEST(false, int());
+HAMON_CPP17_SWAPPABLE_TEST(true,  int(*)());
+HAMON_CPP17_SWAPPABLE_TEST(false, int(&)());
+
+HAMON_CPP17_SWAPPABLE_TEST(true,  char);
+HAMON_CPP17_SWAPPABLE_TEST(true,  long);
+HAMON_CPP17_SWAPPABLE_TEST(true,  float);
+HAMON_CPP17_SWAPPABLE_TEST(true,  double);
+
+enum E { };
+HAMON_CPP17_SWAPPABLE_TEST(true,  E);
+enum class CE { };
+HAMON_CPP17_SWAPPABLE_TEST(true,  CE);
+struct A { };
+HAMON_CPP17_SWAPPABLE_TEST(true,  A);
+union B { };
+HAMON_CPP17_SWAPPABLE_TEST(true,  B);
+
+struct S1
+{
+};
+
+void swap(S1&, S1&) = delete;
+
+HAMON_CPP17_SWAPPABLE_TEST(false, S1);
+
+}	// namespace cpp17_swappable_test
+
+}	// namespace hamon_concepts_test
+
+#undef HAMON_CPP17_SWAPPABLE_TEST
