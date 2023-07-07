f4822bbdec140d5b747a1ac5f9ef60b7b4b27c48
shibainuudon <shibainuudon@gmail.com>
2023-07-07 16:02:52 +0900

    [BigInt]bigint_algo::negate を追加

---
+++ libs/bigint/include/hamon/bigint/bigint_algo/negate.hpp
@@ -0,0 +1,34 @@
+﻿/**
+ *	@file	negate.hpp
+ *
+ *	@brief	negate 関数の定義
+ */
+
+#ifndef HAMON_BIGINT_BIGINT_ALGO_NEGATE_HPP
+#define HAMON_BIGINT_BIGINT_ALGO_NEGATE_HPP
+
+#include <hamon/bigint/bigint_algo/bit_not.hpp>
+#include <hamon/bigint/bigint_algo/add.hpp>
+#include <hamon/array.hpp>
+#include <hamon/cstddef/size_t.hpp>
+#include <hamon/config.hpp>
+
+namespace hamon
+{
+namespace detail
+{
+namespace bigint_algo
+{
+
+template <typename T, hamon::size_t N>
+inline HAMON_CXX11_CONSTEXPR hamon::array<T, N>
+negate(hamon::array<T, N> const& lhs)
+{
+	return bigint_algo::add(bigint_algo::bit_not(lhs), hamon::array<T, N>{1});
+}
+
+}	// namespace bigint_algo
+}	// namespace detail
+}	// namespace hamon
+
+#endif // HAMON_BIGINT_BIGINT_ALGO_NEGATE_HPP
---
+++ libs/bigint/test/src/bigint_algo/unit_test_bigint_algo_negate.cpp
@@ -0,0 +1,86 @@
+﻿/**
+ *	@file	unit_test_bigint_algo_negate.cpp
+ *
+ *	@brief	bigint_algo::negate のテスト
+ */
+
+#include <hamon/bigint/bigint_algo/negate.hpp>
+#include <hamon/array.hpp>
+#include <hamon/cstdint.hpp>
+#include <gtest/gtest.h>
+#include <vector>
+#include "constexpr_test.hpp"
+
+GTEST_TEST(BigIntAlgoTest, NegateTest)
+{
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const a{0x00};
+		HAMON_CXX11_CONSTEXPR auto const b = hamon::detail::bigint_algo::negate(a);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x00u, b[0]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x00u, b[1]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x00u, b[2]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x00u, b[3]);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const a{0x01};
+		HAMON_CXX11_CONSTEXPR auto const b = hamon::detail::bigint_algo::negate(a);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFFu, b[0]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFFu, b[1]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFFu, b[2]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFFu, b[3]);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const a{0xFF, 0xFF, 0xFF, 0xFF};
+		HAMON_CXX11_CONSTEXPR auto const b = hamon::detail::bigint_algo::negate(a);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x01u, b[0]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x00u, b[1]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x00u, b[2]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x00u, b[3]);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const a{0x12, 0x34, 0x56, 0x78};
+		HAMON_CXX11_CONSTEXPR auto const b = hamon::detail::bigint_algo::negate(a);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xEEu, b[0]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xCBu, b[1]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xA9u, b[2]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x87u, b[3]);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint16_t, 3> const a{0x0002};
+		HAMON_CXX11_CONSTEXPR auto const b = hamon::detail::bigint_algo::negate(a);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFFFEu, b[0]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFFFFu, b[1]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFFFFu, b[2]);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint16_t, 2> const a{0xFFFF, 0xFFFF};
+		HAMON_CXX11_CONSTEXPR auto const b = hamon::detail::bigint_algo::negate(a);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x0001u, b[0]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x0000u, b[1]);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint16_t, 3> const a{0x1234, 0x5678, 0x9ABC};
+		HAMON_CXX11_CONSTEXPR auto const b = hamon::detail::bigint_algo::negate(a);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xEDCCu, b[0]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xA987u, b[1]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x6543u, b[2]);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint32_t, 1> const a{0};
+		HAMON_CXX11_CONSTEXPR auto const b = hamon::detail::bigint_algo::negate(a);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x00000000u, b[0]);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint32_t, 3> const a{0x12345678, 0x9ABCDEF0};
+		HAMON_CXX11_CONSTEXPR auto const b = hamon::detail::bigint_algo::negate(a);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xEDCBA988u, b[0]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x6543210Fu, b[1]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFFFFFFFFu, b[2]);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint64_t, 2> const a{0x123456789ABCDEF0};
+		HAMON_CXX11_CONSTEXPR auto const b = hamon::detail::bigint_algo::negate(a);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xEDCBA98765432110u, b[0]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFFFFFFFFFFFFFFFFu, b[1]);
+	}
+}
