8cf13ef708585e7d2293abafabfb932025cb4e47
shibainuudon <shibainuudon@gmail.com>
2023-07-07 16:57:03 +0900

    [BigInt]bigint_algo::compare を追加

---
+++ libs/bigint/include/hamon/bigint/bigint_algo/compare.hpp
@@ -0,0 +1,96 @@
+﻿/**
+ *	@file	compare.hpp
+ *
+ *	@brief	compare 関数の定義
+ */
+
+#ifndef HAMON_BIGINT_BIGINT_ALGO_COMPARE_HPP
+#define HAMON_BIGINT_BIGINT_ALGO_COMPARE_HPP
+
+#include <hamon/array.hpp>
+#include <hamon/cstddef/size_t.hpp>
+#include <hamon/config.hpp>
+#include <vector>
+
+namespace hamon
+{
+namespace detail
+{
+namespace bigint_algo
+{
+
+template <typename T>
+inline int
+compare(std::vector<T> const& lhs, std::vector<T> const& rhs)
+{
+	auto const NA = lhs.size();
+	auto const NB = rhs.size();
+
+	if (NA > NB) { return  1; }
+	if (NA < NB) { return -1; }
+
+	for (hamon::size_t i = NA; i > 0; --i)
+	{
+		if (lhs[i - 1] > rhs[i - 1]) { return  1; }
+		if (lhs[i - 1] < rhs[i - 1]) { return -1; }
+	}
+
+	return 0;
+}
+
+#if defined(HAMON_HAS_CXX14_CONSTEXPR)
+
+template <typename T, hamon::size_t N>
+inline HAMON_CXX14_CONSTEXPR int
+compare(hamon::array<T, N> const& lhs, hamon::array<T, N> const& rhs)
+{
+	for (hamon::size_t i = N; i > 0; --i)
+	{
+		if (lhs[i - 1] > rhs[i - 1]) { return  1; }
+		if (lhs[i - 1] < rhs[i - 1]) { return -1; }
+	}
+
+	return 0;
+}
+
+#else
+
+template <hamon::size_t I>
+struct compare_impl
+{
+	template <typename T, hamon::size_t N>
+	static HAMON_CXX11_CONSTEXPR int
+	invoke(hamon::array<T, N> const& lhs, hamon::array<T, N> const& rhs)
+	{
+		return
+			lhs[I - 1] > rhs[I - 1] ?  1 :
+			lhs[I - 1] < rhs[I - 1] ? -1 :
+			compare_impl<I - 1>::invoke(lhs, rhs);
+	}
+};
+
+template <>
+struct compare_impl<0>
+{
+	template <typename T, hamon::size_t N>
+	static HAMON_CXX11_CONSTEXPR int
+	invoke(hamon::array<T, N> const&, hamon::array<T, N> const&)
+	{
+		return 0;
+	}
+};
+
+template <typename T, hamon::size_t N>
+inline HAMON_CXX11_CONSTEXPR int
+compare(hamon::array<T, N> const& lhs, hamon::array<T, N> const& rhs)
+{
+	return compare_impl<N>::invoke(lhs, rhs);
+}
+
+#endif
+
+}	// namespace bigint_algo
+}	// namespace detail
+}	// namespace hamon
+
+#endif // HAMON_BIGINT_BIGINT_ALGO_COMPARE_HPP
---
+++ libs/bigint/test/src/bigint_algo/unit_test_bigint_algo_compare.cpp
@@ -0,0 +1,77 @@
+﻿/**
+ *	@file	unit_test_bigint_algo_compare.cpp
+ *
+ *	@brief	bigint_algo::compare のテスト
+ */
+
+#include <hamon/bigint/bigint_algo/compare.hpp>
+#include <hamon/array.hpp>
+#include <hamon/cstdint.hpp>
+#include <gtest/gtest.h>
+#include <vector>
+#include "constexpr_test.hpp"
+
+GTEST_TEST(BigIntAlgoTest, CompareTest)
+{
+	{
+		std::vector<hamon::uint8_t> const a{0};
+		std::vector<hamon::uint8_t> const b{0};
+		EXPECT_EQ( 0, hamon::detail::bigint_algo::compare(a, b));
+		EXPECT_EQ( 0, hamon::detail::bigint_algo::compare(b, a));
+	}
+	{
+		std::vector<hamon::uint8_t> const a{0};
+		std::vector<hamon::uint8_t> const b{1};
+		EXPECT_EQ(-1, hamon::detail::bigint_algo::compare(a, b));
+		EXPECT_EQ( 1, hamon::detail::bigint_algo::compare(b, a));
+	}
+	{
+		std::vector<hamon::uint8_t> const a{1};
+		std::vector<hamon::uint8_t> const b{1};
+		EXPECT_EQ( 0, hamon::detail::bigint_algo::compare(a, b));
+		EXPECT_EQ( 0, hamon::detail::bigint_algo::compare(b, a));
+	}
+	{
+		std::vector<hamon::uint8_t> const a{0x00, 0x10};
+		std::vector<hamon::uint8_t> const b{0xFF};
+		EXPECT_EQ( 1, hamon::detail::bigint_algo::compare(a, b));
+		EXPECT_EQ(-1, hamon::detail::bigint_algo::compare(b, a));
+	}
+	{
+		std::vector<hamon::uint8_t> const a{0x00, 0x10};
+		std::vector<hamon::uint8_t> const b{0xFF, 0x0F};
+		EXPECT_EQ( 1, hamon::detail::bigint_algo::compare(a, b));
+		EXPECT_EQ(-1, hamon::detail::bigint_algo::compare(b, a));
+	}
+
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const a{0};
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const b{0};
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ( 0, hamon::detail::bigint_algo::compare(a, b));
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ( 0, hamon::detail::bigint_algo::compare(b, a));
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const a{0x12, 0x34, 0x56, 0x78};
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const b{0xFF, 0xFF, 0xFF, 0xFF};
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(-1, hamon::detail::bigint_algo::compare(a, b));
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ( 1, hamon::detail::bigint_algo::compare(b, a));
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const a{0x12, 0x34, 0x56, 0x78};
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const b{0xFF, 0xFF, 0xFF, 0x00};
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ( 1, hamon::detail::bigint_algo::compare(a, b));
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(-1, hamon::detail::bigint_algo::compare(b, a));
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const a{0x12, 0x34, 0x56, 0x78};
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const b{0x13, 0x34, 0x56, 0x78};
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(-1, hamon::detail::bigint_algo::compare(a, b));
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ( 1, hamon::detail::bigint_algo::compare(b, a));
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const a{0x12, 0x34, 0x56, 0x78};
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const b{0x12, 0x34, 0x56, 0x78};
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ( 0, hamon::detail::bigint_algo::compare(a, b));
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ( 0, hamon::detail::bigint_algo::compare(b, a));
+	}
+}
