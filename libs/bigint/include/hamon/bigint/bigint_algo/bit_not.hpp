df3c1ac25482756f8c291d60df6548668e00eee5
shibainuudon <shibainuudon@gmail.com>
2023-07-07 15:34:31 +0900

    [BigInt]bigint_algo::bit_not を追加

---
+++ libs/bigint/include/hamon/bigint/bigint_algo/bit_not.hpp
@@ -0,0 +1,40 @@
+﻿/**
+ *	@file	bit_not.hpp
+ *
+ *	@brief	bit_not 関数の定義
+ */
+
+#ifndef HAMON_BIGINT_BIGINT_ALGO_BIT_NOT_HPP
+#define HAMON_BIGINT_BIGINT_ALGO_BIT_NOT_HPP
+
+#include <hamon/array.hpp>
+#include <hamon/cstddef/size_t.hpp>
+#include <hamon/utility/make_index_sequence.hpp>
+#include <hamon/config.hpp>
+
+namespace hamon
+{
+namespace detail
+{
+namespace bigint_algo
+{
+
+template <typename T, hamon::size_t N, hamon::size_t... Is>
+inline HAMON_CXX11_CONSTEXPR hamon::array<T, N>
+bit_not_impl(hamon::array<T, N> const& lhs, hamon::index_sequence<Is...>)
+{
+	return hamon::array<T, N>{ static_cast<T>(~lhs[Is])... };
+}
+
+template <typename T, hamon::size_t N>
+inline HAMON_CXX11_CONSTEXPR hamon::array<T, N>
+bit_not(hamon::array<T, N> const& lhs)
+{
+	return bit_not_impl(lhs, hamon::make_index_sequence<N>{});
+}
+
+}	// namespace bigint_algo
+}	// namespace detail
+}	// namespace hamon
+
+#endif // HAMON_BIGINT_BIGINT_ALGO_BIT_NOT_HPP
---
+++ libs/bigint/test/src/bigint_algo/unit_test_bigint_algo_bit_not.cpp
@@ -0,0 +1,44 @@
+﻿/**
+ *	@file	unit_test_bigint_algo_bit_not.cpp
+ *
+ *	@brief	bigint_algo::bit_not のテスト
+ */
+
+#include <hamon/bigint/bigint_algo/bit_not.hpp>
+#include <hamon/array.hpp>
+#include <hamon/cstdint.hpp>
+#include <gtest/gtest.h>
+#include <vector>
+#include "constexpr_test.hpp"
+
+GTEST_TEST(BigIntAlgoTest, BitNotTest)
+{
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const a{0x12, 0x34, 0x56, 0x78};
+		HAMON_CXX11_CONSTEXPR auto const b = hamon::detail::bigint_algo::bit_not(a);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xEDu, b[0]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xCBu, b[1]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xA9u, b[2]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x87u, b[3]);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint16_t, 3> const a{0x1234, 0x5678, 0x9ABC};
+		HAMON_CXX11_CONSTEXPR auto const b = hamon::detail::bigint_algo::bit_not(a);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xEDCBu, b[0]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xA987u, b[1]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x6543u, b[2]);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint32_t, 3> const a{0x12345678, 0x9ABCDEF0};
+		HAMON_CXX11_CONSTEXPR auto const b = hamon::detail::bigint_algo::bit_not(a);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xEDCBA987u, b[0]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x6543210Fu, b[1]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFFFFFFFFu, b[2]);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint64_t, 2> const a{0x123456789ABCDEF0};
+		HAMON_CXX11_CONSTEXPR auto const b = hamon::detail::bigint_algo::bit_not(a);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xEDCBA9876543210Fu, b[0]);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFFFFFFFFFFFFFFFFu, b[1]);
+	}
+}
