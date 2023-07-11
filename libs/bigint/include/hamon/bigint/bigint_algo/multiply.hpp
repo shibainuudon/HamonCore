1ab80b05377bf5dc77ad9748c958ead1a045377e
shibainuudon <shibainuudon@gmail.com>
2023-07-11 09:48:52 +0900

    [BigInt]bigint_algo::multiply を追加

---
+++ libs/bigint/include/hamon/bigint/bigint_algo/multiply.hpp
@@ -0,0 +1,217 @@
+﻿/**
+ *	@file	multiply.hpp
+ *
+ *	@brief	multiply 関数の定義
+ */
+
+#ifndef HAMON_BIGINT_BIGINT_ALGO_MULTIPLY_HPP
+#define HAMON_BIGINT_BIGINT_ALGO_MULTIPLY_HPP
+
+#include <hamon/bigint/bigint_algo/add.hpp>
+#include <hamon/bigint/bigint_algo/bit_shift_left.hpp>
+#include <hamon/array.hpp>
+#include <hamon/bit/bitsof.hpp>
+#include <hamon/cstddef/size_t.hpp>
+#include <hamon/cstdint.hpp>
+#include <hamon/config.hpp>
+#include <vector>
+
+namespace hamon
+{
+namespace detail
+{
+namespace bigint_algo
+{
+
+inline HAMON_CXX11_CONSTEXPR hamon::uint8_t hi(hamon::uint16_t x)
+{
+	return static_cast<hamon::uint8_t>(x >> 8);
+}
+inline HAMON_CXX11_CONSTEXPR hamon::uint16_t hi(hamon::uint32_t x)
+{
+	return static_cast<hamon::uint16_t>(x >> 16);
+}
+inline HAMON_CXX11_CONSTEXPR hamon::uint32_t hi(hamon::uint64_t x)
+{
+	return static_cast<hamon::uint32_t>(x >> 32);
+}
+
+inline HAMON_CXX11_CONSTEXPR hamon::uint8_t lo(hamon::uint16_t x)
+{
+	return static_cast<hamon::uint8_t>(x);
+}
+inline HAMON_CXX11_CONSTEXPR hamon::uint16_t lo(hamon::uint32_t x)
+{
+	return static_cast<hamon::uint16_t>(x);
+}
+inline HAMON_CXX11_CONSTEXPR hamon::uint32_t lo(hamon::uint64_t x)
+{
+	return static_cast<hamon::uint32_t>(x);
+}
+
+inline HAMON_CXX11_CONSTEXPR hamon::uint16_t mul(hamon::uint8_t lhs, hamon::uint8_t rhs)
+{
+	return static_cast<hamon::uint16_t>(lhs) * static_cast<hamon::uint16_t>(rhs);
+}
+inline HAMON_CXX11_CONSTEXPR hamon::uint32_t mul(hamon::uint16_t lhs, hamon::uint16_t rhs)
+{
+	return static_cast<hamon::uint32_t>(lhs) * static_cast<hamon::uint32_t>(rhs);
+}
+inline HAMON_CXX11_CONSTEXPR hamon::uint64_t mul(hamon::uint32_t lhs, hamon::uint32_t rhs)
+{
+	return static_cast<hamon::uint64_t>(lhs) * static_cast<hamon::uint64_t>(rhs);
+}
+#if defined(__SIZEOF_INT128__)
+inline HAMON_CXX11_CONSTEXPR hamon::uint64_t hi(__uint128_t x)
+{
+	return static_cast<hamon::uint64_t>(x >> 64);
+}
+inline HAMON_CXX11_CONSTEXPR hamon::uint64_t lo(__uint128_t x)
+{
+	return static_cast<hamon::uint64_t>(x);
+}
+inline HAMON_CXX11_CONSTEXPR __uint128_t mul(hamon::uint64_t lhs, hamon::uint64_t rhs)
+{
+	return static_cast<__uint128_t>(lhs) * static_cast<__uint128_t>(rhs);
+}
+#else
+inline HAMON_CXX11_CONSTEXPR hamon::uint64_t hi(hamon::array<hamon::uint64_t, 2> const& x)
+{
+	return x[1];
+}
+inline HAMON_CXX11_CONSTEXPR hamon::uint64_t lo(hamon::array<hamon::uint64_t, 2> const& x)
+{
+	return x[0];
+}
+inline HAMON_CXX14_CONSTEXPR hamon::array<hamon::uint64_t, 2>
+mul(hamon::uint64_t lhs, hamon::uint64_t rhs)
+{
+	using ResultType = hamon::array<hamon::uint64_t, 2>;
+
+	auto const h  = mul(hi(lhs), hi(rhs));
+	auto const m1 = mul(hi(lhs), lo(rhs));
+	auto const m2 = mul(lo(lhs), hi(rhs));
+	auto const l  = mul(lo(lhs), lo(rhs));
+
+	auto const a1 = ResultType{l};
+	auto const a2 = bigint_algo::bit_shift_left(ResultType{m1}, 32);
+	auto const a3 = bigint_algo::bit_shift_left(ResultType{m2}, 32);
+	auto const a4 = ResultType{0, h};
+
+	return
+		bigint_algo::add(a1,
+		bigint_algo::add(a2,
+		bigint_algo::add(a3, a4)));
+}
+#endif
+
+template <typename ResultType, typename T>
+inline HAMON_CXX11_CONSTEXPR ResultType
+multiply1_impl(T const& x)
+{
+	return ResultType { lo(x), hi(x) };
+}
+
+template <typename ResultType, typename T>
+inline HAMON_CXX11_CONSTEXPR ResultType
+multiply1(T lhs, T rhs)
+{
+	return multiply1_impl<ResultType>(mul(lhs, rhs));
+}
+
+template <typename T>
+inline std::vector<T>
+multiply(std::vector<T> const& lhs, std::vector<T> const& rhs)
+{
+	auto const NA = lhs.size();
+	auto const NB = rhs.size();
+
+	std::vector<T> result{0};
+
+	for (hamon::size_t i = 0; i < NA; ++i)
+	{
+		for (hamon::size_t j = 0; j < NB; ++j)
+		{
+			auto tmp = multiply1<std::vector<T>>(lhs[i], rhs[j]);
+			result = bigint_algo::add(result, bigint_algo::bit_shift_left(tmp, (i+j) * hamon::bitsof<T>()));
+		}
+	}
+
+	return result;
+}
+
+template <hamon::size_t N, typename T, hamon::size_t M, hamon::size_t... Is>
+inline HAMON_CXX11_CONSTEXPR hamon::array<T, N>
+resized_impl(hamon::array<T, M> const& a, hamon::index_sequence<Is...>)
+{
+	return hamon::array<T, N>{a[Is]...};
+}
+
+template <hamon::size_t N, typename T, hamon::size_t M>
+inline HAMON_CXX11_CONSTEXPR hamon::array<T, N>
+resized(hamon::array<T, M> const& a)
+{
+	return resized_impl<N>(a, hamon::make_index_sequence<(N < M) ? N : M>{});
+}
+
+#if defined(HAMON_HAS_CXX14_CONSTEXPR)
+
+template <typename T, hamon::size_t N>
+inline HAMON_CXX14_CONSTEXPR hamon::array<T, N>
+multiply(hamon::array<T, N> const& lhs, hamon::array<T, N> const& rhs)
+{
+	hamon::array<T, N> result{0};
+
+	for (hamon::size_t i = 0; i < N; ++i)
+	{
+		for (hamon::size_t j = 0; j < N; ++j)
+		{
+			auto tmp = resized<N>(multiply1<hamon::array<T, 2>>(lhs[i], rhs[j]));
+			result = bigint_algo::add(result, bigint_algo::bit_shift_left(tmp, (i+j) * hamon::bitsof<T>()));
+		}
+	}
+
+	return result;
+}
+
+#else
+
+template <typename T, hamon::size_t N>
+inline HAMON_CXX11_CONSTEXPR hamon::array<T, N>
+multiply_impl_2(hamon::array<T, N> const& lhs, hamon::array<T, N> const& rhs,
+	hamon::array<T, N> const& result, hamon::size_t i, hamon::size_t j)
+{
+	return j >= N ? result :
+		multiply_impl_2(lhs, rhs,
+			bigint_algo::add(result,
+				bigint_algo::bit_shift_left(
+					resized<N>(multiply1<hamon::array<T, 2>>(lhs[i], rhs[j])),
+					(i+j) * hamon::bitsof<T>())),
+			i, j+1);
+}
+
+template <typename T, hamon::size_t N>
+inline HAMON_CXX11_CONSTEXPR hamon::array<T, N>
+multiply_impl_1(hamon::array<T, N> const& lhs, hamon::array<T, N> const& rhs,
+	hamon::array<T, N> const& result, hamon::size_t i)
+{
+	return i >= N ? result :
+		multiply_impl_1(lhs, rhs,
+			multiply_impl_2(lhs, rhs, result, i, 0),
+			i+1);
+}
+
+template <typename T, hamon::size_t N>
+inline HAMON_CXX11_CONSTEXPR hamon::array<T, N>
+multiply(hamon::array<T, N> const& lhs, hamon::array<T, N> const& rhs)
+{
+	return multiply_impl_1(lhs, rhs, hamon::array<T, N>{}, 0);
+}
+
+#endif
+
+}	// namespace bigint_algo
+}	// namespace detail
+}	// namespace hamon
+
+#endif // HAMON_BIGINT_BIGINT_ALGO_MULTIPLY_HPP
---
+++ libs/bigint/test/src/bigint_algo/unit_test_bigint_algo_multiply.cpp
@@ -0,0 +1,215 @@
+﻿/**
+ *	@file	unit_test_bigint_algo_multiply.cpp
+ *
+ *	@brief	bigint_algo::multiply のテスト
+ */
+
+#include <hamon/bigint/bigint_algo/multiply.hpp>
+#include <hamon/array.hpp>
+#include <hamon/cstdint.hpp>
+#include <gtest/gtest.h>
+#include <vector>
+#include "constexpr_test.hpp"
+
+GTEST_TEST(BigIntAlgoTest, MultiplyTest)
+{
+	{
+		std::vector<hamon::uint8_t> const a{0};
+		std::vector<hamon::uint8_t> const b{0};
+		auto const c1 = hamon::detail::bigint_algo::multiply(a, b);
+		auto const c2 = hamon::detail::bigint_algo::multiply(b, a);
+		std::vector<hamon::uint8_t> const expected{0};
+		EXPECT_EQ(c1, expected);
+		EXPECT_EQ(c2, expected);
+	}
+	{
+		std::vector<hamon::uint8_t> const a{2};
+		std::vector<hamon::uint8_t> const b{3};
+		auto const c1 = hamon::detail::bigint_algo::multiply(a, b);
+		auto const c2 = hamon::detail::bigint_algo::multiply(b, a);
+		std::vector<hamon::uint8_t> const expected{6};
+		EXPECT_EQ(c1, expected);
+		EXPECT_EQ(c2, expected);
+	}
+	{
+		std::vector<hamon::uint8_t> const a{123};
+		std::vector<hamon::uint8_t> const b{45};
+		auto const c1 = hamon::detail::bigint_algo::multiply(a, b);
+		auto const c2 = hamon::detail::bigint_algo::multiply(b, a);
+		std::vector<hamon::uint8_t> const expected{0x9F, 0x15};	// 5535
+		EXPECT_EQ(c1, expected);
+		EXPECT_EQ(c2, expected);
+	}
+	{
+		std::vector<hamon::uint8_t> const a{0x20, 0x10};
+		std::vector<hamon::uint8_t> const b{0x40, 0x30};
+		auto const c1 = hamon::detail::bigint_algo::multiply(a, b);
+		auto const c2 = hamon::detail::bigint_algo::multiply(b, a);
+		std::vector<hamon::uint8_t> const expected{0x00, 0x08, 0x0A, 0x03};
+		EXPECT_EQ(c1, expected);
+		EXPECT_EQ(c2, expected);
+	}
+	{
+		std::vector<hamon::uint8_t> const a{0xB1, 0x68, 0xDE, 0x3A};	// 987654321
+		std::vector<hamon::uint8_t> const b{3};
+		auto const c1 = hamon::detail::bigint_algo::multiply(a, b);
+		auto const c2 = hamon::detail::bigint_algo::multiply(b, a);
+		std::vector<hamon::uint8_t> const expected{0x13, 0x3A, 0x9B, 0xB0};	// 2962962963
+		EXPECT_EQ(c1, expected);
+		EXPECT_EQ(c2, expected);
+	}
+	{
+		std::vector<hamon::uint8_t> const a{0};
+		std::vector<hamon::uint8_t> const b{0x10, 0x20, 0x30};
+		auto const c1 = hamon::detail::bigint_algo::multiply(a, b);
+		auto const c2 = hamon::detail::bigint_algo::multiply(b, a);
+		std::vector<hamon::uint8_t> const expected{0x00};
+		EXPECT_EQ(c1, expected);
+		EXPECT_EQ(c2, expected);
+	}
+	{
+		std::vector<hamon::uint8_t> const a{0xFF, 0xFF, 0xFF, 0xFF};
+		std::vector<hamon::uint8_t> const b{2};
+		auto const c1 = hamon::detail::bigint_algo::multiply(a, b);
+		auto const c2 = hamon::detail::bigint_algo::multiply(b, a);
+		std::vector<hamon::uint8_t> const expected{0xFE, 0xFF, 0xFF, 0xFF, 0x01};
+		EXPECT_EQ(c1, expected);
+		EXPECT_EQ(c2, expected);
+	}
+	{
+		std::vector<hamon::uint8_t> const a{0x07, 0xFF, 0x88, 0x50, 0x07};	// 31415926535
+		std::vector<hamon::uint8_t> const b{0xEC, 0xE3, 0x38, 0x54, 0x06};	// 27182818284
+		auto const c1 = hamon::detail::bigint_algo::multiply(a, b);
+		auto const c2 = hamon::detail::bigint_algo::multiply(b, a);
+		std::vector<hamon::uint8_t> const expected{0x74, 0x4F, 0xF6, 0xCD, 0xF7, 0x0E, 0x43, 0x4B, 0x2E};	// 853973422224398765940
+		EXPECT_EQ(c1, expected);
+		EXPECT_EQ(c2, expected);
+	}
+	{
+		std::vector<hamon::uint16_t> const a{0xFF07, 0x5088, 0x0007};	// 31415926535
+		std::vector<hamon::uint16_t> const b{0xE3EC, 0x5438, 0x0006};	// 27182818284
+		auto const c1 = hamon::detail::bigint_algo::multiply(a, b);
+		auto const c2 = hamon::detail::bigint_algo::multiply(b, a);
+		std::vector<hamon::uint16_t> const expected{0x4F74, 0xCDF6, 0x0EF7, 0x4B43, 0x002E};	// 853973422224398765940
+		EXPECT_EQ(c1, expected);
+		EXPECT_EQ(c2, expected);
+	}
+	{
+		std::vector<hamon::uint32_t> const a{0x5088FF07, 0x00000007};	// 31415926535
+		std::vector<hamon::uint32_t> const b{0x5438E3EC, 0x00000006};	// 27182818284
+		auto const c1 = hamon::detail::bigint_algo::multiply(a, b);
+		auto const c2 = hamon::detail::bigint_algo::multiply(b, a);
+		std::vector<hamon::uint32_t> const expected{0xCDF64F74, 0x4B430EF7, 0x0000002E};	// 853973422224398765940
+		EXPECT_EQ(c1, expected);
+		EXPECT_EQ(c2, expected);
+	}
+	{
+		std::vector<hamon::uint64_t> const a{0x000000075088FF07};	// 31415926535
+		std::vector<hamon::uint64_t> const b{0x000000065438E3EC};	// 27182818284
+		auto const c1 = hamon::detail::bigint_algo::multiply(a, b);
+		auto const c2 = hamon::detail::bigint_algo::multiply(b, a);
+		std::vector<hamon::uint64_t> const expected{0x4B430EF7CDF64F74, 0x000000000000002E};	// 853973422224398765940
+		EXPECT_EQ(c1, expected);
+		EXPECT_EQ(c2, expected);
+	}
+	{
+		std::vector<hamon::uint64_t> const a{0x07D5EB5B5BA4D7C6, 0x11};	// 314159265358979323846
+		std::vector<hamon::uint64_t> const b{0x000000065438E3EC};	// 27182818284
+		auto const c1 = hamon::detail::bigint_algo::multiply(a, b);
+		auto const c2 = hamon::detail::bigint_algo::multiply(b, a);
+		std::vector<hamon::uint64_t> const expected{0x84D22BFBD29B7C88, 0x6BC95E95CF};	// 853973422224398765940
+		EXPECT_EQ(c1, expected);
+		EXPECT_EQ(c2, expected);
+	}
+	{
+		std::vector<hamon::uint64_t> const a{0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF};
+		std::vector<hamon::uint64_t> const b{0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF};
+		auto const c1 = hamon::detail::bigint_algo::multiply(a, b);
+		auto const c2 = hamon::detail::bigint_algo::multiply(b, a);
+		std::vector<hamon::uint64_t> const expected{1, 0, 0xFFFFFFFFFFFFFFFE, 0xFFFFFFFFFFFFFFFF};
+		EXPECT_EQ(c1, expected);
+		EXPECT_EQ(c2, expected);
+	}
+
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 1> a{2};
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 1> b{3};
+		HAMON_CXX11_CONSTEXPR auto c1 = hamon::detail::bigint_algo::multiply(a, b);
+		HAMON_CXX11_CONSTEXPR auto c2 = hamon::detail::bigint_algo::multiply(b, a);
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 1> expected{6};
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(c1, expected);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(c2, expected);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 2> a{123};
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 2> b{45};
+		HAMON_CXX11_CONSTEXPR auto c1 = hamon::detail::bigint_algo::multiply(a, b);
+		HAMON_CXX11_CONSTEXPR auto c2 = hamon::detail::bigint_algo::multiply(b, a);
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 2> expected{0x9F, 0x15};
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(c1, expected);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(c2, expected);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 2> a{0x20, 0x10};
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 2> b{0x40, 0x30};
+		HAMON_CXX11_CONSTEXPR auto c1 = hamon::detail::bigint_algo::multiply(a, b);
+		HAMON_CXX11_CONSTEXPR auto c2 = hamon::detail::bigint_algo::multiply(b, a);
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 2> expected{0x00, 0x08};
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(c1, expected);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(c2, expected);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> a{0x20, 0x10};
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> b{0x40, 0x30};
+		HAMON_CXX11_CONSTEXPR auto c1 = hamon::detail::bigint_algo::multiply(a, b);
+		HAMON_CXX11_CONSTEXPR auto c2 = hamon::detail::bigint_algo::multiply(b, a);
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> expected{0x00, 0x08, 0x0A, 0x03};
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(c1, expected);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(c2, expected);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint16_t, 4> a{0x68B1, 0x3ADE};
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint16_t, 4> b{0x0003, 0x0000};
+		HAMON_CXX11_CONSTEXPR auto c1 = hamon::detail::bigint_algo::multiply(a, b);
+		HAMON_CXX11_CONSTEXPR auto c2 = hamon::detail::bigint_algo::multiply(b, a);
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint16_t, 4> expected{0x3A13, 0xB09B};
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(c1, expected);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(c2, expected);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint16_t, 4> a{0xFF07, 0x5088, 0x0007};
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint16_t, 4> b{0xE3EC, 0x5438, 0x0006};
+		HAMON_CXX11_CONSTEXPR auto c1 = hamon::detail::bigint_algo::multiply(a, b);
+		HAMON_CXX11_CONSTEXPR auto c2 = hamon::detail::bigint_algo::multiply(b, a);
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint16_t, 4> expected{0x4F74, 0xCDF6, 0x0EF7, 0x4B43};
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(c1, expected);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(c2, expected);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint32_t, 4> a{0x5088FF07, 0x00000007};
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint32_t, 4> b{0x5438E3EC, 0x00000006};
+		HAMON_CXX11_CONSTEXPR auto c1 = hamon::detail::bigint_algo::multiply(a, b);
+		HAMON_CXX11_CONSTEXPR auto c2 = hamon::detail::bigint_algo::multiply(b, a);
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint32_t, 4> expected{0xCDF64F74, 0x4B430EF7, 0x0000002E};
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(c1, expected);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(c2, expected);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint64_t, 4> a{0x000000075088FF07};
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint64_t, 4> b{0x000000065438E3EC};
+		HAMON_CXX11_CONSTEXPR auto c1 = hamon::detail::bigint_algo::multiply(a, b);
+		HAMON_CXX11_CONSTEXPR auto c2 = hamon::detail::bigint_algo::multiply(b, a);
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint64_t, 4> expected{0x4B430EF7CDF64F74, 0x000000000000002E};
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(c1, expected);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(c2, expected);
+	}
+	{
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint64_t, 4> a{0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF};
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint64_t, 4> b{0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF};
+		HAMON_CXX11_CONSTEXPR auto c1 = hamon::detail::bigint_algo::multiply(a, b);
+		HAMON_CXX11_CONSTEXPR auto c2 = hamon::detail::bigint_algo::multiply(b, a);
+		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint64_t, 4> expected{0x0000000000000001, 0x0000000000000000, 0xFFFFFFFFFFFFFFFE, 0xFFFFFFFFFFFFFFFF};
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(c1, expected);
+		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(c2, expected);
+	}
+}
