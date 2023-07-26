62ad8dc2bbae760e4d805c1e6bd17c62f0f12ec5
shibainuudon <shibainuudon@gmail.com>
2023-07-26 14:02:35 +0900

    [BigInt]bigint_algo::bit_or の仕様を変更

--- libs/bigint/include/hamon/bigint/bigint_algo/bit_or.hpp
+++ libs/bigint/include/hamon/bigint/bigint_algo/bit_or.hpp
@@ -12,7 +12,6 @@
 #include <hamon/array.hpp>
 #include <hamon/algorithm/max.hpp>
 #include <hamon/cstddef/size_t.hpp>
-#include <hamon/utility/make_index_sequence.hpp>
 #include <hamon/config.hpp>
 #include <vector>
 
@@ -22,33 +21,28 @@ namespace bigint_algo
 {
 
 template <typename T>
-inline std::vector<T>
-bit_or(std::vector<T> const& lhs, std::vector<T> const& rhs)
+inline void
+bit_or(std::vector<T>& lhs, std::vector<T> const& rhs)
 {
 	auto const N = hamon::max(lhs.size(), rhs.size());
-	std::vector<T> result(N);
+	lhs.resize(N);
 	for (hamon::size_t i = 0; i < N; ++i)
 	{
-		result[i] = static_cast<T>(
+		lhs[i] = static_cast<T>(
 			detail::get(lhs, i) |
 			detail::get(rhs, i));
 	}
-	bigint_algo::normalize(result);
-	return result;
-}
-
-template <typename T, hamon::size_t N, hamon::size_t... Is>
-inline HAMON_CXX11_CONSTEXPR hamon::array<T, N>
-bit_or_impl(hamon::array<T, N> const& lhs, hamon::array<T, N> const& rhs, hamon::index_sequence<Is...>)
-{
-	return hamon::array<T, N>{ static_cast<T>(lhs[Is] | rhs[Is])... };
+	bigint_algo::normalize(lhs);
 }
 
 template <typename T, hamon::size_t N>
-inline HAMON_CXX11_CONSTEXPR hamon::array<T, N>
-bit_or(hamon::array<T, N> const& lhs, hamon::array<T, N> const& rhs)
+inline HAMON_CXX14_CONSTEXPR void
+bit_or(hamon::array<T, N>& lhs, hamon::array<T, N> const& rhs)
 {
-	return bit_or_impl(lhs, rhs, hamon::make_index_sequence<N>{});
+	for (hamon::size_t i = 0; i < N; ++i)
+	{
+		lhs[i] = static_cast<T>(lhs[i] | rhs[i]);
+	}
 }
 
 }	// namespace bigint_algo
--- libs/bigint/test/src/bigint_algo/unit_test_bigint_algo_bit_or.cpp
+++ libs/bigint/test/src/bigint_algo/unit_test_bigint_algo_bit_or.cpp
@@ -11,84 +11,116 @@
 #include <vector>
 #include "constexpr_test.hpp"
 
+namespace hamon_bigint_test
+{
+
+namespace bigint_algo_bit_or_test
+{
+
+#define VERIFY(...)	if (!(__VA_ARGS__)) { return false; }
+
+template <typename VectorType>
+inline HAMON_CXX14_CONSTEXPR bool
+BitOrTest(VectorType const& a, VectorType const& b, VectorType const& expected)
+{
+	// a | b
+	{
+		VectorType c = a;
+		hamon::bigint_algo::bit_or(c, b);
+		VERIFY(c == expected);
+	}
+	// b | a
+	{
+		VectorType c = b;
+		hamon::bigint_algo::bit_or(c, a);
+		VERIFY(c == expected);
+	}
+	return true;
+}
+
+#undef VERIFY
+
 GTEST_TEST(BigIntAlgoTest, BitOrTest)
 {
-	{
-		std::vector<hamon::uint8_t> const a{0};
-		std::vector<hamon::uint8_t> const b{0};
-		auto const c1 = hamon::bigint_algo::bit_or(a, b);
-		auto const c2 = hamon::bigint_algo::bit_or(b, a);
-		std::vector<hamon::uint8_t> const expected{0};
-		EXPECT_EQ(c1, expected);
-		EXPECT_EQ(c2, expected);
-	}
-	{
-		std::vector<hamon::uint8_t> const a{0x12, 0x34, 0x56, 0x78};
-		std::vector<hamon::uint8_t> const b{0xFF, 0xFF, 0xFF};
-		auto const c1 = hamon::bigint_algo::bit_or(a, b);
-		auto const c2 = hamon::bigint_algo::bit_or(b, a);
-		std::vector<hamon::uint8_t> const expected{0xFF, 0xFF, 0xFF, 0x78};
-		EXPECT_EQ(c1, expected);
-		EXPECT_EQ(c2, expected);
-	}
-	{
-		std::vector<hamon::uint8_t> const a{0x12, 0x34, 0x56, 0x78};
-		std::vector<hamon::uint8_t> const b{0xFF, 0x0F, 0xF0, 0x00};
-		auto const c1 = hamon::bigint_algo::bit_or(a, b);
-		auto const c2 = hamon::bigint_algo::bit_or(b, a);
-		std::vector<hamon::uint8_t> const expected{0xFF, 0x3F, 0xF6, 0x78};
-		EXPECT_EQ(c1, expected);
-		EXPECT_EQ(c2, expected);
-	}
+	EXPECT_TRUE(BitOrTest(
+		std::vector<hamon::uint8_t>{0},
+		std::vector<hamon::uint8_t>{0},
+		std::vector<hamon::uint8_t>{0}));
+	EXPECT_TRUE(BitOrTest(
+		std::vector<hamon::uint8_t>{0x12, 0x34, 0x56, 0x78},
+		std::vector<hamon::uint8_t>{0xFF, 0xFF, 0xFF},
+		std::vector<hamon::uint8_t>{0xFF, 0xFF, 0xFF, 0x78}));
+	EXPECT_TRUE(BitOrTest(
+		std::vector<hamon::uint8_t>{0x12, 0x34, 0x56, 0x78},
+		std::vector<hamon::uint8_t>{0xFF, 0x0F, 0xF0},
+		std::vector<hamon::uint8_t>{0xFF, 0x3F, 0xF6, 0x78}));
+	EXPECT_TRUE(BitOrTest(
+		std::vector<hamon::uint16_t>{0x4996, 0x02D2},
+		std::vector<hamon::uint16_t>{0x12B9, 0xB0A1},
+		std::vector<hamon::uint16_t>{0x5BBF, 0xB2F3}));
+	EXPECT_TRUE(BitOrTest(
+		std::vector<hamon::uint16_t>{0x4996, 0x02D2, 0xFFFF, 0x1234},
+		std::vector<hamon::uint16_t>{0x0000},
+		std::vector<hamon::uint16_t>{0x4996, 0x02D2, 0xFFFF, 0x1234}));
+	EXPECT_TRUE(BitOrTest(
+		std::vector<hamon::uint16_t>{0xFFFF, 0xFF00, 0x0000},
+		std::vector<hamon::uint16_t>{0x0000, 0x00FF, 0x0F0F},
+		std::vector<hamon::uint16_t>{0xFFFF, 0xFFFF, 0x0F0F}));
+	EXPECT_TRUE(BitOrTest(
+		std::vector<hamon::uint16_t>{0x1234, 0x5678, 0x9ABC},
+		std::vector<hamon::uint16_t>{0x1234, 0x5678, 0x9ABC},
+		std::vector<hamon::uint16_t>{0x1234, 0x5678, 0x9ABC}));
+	EXPECT_TRUE(BitOrTest(
+		std::vector<hamon::uint32_t>{0},
+		std::vector<hamon::uint32_t>{0},
+		std::vector<hamon::uint32_t>{0}));
+	EXPECT_TRUE(BitOrTest(
+		std::vector<hamon::uint32_t>{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF},
+		std::vector<hamon::uint32_t>{0xFFFF0000, 0xFF00FF00, 0xF0F0F0F0},
+		std::vector<hamon::uint32_t>{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF}));
+	EXPECT_TRUE(BitOrTest(
+		std::vector<hamon::uint64_t>{0x0123456789ABCDEF, 0x1122334455667788},
+		std::vector<hamon::uint64_t>{0xFFFF0000FF00FF00, 0xFFFFFFFF00000000},
+		std::vector<hamon::uint64_t>{0xFFFF4567FFABFFEF, 0xFFFFFFFF55667788}));
 
-	{
-		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const a{0x12, 0x34, 0x56, 0x78};
-		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const b{0xFF, 0xFF, 0xFF, 0xFF};
-		HAMON_CXX11_CONSTEXPR auto const c = hamon::bigint_algo::bit_or(a, b);
-		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFFu, c[0]);
-		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFFu, c[1]);
-		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFFu, c[2]);
-		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFFu, c[3]);
-	}
-	{
-		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const a{0x12, 0x34, 0x56, 0x78};
-		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 4> const b{0x00, 0x00, 0x00, 0x00};
-		HAMON_CXX11_CONSTEXPR auto const c = hamon::bigint_algo::bit_or(a, b);
-		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x12u, c[0]);
-		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x34u, c[1]);
-		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x56u, c[2]);
-		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x78u, c[3]);
-	}
-	{
-		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 2> const a{0xCB, 0xE1};
-		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint8_t, 2> const b{0xFA, 0x0F};
-		HAMON_CXX11_CONSTEXPR auto const c = hamon::bigint_algo::bit_or(a, b);
-		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFBu, c[0]);
-		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xEFu, c[1]);
-	}
-
-	{
-		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint16_t, 2> const a{0x4996, 0x02D2};
-		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint16_t, 2> const b{0x12B9, 0xB0A1};
-		HAMON_CXX11_CONSTEXPR auto const c = hamon::bigint_algo::bit_or(a, b);
-		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x5BBFu, c[0]);
-		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xB2F3u, c[1]);
-	}
-
-	{
-		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint32_t, 3> const a{0};
-		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint32_t, 3> const b{0};
-		HAMON_CXX11_CONSTEXPR auto const c = hamon::bigint_algo::bit_or(a, b);
-		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x00000000u, c[0]);
-		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x00000000u, c[1]);
-		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0x00000000u, c[2]);
-	}
-	{
-		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint32_t, 3> const a{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF};
-		HAMON_CXX11_CONSTEXPR hamon::array<hamon::uint32_t, 3> const b{0xFFFF0000, 0xFF00FF00, 0xF0F0F0F0};
-		HAMON_CXX11_CONSTEXPR auto const c = hamon::bigint_algo::bit_or(a, b);
-		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFFFFFFFFu, c[0]);
-		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFFFFFFFFu, c[1]);
-		HAMON_CXX11_CONSTEXPR_EXPECT_EQ(0xFFFFFFFFu, c[2]);
-	}
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(BitOrTest(
+		hamon::array<hamon::uint8_t, 4>{0x12, 0x34, 0x56, 0x78},
+		hamon::array<hamon::uint8_t, 4>{0xFF, 0xFF, 0xFF, 0xFF},
+		hamon::array<hamon::uint8_t, 4>{0xFF, 0xFF, 0xFF, 0xFF}));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(BitOrTest(
+		hamon::array<hamon::uint8_t, 4>{0x12, 0x34, 0x56, 0x78},
+		hamon::array<hamon::uint8_t, 4>{0x00, 0x00, 0x00, 0x00},
+		hamon::array<hamon::uint8_t, 4>{0x12, 0x34, 0x56, 0x78}));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(BitOrTest(
+		hamon::array<hamon::uint8_t, 2>{0xCB, 0xE1},
+		hamon::array<hamon::uint8_t, 2>{0xFA, 0x0F},
+		hamon::array<hamon::uint8_t, 2>{0xFB, 0xEF}));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(BitOrTest(
+		hamon::array<hamon::uint16_t, 2>{0x4996, 0x02D2},
+		hamon::array<hamon::uint16_t, 2>{0x12B9, 0xB0A1},
+		hamon::array<hamon::uint16_t, 2>{0x5BBF, 0xB2F3}));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(BitOrTest(
+		hamon::array<hamon::uint16_t, 3>{0xFFFF, 0xFF00, 0x0000},
+		hamon::array<hamon::uint16_t, 3>{0x0000, 0x00FF, 0x0F0F},
+		hamon::array<hamon::uint16_t, 3>{0xFFFF, 0xFFFF, 0x0F0F}));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(BitOrTest(
+		hamon::array<hamon::uint16_t, 3>{0x1234, 0x5678, 0x9ABC},
+		hamon::array<hamon::uint16_t, 3>{0x1234, 0x5678, 0x9ABC},
+		hamon::array<hamon::uint16_t, 3>{0x1234, 0x5678, 0x9ABC}));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(BitOrTest(
+		hamon::array<hamon::uint32_t, 3>{0},
+		hamon::array<hamon::uint32_t, 3>{0},
+		hamon::array<hamon::uint32_t, 3>{0}));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(BitOrTest(
+		hamon::array<hamon::uint32_t, 3>{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF},
+		hamon::array<hamon::uint32_t, 3>{0xFFFF0000, 0xFF00FF00, 0xF0F0F0F0},
+		hamon::array<hamon::uint32_t, 3>{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF}));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(BitOrTest(
+		hamon::array<hamon::uint64_t, 2>{0x0123456789ABCDEF, 0x1122334455667788},
+		hamon::array<hamon::uint64_t, 2>{0xFFFF0000FF00FF00, 0xFFFFFFFF00000000},
+		hamon::array<hamon::uint64_t, 2>{0xFFFF4567FFABFFEF, 0xFFFFFFFF55667788}));
 }
+
+}	// namespace bigint_algo_bit_or_test
+
+}	// namespace hamon_bigint_test
