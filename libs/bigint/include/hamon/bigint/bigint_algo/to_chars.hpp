b97b1225dbc5fdf9cd648c54cba1369e8b448483
shibainuudon <shibainuudon@gmail.com>
2023-07-28 17:20:02 +0900

    [BigInt]bigint_algo::to_chars の最適化

--- libs/bigint/include/hamon/bigint/bigint_algo/to_chars.hpp
+++ libs/bigint/include/hamon/bigint/bigint_algo/to_chars.hpp
@@ -10,9 +10,13 @@
 #include <hamon/bigint/bigint_algo/compare.hpp>
 #include <hamon/bigint/bigint_algo/div_mod.hpp>
 #include <hamon/algorithm/reverse.hpp>
+#include <hamon/algorithm/min.hpp>
+#include <hamon/cmath/log2.hpp>
+#include <hamon/cmath/floor.hpp>
 #include <hamon/ranges/range_value_t.hpp>
 #include <hamon/charconv/to_chars.hpp>
 #include <hamon/config.hpp>
+#include <limits>
 
 namespace hamon
 {
@@ -22,26 +26,66 @@ namespace bigint_algo
 namespace to_chars_detail
 {
 
+template <typename T>
+inline HAMON_CXX14_CONSTEXPR void
+to_chars_reverse(char* first, char* last, T value, int base)
+{
+	for (char* p = first; p != last; ++p)
+	{
+		auto c = value % static_cast<T>(base);
+		value = value / static_cast<T>(base);
+		*p = "0123456789abcdefghijklmnopqrstuvwxyz"[c];
+	}
+}
+
+inline HAMON_CXX14_CONSTEXPR char*
+remove_trailing_zeros(char* first, char* last)
+{
+	char* p = last - 1;
+	while (p > first)
+	{
+		if (*p != '0')
+		{
+			break;
+		}
+		--p;
+	}
+	return p+1;
+}
+
 template <typename VectorType>
 inline HAMON_CXX14_CONSTEXPR hamon::to_chars_result
 to_chars(char* first, char* last, VectorType value, int base)
 {
 	using T = hamon::ranges::range_value_t<VectorType>;
+	
+	auto const digits = static_cast<hamon::ptrdiff_t>(hamon::floor(std::numeric_limits<T>::digits / hamon::log2(base)));
 
-	auto const base2 = VectorType{static_cast<T>(base)};
+	// base2 = pow_n(base, digits)
+	auto base2 = VectorType{1};
+	VectorType tmp;
+	for (int i = 0; i < digits; ++i)
+	{
+		bigint_algo::multiply(tmp, base2, static_cast<T>(base));
+		base2 = tmp;
+	}
 
 	auto p = first;
 	while (p != last)
 	{
 		auto r = bigint_algo::div_mod(value, base2);
 		value = r.quo;
-		p = hamon::to_chars(p, p+1, r.rem[0], base).ptr;
+		auto p2 = hamon::min(p + digits, last);
+		to_chars_reverse(p, p2, r.rem[0], base);
+		p = p2;
 
 		if (bigint_algo::compare(value, VectorType{0}) == 0)
 		{
 			break;
 		}
 	}
+
+	p = remove_trailing_zeros(first, p);
 
 	if (p == last)
 	{
