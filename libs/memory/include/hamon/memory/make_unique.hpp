b938ff90ab0c975118be446dd78d2682ee8f2a3a
shibainuudon <shibainuudon@gmail.com>
2023-08-18 16:42:08 +0900

    [Memory]make_unique関数を追加 refs #120

---
+++ libs/memory/include/hamon/memory/make_unique.hpp
@@ -0,0 +1,59 @@
+﻿/**
+ *	@file	make_unique.hpp
+ *
+ *	@brief	make_unique を定義
+ */
+
+#ifndef HAMON_MEMORY_MAKE_UNIQUE_HPP
+#define HAMON_MEMORY_MAKE_UNIQUE_HPP
+
+#include <memory>
+
+#if defined(__cpp_lib_make_unique) && (__cpp_lib_make_unique >= 201304)
+
+namespace hamon
+{
+
+using std::make_unique;
+
+}	// namespace hamon
+
+#else
+
+#include <hamon/type_traits/enable_if.hpp>
+#include <hamon/type_traits/is_array.hpp>
+#include <hamon/type_traits/is_bounded_array.hpp>
+#include <hamon/type_traits/is_unbounded_array.hpp>
+#include <hamon/type_traits/remove_extent.hpp>
+#include <hamon/utility/forward.hpp>
+#include <hamon/cstddef/size_t.hpp>
+#include <hamon/config.hpp>
+
+namespace hamon
+{
+
+template <typename T, typename... Args>
+inline /*HAMON_CXX23_CONSTEXPR*/
+hamon::enable_if_t<!hamon::is_array<T>::value, std::unique_ptr<T>>
+make_unique(Args&&... args)
+{
+	return std::unique_ptr<T>(new T(hamon::forward<Args>(args)...));
+}
+
+template <typename T>
+inline /*HAMON_CXX23_CONSTEXPR*/
+hamon::enable_if_t<hamon::is_unbounded_array<T>::value, std::unique_ptr<T>>
+make_unique(hamon::size_t n)
+{
+	return std::unique_ptr<T>(new hamon::remove_extent_t<T>[n]());
+}
+
+template <typename T, typename... Args>
+hamon::enable_if_t<hamon::is_bounded_array<T>::value>
+make_unique(Args&&...) = delete;
+
+}	// namespace hamon
+
+#endif
+
+#endif // HAMON_MEMORY_MAKE_UNIQUE_HPP
---
+++ libs/memory/test/src/unit_test_memory_make_unique.cpp
@@ -0,0 +1,201 @@
+﻿/**
+ *	@file	unit_test_memory_make_unique.cpp
+ *
+ *	@brief	make_unique のテスト
+ */
+
+#include <hamon/memory/make_unique.hpp>
+#include <gtest/gtest.h>
+
+namespace hamon_memory_test
+{
+
+namespace make_unique_test
+{
+
+class Foo
+{
+public:
+	static unsigned int instances;
+
+	Foo(
+		int v1 = 0,
+		int v2 = 0,
+		int v3 = 0,
+		int v4 = 0,
+		int v5 = 0,
+		int v6 = 0,
+		int v7 = 0,
+		int v8 = 0,
+		int v9 = 0)
+		: sum(v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9)
+	{
+		instances++;
+	}
+
+	~Foo()
+	{
+		instances--;
+	}
+
+	const int sum;
+
+private:
+	Foo(const Foo&);
+	Foo& operator=(const Foo&);
+};
+
+unsigned int Foo::instances = 0;
+
+GTEST_TEST(MemoryTest, MakeUniqueArgsTest)
+{
+	using hamon::make_unique;
+	using std::unique_ptr;
+
+	EXPECT_EQ(0u, Foo::instances);
+	{
+		unique_ptr<Foo> x = make_unique<Foo>();
+		EXPECT_EQ(1u, Foo::instances);
+		EXPECT_EQ(0, x->sum);
+		x.reset();
+		EXPECT_EQ(0u, Foo::instances);
+	}
+	{
+		unique_ptr<Foo> x = make_unique<Foo>(1);
+		EXPECT_EQ(1u, Foo::instances);
+		EXPECT_EQ(1, x->sum);
+		x.reset();
+		EXPECT_EQ(0u, Foo::instances);
+	}
+	{
+		unique_ptr<Foo> x = make_unique<Foo>(1, 2);
+		EXPECT_EQ(1u, Foo::instances);
+		EXPECT_EQ(1 + 2, x->sum);
+		x.reset();
+		EXPECT_EQ(0u, Foo::instances);
+	}
+	{
+		unique_ptr<Foo> x = make_unique<Foo>(1, 2, 3);
+		EXPECT_EQ(1u, Foo::instances);
+		EXPECT_EQ(1 + 2 + 3, x->sum);
+		x.reset();
+		EXPECT_EQ(0u, Foo::instances);
+	}
+	{
+		unique_ptr<Foo> x = make_unique<Foo>(1, 2, 3, 4);
+		EXPECT_EQ(1u, Foo::instances);
+		EXPECT_EQ(1 + 2 + 3 + 4, x->sum);
+		x.reset();
+		EXPECT_EQ(0u, Foo::instances);
+	}
+	{
+		unique_ptr<Foo> x = make_unique<Foo>(1, 2, 3, 4, 5);
+		EXPECT_EQ(1u, Foo::instances);
+		EXPECT_EQ(1 + 2 + 3 + 4 + 5, x->sum);
+		x.reset();
+		EXPECT_EQ(0u, Foo::instances);
+	}
+	{
+		unique_ptr<Foo> x = make_unique<Foo>(1, 2, 3, 4, 5, 6);
+		EXPECT_EQ(1u, Foo::instances);
+		EXPECT_EQ(1 + 2 + 3 + 4 + 5 + 6, x->sum);
+		x.reset();
+		EXPECT_EQ(0u, Foo::instances);
+	}
+	{
+		unique_ptr<Foo> x = make_unique<Foo>(1, 2, 3, 4, 5, 6, 7);
+		EXPECT_EQ(1u, Foo::instances);
+		EXPECT_EQ(1 + 2 + 3 + 4 + 5 + 6 + 7, x->sum);
+		x.reset();
+		EXPECT_EQ(0u, Foo::instances);
+	}
+	{
+		unique_ptr<Foo> x = make_unique<Foo>(1, 2, 3, 4, 5, 6, 7, 8);
+		EXPECT_EQ(1u, Foo::instances);
+		EXPECT_EQ(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8, x->sum);
+		x.reset();
+		EXPECT_EQ(0u, Foo::instances);
+	}
+	{
+		unique_ptr<Foo> x = make_unique<Foo>(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		EXPECT_EQ(1u, Foo::instances);
+		EXPECT_EQ(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9, x->sum);
+		x.reset();
+		EXPECT_EQ(0u, Foo::instances);
+	}
+}
+
+GTEST_TEST(MemoryTest, MakeUniqueArrayTest)
+{
+	using hamon::make_unique;
+	using std::unique_ptr;
+
+	{
+		unique_ptr<int[]> a1 = make_unique<int[]>(3);
+		EXPECT_TRUE(a1.get() != nullptr);
+		EXPECT_EQ(0, a1[0]);
+		EXPECT_EQ(0, a1[1]);
+		EXPECT_EQ(0, a1[2]);
+	}
+
+	{
+		unique_ptr<int[][2]> a1 = make_unique<int[][2]>(2);
+		EXPECT_TRUE(a1.get() != nullptr);
+		EXPECT_EQ(0, a1[0][0]);
+		EXPECT_EQ(0, a1[0][1]);
+		EXPECT_EQ(0, a1[1][0]);
+		EXPECT_EQ(0, a1[1][1]);
+	}
+
+	{
+		unique_ptr<const int[]> a1 = make_unique<const int[]>(3);
+		EXPECT_TRUE(a1.get() != nullptr);
+		EXPECT_EQ(0, a1[0]);
+		EXPECT_EQ(0, a1[1]);
+		EXPECT_EQ(0, a1[2]);
+	}
+
+	{
+		unique_ptr<const int[][2]> a1 = make_unique<const int[][2]>(2);
+		EXPECT_TRUE(a1.get() != nullptr);
+		EXPECT_EQ(0, a1[0][0]);
+		EXPECT_EQ(0, a1[0][1]);
+		EXPECT_EQ(0, a1[1][0]);
+		EXPECT_EQ(0, a1[1][1]);
+	}
+
+	EXPECT_EQ(0u, Foo::instances);
+	{
+		unique_ptr<Foo[]> a1 = make_unique<Foo[]>(3);
+		EXPECT_TRUE(a1.get() != nullptr);
+		EXPECT_EQ(3u, Foo::instances);
+		a1.reset();
+		EXPECT_EQ(0u, Foo::instances);
+	}
+
+	{
+		unique_ptr<Foo[][2]> a1 = make_unique<Foo[][2]>(2);
+		EXPECT_TRUE(a1.get() != nullptr);
+		EXPECT_EQ(4u, Foo::instances);
+	}
+	EXPECT_EQ(0u, Foo::instances);
+
+	{
+		unique_ptr<const Foo[]> a1 = make_unique<const Foo[]>(3);
+		EXPECT_TRUE(a1.get() != nullptr);
+		EXPECT_EQ(3u, Foo::instances);
+		a1.reset();
+		EXPECT_EQ(0u, Foo::instances);
+	}
+
+	{
+		unique_ptr<const Foo[][2]> a1 = make_unique<const Foo[][2]>(2);
+		EXPECT_TRUE(a1.get() != nullptr);
+		EXPECT_EQ(4u, Foo::instances);
+	}
+	EXPECT_EQ(0u, Foo::instances);
+}
+
+}	// namespace make_unique_test
+
+}	// namespace hamon_memory_test
