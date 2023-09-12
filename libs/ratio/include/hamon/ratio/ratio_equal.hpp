64646f5a6eab24020793176f8cd626c9ed506285
shibainuudon <shibainuudon@gmail.com>
2023-09-12 17:38:17 +0900

    [Ratio]ratio_equal を追加

---
+++ libs/ratio/include/hamon/ratio/ratio_equal.hpp
@@ -0,0 +1,19 @@
+﻿/**
+ *	@file	ratio_equal.hpp
+ *
+ *	@brief	ratio_equal の定義
+ */
+
+#ifndef HAMON_RATIO_RATIO_EQUAL_HPP
+#define HAMON_RATIO_RATIO_EQUAL_HPP
+
+#include <ratio>
+
+namespace hamon
+{
+
+using std::ratio_equal;
+
+}	// namespace hamon
+
+#endif // HAMON_RATIO_RATIO_EQUAL_HPP
