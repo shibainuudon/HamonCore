09c575cd4ec04e0ebba5af60fccb7c4587f81753
shibainuudon <shibainuudon@gmail.com>
2023-09-12 17:39:46 +0900

    [Ratio]ratio_less を追加

---
+++ libs/ratio/include/hamon/ratio/ratio_less.hpp
@@ -0,0 +1,19 @@
+﻿/**
+ *	@file	ratio_less.hpp
+ *
+ *	@brief	ratio_less の定義
+ */
+
+#ifndef HAMON_RATIO_RATIO_LESS_HPP
+#define HAMON_RATIO_RATIO_LESS_HPP
+
+#include <ratio>
+
+namespace hamon
+{
+
+using std::ratio_less;
+
+}	// namespace hamon
+
+#endif // HAMON_RATIO_RATIO_LESS_HPP
