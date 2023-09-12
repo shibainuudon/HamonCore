1b32313857332e374730944975ae4355c460bfe8
shibainuudon <shibainuudon@gmail.com>
2023-09-12 17:40:30 +0900

    [Ratio]ratio_less_equal を追加

--- libs/ratio/include/hamon/ratio.hpp
+++ libs/ratio/include/hamon/ratio.hpp
@@ -13,6 +13,7 @@
 #include <hamon/ratio/ratio_equal.hpp>
 #include <hamon/ratio/ratio_gcd.hpp>
 #include <hamon/ratio/ratio_less.hpp>
+#include <hamon/ratio/ratio_less_equal.hpp>
 #include <hamon/ratio/ratio_multiply.hpp>
 #include <hamon/ratio/ratio_not_equal.hpp>
 #include <hamon/ratio/ratio_subtract.hpp>
