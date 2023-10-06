6a8182b5cdee18d3db25740d8490c6a6a2dd5ca0
shibainuudon <shibainuudon@gmail.com>
2023-10-06 13:52:48 +0900

    [String]front, back のテストを追加

---
+++ libs/string/test/src/unit_test_string_back.cpp
@@ -0,0 +1,72 @@
+﻿/**
+ *	@file	unit_test_string_back.cpp
+ *
+ *	@brief	back のテスト
+ *
+ *	constexpr const charT& back() const;
+ *	constexpr charT&       back();
+ */
+
+#include <hamon/string/basic_string.hpp>
+#include <hamon/type_traits.hpp>
+#include <hamon/config.hpp>
+#include "constexpr_test.hpp"
+#include "string_test_helper.hpp"
+
+HAMON_WARNING_PUSH()
+HAMON_WARNING_DISABLE_GCC("-Wmaybe-uninitialized")
+
+namespace hamon_test
+{
+namespace string_test
+{
+namespace back_test
+{
+
+#define VERIFY(...)	if (!(__VA_ARGS__)) { return false; }
+
+template <typename CharT>
+inline /*HAMON_CXX14_CONSTEXPR*/ bool
+BackTest()
+{
+	using string = hamon::basic_string<CharT>;
+	using Reference = typename string::reference;
+	using ConstReference = typename string::const_reference;
+	using Helper = StringTestHelper<CharT>;
+
+	{
+		auto p = Helper::abcde();
+		string const s = p;
+		static_assert(noexcept(s.back()), "");
+		static_assert(hamon::is_same<decltype(s.back()), ConstReference>::value, "");
+		VERIFY(s.back() == p[4]);
+		VERIFY(s.back() == s[4]);
+	}
+	{
+		auto p = Helper::abcde();
+		string s = p;
+		static_assert(noexcept(s.back()), "");
+		static_assert(hamon::is_same<decltype(s.back()), Reference>::value, "");
+		VERIFY(s.back() == p[4]);
+		VERIFY(s.back() == s[4]);
+		s.back() = CharT{};
+		VERIFY(s.back() != p[4]);
+		VERIFY(s.back() == CharT{});
+		VERIFY(s.back() == s[4]);
+	}
+
+	return true;
+}
+
+#undef VERIFY
+
+TYPED_TEST(StringTest, BackTest)
+{
+	/*HAMON_CXX14_CONSTEXPR_*/EXPECT_TRUE(BackTest<TypeParam>());
+}
+
+}	// namespace back_test
+}	// namespace string_test
+}	// namespace hamon_test
+
+HAMON_WARNING_POP()
---
+++ libs/string/test/src/unit_test_string_front.cpp
@@ -0,0 +1,67 @@
+﻿/**
+ *	@file	unit_test_string_front.cpp
+ *
+ *	@brief	front のテスト
+ *
+ *	constexpr const charT& front() const;
+ *	constexpr charT&       front();
+ */
+
+#include <hamon/string/basic_string.hpp>
+#include <hamon/type_traits.hpp>
+#include <hamon/config.hpp>
+#include "constexpr_test.hpp"
+#include "string_test_helper.hpp"
+
+namespace hamon_test
+{
+namespace string_test
+{
+namespace front_test
+{
+
+#define VERIFY(...)	if (!(__VA_ARGS__)) { return false; }
+
+template <typename CharT>
+inline /*HAMON_CXX14_CONSTEXPR*/ bool
+FrontTest()
+{
+	using string = hamon::basic_string<CharT>;
+	using Reference = typename string::reference;
+	using ConstReference = typename string::const_reference;
+	using Helper = StringTestHelper<CharT>;
+
+	{
+		auto p = Helper::abcde();
+		string const s = p;
+		static_assert(noexcept(s.front()), "");
+		static_assert(hamon::is_same<decltype(s.front()), ConstReference>::value, "");
+		VERIFY(s.front() == p[0]);
+		VERIFY(s.front() == s[0]);
+	}
+	{
+		auto p = Helper::abcde();
+		string s = p;
+		static_assert(noexcept(s.front()), "");
+		static_assert(hamon::is_same<decltype(s.front()), Reference>::value, "");
+		VERIFY(s.front() == p[0]);
+		VERIFY(s.front() == s[0]);
+		s.front() = CharT{};
+		VERIFY(s.front() != p[0]);
+		VERIFY(s.front() == CharT{});
+		VERIFY(s.front() == s[0]);
+	}
+
+	return true;
+}
+
+#undef VERIFY
+
+TYPED_TEST(StringTest, FrontTest)
+{
+	/*HAMON_CXX14_CONSTEXPR_*/EXPECT_TRUE(FrontTest<TypeParam>());
+}
+
+}	// namespace front_test
+}	// namespace string_test
+}	// namespace hamon_test
