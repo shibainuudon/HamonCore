6845041d00264a65881022a876c5414675e08d08
shibainuudon <shibainuudon@gmail.com>
2023-10-10 14:58:01 +0900

    [String]op_string_view のテストを追加

---
+++ libs/string/test/src/unit_test_string_op_string_view.cpp
@@ -0,0 +1,44 @@
+﻿/**
+ *	@file	unit_test_string_op_string_view.cpp
+ *
+ *	@brief	op_string_view のテスト
+ *
+ *	constexpr operator basic_string_view<charT, traits>() const noexcept;
+ */
+
+#include <hamon/string/basic_string.hpp>
+#include <hamon/type_traits.hpp>
+#include <hamon/config.hpp>
+#include "constexpr_test.hpp"
+#include "string_test_helper.hpp"
+
+namespace hamon_test
+{
+namespace string_test
+{
+namespace op_string_view_test
+{
+
+#define VERIFY(...)	if (!(__VA_ARGS__)) { return false; }
+
+template <typename CharT>
+inline /*HAMON_CXX14_CONSTEXPR*/ bool
+OpStringViewTest()
+{
+	//using string = hamon::basic_string<CharT>;
+
+	// TODO
+
+	return true;
+}
+
+#undef VERIFY
+
+TYPED_TEST(StringTest, OpStringViewTest)
+{
+	/*HAMON_CXX14_CONSTEXPR_*/EXPECT_TRUE(OpStringViewTest<TypeParam>());
+}
+
+}	// namespace op_string_view_test
+}	// namespace string_test
+}	// namespace hamon_test
