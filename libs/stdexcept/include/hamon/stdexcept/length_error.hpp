71e8904f60d0dfcfa02ee946cda164eb04958ddf
shibainuudon <shibainuudon@gmail.com>
2023-10-18 15:32:04 +0900

    [StdExcept]throw_length_error を追加

--- libs/stdexcept/include/hamon/stdexcept/length_error.hpp
+++ libs/stdexcept/include/hamon/stdexcept/length_error.hpp
@@ -7,12 +7,30 @@
 #ifndef HAMON_STDEXCEPT_LENGTH_ERROR_HPP
 #define HAMON_STDEXCEPT_LENGTH_ERROR_HPP
 
+#include <hamon/cstdlib/abort.hpp>
+#include <hamon/config.hpp>
 #include <stdexcept>
 
 namespace hamon
 {
 
 using std::length_error;
+
+namespace detail
+{
+
+HAMON_NORETURN inline void
+throw_length_error(char const* msg)
+{
+#if !defined(HAMON_NO_EXCEPTIONS)
+	throw hamon::length_error(msg);
+#else
+	(void)msg;
+	hamon::abort();
+#endif
+}
+
+}	// namespace detail
 
 }	// namespace hamon
 
