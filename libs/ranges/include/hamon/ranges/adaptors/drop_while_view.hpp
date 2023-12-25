4dd15042de30c4c06513b8513e5193fa45c483f9
shibainuudon <shibainuudon@gmail.com>
2023-12-25 17:43:06 +0900

    [Ranges]drop_while_viewを追加 refs #215

---
+++ libs/ranges/include/hamon/ranges/adaptors/drop_while_view.hpp
@@ -0,0 +1,240 @@
+﻿/**
+ *	@file	drop_while_view.hpp
+ *
+ *	@brief	drop_while_view の定義
+ */
+
+#ifndef HAMON_RANGES_ADAPTORS_DROP_WHILE_VIEW_HPP
+#define HAMON_RANGES_ADAPTORS_DROP_WHILE_VIEW_HPP
+
+#include <hamon/ranges/config.hpp>
+
+#if defined(HAMON_USE_STD_RANGES)
+
+namespace hamon {
+namespace ranges {
+
+using std::ranges::drop_while_view;
+
+namespace views {
+
+using std::ranges::views::drop_while;
+
+}	// namespace views
+
+}	// namespace ranges
+}	// namespace hamon
+
+#else
+
+#include <hamon/ranges/adaptors/all.hpp>
+#include <hamon/ranges/adaptors/detail/cached_value.hpp>
+#include <hamon/ranges/adaptors/detail/range_adaptor.hpp>
+#include <hamon/ranges/concepts/view.hpp>
+#include <hamon/ranges/concepts/input_range.hpp>
+#include <hamon/ranges/concepts/forward_range.hpp>
+#include <hamon/ranges/concepts/viewable_range.hpp>
+#include <hamon/ranges/concepts/enable_borrowed_range.hpp>
+#include <hamon/ranges/detail/movable_box.hpp>
+#include <hamon/ranges/utility/view_interface.hpp>
+#include <hamon/ranges/iterator_t.hpp>
+#include <hamon/ranges/sentinel_t.hpp>
+#include <hamon/ranges/end.hpp>
+#include <hamon/algorithm/ranges/find_if_not.hpp>
+#include <hamon/concepts/default_initializable.hpp>
+#include <hamon/concepts/copy_constructible.hpp>
+#include <hamon/concepts/constructible_from.hpp>
+#include <hamon/concepts/detail/constrained_param.hpp>
+#include <hamon/functional/bind_back.hpp>
+#include <hamon/functional/cref.hpp>
+#include <hamon/iterator/concepts/indirect_unary_predicate.hpp>
+#include <hamon/type_traits/decay.hpp>
+#include <hamon/type_traits/enable_if.hpp>
+#include <hamon/type_traits/is_object.hpp>
+#include <hamon/type_traits/is_nothrow_copy_constructible.hpp>
+#include <hamon/type_traits/is_nothrow_default_constructible.hpp>
+#include <hamon/type_traits/is_nothrow_move_constructible.hpp>
+#include <hamon/utility/declval.hpp>
+#include <hamon/utility/forward.hpp>
+#include <hamon/utility/move.hpp>
+#include <hamon/config.hpp>
+
+namespace hamon {
+namespace ranges {
+
+// 26.7.13.2 Class template drop_while_view[range.drop.while.view]
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+template <hamon::ranges::view V, typename Pred>
+	requires hamon::ranges::input_range<V> && hamon::is_object_v<Pred> &&
+		hamon::indirect_unary_predicate<Pred const, hamon::ranges::iterator_t<V>>
+#else
+template <typename V, typename Pred,
+	typename = hamon::enable_if_t<hamon::conjunction<
+		hamon::ranges::view_t<V>,
+		hamon::ranges::input_range_t<V>,
+		hamon::is_object<Pred>,
+		hamon::indirect_unary_predicate_t<Pred const, hamon::ranges::iterator_t<V>>
+	>::value>
+>
+#endif
+class drop_while_view : public hamon::ranges::view_interface<drop_while_view<V, Pred>>
+{
+public:
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+	drop_while_view()
+		requires hamon::default_initializable<V> && hamon::default_initializable<Pred>
+	= default;
+#else
+	template <
+		HAMON_CONSTRAINED_PARAM_D(hamon::default_initializable, V2, V),
+		HAMON_CONSTRAINED_PARAM_D(hamon::default_initializable, Pred2, Pred)>
+	HAMON_CXX11_CONSTEXPR
+	drop_while_view() HAMON_NOEXCEPT_IF(
+		hamon::is_nothrow_default_constructible<V2>::value &&
+		hamon::is_nothrow_default_constructible<Pred2>::value)
+	{}
+#endif
+
+	HAMON_CXX11_CONSTEXPR explicit
+	drop_while_view(V base, Pred pred)
+		HAMON_NOEXCEPT_IF(			// noexcept as an extension
+			hamon::is_nothrow_move_constructible<V>::value &&
+			hamon::is_nothrow_move_constructible<Pred>::value)
+		// [range.drop.while.view]/1
+		: m_base(hamon::move(base))
+		, m_pred(hamon::move(pred))
+	{}
+
+	template <HAMON_CONSTRAINED_PARAM_D(hamon::copy_constructible, V2, V)>
+	HAMON_NODISCARD HAMON_CXX11_CONSTEXPR	// nodiscard as an extension
+	V base() const& HAMON_NOEXCEPT_IF(		// noexcept as an extension
+		hamon::is_nothrow_copy_constructible<V>::value)
+	{
+		return m_base;
+	}
+	
+	HAMON_NODISCARD HAMON_CXX14_CONSTEXPR	// nodiscard as an extension
+	V base() && HAMON_NOEXCEPT_IF(			// noexcept as an extension
+		hamon::is_nothrow_move_constructible<V>::value)
+	{
+		return hamon::move(m_base);
+	}
+
+	HAMON_NODISCARD HAMON_CXX11_CONSTEXPR	// nodiscard as an extension
+	Pred const& pred() const HAMON_NOEXCEPT	// noexcept as an extension
+	{
+		// [range.drop.while.view]/2
+		return *m_pred;
+	}
+
+	HAMON_NODISCARD HAMON_CXX14_CONSTEXPR	// nodiscard as an extension
+	auto begin()
+	-> hamon::ranges::iterator_t<V>
+	{
+		// [range.drop.while.view]/5
+		if (m_cache.has_value())
+		{
+			return m_cache.value();
+		}
+
+		// [range.drop.while.view]/3
+		HAMON_ASSERT(m_pred.has_value());
+
+		// [range.drop.while.view]/4
+		auto it = hamon::ranges::find_if_not(m_base, hamon::cref(*m_pred));
+		m_cache = it;
+
+		return it;
+	}
+
+	HAMON_NODISCARD HAMON_CXX14_CONSTEXPR	// nodiscard as an extension
+	auto end() HAMON_NOEXCEPT_IF_EXPR(		// noexcept as an extension
+		hamon::ranges::end(hamon::declval<V&>()))
+	-> hamon::ranges::sentinel_t<V>
+	{
+		return hamon::ranges::end(m_base);
+	}
+
+private:
+	using cache_type =
+		hamon::ranges::detail::cached_value<
+			hamon::ranges::forward_range_t<V>::value,
+			hamon::ranges::iterator_t<V>
+		>;
+
+	HAMON_NO_UNIQUE_ADDRESS cache_type m_cache;
+	HAMON_NO_UNIQUE_ADDRESS V m_base = V();
+	hamon::ranges::detail::movable_box<Pred> m_pred;
+};
+
+#if defined(HAMON_HAS_CXX17_DEDUCTION_GUIDES)
+
+template <typename R, typename Pred>
+drop_while_view(R&&, Pred) -> drop_while_view<views::all_t<R>, Pred>;
+
+#endif
+
+#define HAMON_NOEXCEPT_DECLTYPE_RETURN(...) \
+	HAMON_NOEXCEPT_IF_EXPR(__VA_ARGS__)     \
+	-> decltype(__VA_ARGS__)                \
+	{ return __VA_ARGS__; }
+
+// extension: deduction guides が使えないときのために追加
+template <typename R, typename Pred>
+HAMON_NODISCARD HAMON_CXX11_CONSTEXPR auto
+make_drop_while_view(R&& r, Pred pred)
+	HAMON_NOEXCEPT_DECLTYPE_RETURN(
+		drop_while_view<hamon::views::all_t<R>, Pred>(hamon::forward<R>(r), hamon::move(pred)))
+
+// enable_borrowed_range の特殊化
+template <typename T, typename Pred>
+HAMON_RANGES_SPECIALIZE_ENABLE_BORROWED_RANGE(
+	HAMON_RANGES_ENABLE_BORROWED_RANGE(T),
+	hamon::ranges::drop_while_view<T, Pred>);
+
+namespace views {
+
+namespace detail {
+
+struct drop_while_fn
+{
+	template <HAMON_CONSTRAINED_PARAM(hamon::ranges::viewable_range, R), typename Pred>
+	HAMON_NODISCARD HAMON_CXX11_CONSTEXPR auto
+	operator()(R&& r, Pred pred) const
+	HAMON_NOEXCEPT_DECLTYPE_RETURN(
+		// [range.drop.while.overview]/2
+		hamon::ranges::make_drop_while_view(
+			hamon::forward<R>(r), hamon::move(pred)))
+
+	template <typename Pred,
+		typename = hamon::enable_if_t<
+			hamon::constructible_from_t<hamon::decay_t<Pred>, Pred
+		>::value>>
+	HAMON_NODISCARD HAMON_CXX11_CONSTEXPR auto
+	operator()(Pred&& pred) const
+	HAMON_NOEXCEPT_DECLTYPE_RETURN(
+		hamon::ranges::detail::make_range_adaptor(
+			hamon::bind_back(*this, hamon::forward<Pred>(pred))))
+};
+
+} // namespace detail
+
+inline namespace cpo
+{
+
+HAMON_INLINE_VAR HAMON_CONSTEXPR
+detail::drop_while_fn drop_while{};
+
+}	// inline namespace cpo
+
+}	// namespace views
+
+#undef HAMON_NOEXCEPT_DECLTYPE_RETURN
+
+}	// namespace ranges
+}	// namespace hamon
+
+#endif
+
+#endif // HAMON_RANGES_ADAPTORS_DROP_WHILE_VIEW_HPP
---
+++ libs/ranges/test/src/adaptors/unit_test_ranges_drop_while_view.cpp
@@ -0,0 +1,356 @@
+﻿/**
+ *	@file	unit_test_ranges_drop_while_view.cpp
+ *
+ *	@brief	ranges::drop_while_view のテスト
+ */
+
+#include <hamon/ranges/adaptors/drop_while_view.hpp>
+#include <hamon/ranges/factories/iota_view.hpp>
+#include <hamon/ranges/concepts.hpp>
+#include <hamon/ranges/view_base.hpp>
+#include <hamon/ranges/iterator_t.hpp>
+#include <hamon/ranges/sentinel_t.hpp>
+#include <hamon/concepts.hpp>
+#include <hamon/iterator.hpp>
+#include <hamon/string_view.hpp>
+#include <hamon/type_traits.hpp>
+#include <hamon/config.hpp>
+#include <gtest/gtest.h>
+#include <sstream>
+#include "constexpr_test.hpp"
+#include "ranges_test.hpp"
+
+namespace hamon_ranges_test
+{
+namespace drop_while_view_test
+{
+
+struct LessThanFive
+{
+	HAMON_CXX11_CONSTEXPR bool operator()(int x) const HAMON_NOEXCEPT
+	{
+		return x < 5;
+	}
+};
+
+struct NoDefaultFunc
+{
+	HAMON_CXX11_CONSTEXPR bool operator()(int x) const HAMON_NOEXCEPT
+	{
+		return x < 5;
+	}
+
+	NoDefaultFunc() = delete;
+};
+static_assert(!hamon::is_default_constructible<NoDefaultFunc>::value, "");
+
+template <typename T>
+struct MoveOnlyView : hamon::ranges::view_base
+{
+	MoveOnlyView(MoveOnlyView&&) = default;
+	MoveOnlyView& operator=(MoveOnlyView&&) = default;
+
+	HAMON_CXX11_CONSTEXPR T* begin() const noexcept { return nullptr; }
+	HAMON_CXX11_CONSTEXPR T* end()   const noexcept { return nullptr; }
+};
+static_assert(hamon::ranges::view_t<MoveOnlyView<int>>::value, "");
+static_assert(!hamon::copyable_t<MoveOnlyView<int>>::value, "");
+
+template <typename T, typename Pred, typename = void>
+struct CanInstantiateDropWhileView
+	: hamon::false_type {};
+
+template <typename T, typename Pred>
+struct CanInstantiateDropWhileView<T, Pred, hamon::void_t<hamon::ranges::drop_while_view<T, Pred>>>
+	: hamon::true_type {};
+
+static_assert( CanInstantiateDropWhileView<test_input_view<int>, LessThanFive>::value, "");
+static_assert(!CanInstantiateDropWhileView<test_input_range<int>, LessThanFive>::value, "");	// view<V>
+static_assert(!CanInstantiateDropWhileView<test_output_view<int>, LessThanFive>::value, "");	// input_range<V>
+static_assert(!CanInstantiateDropWhileView<test_input_view<int>, LessThanFive&>::value, "");	// is_object_v<Pred>
+static_assert(!CanInstantiateDropWhileView<test_input_view<int*>, LessThanFive>::value, "");	// indirect_unary_predicate<const Pred, iterator_t<V>>
+
+template <typename T, typename = void>
+struct has_base
+	: public hamon::false_type {};
+
+template <typename T>
+struct has_base<T, hamon::void_t<decltype(hamon::declval<T>().base())>>
+	: public hamon::true_type {};
+
+template <typename T, typename = void>
+struct has_begin
+	: public hamon::false_type {};
+
+template <typename T>
+struct has_begin<T, hamon::void_t<decltype(hamon::declval<T>().begin())>>
+	: public hamon::true_type {};
+
+template <typename T, typename = void>
+struct has_end
+	: public hamon::false_type {};
+
+template <typename T>
+struct has_end<T, hamon::void_t<decltype(hamon::declval<T>().end())>>
+	: public hamon::true_type {};
+
+#define VERIFY(...)	if (!(__VA_ARGS__)) { return false; }
+
+template <template <typename> class View, typename Pred>
+HAMON_CXX14_CONSTEXPR bool test00_impl()
+{
+	using T = int;
+	using V = View<T>;
+	using RV = hamon::ranges::drop_while_view<V, Pred>;
+	using I = hamon::ranges::iterator_t<RV>;
+	using S = hamon::ranges::sentinel_t<RV>;
+
+	static_assert(hamon::ranges::range_t<RV>::value == true, "");
+	static_assert(hamon::ranges::borrowed_range_t<RV>::value == hamon::ranges::borrowed_range_t<V>::value, "");
+	static_assert(hamon::ranges::sized_range_t<RV>::value == hamon::sized_sentinel_for_t<S, I>::value, "");
+	static_assert(hamon::ranges::output_range_t<RV, T>::value == hamon::ranges::output_range_t<V, T>::value, "");
+	static_assert(hamon::ranges::input_range_t<RV>::value == hamon::ranges::input_range_t<V>::value, "");
+	static_assert(hamon::ranges::forward_range_t<RV>::value == hamon::ranges::forward_range_t<V>::value, "");
+	static_assert(hamon::ranges::bidirectional_range_t<RV>::value == hamon::ranges::bidirectional_range_t<V>::value, "");
+	static_assert(hamon::ranges::random_access_range_t<RV>::value == hamon::ranges::random_access_range_t<V>::value, "");
+	static_assert(hamon::ranges::contiguous_range_t<RV>::value == hamon::ranges::contiguous_range_t<V>::value, "");
+	static_assert(hamon::ranges::common_range_t<RV>::value == hamon::ranges::common_range_t<V>::value, "");
+	static_assert(hamon::ranges::viewable_range_t<RV>::value == true, "");
+	static_assert(hamon::ranges::view_t<RV>::value == true, "");
+
+	static_assert(hamon::is_default_constructible<RV>::value ==
+		(hamon::is_default_constructible<V>::value && hamon::is_default_constructible<Pred>::value), "");
+	static_assert(hamon::is_nothrow_default_constructible<RV>::value ==
+		(hamon::is_nothrow_default_constructible<V>::value && hamon::is_nothrow_default_constructible<Pred>::value), "");
+
+	static_assert(!hamon::is_constructible<RV, V>::value, "");
+	static_assert(!hamon::is_constructible<RV, Pred>::value, "");
+	static_assert( hamon::is_constructible<RV, V, Pred>::value, "");
+	static_assert(!hamon::is_constructible<RV, Pred, V>::value, "");
+	static_assert(!hamon::is_constructible<RV, V, Pred, Pred>::value, "");
+
+	static_assert(has_base<RV&>::value == hamon::copy_constructible_t<V>::value, "");
+	static_assert(has_base<RV&&>::value, "");
+	static_assert(has_base<RV const&>::value == hamon::copy_constructible_t<V>::value, "");
+	static_assert(has_base<RV const&&>::value == hamon::copy_constructible_t<V>::value, "");
+
+	static_assert(hamon::same_as_t<decltype(hamon::declval<RV&&>().base()), V>::value, "");
+#if defined(HAMON_HAS_CXX17_IF_CONSTEXPR)
+	if constexpr (hamon::copy_constructible_t<V>::value)
+	{
+		static_assert(hamon::same_as_t<decltype(hamon::declval<RV&>().base()), V>::value, "");
+		static_assert(hamon::same_as_t<decltype(hamon::declval<RV const&>().base()), V>::value, "");
+		static_assert(hamon::same_as_t<decltype(hamon::declval<RV const&&>().base()), V>::value, "");
+	}
+#endif
+
+	static_assert(hamon::same_as_t<decltype(hamon::declval<RV const>().pred()), Pred const&>::value, "");
+
+	static_assert( has_begin<RV>::value, "");
+	static_assert(!has_begin<RV const>::value, "");
+	static_assert(hamon::same_as_t<decltype(hamon::declval<RV>().begin()), I>::value, "");
+
+	static_assert( has_end<RV>::value, "");
+	static_assert(!has_end<RV const>::value, "");
+	static_assert(hamon::same_as_t<decltype(hamon::declval<RV>().end()), S>::value, "");
+
+	return true;
+}
+
+template <template <typename> class View>
+HAMON_CXX14_CONSTEXPR bool test00()
+{
+	VERIFY(test00_impl<View, LessThanFive>());
+	VERIFY(test00_impl<View, NoDefaultFunc>());
+	return true;
+}
+
+HAMON_CXX14_CONSTEXPR bool test01()
+{
+	using V = test_random_access_view<int>;
+	hamon::ranges::drop_while_view<V, LessThanFive> rv{};
+
+	VERIFY(rv.begin() == rv.end());
+	VERIFY(rv.begin() == rv.base().begin());
+
+	{
+		auto& p = rv.pred();
+		static_assert(hamon::same_as_t<decltype(p), LessThanFive const&>::value, "");
+	}
+	{
+		auto b = rv.base();
+		static_assert(hamon::same_as_t<decltype(b), V>::value, "");
+		VERIFY(b.begin() == b.end());
+	}
+	{
+		auto b = hamon::move(rv).base();
+		static_assert(hamon::same_as_t<decltype(b), V>::value, "");
+		VERIFY(b.begin() == b.end());
+	}
+
+	return true;
+}
+
+HAMON_CXX14_CONSTEXPR bool test02()
+{
+	int a[] = {1, 2, 3, 4, 5, 6, 7, 8};
+
+	using V = test_input_view<int>;
+	V v(a);
+
+#if defined(HAMON_HAS_CXX17_DEDUCTION_GUIDES)
+	hamon::ranges::drop_while_view rv{v, LessThanFive{}};
+	static_assert(hamon::same_as_t<decltype(rv), hamon::ranges::drop_while_view<V, LessThanFive>>::value, "");
+#else
+	hamon::ranges::drop_while_view<V, LessThanFive> rv{v, LessThanFive{}};
+#endif
+
+	{
+		auto it = rv.begin();
+		VERIFY(it != rv.end());
+		VERIFY(*it == 5);
+		++it;
+		VERIFY(it != rv.end());
+		VERIFY(*it == 6);
+		++it;
+		VERIFY(it != rv.end());
+		VERIFY(*it == 7);
+		++it;
+		VERIFY(it != rv.end());
+		VERIFY(*it == 8);
+		++it;
+		VERIFY(it == rv.end());
+	}
+	{
+		auto b = rv.base();
+		static_assert(hamon::same_as_t<decltype(b), V>::value, "");
+		VERIFY(rv.begin() != b.begin());
+	}
+	{
+		auto b = hamon::move(rv).base();
+		static_assert(hamon::same_as_t<decltype(b), V>::value, "");
+		VERIFY(rv.begin() != b.begin());
+	}
+
+	return true;
+}
+
+HAMON_CXX14_CONSTEXPR bool test03()
+{
+	int a[] = {10, 1, 2};
+
+	using V = test_input_view<int>;
+	V v(a);
+
+#if defined(HAMON_HAS_CXX17_DEDUCTION_GUIDES)
+	hamon::ranges::drop_while_view rv{v, LessThanFive{}};
+	static_assert(hamon::same_as_t<decltype(rv), hamon::ranges::drop_while_view<V, LessThanFive>>::value, "");
+#else
+	hamon::ranges::drop_while_view<V, LessThanFive> rv{v, LessThanFive{}};
+#endif
+
+	{
+		auto it = rv.begin();
+		VERIFY(it != rv.end());
+		VERIFY(*it == 10);
+		++it;
+		VERIFY(it != rv.end());
+		VERIFY(*it == 1);
+		++it;
+		VERIFY(it != rv.end());
+		VERIFY(*it == 2);
+		++it;
+		VERIFY(it == rv.end());
+	}
+	{
+		auto b = rv.base();
+		static_assert(hamon::same_as_t<decltype(b), V>::value, "");
+		VERIFY(rv.begin() == b.begin());
+	}
+	{
+		auto b = hamon::move(rv).base();
+		static_assert(hamon::same_as_t<decltype(b), V>::value, "");
+		VERIFY(rv.begin() == b.begin());
+	}
+
+	return true;
+}
+
+HAMON_CXX17_CONSTEXPR bool test04()
+{
+	{
+		int a[] = { 3, 1, 4, 1, 5, 9 };
+		auto rv = a | hamon::views::drop_while([](int x) { return x % 2 != 0; });
+		auto it = rv.begin();
+		VERIFY(it[0] == 4);
+		VERIFY(it[1] == 1);
+		VERIFY(it[2] == 5);
+		VERIFY(it[3] == 9);
+	}
+	{
+		auto rv = hamon::views::iota(1, 10) | hamon::views::drop_while([](int x) { return x < 5; });
+		auto it = rv.begin();
+		VERIFY(*it++ == 5);
+		VERIFY(*it++ == 6);
+		VERIFY(*it++ == 7);
+		VERIFY(*it++ == 8);
+		VERIFY(*it++ == 9);
+		VERIFY(it == rv.end());
+	}
+	return true;
+}
+
+#undef VERIFY
+
+GTEST_TEST(RangesTest, DropWhileViewTest)
+{
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_input_view>());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_forward_view>());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_bidirectional_view>());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_random_access_view>());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_contiguous_view>());
+
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_input_common_view>());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_forward_common_view>());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_bidirectional_common_view>());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_random_access_common_view>());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_contiguous_common_view>());
+
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_input_borrowed_view>());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_forward_borrowed_view>());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_bidirectional_borrowed_view>());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_random_access_borrowed_view>());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_contiguous_borrowed_view>());
+
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_input_sized_view>());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_forward_sized_view>());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_bidirectional_sized_view>());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_random_access_sized_view>());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<test_contiguous_sized_view>());
+
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test00<MoveOnlyView>());
+
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test01());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test02());
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(test03());
+	HAMON_CXX17_CONSTEXPR_EXPECT_TRUE(test04());
+
+#if defined(HAMON_HAS_CXX14_GENERIC_LAMBDAS)
+	// [range.drop.while.overview]/3
+	// Example 1:
+	{
+		std::stringstream ss;
+		constexpr auto source = hamon::string_view{"  \t   \t   \t   hello there"};
+		auto is_invisible = [](const auto x) { return x == ' ' || x == '\t'; };
+		auto skip_ws = hamon::views::drop_while(source, is_invisible);
+		for (auto c : skip_ws)
+		{
+			ss << c;                                    // prints hello there with no leading space
+		}
+		EXPECT_EQ("hello there", ss.str());
+	}
+#endif
+}
+
+}	// namespace drop_while_view_test
+}	// namespace hamon_ranges_test
