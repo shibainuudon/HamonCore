7294958a143738362f8198917d6333fc8fcb0e74
shibainuudon <shibainuudon@gmail.com>
2024-02-24 16:29:55 +0900

    [Config]msvc.hppを更新
    * Visual Studio 2022 Version 17.9.1

--- libs/config/include/hamon/config/compiler/msvc.hpp
+++ libs/config/include/hamon/config/compiler/msvc.hpp
@@ -979,6 +979,10 @@
 #if (_MSC_FULL_VER >= 193933519)
 #endif
 
+// Visual Studio 2022 Version 17.9.1
+#if (_MSC_FULL_VER >= 193933520)
+#endif
+
 #if (HAMON_CXX_STANDARD >= 17)
 #  if defined(__cpp_constexpr) && (__cpp_constexpr >= 201603)
 #    define HAMON_HAS_CXX17_CONSTEXPR
