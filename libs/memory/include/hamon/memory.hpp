c5209fc5134360629c2ea74bedb1979cf08a1cca
shibainuudon <shibainuudon@gmail.com>
2024-05-08 11:04:43 +0900

    [Memory]uninitialized_moveを追加

--- libs/memory/include/hamon/memory.hpp
+++ libs/memory/include/hamon/memory.hpp
@@ -8,17 +8,43 @@
 #define HAMON_MEMORY_HPP
 
 #include <hamon/memory/addressof.hpp>
+//#include <hamon/memory/align.hpp>
+//#include <hamon/memory/allocate_shared.hpp>
+//#include <hamon/memory/allocate_shared_for_overwrite.hpp>
+//#include <hamon/memory/allocation_result.hpp>
+//#include <hamon/memory/allocator.hpp>
 #include <hamon/memory/allocator_arg_t.hpp>
+//#include <hamon/memory/allocator_traits.hpp>
+//#include <hamon/memory/assume_aligned.hpp>
+//#include <hamon/memory/bad_weak_ptr.hpp>
 #include <hamon/memory/construct_at.hpp>
+//#include <hamon/memory/const_pointer_cast.hpp>
+//#include <hamon/memory/default_delete.hpp>
 #include <hamon/memory/destroy.hpp>
 #include <hamon/memory/destroy_at.hpp>
 //#include <hamon/memory/destroy_n.hpp>
+//#include <hamon/memory/dynamic_pointer_cast.hpp>
+//#include <hamon/memory/enable_shared_from_this.hpp>
+//#include <hamon/memory/get_deleter.hpp>
+//#include <hamon/memory/inout_ptr.hpp>
+//#include <hamon/memory/inout_ptr_t.hpp>
+#include <hamon/memory/make_obj_using_allocator.hpp>
 //#include <hamon/memory/make_shared.hpp>
+//#include <hamon/memory/make_shared_for_overwrite.hpp>
 #include <hamon/memory/make_unique.hpp>
 #include <hamon/memory/make_unique_for_overwrite.hpp>
+//#include <hamon/memory/out_ptr.hpp>
+//#include <hamon/memory/out_ptr_t.hpp>
+//#include <hamon/memory/owner_less.hpp>
+//#include <hamon/memory/owner_hash.hpp>
+//#include <hamon/memory/owner_equal.hpp>
 //#include <hamon/memory/pointer_traits.hpp>
+//#include <hamon/memory/ranges.hpp>
+//#include <hamon/memory/reinterpret_pointer_cast.hpp>
 //#include <hamon/memory/shared_ptr.hpp>
-#include <hamon/memory/make_obj_using_allocator.hpp>
+//#include <hamon/memory/start_lifetime_as.hpp>
+//#include <hamon/memory/start_lifetime_as_array.hpp>
+//#include <hamon/memory/static_pointer_cast.hpp>
 #include <hamon/memory/to_address.hpp>
 #include <hamon/memory/uninitialized_construct_using_allocator.hpp>
 #include <hamon/memory/uninitialized_copy.hpp>
@@ -27,11 +53,24 @@
 //#include <hamon/memory/uninitialized_default_construct_n.hpp>
 #include <hamon/memory/uninitialized_fill.hpp>
 #include <hamon/memory/uninitialized_fill_n.hpp>
-//#include <hamon/memory/uninitialized_move.hpp>
+#include <hamon/memory/uninitialized_move.hpp>
 //#include <hamon/memory/uninitialized_move_n.hpp>
 //#include <hamon/memory/uninitialized_value_construct.hpp>
 //#include <hamon/memory/uninitialized_value_construct_n.hpp>
 //#include <hamon/memory/unique_ptr.hpp>
+//#include <hamon/memory/uses_allocator.hpp>
 #include <hamon/memory/uses_allocator_construction_args.hpp>
+//#include <hamon/memory/weak_ptr.hpp>
+
+//#include <hamon/memory/auto_ptr.hpp>				// (removed in C++17)
+//#include <hamon/memory/raw_storage_iterator.hpp>	// (removed in C++20)
+//#include <hamon/memory/get_temporary_buffer.hpp>	// (removed in C++20)
+//#include <hamon/memory/return_temporary_buffer.hpp>	// (removed in C++20)
+//#include <hamon/memory/pointer_safety.hpp>		// (removed in C++23)
+//#include <hamon/memory/declare_reachable.hpp>		// (removed in C++23)
+//#include <hamon/memory/undeclare_reachable.hpp>	// (removed in C++23)
+//#include <hamon/memory/declare_no_pointers.hpp>	// (removed in C++23)
+//#include <hamon/memory/undeclare_no_pointers.hpp>	// (removed in C++23)
+//#include <hamon/memory/get_pointer_safety.hpp>	// (removed in C++23)
 
 #endif // HAMON_MEMORY_HPP
---
+++ libs/memory/include/hamon/memory/uninitialized_move.hpp
@@ -0,0 +1,97 @@
+﻿/**
+ *	@file	uninitialized_move.hpp
+ *
+ *	@brief	uninitialized_move を定義
+ */
+
+#ifndef HAMON_MEMORY_UNINITIALIZED_MOVE_HPP
+#define HAMON_MEMORY_UNINITIALIZED_MOVE_HPP
+
+#include <memory>
+
+#if defined(__cpp_lib_raw_memory_algorithms) && (__cpp_lib_raw_memory_algorithms >= 201606)
+
+namespace hamon
+{
+
+using std::uninitialized_move;
+
+}	// namespace hamon
+
+#else
+
+#include <hamon/memory/addressof.hpp>
+#include <hamon/memory/construct_at.hpp>
+#include <hamon/memory/destroy.hpp>
+#include <hamon/algorithm/move.hpp>
+#include <hamon/detail/overload_priority.hpp>
+#include <hamon/iterator/iter_rvalue_reference_t.hpp>
+#include <hamon/iterator/iter_reference_t.hpp>
+#include <hamon/iterator/iter_value_t.hpp>
+#include <hamon/type_traits/enable_if.hpp>
+#include <hamon/type_traits/is_trivially_assignable.hpp>
+#include <hamon/type_traits/is_trivially_constructible.hpp>
+#include <hamon/utility/move.hpp>
+
+namespace hamon
+{
+
+namespace detail
+{
+
+template <typename InputIterator, typename ForwardIterator,
+	typename SrcType = hamon::iter_rvalue_reference_t<InputIterator>,
+	typename RefType = hamon::iter_reference_t<ForwardIterator>,
+	typename ValueType = hamon::iter_value_t<ForwardIterator>,
+	typename = hamon::enable_if_t<
+		hamon::is_trivially_assignable<RefType, SrcType>::value &&
+		hamon::is_trivially_constructible<ValueType, SrcType>::value
+	>
+>
+inline ForwardIterator
+uninitialized_move_impl(
+	InputIterator first, InputIterator last, ForwardIterator result,
+	hamon::detail::overload_priority<1>)
+{
+	return hamon::move(first, last, result);
+}
+
+template <typename InputIterator, typename ForwardIterator>
+inline ForwardIterator
+uninitialized_move_impl(
+	InputIterator first, InputIterator last, ForwardIterator result,
+	hamon::detail::overload_priority<0>)
+{
+	ForwardIterator current = result;
+	try
+	{
+		for (; first != last; ++first)
+		{
+			hamon::construct_at(hamon::addressof(*current), hamon::move(*first));
+			++current;
+		}
+		return current;
+	}
+	catch (...)
+	{
+		hamon::destroy(result, current);
+		throw;
+	}
+}
+
+}	// namespace detail
+
+template <typename InputIterator, typename ForwardIterator>
+inline ForwardIterator
+uninitialized_move(InputIterator first, InputIterator last, ForwardIterator result)
+{
+	return hamon::detail::uninitialized_move_impl(
+		first, last, result,
+		hamon::detail::overload_priority<1>{});
+}
+
+}	// namespace hamon
+
+#endif
+
+#endif // HAMON_MEMORY_UNINITIALIZED_MOVE_HPP
