ceb88a4acaae01ebea9b730ee2929b4f89169cae
shibainuudon <shibainuudon@gmail.com>
2024-05-08 10:04:58 +0900

    [Memory]uninitialized_fillを追加

---
+++ libs/memory/include/hamon/memory/uninitialized_fill.hpp
@@ -0,0 +1,92 @@
+﻿/**
+ *	@file	uninitialized_fill.hpp
+ *
+ *	@brief	uninitialized_fill を定義
+ */
+
+#ifndef HAMON_MEMORY_UNINITIALIZED_FILL_HPP
+#define HAMON_MEMORY_UNINITIALIZED_FILL_HPP
+
+#if 0
+
+#include <memory>
+
+namespace hamon
+{
+
+using std::uninitialized_fill;
+
+}	// namespace hamon
+
+#else
+
+#include <hamon/memory/construct_at.hpp>
+#include <hamon/memory/addressof.hpp>
+#include <hamon/memory/destroy.hpp>
+#include <hamon/detail/overload_priority.hpp>
+#include <hamon/iterator/iter_value_t.hpp>
+#include <hamon/iterator/iter_reference_t.hpp>
+#include <hamon/type_traits/enable_if.hpp>
+#include <hamon/type_traits/is_trivially_assignable.hpp>
+#include <hamon/type_traits/is_trivially_constructible.hpp>
+#include <hamon/algorithm/fill.hpp>
+
+namespace hamon
+{
+
+namespace detail
+{
+
+template <typename ForwardIterator, typename T,
+	typename RefType = hamon::iter_reference_t<ForwardIterator>,
+	typename ValueType = hamon::iter_value_t<ForwardIterator>,
+	typename = hamon::enable_if_t<
+		hamon::is_trivially_assignable<RefType, T const&>::value &&
+		hamon::is_trivially_constructible<ValueType, T const&>::value
+	>
+>
+inline void
+uninitialized_fill_impl(
+	ForwardIterator first, ForwardIterator last, T const& x,
+	hamon::detail::overload_priority<1>)
+{
+	hamon::fill(first, last, x);
+}
+
+template <typename ForwardIterator, typename T>
+inline void
+uninitialized_fill_impl(
+	ForwardIterator first, ForwardIterator last, T const& x,
+	hamon::detail::overload_priority<0>)
+{
+	ForwardIterator current = first;
+	try
+	{
+		for (; current != last; ++current)
+		{
+			hamon::construct_at(hamon::addressof(*current), x);
+		}
+	}
+	catch(...)
+	{
+		hamon::destroy(first, current);
+		throw;
+	}
+}
+
+}	// namespace detail
+
+template <typename ForwardIterator, typename T>
+inline void
+uninitialized_fill(ForwardIterator first, ForwardIterator last, T const& x)
+{
+	return hamon::detail::uninitialized_fill_impl(
+		first, last, x,
+		hamon::detail::overload_priority<1>{});
+}
+
+}	// namespace hamon
+
+#endif
+
+#endif // HAMON_MEMORY_UNINITIALIZED_FILL_HPP
---
+++ libs/memory/test/src/unit_test_memory_uninitialized_fill.cpp
@@ -0,0 +1,157 @@
+﻿/**
+ *	@file	unit_test_memory_uninitialized_fill.cpp
+ *
+ *	@brief	uninitialized_fill のテスト
+ */
+
+#include <hamon/memory/uninitialized_fill.hpp>
+#include <hamon/memory/destroy.hpp>
+#include <hamon/type_traits/is_trivial.hpp>
+#include <gtest/gtest.h>
+
+namespace hamon_memory_test
+{
+
+namespace uninitialized_fill_test
+{
+
+struct X
+{
+};
+
+static_assert(hamon::is_trivial<X>::value, "X is trivial");
+
+static int constructed = 0;
+static int assigned = 0;
+
+struct Y
+{
+	Y() = default;
+	Y(Y const&) = default;
+	Y& operator=(Y const&) = default;
+
+	Y(X const&) { ++constructed; }
+	Y& operator=(X const&)& { ++assigned; return *this; }
+	Y& operator=(X const&) && = delete;
+	Y& operator=(X&&) = delete;
+};
+
+static_assert(hamon::is_trivial<Y>::value, "Y is trivial");
+
+struct Z
+{
+	int value;
+	Z() = delete;
+	Z(int v) : value(v){}
+	Z(Z const& z) : value(z.value) {}
+};
+
+struct ThrowOnCopy
+{
+	int value;
+
+	static int count;
+	static int copy_count;
+
+	ThrowOnCopy() = delete;
+
+	ThrowOnCopy(int v)
+		: value(v)
+	{
+		count++;
+	}
+
+	ThrowOnCopy(ThrowOnCopy const& x)
+		: value(x.value)
+	{
+		if (copy_count >= 5)
+		{
+			throw 0;
+		}
+		count++;
+		copy_count++;
+	}
+
+	~ThrowOnCopy()
+	{
+		count--;
+		copy_count--;
+	}
+};
+
+int ThrowOnCopy::count = 0;
+int ThrowOnCopy::copy_count = 0;
+
+GTEST_TEST(MemoryTest, UninitializedFillTest)
+{
+	{
+		int a[32];
+		hamon::uninitialized_fill(a, a + 4, 42);
+		EXPECT_EQ(42, a[0]);
+		EXPECT_EQ(42, a[1]);
+		EXPECT_EQ(42, a[2]);
+		EXPECT_EQ(42, a[3]);
+	}
+	{
+		Y b[100];
+
+		hamon::uninitialized_fill(b, b + 10, X{});
+
+		EXPECT_EQ(10, constructed);
+		EXPECT_EQ( 0, assigned);
+	}
+	{
+		std::allocator<Z> alloc;
+
+		// メモリ確保。
+		// この段階では、[p, p + size)の領域は未初期化
+		const std::size_t size = 3;
+		Z* p = alloc.allocate(size);
+
+		// 未初期化領域pを初期化しつつ、値42で埋める
+		hamon::uninitialized_fill(p, p + size, 42);
+
+		// pの領域が初期化され、かつvからpに要素がコピーされているか確認
+		EXPECT_EQ(42, p[0].value);
+		EXPECT_EQ(42, p[1].value);
+		EXPECT_EQ(42, p[2].value);
+
+		// 要素を破棄
+		hamon::destroy(p, p+size);
+
+		// メモリ解放
+		alloc.deallocate(p, size);
+	}
+	{
+		std::allocator<ThrowOnCopy> alloc;
+
+		const std::size_t size = 3;
+		auto* p = alloc.allocate(size);
+
+		const ThrowOnCopy x{42};
+
+		EXPECT_EQ(1, ThrowOnCopy::count);
+		EXPECT_EQ(0, ThrowOnCopy::copy_count);
+
+		hamon::uninitialized_fill(p, p + size, x);
+
+		EXPECT_EQ(4, ThrowOnCopy::count);
+		EXPECT_EQ(3, ThrowOnCopy::copy_count);
+
+		EXPECT_ANY_THROW(hamon::uninitialized_fill(p, p + size, x));
+
+		EXPECT_EQ(4, ThrowOnCopy::count);
+		EXPECT_EQ(3, ThrowOnCopy::copy_count);
+
+		hamon::destroy(p, p+size);
+		alloc.deallocate(p, size);
+
+		EXPECT_EQ(1, ThrowOnCopy::count);
+		EXPECT_EQ(0, ThrowOnCopy::copy_count);
+	}
+	EXPECT_EQ(0, ThrowOnCopy::count);
+}
+
+}	// namespace uninitialized_fill_test
+
+}	// namespace hamon_memory_test
