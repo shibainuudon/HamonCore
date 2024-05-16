124ce4a9fe71c2f63a5e44036fc74f91a4611ee6
shibainuudon <shibainuudon@gmail.com>
2024-05-16 14:24:05 +0900

    [Memory]uninitialized_value_construct_n_implのconstexpr対応

--- libs/memory/include/hamon/memory/uninitialized_value_construct_n.hpp
+++ libs/memory/include/hamon/memory/uninitialized_value_construct_n.hpp
@@ -20,75 +20,16 @@ using std::uninitialized_value_construct_n;
 
 #else
 
-#include <hamon/memory/construct_at.hpp>
-#include <hamon/memory/addressof.hpp>
-#include <hamon/memory/destroy.hpp>
-#include <hamon/algorithm/fill_n.hpp>
-#include <hamon/detail/overload_priority.hpp>
-#include <hamon/iterator/iter_value_t.hpp>
-#include <hamon/type_traits/enable_if.hpp>
-#include <hamon/type_traits/is_trivial.hpp>
-#include <hamon/type_traits/is_copy_assignable.hpp>
+#include <hamon/memory/detail/uninitialized_value_construct_n_impl.hpp>
 
 namespace hamon
 {
 
-namespace detail
-{
-
-template <typename ForwardIterator, typename Size,
-	typename ValueType = hamon::iter_value_t<ForwardIterator>,
-	typename = hamon::enable_if_t<
-		hamon::is_trivial<ValueType>::value &&
-		hamon::is_copy_assignable<ValueType>::value
-	>
->
-inline ForwardIterator
-uninitialized_value_construct_n_impl(
-	ForwardIterator first, Size n,
-	hamon::detail::overload_priority<1>)
-{
-	if (n > 0)
-	{
-		auto* val = hamon::addressof(*first);
-		hamon::construct_at(val);
-		++first;
-		first = hamon::fill_n(first, n - 1, *val);
-	}
-	return first;
-}
-
 template <typename ForwardIterator, typename Size>
 inline ForwardIterator
-uninitialized_value_construct_n_impl(
-	ForwardIterator first, Size n,
-	hamon::detail::overload_priority<0>)
+uninitialized_value_construct_n(ForwardIterator first, Size n)
 {
-	ForwardIterator current = first;
-	try
-	{
-		for (; n > 0; --n, (void)++current)
-		{
-			hamon::construct_at(hamon::addressof(*current));
-		}
-		return current;
-	}
-	catch(...)
-	{
-		hamon::destroy(first, current);
-		throw;
-	}
-}
-
-}	// namespace detail
-
-template <typename ForwardIterator, typename Size>
-inline ForwardIterator
-uninitialized_value_construct_n(ForwardIterator first, Size count)
-{
-	return hamon::detail::uninitialized_value_construct_n_impl(
-		first, count,
-		hamon::detail::overload_priority<1>{});
+	return hamon::detail::uninitialized_value_construct_n_impl(first, n);
 }
 
 }	// namespace hamon
