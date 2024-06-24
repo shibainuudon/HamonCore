f245d9ddb0e36db1913fddcba6a15d8f653a1e9a
shibainuudon <shibainuudon@gmail.com>
2024-06-24 17:21:55 +0900

    [Chrono]is_clockを追加

---
+++ libs/chrono/include/hamon/chrono/is_clock.hpp
@@ -0,0 +1,78 @@
+﻿/**
+ *	@file	is_clock.hpp
+ *
+ *	@brief	is_clock の定義
+ */
+
+#ifndef HAMON_CHRONO_IS_CLOCK_HPP
+#define HAMON_CHRONO_IS_CLOCK_HPP
+
+#include <hamon/chrono/config.hpp>
+
+#if defined(HAMON_USE_STD_CHRONO)
+
+#include <chrono>
+
+namespace hamon {
+namespace chrono {
+
+using std::chrono::is_clock;
+using std::chrono::is_clock_v;
+
+}	// namespace chrono
+}	// namespace hamon
+
+#else
+
+#include <hamon/type_traits/bool_constant.hpp>
+#include <hamon/config.hpp>
+
+namespace hamon {
+namespace chrono {
+
+namespace detail
+{
+
+template <typename T>
+struct is_clock_impl
+{
+private:
+	template <typename U,
+		typename = typename U::rep,			// [time.traits.is.clock]/1.1
+		typename = typename U::period,		// [time.traits.is.clock]/1.1
+		typename = typename U::duration,	// [time.traits.is.clock]/1.1
+		typename = typename U::time_point,	// [time.traits.is.clock]/1.1
+		typename = decltype(U::is_steady),	// [time.traits.is.clock]/1.2
+		typename = decltype(U::now())		// [time.traits.is.clock]/1.3
+	>
+	static auto test(int) -> hamon::true_type;
+
+	template <typename U>
+	static auto test(...) -> hamon::false_type;
+
+public:
+	using type = decltype(test<T>(0));
+};
+
+}
+
+// 29.4.4 Class template is_clock[time.traits.is.clock]
+
+template <typename T>
+struct is_clock
+	: public hamon::chrono::detail::is_clock_impl<T>::type
+{};
+
+#if defined(HAMON_HAS_CXX14_VARIABLE_TEMPLATES)
+
+template <typename T>
+constexpr bool is_clock_v = is_clock<T>::value;
+
+#endif
+
+}	// namespace chrono
+}	// namespace hamon
+
+#endif
+
+#endif // HAMON_CHRONO_IS_CLOCK_HPP
---
+++ libs/chrono/test/src/unit_test_chrono_is_clock.cpp
@@ -0,0 +1,123 @@
+﻿/**
+ *	@file	unit_test_chrono_is_clock.cpp
+ *
+ *	@brief	is_clock のテスト
+ */
+
+#include <hamon/chrono/is_clock.hpp>
+#include <hamon/chrono/duration.hpp>
+#include <hamon/chrono/time_point.hpp>
+#include <hamon/config.hpp>
+
+#if defined(HAMON_HAS_CXX14_VARIABLE_TEMPLATES)
+
+#define HAMON_IS_CLOCK_TEST(b, T)	\
+	static_assert(hamon::chrono::is_clock_v<T>      == b, #T);	\
+	static_assert(hamon::chrono::is_clock<T>::value == b, #T);	\
+	static_assert(hamon::chrono::is_clock<T>{}()    == b, #T);	\
+	static_assert(hamon::chrono::is_clock<T>{}      == b, #T)
+
+#else
+
+#define HAMON_IS_CLOCK_TEST(b, T)	\
+	static_assert(hamon::chrono::is_clock<T>::value == b, #T);	\
+	static_assert(hamon::chrono::is_clock<T>{}()    == b, #T);	\
+	static_assert(hamon::chrono::is_clock<T>{}      == b, #T)
+
+#endif
+
+namespace hamon_chrono_test
+{
+
+struct Clock1
+{
+	using rep = int;
+	using period = std::ratio<1>;
+	using duration = hamon::chrono::duration<rep, period>;
+	using time_point = hamon::chrono::time_point<Clock1, duration>;
+	static constexpr bool is_steady = false;
+	static constexpr int now() { return 0; }
+};
+
+struct Clock2
+{
+	using rep = void;
+	using period = void;
+	using duration = void;
+	using time_point = void;
+	static constexpr int is_steady = 0;
+	static constexpr int now() { return 0; }
+};
+
+struct Clock3
+{
+//	using rep = void;
+	using period = void;
+	using duration = void;
+	using time_point = void;
+	static constexpr int is_steady = 0;
+	static constexpr int now() { return 0; }
+};
+
+struct Clock4
+{
+	using rep = void;
+//	using period = void;
+	using duration = void;
+	using time_point = void;
+	static constexpr int is_steady = 0;
+	static constexpr int now() { return 0; }
+};
+
+struct Clock5
+{
+	using rep = void;
+	using period = void;
+//	using duration = void;
+	using time_point = void;
+	static constexpr int is_steady = 0;
+	static constexpr int now() { return 0; }
+};
+
+struct Clock6
+{
+	using rep = void;
+	using period = void;
+	using duration = void;
+//	using time_point = void;
+	static constexpr int is_steady = 0;
+	static constexpr int now() { return 0; }
+};
+
+struct Clock7
+{
+	using rep = void;
+	using period = void;
+	using duration = void;
+	using time_point = void;
+//	static constexpr int is_steady = 0;
+	static constexpr int now() { return 0; }
+};
+
+struct Clock8
+{
+	using rep = void;
+	using period = void;
+	using duration = void;
+	using time_point = void;
+	static constexpr int is_steady = 0;
+//	static constexpr int now() { return 0; }
+};
+
+HAMON_IS_CLOCK_TEST(true,  Clock1);
+HAMON_IS_CLOCK_TEST(true,  Clock2);
+HAMON_IS_CLOCK_TEST(false, Clock3);
+HAMON_IS_CLOCK_TEST(false, Clock4);
+HAMON_IS_CLOCK_TEST(false, Clock5);
+HAMON_IS_CLOCK_TEST(false, Clock6);
+HAMON_IS_CLOCK_TEST(false, Clock7);
+HAMON_IS_CLOCK_TEST(false, Clock8);
+
+}	// namespace hamon_chrono_test
+
+#undef HAMON_IS_CLOCK_TEST
