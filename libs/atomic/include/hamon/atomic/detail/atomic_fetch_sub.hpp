6a14563dee602816e0aa6389acff65c4a6580ab2
shibainuudon <shibainuudon@gmail.com>
2024-09-24 15:04:27 +0900

    [Atomic]atomic_fetch_sub_implを追加

---
+++ libs/atomic/include/hamon/atomic/detail/atomic_fetch_sub.hpp
@@ -0,0 +1,34 @@
+﻿/**
+ *	@file	atomic_fetch_sub.hpp
+ *
+ *	@brief	atomic_fetch_sub の実装
+ */
+
+#ifndef HAMON_ATOMIC_DETAIL_ATOMIC_FETCH_SUB_HPP
+#define HAMON_ATOMIC_DETAIL_ATOMIC_FETCH_SUB_HPP
+
+#include <hamon/config.hpp>
+#if defined(HAMON_MSVC)
+#include <intrin.h>
+#endif
+
+namespace hamon
+{
+
+namespace detail
+{
+
+long atomic_fetch_sub(long* x, long a)
+{
+#if defined(HAMON_MSVC)
+	return _InterlockedExchangeAdd(x, -a);
+#else
+	return __atomic_fetch_sub(x, a, __ATOMIC_SEQ_CST);
+#endif
+}
+
+}	// namespace detail
+
+}	// namespace hamon
+
+#endif // HAMON_ATOMIC_DETAIL_ATOMIC_FETCH_SUB_HPP
---
+++ libs/atomic/test/src/detail/unit_test_atomic_detail_atomic_fetch_sub.cpp
@@ -0,0 +1,39 @@
+﻿/**
+ *	@file	unit_test_atomic_detail_atomic_fetch_sub.cpp
+ *
+ *	@brief	atomic_fetch_sub のテスト
+ */
+
+#include <hamon/atomic/detail/atomic_fetch_sub.hpp>
+#include <gtest/gtest.h>
+#include <thread>
+
+GTEST_TEST(Atomic, AtomicFetchSubTest)
+{
+	{
+		long x = 3;
+		long before = hamon::detail::atomic_fetch_sub(&x, 2);
+		EXPECT_EQ(3, before);
+		EXPECT_EQ(1, x);
+	}
+	{
+		long x = 0;
+		std::thread t1{[&x]{
+			for (int i = 0; i < 10000; ++i)
+			{
+				// x -= 2;
+				hamon::detail::atomic_fetch_sub(&x, 2);
+			}
+		}};
+		std::thread t2{[&x]{
+			for (int i = 0; i < 10000; ++i)
+			{
+				// x -= 3;
+				hamon::detail::atomic_fetch_sub(&x, 3);
+			}
+		}};
+		t1.join();
+		t2.join();
+		EXPECT_EQ(-50000, x);
+	}
+}
