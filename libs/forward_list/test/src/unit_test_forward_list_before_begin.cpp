d7742de0398557fd0513f28ffc5e307aa08b6829
shibainuudon <shibainuudon@gmail.com>
2024-10-09 12:53:54 +0900

    wip forward_list

--- libs/forward_list/test/src/unit_test_forward_list_before_begin.cpp
+++ libs/forward_list/test/src/unit_test_forward_list_before_begin.cpp
@@ -20,27 +20,107 @@ namespace hamon_forward_list_test
 namespace before_begin_test
 {
 
-struct S1
-{
-	S1() = delete;
-};
-
-struct S2
-{
-	S2() {}
-};
-
-struct S3
-{
-	int x;
-	float y;
-};
-
 #define VERIFY(...)	if (!(__VA_ARGS__)) { return false; }
 
 template <typename T>
 HAMON_CXX20_CONSTEXPR bool test()
 {
+	using ForwardList = hamon::forward_list<T>;
+	using Iterator = typename ForwardList::iterator;
+	using ConstIterator = typename ForwardList::const_iterator;
+
+	static_assert(hamon::is_same<decltype(hamon::declval<ForwardList&>().before_begin()), Iterator>::value, "");
+	static_assert(hamon::is_same<decltype(hamon::declval<ForwardList const&>().before_begin()), ConstIterator>::value, "");
+	static_assert(hamon::is_same<decltype(hamon::declval<ForwardList&>().cbefore_begin()), ConstIterator>::value, "");
+	static_assert(hamon::is_same<decltype(hamon::declval<ForwardList const&>().cbefore_begin()), ConstIterator>::value, "");
+
+	static_assert(noexcept(hamon::declval<ForwardList&>().before_begin()), "");
+	static_assert(noexcept(hamon::declval<ForwardList const&>().before_begin()), "");
+	static_assert(noexcept(hamon::declval<ForwardList&>().cbefore_begin()), "");
+	static_assert(noexcept(hamon::declval<ForwardList const&>().cbefore_begin()), "");
+
+	{
+		ForwardList v;
+		auto it = v.before_begin();
+		VERIFY(it != v.begin());
+		VERIFY(it != v.end());
+		++it;
+		VERIFY(it == v.begin());
+		VERIFY(it == v.end());
+	}
+	{
+		ForwardList const v;
+		auto it = v.before_begin();
+		VERIFY(it != v.begin());
+		VERIFY(it != v.end());
+		++it;
+		VERIFY(it == v.begin());
+		VERIFY(it == v.end());
+	}
+	{
+		ForwardList v;
+		auto it = v.cbefore_begin();
+		VERIFY(it != v.cbegin());
+		VERIFY(it != v.cend());
+		++it;
+		VERIFY(it == v.cbegin());
+		VERIFY(it == v.cend());
+	}
+	{
+		ForwardList v{1,2,3};
+		auto it = v.before_begin();
+		VERIFY(it != v.begin());
+		VERIFY(it != v.end());
+		++it;
+		VERIFY(it == v.begin());
+		VERIFY(it != v.end());
+		++it;
+		VERIFY(it != v.begin());
+		VERIFY(it != v.end());
+		++it;
+		VERIFY(it != v.begin());
+		VERIFY(it != v.end());
+		++it;
+		VERIFY(it != v.begin());
+		VERIFY(it == v.end());
+	}
+	{
+		ForwardList const v{1,2,3};
+		auto it = v.before_begin();
+		VERIFY(it != v.begin());
+		VERIFY(it != v.end());
+		++it;
+		VERIFY(it == v.begin());
+		VERIFY(it != v.end());
+		++it;
+		VERIFY(it != v.begin());
+		VERIFY(it != v.end());
+		++it;
+		VERIFY(it != v.begin());
+		VERIFY(it != v.end());
+		++it;
+		VERIFY(it != v.begin());
+		VERIFY(it == v.end());
+	}
+	{
+		ForwardList v{1,2,3};
+		auto it = v.cbefore_begin();
+		VERIFY(it != v.cbegin());
+		VERIFY(it != v.cend());
+		++it;
+		VERIFY(it == v.cbegin());
+		VERIFY(it != v.cend());
+		++it;
+		VERIFY(it != v.cbegin());
+		VERIFY(it != v.cend());
+		++it;
+		VERIFY(it != v.cbegin());
+		VERIFY(it != v.cend());
+		++it;
+		VERIFY(it != v.cbegin());
+		VERIFY(it == v.cend());
+	}
+
 	return true;
 }
 
@@ -48,12 +128,9 @@ HAMON_CXX20_CONSTEXPR bool test()
 
 GTEST_TEST(ForwardListTest, BeforeBeginTest)
 {
-	/*HAMON_CXX20_CONSTEXPR_*/EXPECT_TRUE(test<int>());
-	/*HAMON_CXX20_CONSTEXPR_*/EXPECT_TRUE(test<char>());
-	/*HAMON_CXX20_CONSTEXPR_*/EXPECT_TRUE(test<float>());
-	/*HAMON_CXX20_CONSTEXPR_*/EXPECT_TRUE(test<S1>());
-	/*HAMON_CXX20_CONSTEXPR_*/EXPECT_TRUE(test<S2>());
-	/*HAMON_CXX20_CONSTEXPR_*/EXPECT_TRUE(test<S3>());
+	HAMON_CXX20_CONSTEXPR_EXPECT_TRUE(test<int>());
+	HAMON_CXX20_CONSTEXPR_EXPECT_TRUE(test<char>());
+	HAMON_CXX20_CONSTEXPR_EXPECT_TRUE(test<float>());
 }
 
 }	// namespace before_begin_test
