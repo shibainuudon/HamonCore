7cda80c44bcf3ab720afad60fc876166ce52aa4c
shibainuudon <shibainuudon@gmail.com>
2025-02-17 17:26:01 +0900

    [CMath]laguerre を追加

---
+++ libs/cmath/include/hamon/cmath/laguerre.hpp
@@ -0,0 +1,105 @@
+﻿/**
+ *	@file	laguerre.hpp
+ *
+ *	@brief	laguerre 関数の定義
+ */
+
+#ifndef HAMON_CMATH_LAGUERRE_HPP
+#define HAMON_CMATH_LAGUERRE_HPP
+
+#include <hamon/cmath/isnan.hpp>
+#include <hamon/cmath/assoc_laguerre.hpp>
+#include <hamon/concepts/floating_point.hpp>
+#include <hamon/concepts/integral.hpp>
+#include <hamon/concepts/detail/constrained_param.hpp>
+#include <hamon/type_traits/float_promote.hpp>
+#include <hamon/limits.hpp>
+#include <hamon/utility/swap.hpp>
+#include <hamon/config.hpp>
+#include <cmath>
+
+namespace hamon
+{
+
+namespace detail
+{
+
+template <typename T>
+HAMON_CXX14_CONSTEXPR T
+laguerre_unchecked(unsigned int n, T x)
+{
+#if defined(HAMON_HAS_CXX20_IS_CONSTANT_EVALUATED)
+#if defined(__cpp_lib_math_special_functions) && (__cpp_lib_math_special_functions >= 201603)
+	if (!hamon::is_constant_evaluated())
+	{
+HAMON_WARNING_PUSH()
+HAMON_WARNING_DISABLE_MSVC(4244)
+		return std::laguerre(n, x);
+HAMON_WARNING_POP()
+	}
+#endif
+#endif
+
+	return hamon::detail::assoc_laguerre_unchecked(n, 0, x);
+}
+
+template <typename T>
+HAMON_CXX14_CONSTEXPR T
+laguerre_impl(unsigned int n, T x)
+{
+	if (hamon::isnan(x))
+	{
+		return hamon::numeric_limits<T>::quiet_NaN();
+	}
+
+	if (x < T(0))
+	{
+		return hamon::numeric_limits<T>::quiet_NaN();
+	}
+
+	return laguerre_unchecked(n, x);
+}
+
+}	// namespace detail
+
+/**
+ *	@brief	ラゲール多項式 (Laguerre polynomials) を計算する。
+ *
+ *	@param	n
+ *	@param	x
+ *
+ *	@return	引数 n, x のラゲール多項式を返す
+ *
+ *	引数 x が NaN のとき、NaN を返す
+ *	x が負の値のとき、NaN を返す(std::laguerre と違い、例外を投げない)
+ *	n >= 128 のとき、振る舞いは実装定義
+ */
+template <HAMON_CONSTRAINED_PARAM(hamon::floating_point, FloatType)>
+HAMON_NODISCARD HAMON_CXX14_CONSTEXPR FloatType
+laguerre(unsigned int n, FloatType x) HAMON_NOEXCEPT
+{
+	return detail::laguerre_impl(n, x);
+}
+
+template <HAMON_CONSTRAINED_PARAM(hamon::integral, IntegralType)>
+HAMON_NODISCARD HAMON_CXX14_CONSTEXPR double
+laguerre(unsigned int n, IntegralType x) HAMON_NOEXCEPT
+{
+	return detail::laguerre_impl(n, static_cast<double>(x));
+}
+
+HAMON_NODISCARD inline HAMON_CXX14_CONSTEXPR float
+laguerref(unsigned int n, float x) HAMON_NOEXCEPT
+{
+	return detail::laguerre_impl(n, x);
+}
+
+HAMON_NODISCARD inline HAMON_CXX14_CONSTEXPR long double
+laguerrel(unsigned int n, long double x) HAMON_NOEXCEPT
+{
+	return detail::laguerre_impl(n, x);
+}
+
+}	// namespace hamon
+
+#endif // HAMON_CMATH_LAGUERRE_HPP
---
+++ libs/cmath/test/src/unit_test_cmath_laguerre.cpp
@@ -0,0 +1,194 @@
+﻿/**
+ *	@file	unit_test_cmath_laguerre.cpp
+ *
+ *	@brief	laguerre のテスト
+ */
+
+#include <hamon/cmath/laguerre.hpp>
+#include <hamon/cmath/isnan.hpp>
+#include <hamon/cmath/abs.hpp>
+#include <hamon/type_traits/is_same.hpp>
+#include <gtest/gtest.h>
+#include <limits>
+#include "constexpr_test.hpp"
+
+namespace hamon_cmath_test
+{
+
+namespace laguerre_test
+{
+
+static_assert(hamon::is_same<float,       decltype(hamon::laguerre(0u, 0.0f))>::value, "");
+static_assert(hamon::is_same<float,       decltype(hamon::laguerref(0u, 0.0f))>::value, "");
+static_assert(hamon::is_same<double,      decltype(hamon::laguerre(0u, 0.0))>::value, "");
+static_assert(hamon::is_same<long double, decltype(hamon::laguerre(0u, 0.0l))>::value, "");
+static_assert(hamon::is_same<long double, decltype(hamon::laguerrel(0u, 0.0l))>::value, "");
+static_assert(hamon::is_same<double,      decltype(hamon::laguerre(0u, 0))>::value, "");
+
+template <typename T>
+void LaguerreTest(double error)
+{
+	using Promoted = hamon::float_promote_t<T>;
+	HAMON_CXX11_CONSTEXPR auto nan = std::numeric_limits<Promoted>::quiet_NaN();
+	HAMON_CXX11_CONSTEXPR auto inf = std::numeric_limits<Promoted>::infinity();
+	HAMON_CXX11_CONSTEXPR auto min = std::numeric_limits<Promoted>::lowest();
+	HAMON_CXX11_CONSTEXPR auto max = std::numeric_limits<Promoted>::max();
+
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(hamon::isnan(hamon::laguerre(0, nan)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(hamon::isnan(hamon::laguerre(1, nan)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(hamon::isnan(hamon::laguerre(0, T(-1))));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(hamon::isnan(hamon::laguerre(1, T(-1))));
+
+	HAMON_CXX14_CONSTEXPR_EXPECT_EQ(T(  1.0), hamon::laguerre( 0, T(0)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_EQ(T( -9.0), hamon::laguerre( 1, T(10)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_EQ(T(161.0), hamon::laguerre( 2, T(20)));
+
+	struct testcase
+	{
+		double expected;
+		unsigned int n;
+		T x;
+	};
+
+	static testcase const testcases[] =
+	{
+		{ 1.0000000000000000, 0,   0},
+		{ 1.0000000000000000, 0,  10},
+		{ 1.0000000000000000, 0,  20},
+		{ 1.0000000000000000, 0,  30},
+		{ 1.0000000000000000, 0,  40},
+		{ 1.0000000000000000, 0,  50},
+		{ 1.0000000000000000, 0,  60},
+		{ 1.0000000000000000, 0,  70},
+		{ 1.0000000000000000, 0,  80},
+		{ 1.0000000000000000, 0,  90},
+		{ 1.0000000000000000, 0, 100},
+
+		{ 1.0000000000000000, 1,   0},
+		{-9.0000000000000000, 1,  10},
+		{-19.000000000000000, 1,  20},
+		{-29.000000000000000, 1,  30},
+		{-39.000000000000000, 1,  40},
+		{-49.000000000000000, 1,  50},
+		{-59.000000000000000, 1,  60},
+		{-69.000000000000000, 1,  70},
+		{-79.000000000000000, 1,  80},
+		{-89.000000000000000, 1,  90},
+		{-99.000000000000000, 1, 100},
+
+		{ 1.0000000000000000, 2,   0},
+		{ 31.000000000000000, 2,  10},
+		{ 161.00000000000000, 2,  20},
+		{ 391.00000000000000, 2,  30},
+		{ 721.00000000000000, 2,  40},
+		{ 1151.0000000000000, 2,  50},
+		{ 1681.0000000000000, 2,  60},
+		{ 2311.0000000000000, 2,  70},
+		{ 3041.0000000000000, 2,  80},
+		{ 3871.0000000000000, 2,  90},
+		{ 4801.0000000000000, 2, 100},
+
+		{ 1.0000000000000000, 5,   0},
+		{ 34.333333333333333, 5,  10},
+		{-4765.6666666666666, 5,  20},
+		{-74399.000000000000, 5,  30},
+		{-418865.66666666666, 5,  40},
+		{-1498165.6666666666, 5,  50},
+		{-4122299.0000000000, 5,  60},
+		{-9551265.6666666666, 5,  70},
+		{-19595065.666666666, 5,  80},
+		{-36713699.000000000, 5,  90},
+		{-64117165.666666666, 5, 100},
+
+		{ 1.0000000000000000, 10,   0},
+		{ 27.984126984126977, 10,  10},
+		{ 3227.8077601410932, 10,  20},
+		{ 15129.571428571455, 10,  30},
+		{ 79724066.608465582, 10,  40},
+		{ 2037190065.3738980, 10,  50},
+		{ 21804200401.000000, 10,  60},
+		{ 144688291819.51855, 10,  70},
+		{ 703324772760.08276, 10,  80},
+		{ 2741055412243.8569, 10,  90},
+		{ 9051283795429.5723, 10, 100},
+
+		{ 1.0000000000000000, 20,   0},
+		{-11.961333867812119, 20,  10},
+		{ 2829.4728613531743, 20,  20},
+		{-18439.424502520938, 20,  30},
+		{ 24799805.877530713, 20,  40},
+		{ 7551960453.7672548, 20,  50},
+		{-1379223608444.9155, 20,  60},
+		{ 165423821874449.94, 20,  70},
+		{ 29500368536981676., 20,  80},
+		{ 1.1292309514432901e+18, 20,  90},
+		{ 2.2061882785931735e+19, 20, 100},
+
+		{ 1.0000000000000000, 50,   0},
+		{ 17.534183446338233, 50,  10},
+		{ 980.26961889791028, 50,  20},
+		{ 293000.50735962362, 50,  30},
+		{-14896937.968694873, 50,  40},
+		{ 2513677852.6916871, 50,  50},
+		{-883876565337.99219, 50,  60},
+		{-80967880733583.234, 50,  70},
+		{-8217471769564841.0, 50,  80},
+		{-2.1140031308048891e+18, 50,  90},
+		{-3.9710103487094692e+20, 50, 100},
+
+		{ 1.0000000000000000, 100,   0},
+		{ 13.277662844303450, 100,  10},
+		{ 1854.0367283243388, 100,  20},
+		{ 170141.86987046551, 100,  30},
+		{-7272442.3156006960, 100,  40},
+		{ 4847420871.2690506, 100,  50},
+		{ 693492765740.29688, 100,  60},
+		{ 17125518672239.770, 100,  70},
+		{-13763178176383768., 100,  80},
+		{ 2.1307220490380173e+18, 100,  90},
+		{-2.6292260693068916e+20, 100, 100},
+	};
+
+	for (auto const& data : testcases)
+	{
+		auto const r = hamon::laguerre(data.n, data.x);
+
+		if (data.expected > max)
+		{
+			EXPECT_EQ(+inf, r);
+		}
+		else if (data.expected < min)
+		{
+			EXPECT_EQ(-inf, r);
+		}
+		else
+		{
+			double tole = hamon::abs(data.expected) * error;
+			EXPECT_NEAR(data.expected, (double)r, tole);
+		}
+	}
+}
+
+GTEST_TEST(CMathTest, LaguerreTest)
+{
+	LaguerreTest<float>      (0.0001);
+	LaguerreTest<double>     (0.0000000000001);
+	LaguerreTest<long double>(0.0000000000001);
+	LaguerreTest<int>        (0.0000000000001);
+
+	HAMON_CXX11_CONSTEXPR auto nanf = std::numeric_limits<float>::quiet_NaN();
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(hamon::isnan(hamon::laguerref(0, nanf)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(hamon::isnan(hamon::laguerref(1, nanf)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(hamon::isnan(hamon::laguerref(0, -1.0f)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(hamon::isnan(hamon::laguerref(1, -1.0f)));
+
+	HAMON_CXX11_CONSTEXPR auto nanl = std::numeric_limits<long double>::quiet_NaN();
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(hamon::isnan(hamon::laguerrel(0, nanl)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(hamon::isnan(hamon::laguerrel(1, nanl)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(hamon::isnan(hamon::laguerrel(0, -1.0l)));
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(hamon::isnan(hamon::laguerrel(1, -1.0l)));
+}
+
+}	// namespace laguerre_test
+
+}	// namespace hamon_cmath_test
