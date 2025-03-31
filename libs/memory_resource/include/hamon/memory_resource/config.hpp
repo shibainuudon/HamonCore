ec88be441eb35a36c8d55b4744d8a29020f3bc3f
shibainuudon <shibainuudon@gmail.com>
2025-03-31 09:17:16 +0900

    [MemoryResource]AppleClangではstd::polymorphic_allocatorを使わないようにした
    aligned_allocにおいて、POSIXの実装ではアラインメントが2の累乗およびsizeof(void*)の倍数である必要があるが、
    AppleClangのpolymorphic_allocatorではこの対策がされていないようなので。

--- libs/memory_resource/include/hamon/memory_resource/config.hpp
+++ libs/memory_resource/include/hamon/memory_resource/config.hpp
@@ -16,8 +16,14 @@
 
 #if defined(HAMON_HAS_STD_MEMORY_RESOURCE) && \
 	defined(__cpp_lib_memory_resource)       && (__cpp_lib_memory_resource       >= 201603L) &&	\
-	defined(__cpp_lib_polymorphic_allocator) && (__cpp_lib_polymorphic_allocator >= 201902L)
+	defined(__cpp_lib_polymorphic_allocator) && (__cpp_lib_polymorphic_allocator >= 201902L) && \
+	!defined(HAMON_APPLE_CLANG)	// AppleClangなら標準ライブラリを使わない
+// aligned_allocにおいて、POSIXの実装ではアラインメントが2の累乗およびsizeof(void*)の倍数である必要があるが、
+// AppleClangのpolymorphic_allocatorではこの対策がされていないようなので、
+// std::polymorphic_allocatorは使わない(常に独自実装を使う)
+
 #  define HAMON_USE_STD_MEMORY_RESOURCE
+
 #endif
 
 #endif // HAMON_MEMORY_RESOURCE_CONFIG_HPP
