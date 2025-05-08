c229fea1840613c32af728485d09f53cb66b8027
shibainuudon <shibainuudon@gmail.com>
2025-05-08 16:04:39 +0900

    [Container]cpp17_erasableを追加

---
+++ libs/container/include/hamon/container/detail/cpp17_erasable.hpp
@@ -0,0 +1,66 @@
+﻿/**
+ *	@file	cpp17_erasable.hpp
+ *
+ *	@brief	cpp17_erasable の定義
+ */
+
+#ifndef HAMON_CONTAINER_DETAIL_CPP17_ERASABLE_HPP
+#define HAMON_CONTAINER_DETAIL_CPP17_ERASABLE_HPP
+
+#include <hamon/memory/allocator_traits.hpp>
+#include <hamon/type_traits/bool_constant.hpp>
+#include <hamon/utility/declval.hpp>
+#include <hamon/utility/move.hpp>
+#include <hamon/config.hpp>
+
+namespace hamon {
+namespace detail {
+
+// [container.alloc.reqmts]/2.6
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+
+template <typename T, typename A>
+concept cpp17_erasable =
+	requires(A& m, T* p)
+	{
+		{ hamon::allocator_traits<A>::destroy(m, p) };
+	};
+
+#else
+
+template <typename T, typename A>
+struct cpp17_erasable_impl
+{
+private:
+	template <typename U, typename UA,
+		typename = decltype(hamon::allocator_traits<UA>::destroy(
+			hamon::declval<UA&>(), hamon::declval<U*>()))
+	>
+	static auto test(int) -> hamon::true_type;
+
+	template <typename U, typename UA>
+	static auto test(...) -> hamon::false_type;
+
+public:
+	using type = decltype(test<T, A>(0));
+};
+
+template <typename T, typename A>
+using cpp17_erasable =
+	typename cpp17_erasable_impl<T, A>::type;
+
+#endif
+
+template <typename T, typename A>
+using cpp17_erasable_t =
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+	hamon::bool_constant<hamon::detail::cpp17_erasable<T, A>>;
+#else
+	hamon::detail::cpp17_erasable<T, A>;
+#endif
+
+}	// namespace detail
+}	// namespace hamon
+
+#endif // HAMON_CONTAINER_DETAIL_CPP17_ERASABLE_HPP
---
+++ libs/container/test/src/detail/unit_test_container_detail_cpp17_erasable.cpp
@@ -0,0 +1,54 @@
+﻿/**
+ *	@file	unit_test_container_detail_cpp17_erasable.cpp
+ *
+ *	@brief	cpp17_erasable のテスト
+ */
+
+#include <hamon/container/detail/cpp17_erasable.hpp>
+#include <hamon/memory/allocator.hpp>
+
+namespace hamon_container_test
+{
+
+namespace cpp17_erasable_test
+{
+
+static_assert( hamon::detail::cpp17_erasable_t<int, hamon::allocator<int>>::value, "");
+static_assert( hamon::detail::cpp17_erasable_t<float, hamon::allocator<float>>::value, "");
+
+static_assert( hamon::detail::cpp17_erasable_t<int*, hamon::allocator<int>>::value, "");
+static_assert(!hamon::detail::cpp17_erasable_t<int&, hamon::allocator<int>>::value, "");
+static_assert(!hamon::detail::cpp17_erasable_t<int&&, hamon::allocator<int>>::value, "");
+
+struct S0
+{
+	S0(S0 const&) = delete;
+	S0(S0 &&) = delete;
+};
+
+struct S1
+{
+	S1(S1 const&);
+	S1(S1 &&) = delete;
+};
+
+struct S2
+{
+	S2(S2 const&) = delete;
+	S2(S2 &&);
+};
+
+struct S3
+{
+	S3(S3 const&);
+	S3(S3 &&);
+};
+
+static_assert( hamon::detail::cpp17_erasable_t<S0, hamon::allocator<S0>>::value, "");
+static_assert( hamon::detail::cpp17_erasable_t<S1, hamon::allocator<S1>>::value, "");
+static_assert( hamon::detail::cpp17_erasable_t<S2, hamon::allocator<S2>>::value, "");
+static_assert( hamon::detail::cpp17_erasable_t<S3, hamon::allocator<S3>>::value, "");
+
+}	// namespace cpp17_erasable_test
+
+}	// namespace hamon_container_test
