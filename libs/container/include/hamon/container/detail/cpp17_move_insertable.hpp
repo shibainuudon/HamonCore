50956622db49cf167f1573343024a89cb08fb3e4
shibainuudon <shibainuudon@gmail.com>
2025-05-08 15:36:00 +0900

    [Container]cpp17_move_insertableを追加

---
+++ libs/container/include/hamon/container/detail/cpp17_move_insertable.hpp
@@ -0,0 +1,66 @@
+﻿/**
+ *	@file	cpp17_move_insertable.hpp
+ *
+ *	@brief	cpp17_move_insertable の定義
+ */
+
+#ifndef HAMON_CONTAINER_DETAIL_CPP17_MOVE_INSERTABLE_HPP
+#define HAMON_CONTAINER_DETAIL_CPP17_MOVE_INSERTABLE_HPP
+
+#include <hamon/memory/allocator_traits.hpp>
+#include <hamon/type_traits/bool_constant.hpp>
+#include <hamon/utility/declval.hpp>
+#include <hamon/utility/move.hpp>
+#include <hamon/config.hpp>
+
+namespace hamon {
+namespace detail {
+
+// [container.alloc.reqmts]/2.3
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+
+template <typename T, typename A>
+concept cpp17_move_insertable =
+	requires(A& m, T* p, T&& rv)
+	{
+		{ hamon::allocator_traits<A>::construct(m, p, hamon::move(rv)) };
+	};
+
+#else
+
+template <typename T, typename A>
+struct cpp17_move_insertable_impl
+{
+private:
+	template <typename U, typename UA,
+		typename = decltype(hamon::allocator_traits<UA>::construct(
+			hamon::declval<UA&>(), hamon::declval<U*>(), hamon::declval<U&&>()))
+	>
+	static auto test(int) -> hamon::true_type;
+
+	template <typename U, typename UA>
+	static auto test(...) -> hamon::false_type;
+
+public:
+	using type = decltype(test<T, A>(0));
+};
+
+template <typename T, typename A>
+using cpp17_move_insertable =
+	typename cpp17_move_insertable_impl<T, A>::type;
+
+#endif
+
+template <typename T, typename A>
+using cpp17_move_insertable_t =
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+	hamon::bool_constant<hamon::detail::cpp17_move_insertable<T, A>>;
+#else
+	hamon::detail::cpp17_move_insertable<T, A>;
+#endif
+
+}	// namespace detail
+}	// namespace hamon
+
+#endif // HAMON_CONTAINER_DETAIL_CPP17_MOVE_INSERTABLE_HPP
---
+++ libs/container/test/src/detail/unit_test_container_detail_cpp17_move_insertable.cpp
@@ -0,0 +1,50 @@
+﻿/**
+ *	@file	unit_test_container_detail_cpp17_move_insertable.cpp
+ *
+ *	@brief	cpp17_move_insertable のテスト
+ */
+
+#include <hamon/container/detail/cpp17_move_insertable.hpp>
+#include <hamon/memory/allocator.hpp>
+
+namespace hamon_container_test
+{
+
+namespace cpp17_move_insertable_test
+{
+
+static_assert( hamon::detail::cpp17_move_insertable_t<int, hamon::allocator<int>>::value, "");
+static_assert( hamon::detail::cpp17_move_insertable_t<float, hamon::allocator<float>>::value, "");
+
+struct S0
+{
+	S0(S0 const&) = delete;
+	S0(S0 &&) = delete;
+};
+
+struct S1
+{
+	S1(S1 const&);
+	S1(S1 &&) = delete;
+};
+
+struct S2
+{
+	S2(S2 const&) = delete;
+	S2(S2 &&);
+};
+
+struct S3
+{
+	S3(S3 const&);
+	S3(S3 &&);
+};
+
+static_assert(!hamon::detail::cpp17_move_insertable_t<S0, hamon::allocator<S0>>::value, "");
+static_assert(!hamon::detail::cpp17_move_insertable_t<S1, hamon::allocator<S1>>::value, "");
+static_assert( hamon::detail::cpp17_move_insertable_t<S2, hamon::allocator<S2>>::value, "");
+static_assert( hamon::detail::cpp17_move_insertable_t<S3, hamon::allocator<S3>>::value, "");
+
+}	// namespace cpp17_move_insertable_test
+
+}	// namespace hamon_container_test
