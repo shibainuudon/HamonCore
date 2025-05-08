a6ce61adb7930ec606d4bdf160fd8424af367e73
shibainuudon <shibainuudon@gmail.com>
2025-05-08 14:30:27 +0900

    [Container]cpp17_emplace_constructibleを追加

---
+++ libs/container/include/hamon/container/detail/cpp17_emplace_constructible.hpp
@@ -0,0 +1,66 @@
+﻿/**
+ *	@file	cpp17_emplace_constructible.hpp
+ *
+ *	@brief	cpp17_emplace_constructible の定義
+ */
+
+#ifndef HAMON_CONTAINER_DETAIL_CPP17_EMPLACE_CONSTRUCTIBLE_HPP
+#define HAMON_CONTAINER_DETAIL_CPP17_EMPLACE_CONSTRUCTIBLE_HPP
+
+#include <hamon/memory/allocator_traits.hpp>
+#include <hamon/type_traits/bool_constant.hpp>
+#include <hamon/utility/declval.hpp>
+#include <hamon/utility/forward.hpp>
+#include <hamon/config.hpp>
+
+namespace hamon {
+namespace detail {
+
+// [container.alloc.reqmts]/2.5
+
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+
+template <typename T, typename A, typename... Args>
+concept cpp17_emplace_constructible =
+	requires(A& m, T* p, Args&&... args)
+	{
+		{ hamon::allocator_traits<A>::construct(m, p, hamon::forward<Args>(args)...) };
+	};
+
+#else
+
+template <typename T, typename A, typename... Args>
+struct cpp17_emplace_constructible_impl
+{
+private:
+	template <typename U, typename UA, typename... UArgs,
+		typename = decltype(hamon::allocator_traits<UA>::construct(
+			hamon::declval<UA&>(), hamon::declval<U*>(), hamon::declval<UArgs>()...))
+	>
+	static auto test(int) -> hamon::true_type;
+
+	template <typename U, typename UA, typename... UArgs>
+	static auto test(...) -> hamon::false_type;
+
+public:
+	using type = decltype(test<T, A, Args...>(0));
+};
+
+template <typename T, typename A, typename... Args>
+using cpp17_emplace_constructible =
+	typename cpp17_emplace_constructible_impl<T, A, Args...>::type;
+
+#endif
+
+template <typename T, typename A, typename... Args>
+using cpp17_emplace_constructible_t =
+#if defined(HAMON_HAS_CXX20_CONCEPTS)
+	hamon::bool_constant<hamon::detail::cpp17_emplace_constructible<T, A, Args...>>;
+#else
+	hamon::detail::cpp17_emplace_constructible<T, A, Args...>;
+#endif
+
+}	// namespace detail
+}	// namespace hamon
+
+#endif // HAMON_CONTAINER_DETAIL_CPP17_EMPLACE_CONSTRUCTIBLE_HPP
---
+++ libs/container/test/src/detail/unit_test_container_detail_cpp17_emplace_constructible.cpp
@@ -0,0 +1,68 @@
+﻿/**
+ *	@file	unit_test_container_detail_cpp17_emplace_constructible.cpp
+ *
+ *	@brief	cpp17_emplace_constructible のテスト
+ */
+
+#include <hamon/container/detail/cpp17_emplace_constructible.hpp>
+#include <hamon/memory/allocator.hpp>
+#include <hamon/pair.hpp>
+#include <hamon/tuple.hpp>
+
+namespace hamon_container_test
+{
+
+namespace cpp17_emplace_constructible_test
+{
+
+static_assert( hamon::detail::cpp17_emplace_constructible_t<int, hamon::allocator<int>>::value, "");
+static_assert( hamon::detail::cpp17_emplace_constructible_t<int, hamon::allocator<int>, int>::value, "");
+static_assert(!hamon::detail::cpp17_emplace_constructible_t<int, hamon::allocator<int>, int, int>::value, "");
+
+struct S1
+{
+	S1(int);
+};
+
+static_assert(!hamon::detail::cpp17_emplace_constructible_t<S1, hamon::allocator<S1>>::value, "");
+static_assert( hamon::detail::cpp17_emplace_constructible_t<S1, hamon::allocator<S1>, int>::value, "");
+static_assert(!hamon::detail::cpp17_emplace_constructible_t<S1, hamon::allocator<S1>, int, int>::value, "");
+
+struct S2
+{
+	S2(int, int);
+};
+
+static_assert(!hamon::detail::cpp17_emplace_constructible_t<S2, hamon::allocator<S2>>::value, "");
+static_assert(!hamon::detail::cpp17_emplace_constructible_t<S2, hamon::allocator<S2>, int>::value, "");
+static_assert( hamon::detail::cpp17_emplace_constructible_t<S2, hamon::allocator<S2>, int, int>::value, "");
+
+using P = hamon::pair<int, S2>;
+
+static_assert( hamon::detail::cpp17_emplace_constructible_t<P, hamon::allocator<P>,
+	hamon::piecewise_construct_t,
+	hamon::tuple<int&&>,
+	hamon::tuple<int&&, int&&>
+>::value, "");
+
+struct S3
+{
+	S3(S3 const&);
+	S3(S3 &&) = delete;
+};
+
+static_assert( hamon::detail::cpp17_emplace_constructible_t<S3, hamon::allocator<S3>, S3 const&>::value, "");
+static_assert(!hamon::detail::cpp17_emplace_constructible_t<S3, hamon::allocator<S3>, S3 &&>::value, "");
+
+struct S4
+{
+	S4(S4 const&) = delete;
+	S4(S4 &&);
+};
+
+static_assert(!hamon::detail::cpp17_emplace_constructible_t<S4, hamon::allocator<S4>, S4 const&>::value, "");
+static_assert( hamon::detail::cpp17_emplace_constructible_t<S4, hamon::allocator<S4>, S4 &&>::value, "");
+
+}	// namespace cpp17_emplace_constructible_test
+
+}	// namespace hamon_container_test
