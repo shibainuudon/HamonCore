3f328fc42579300748a9dc282855eb6583938087
shibainuudon <shibainuudon@gmail.com>
2025-06-24 22:09:33 +0900

    [Config]msvc.hppを更新
    * Visual Studio 2022 Preview Version 17.14.7 Preview 1.0
    * Visual Studio 2022 Version 17.14.7

--- libs/config/include/hamon/config/compiler/msvc.hpp
+++ libs/config/include/hamon/config/compiler/msvc.hpp
@@ -985,6 +985,10 @@
 #if (_MSC_FULL_VER >= 194435211)
 #endif
 
+// Visual Studio 2022 Preview Version 17.14.7 Preview 1.0
+#if (_MSC_FULL_VER >= 194435211)
+#endif
+
 // Visual Studio 2022 Version 17.0.(0-1)
 #if (_MSC_FULL_VER >= 193030705)
 # define HAMON_HAS_CXX23_CHARACTER_LITERALS_IN_PREPROCESSOR		// P2316R2	Consistent character literal encoding
@@ -1224,7 +1228,7 @@
 #if (_MSC_FULL_VER >= 194435209)
 #endif
 
-// Visual Studio 2022 Version 17.14.6
+// Visual Studio 2022 Version 17.14.(6-7)
 #if (_MSC_FULL_VER >= 194435211)
 #endif
 
