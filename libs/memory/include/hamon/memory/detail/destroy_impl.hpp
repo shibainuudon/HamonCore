c36f04eb113162b33c434542305711286ed47a6d
shibainuudon <shibainuudon@gmail.com>
2025-07-17 17:02:54 +0900

    [Memory]detail::destroy_implに、アロケータを引数に取るオーバーロードを追加

--- libs/memory/include/hamon/memory/detail/destroy_impl.hpp
+++ libs/memory/include/hamon/memory/detail/destroy_impl.hpp
@@ -7,8 +7,9 @@
 #ifndef HAMON_MEMORY_DETAIL_DESTROY_IMPL_HPP
 #define HAMON_MEMORY_DETAIL_DESTROY_IMPL_HPP
 
-#include <hamon/memory/destroy_at.hpp>
 #include <hamon/memory/addressof.hpp>
+#include <hamon/memory/allocator.hpp>
+#include <hamon/memory/allocator_traits.hpp>
 #include <hamon/iterator/iter_value_t.hpp>
 #include <hamon/iterator/ranges/advance.hpp>
 #include <hamon/detail/overload_priority.hpp>
@@ -22,35 +23,43 @@ namespace hamon
 namespace detail
 {
 
-template <typename Iterator, typename Sentinel,
+template <typename Allocator, typename Iterator, typename Sentinel,
 	typename ValueType = hamon::iter_value_t<Iterator>,
 	typename = hamon::enable_if_t<
 		hamon::is_trivially_destructible<ValueType>::value
 	>
 >
 HAMON_CXX14_CONSTEXPR Iterator
-destroy_impl2(Iterator first, Sentinel last, hamon::detail::overload_priority<1>)
+destroy_impl2(Allocator&, Iterator first, Sentinel last, hamon::detail::overload_priority<1>)
 {
 	hamon::ranges::advance(first, last);
 	return first;
 }
 
-template <typename Iterator, typename Sentinel>
+template <typename Allocator, typename Iterator, typename Sentinel>
 HAMON_CXX14_CONSTEXPR Iterator
-destroy_impl2(Iterator first, Sentinel last, hamon::detail::overload_priority<0>)
+destroy_impl2(Allocator& allocator, Iterator first, Sentinel last, hamon::detail::overload_priority<0>)
 {
 	for (; first != last; ++first)
 	{
-		hamon::destroy_at(hamon::addressof(*first));
+		hamon::allocator_traits<Allocator>::destroy(allocator, hamon::addressof(*first));
 	}
 	return first;
+}
+
+template <typename Allocator, typename Iterator, typename Sentinel>
+HAMON_CXX14_CONSTEXPR Iterator
+destroy_impl(Allocator& allocator, Iterator first, Sentinel last)
+{
+	return hamon::detail::destroy_impl2(allocator, first, last, hamon::detail::overload_priority<1>{});
 }
 
 template <typename Iterator, typename Sentinel>
 HAMON_CXX14_CONSTEXPR Iterator
 destroy_impl(Iterator first, Sentinel last)
 {
-	return hamon::detail::destroy_impl2(first, last, hamon::detail::overload_priority<1>{});
+	hamon::allocator<hamon::iter_value_t<Iterator>> alloc;
+	return hamon::detail::destroy_impl(alloc, first, last);
 }
 
 }	// namespace detail
