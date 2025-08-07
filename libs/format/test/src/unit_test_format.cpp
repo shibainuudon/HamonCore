5a17b6592e3e45271b6bc3974fe80ce6bb9bac31
shibainuudon <shibainuudon@gmail.com>
2025-08-07 14:55:06 +0900

    [Format]formatライブラリを追加

---
+++ libs/format/test/src/unit_test_format.cpp
@@ -0,0 +1,12 @@
+﻿/**
+ *	@file	unit_test_format.cpp
+ *
+ *	@brief	format のテスト
+ */
+
+#include <hamon/format.hpp>
+#include <gtest/gtest.h>
+
+GTEST_TEST(FormatTest, OverviewTest)
+{
+}
---
+++ libs/format/test/src/unit_test_format_format.cpp
@@ -0,0 +1,14 @@
+﻿/**
+ *	@file	unit_test_format_format.cpp
+ *
+ *	@brief	format のテスト
+ */
+
+#include <hamon/format/format.hpp>
+#include <gtest/gtest.h>
+
+GTEST_TEST(FormatTest, FormatTest)
+{
+	auto s = hamon::format("The answer is {}.", 42);
+	EXPECT_EQ("The answer is 42.", s);
+}
