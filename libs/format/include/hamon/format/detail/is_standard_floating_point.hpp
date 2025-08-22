d23daa9b29a393bc703264dd247ff9d0d5de23a8
shibainuudon <shibainuudon@gmail.com>
2025-08-22 16:32:17 +0900

    wip formattable_with

---
+++ libs/format/include/hamon/format/detail/is_standard_floating_point.hpp
@@ -0,0 +1,22 @@
+﻿/**
+ *	@file	is_standard_floating_point.hpp
+ *
+ *	@brief	is_standard_floating_point の定義
+ */
+
+#ifndef HAMON_FORMAT_DETAIL_IS_STANDARD_FLOATING_POINT_HPP
+#define HAMON_FORMAT_DETAIL_IS_STANDARD_FLOATING_POINT_HPP
+
+
+namespace hamon
+{
+
+namespace detail
+{
+
+
+}	// namespace detail
+
+}	// namespace hamon
+
+#endif // HAMON_FORMAT_DETAIL_IS_STANDARD_FLOATING_POINT_HPP
---
+++ libs/format/include/hamon/format/detail/is_unsigned_integer.hpp
@@ -0,0 +1,22 @@
+﻿/**
+ *	@file	is_unsigned_integer.hpp
+ *
+ *	@brief	is_unsigned_integer の定義
+ */
+
+#ifndef HAMON_FORMAT_DETAIL_IS_UNSIGNED_INTEGER_HPP
+#define HAMON_FORMAT_DETAIL_IS_UNSIGNED_INTEGER_HPP
+
+
+namespace hamon
+{
+
+namespace detail
+{
+
+
+}	// namespace detail
+
+}	// namespace hamon
+
+#endif // HAMON_FORMAT_DETAIL_IS_UNSIGNED_INTEGER_HPP
