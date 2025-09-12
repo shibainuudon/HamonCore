9960ad1db4b31f71d2c41def8e5ba577a2a17938
shibainuudon <shibainuudon@gmail.com>
2025-09-12 09:38:07 +0900

    [Vector]Apple Clangではstd::vectorを使わないようにした

--- libs/vector/include/hamon/vector/config.hpp
+++ libs/vector/include/hamon/vector/config.hpp
@@ -7,13 +7,15 @@
 #ifndef HAMON_VECTOR_CONFIG_HPP
 #define HAMON_VECTOR_CONFIG_HPP
 
+#include <hamon/config.hpp>
 #include <vector>
 
 #if defined(__cpp_lib_allocator_traits_is_always_equal) && (__cpp_lib_allocator_traits_is_always_equal >= 201411L) && \
 	defined(__cpp_lib_incomplete_container_elements)    && (__cpp_lib_incomplete_container_elements    >= 201505L) && \
 	defined(__cpp_lib_constexpr_vector)                 && (__cpp_lib_constexpr_vector                 >= 201907L) && \
 	defined(__cpp_lib_three_way_comparison)             && (__cpp_lib_three_way_comparison             >= 201907L) && \
-	defined(__cpp_lib_containers_ranges)                && (__cpp_lib_containers_ranges                >= 202202L)
+	defined(__cpp_lib_containers_ranges)                && (__cpp_lib_containers_ranges                >= 202202L) && \
+	!defined(HAMON_APPLE_CLANG)	/* Apple Clangのstd::vectorは使わない(insert関数においてvalue_typeがcopy assignableであることを要求するので) */
 
 #  define HAMON_USE_STD_VECTOR
 
