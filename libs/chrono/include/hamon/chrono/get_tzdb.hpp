703a4377b842b6146b44d23d60e8ff6ac60664b2
shibainuudon <shibainuudon@gmail.com>
2025-10-21 10:58:45 +0900

    [Chrono]get_tzdbを追加

---
+++ libs/chrono/include/hamon/chrono/get_tzdb.hpp
@@ -0,0 +1,45 @@
+﻿/**
+ *	@file	get_tzdb.hpp
+ *
+ *	@brief	get_tzdb の定義
+ */
+
+#ifndef HAMON_CHRONO_GET_TZDB_HPP
+#define HAMON_CHRONO_GET_TZDB_HPP
+
+#include <hamon/chrono/config.hpp>
+
+#if defined(HAMON_USE_STD_CHRONO)
+
+#include <chrono>
+
+namespace hamon {
+namespace chrono {
+
+using std::chrono::get_tzdb;
+
+}	// namespace chrono
+}	// namespace hamon
+
+#else
+
+#include <hamon/chrono/tzdb.hpp>
+#include <hamon/chrono/get_tzdb_list.hpp>
+
+namespace hamon {
+namespace chrono {
+
+// 30.11.2.3 Time zone database access[time.zone.db.access]
+
+inline const hamon::chrono::tzdb& get_tzdb()
+{
+	// [time.zone.db.access]/5
+	return hamon::chrono::get_tzdb_list().front();
+}
+
+}	// namespace chrono
+}	// namespace hamon
+
+#endif
+
+#endif // HAMON_CHRONO_GET_TZDB_HPP
---
+++ libs/chrono/test/src/unit_test_chrono_get_tzdb.cpp
@@ -0,0 +1,27 @@
+﻿/**
+ *	@file	unit_test_chrono_get_tzdb.cpp
+ *
+ *	@brief	get_tzdb のテスト
+ *
+ *	const tzdb& get_tzdb();
+ */
+
+#include <hamon/chrono/get_tzdb.hpp>
+#include <hamon/chrono/tzdb.hpp>
+#include <hamon/type_traits/is_same.hpp>
+#include <gtest/gtest.h>
+
+namespace hamon_chrono_test
+{
+
+GTEST_TEST(ChronoTest, GetTzdbTest)
+{
+	static_assert(hamon::is_same<decltype(hamon::chrono::get_tzdb()), hamon::chrono::tzdb const&>::value, "");
+	static_assert(!noexcept(hamon::chrono::get_tzdb()), "");
+
+	auto& r1 = hamon::chrono::get_tzdb();
+	auto& r2 = hamon::chrono::get_tzdb();
+	EXPECT_TRUE(&r1 == &r2);
+}
+
+}	// namespace hamon_chrono_test
