36112f005adf6972e6d1ccbcb3f3804a0efbdb5b
shibainuudon <shibainuudon@gmail.com>
2025-10-21 13:15:18 +0900

    [Chrono]high_resolution_clockのテストを修正

--- libs/chrono/test/src/unit_test_chrono_high_resolution_clock.cpp
+++ libs/chrono/test/src/unit_test_chrono_high_resolution_clock.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <hamon/chrono/high_resolution_clock.hpp>
+#include <hamon/type_traits/is_same.hpp>
 #include <gtest/gtest.h>
 
 namespace hamon_chrono_test
@@ -12,7 +13,25 @@ namespace hamon_chrono_test
 
 GTEST_TEST(ChronoTest, HighResolutionClockTest)
 {
-	// TODO
+	using Clock = hamon::chrono::high_resolution_clock;
+	using rep        = typename Clock::rep;
+	using period     = typename Clock::period;
+	using duration   = typename Clock::duration;
+	using time_point = typename Clock::time_point;
+
+	static_assert(hamon::is_same<rep, typename duration::rep>::value, "");
+	static_assert(hamon::is_same<period, typename duration::period>::value, "");
+	static_assert(hamon::is_same<duration, typename time_point::duration>::value, "");
+	static_assert(Clock::is_steady || !Clock::is_steady, "");
+
+	static_assert(noexcept(Clock::now()), "");
+	static_assert(hamon::is_same<time_point, decltype(Clock::now())>::value, "");
+	{
+		Clock::time_point t1 = Clock::now();
+		EXPECT_TRUE(t1.time_since_epoch().count() != 0);
+		EXPECT_TRUE(Clock::time_point::min() < t1);
+		EXPECT_TRUE(Clock::time_point::max() > t1);
+	}
 }
 
 }	// namespace hamon_chrono_test
