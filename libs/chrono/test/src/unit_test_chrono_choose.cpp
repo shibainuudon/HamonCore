a2108cde7a42793ba7bde3f93b8f8532d6aaf994
shibainuudon <shibainuudon@gmail.com>
2025-10-21 13:40:42 +0900

    [Chrono]chooseのテストを追加

---
+++ libs/chrono/test/src/unit_test_chrono_choose.cpp
@@ -0,0 +1,19 @@
+﻿/**
+ *	@file	unit_test_chrono_choose.cpp
+ *
+ *	@brief	choose のテスト
+ */
+
+#include <hamon/chrono/choose.hpp>
+#include <hamon/type_traits.hpp>
+#include <gtest/gtest.h>
+
+namespace hamon_chrono_test
+{
+
+GTEST_TEST(ChronoTest, ChooseTest)
+{
+	static_assert(hamon::is_enum<hamon::chrono::choose>::value, "");
+}
+
+}	// namespace hamon_chrono_test
