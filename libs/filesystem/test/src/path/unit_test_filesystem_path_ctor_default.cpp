1e83bdc4c6b87dc6a9b3363e564b395574bb8f49
shibainuudon <shibainuudon@gmail.com>
2025-10-22 13:22:38 +0900

    path wip

---
+++ libs/filesystem/test/src/path/unit_test_filesystem_path_ctor_default.cpp
@@ -0,0 +1,21 @@
+﻿/**
+ *	@file	unit_test_filesystem_path_ctor_default.cpp
+ *
+ *	@brief	デフォルトコンストラクタのテスト
+ *
+ *	path() noexcept;
+ */
+
+#include <hamon/filesystem/path.hpp>
+#include <hamon/type_traits.hpp>
+#include <gtest/gtest.h>
+
+GTEST_TEST(FilesystemPathTest, CtorDefaultTest)
+{
+	static_assert( hamon::is_default_constructible<hamon::filesystem::path>::value, "");
+	static_assert( hamon::is_nothrow_default_constructible<hamon::filesystem::path>::value, "");
+	static_assert(!hamon::is_trivially_default_constructible<hamon::filesystem::path>::value, "");
+	static_assert( hamon::is_implicitly_default_constructible<hamon::filesystem::path>::value, "");
+	hamon::filesystem::path p;
+	EXPECT_TRUE(p.empty());
+}
