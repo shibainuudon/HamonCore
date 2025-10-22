3b4da0c467e678eddb291357baaf8f9959855d4c
shibainuudon <shibainuudon@gmail.com>
2025-10-22 13:22:38 +0900

    path wip

---
+++ libs/filesystem/test/src/path/unit_test_filesystem_path_ctor_copy.cpp
@@ -0,0 +1,24 @@
+﻿/**
+ *	@file	unit_test_filesystem_path_ctor_copy.cpp
+ *
+ *	@brief	コピーコンストラクタのテスト
+ *
+ *	path(const path& p);
+ */
+
+#include <hamon/filesystem/path.hpp>
+#include <hamon/type_traits.hpp>
+#include <gtest/gtest.h>
+
+GTEST_TEST(FilesystemPathTest, CtorCopyTest)
+{
+	static_assert( hamon::is_copy_constructible<hamon::filesystem::path>::value, "");
+	static_assert(!hamon::is_nothrow_copy_constructible<hamon::filesystem::path>::value, "");
+	static_assert(!hamon::is_trivially_copy_constructible<hamon::filesystem::path>::value, "");
+	static_assert( hamon::is_implicitly_copy_constructible<hamon::filesystem::path>::value, "");
+	const hamon::filesystem::path::string_type s(L"foo");
+	const hamon::filesystem::path p(s);
+	hamon::filesystem::path p2(p);
+	EXPECT_TRUE(p.native() == s);
+	EXPECT_TRUE(p2.native() == s);
+}
---
+++ libs/filesystem/test/src/path/unit_test_filesystem_path_ctor_move.cpp
@@ -0,0 +1,24 @@
+﻿/**
+ *	@file	unit_test_filesystem_path_ctor_move.cpp
+ *
+ *	@brief	ムーブコンストラクタのテスト
+ *
+ *	path(path&& p) noexcept;
+ */
+
+#include <hamon/filesystem/path.hpp>
+#include <hamon/type_traits.hpp>
+#include <hamon/utility/move.hpp>
+#include <gtest/gtest.h>
+
+GTEST_TEST(FilesystemPathTest, CtorMoveTest)
+{
+	static_assert( hamon::is_move_constructible<hamon::filesystem::path>::value, "");
+	static_assert( hamon::is_nothrow_move_constructible<hamon::filesystem::path>::value, "");
+	static_assert(!hamon::is_trivially_move_constructible<hamon::filesystem::path>::value, "");
+	static_assert( hamon::is_implicitly_move_constructible<hamon::filesystem::path>::value, "");
+	const hamon::filesystem::path::string_type s(L"foo");
+	const hamon::filesystem::path p(s);
+	hamon::filesystem::path p2(hamon::move(p));
+	EXPECT_TRUE(p2.native() == s);
+}
