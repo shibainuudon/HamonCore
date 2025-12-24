0e25043692cec04b5952e3c0dd5dbe8806acad29
shibainuudon <shibainuudon@gmail.com>
2025-12-24 17:50:29 +0900

    [CString]memcpyのconstexpr版(hamon::ct::memcpy)を追加

--- libs/cstring/include/hamon/cstring/memcpy.hpp
+++ libs/cstring/include/hamon/cstring/memcpy.hpp
@@ -7,12 +7,48 @@
 #ifndef HAMON_CSTRING_MEMCPY_HPP
 #define HAMON_CSTRING_MEMCPY_HPP
 
+#include <hamon/type_traits/is_constant_evaluated.hpp>
+#include <hamon/config.hpp>
+#include <hamon/assert.hpp>
 #include <cstring>
 
 namespace hamon
 {
 
-using std::memcpy;
+namespace ct
+{
+
+/**
+ *	constexpr版のmemcpy
+ *	実行時のパフォーマンスはstd::memcpyより劣る可能性がある
+ *	is_constant_evaluatedが使える場合は、実行時にはstd::memcpyを呼ぶ
+ */
+template <typename T>
+inline HAMON_CXX14_CONSTEXPR
+T* memcpy(T* dest, T const* src, std::size_t count) HAMON_NOEXCEPT
+{
+#if defined(HAMON_HAS_CXX20_IS_CONSTANT_EVALUATED)
+	if (!hamon::is_constant_evaluated())
+	{
+		return static_cast<T*>(std::memcpy(dest, src, count));
+	}
+#endif
+
+	HAMON_ASSERT((count % sizeof(T)) == 0u);
+	count /= sizeof(T);
+	for (std::size_t i = 0; i < count; ++i)
+	{
+		dest[i] = src[i];
+	}
+	return dest;
+}
+
+}	// namespace ct
+
+inline void* memcpy(void* dest, void const* src, std::size_t count) HAMON_NOEXCEPT
+{
+	return std::memcpy(dest, src, count);
+}
 
 }	// namespace hamon
 
--- libs/cstring/test/src/unit_test_cstring_memcpy.cpp
+++ libs/cstring/test/src/unit_test_cstring_memcpy.cpp
@@ -6,18 +6,80 @@
 
 #include <hamon/cstring/memcpy.hpp>
 #include <gtest/gtest.h>
+#include "constexpr_test.hpp"
+
+namespace hamon_cstring_test
+{
+
+namespace memcpy_test
+{
+
+#define VERIFY(...)	if (!(__VA_ARGS__)) { return false; }
+
+inline HAMON_CXX14_CONSTEXPR bool constexpr_test()
+{
+	{
+		const char a1[] = { 1,2,3 };
+		char a2[3]{};
+
+		auto p = hamon::ct::memcpy(a2, a1, sizeof(a1));
+
+		VERIFY(1 == a2[0]);
+		VERIFY(2 == a2[1]);
+		VERIFY(3 == a2[2]);
+
+		VERIFY(p == a2);
+	}
+	{
+		const double a1[] = { 10,11,12,13,14 };
+		double a2[5]{};
+
+		auto p = hamon::ct::memcpy(a2, a1, sizeof(a1));
+
+		VERIFY(10 == a2[0]);
+		VERIFY(11 == a2[1]);
+		VERIFY(12 == a2[2]);
+		VERIFY(13 == a2[3]);
+		VERIFY(14 == a2[4]);
+
+		VERIFY(p == a2);
+	}
+	{
+		const int a1[] = { 2, 1, 0, 3 };
+		int a2[4]{};
+
+		auto p = hamon::ct::memcpy(a2, a1, sizeof(a1));
+
+		VERIFY(2 == a2[0]);
+		VERIFY(1 == a2[1]);
+		VERIFY(0 == a2[2]);
+		VERIFY(3 == a2[3]);
+
+		VERIFY(p == a2);
+	}
+
+	return true;
+}
+
+#undef VERIFY
 
 GTEST_TEST(CStringTest, MemCpyTest)
 {
-	const int a1[] = { 2, 1, 0, 3 };
-	int a2[4];
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(constexpr_test());
 
-	auto p = hamon::memcpy(a2, a1, sizeof(a1));
+	{
+		float f = 1532.625f;	// 0x44bf9400
+		unsigned char c[4];
+		auto p = hamon::memcpy(c, &f, sizeof(f));
+		EXPECT_EQ(0x00, c[0]);
+		EXPECT_EQ(0x94, c[1]);
+		EXPECT_EQ(0xbf, c[2]);
+		EXPECT_EQ(0x44, c[3]);
 
-	EXPECT_EQ(2, a2[0]);
-	EXPECT_EQ(1, a2[1]);
-	EXPECT_EQ(0, a2[2]);
-	EXPECT_EQ(3, a2[3]);
-
-	EXPECT_EQ(p, a2);
+		EXPECT_EQ(p, c);
+	}
 }
+
+}	// namespace memcpy_test
+
+}	// namespace hamon_cstring_test
