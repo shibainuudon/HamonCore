8c1bfa8cab55f018d2ee08c869e81bc68fb2ebdb
shibainuudon <shibainuudon@gmail.com>
2025-12-25 13:27:24 +0900

    [CString]memsetのconstexpr版(hamon::ct::memset)を追加

--- libs/cstring/include/hamon/cstring/memset.hpp
+++ libs/cstring/include/hamon/cstring/memset.hpp
@@ -7,12 +7,45 @@
 #ifndef HAMON_CSTRING_MEMSET_HPP
 #define HAMON_CSTRING_MEMSET_HPP
 
+#include <hamon/type_traits/is_constant_evaluated.hpp>
+#include <hamon/config.hpp>
+#include <hamon/assert.hpp>
 #include <cstring>
 
 namespace hamon
 {
 
-using std::memset;
+namespace ct
+{
+
+/**
+ *	constexpr版のmemset
+ *	実行時のパフォーマンスはstd::memsetより劣る可能性がある
+ *	is_constant_evaluatedが使える場合は、実行時にはstd::memsetを呼ぶ
+ */
+inline HAMON_CXX14_CONSTEXPR
+char* memset(char* dest, char ch, std::size_t count) HAMON_NOEXCEPT
+{
+#if defined(HAMON_HAS_CXX20_IS_CONSTANT_EVALUATED)
+	if (!hamon::is_constant_evaluated())
+	{
+		return static_cast<char*>(std::memset(dest, ch, count));
+	}
+#endif
+
+	for (std::size_t i = 0; i < count; ++i)
+	{
+		dest[i] = ch;
+	}
+	return dest;
+}
+
+}	// namespace ct
+
+inline void* memset(void* dest, int ch, std::size_t count) HAMON_NOEXCEPT
+{
+	return std::memset(dest, ch, count);
+}
 
 }	// namespace hamon
 
--- libs/cstring/test/src/unit_test_cstring_memset.cpp
+++ libs/cstring/test/src/unit_test_cstring_memset.cpp
@@ -5,38 +5,69 @@
  */
 
 #include <hamon/cstring/memset.hpp>
+#include <hamon/cstdint/uint32_t.hpp>
 #include <gtest/gtest.h>
+#include "constexpr_test.hpp"
 
-GTEST_TEST(CStringTest, MemSetTest)
+namespace hamon_cstring_test
+{
+
+namespace memset_test
+{
+
+#define VERIFY(...)	if (!(__VA_ARGS__)) { return false; }
+
+inline HAMON_CXX14_CONSTEXPR bool constexpr_test()
 {
 	{
-		char a[3];
-		auto p = hamon::memset(a, 0, sizeof(a));
+		char a[3]{1,2,3};
+		auto p = hamon::ct::memset(a, 0, sizeof(a));
 
-		EXPECT_EQ(0, a[0]);
-		EXPECT_EQ(0, a[1]);
-		EXPECT_EQ(0, a[2]);
-		EXPECT_EQ(p, a);
-	}
-	{
-		int a[4];
-		auto p = hamon::memset(a, 0, sizeof(a));
-
-		EXPECT_EQ(0, a[0]);
-		EXPECT_EQ(0, a[1]);
-		EXPECT_EQ(0, a[2]);
-		EXPECT_EQ(0, a[3]);
-		EXPECT_EQ(p, a);
+		VERIFY(0 == a[0]);
+		VERIFY(0 == a[1]);
+		VERIFY(0 == a[2]);
+		VERIFY(p == a);
 	}
 	{
 		char a[5] = {};
-		auto p = hamon::memset(a, 42, sizeof(char) * 3);
+		auto p = hamon::ct::memset(a, 42, sizeof(char) * 3);
 
-		EXPECT_EQ(42, a[0]);
-		EXPECT_EQ(42, a[1]);
-		EXPECT_EQ(42, a[2]);
-		EXPECT_EQ( 0, a[3]);
-		EXPECT_EQ( 0, a[4]);
+		VERIFY(42 == a[0]);
+		VERIFY(42 == a[1]);
+		VERIFY(42 == a[2]);
+		VERIFY( 0 == a[3]);
+		VERIFY( 0 == a[4]);
+		VERIFY( p == a);
+	}
+
+	return true;
+}
+
+#undef VERIFY
+
+GTEST_TEST(CStringTest, MemSetTest)
+{
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(constexpr_test());
+
+	{
+		hamon::uint32_t a[4];
+		auto p = hamon::memset(a, 0x12, sizeof(a));
+
+		EXPECT_EQ(0x12121212UL, a[0]);
+		EXPECT_EQ(0x12121212UL, a[1]);
+		EXPECT_EQ(0x12121212UL, a[2]);
+		EXPECT_EQ(0x12121212UL, a[3]);
 		EXPECT_EQ(p, a);
 	}
+	{
+		float f;
+		auto p = hamon::memset(&f, 0, sizeof(f));
+
+		EXPECT_EQ(0, f);
+		EXPECT_EQ(p, &f);
+	}
 }
+
+}	// namespace memset_test
+
+}	// namespace hamon_cstring_test
