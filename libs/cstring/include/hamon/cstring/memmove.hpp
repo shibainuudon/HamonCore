20b1903cd7d4ebb8cc131909b8746f5ebcb9da66
shibainuudon <shibainuudon@gmail.com>
2025-12-25 14:06:28 +0900

    [CString]memmoveのconstexpr版(hamon::ct::memmove)を追加

--- libs/cstring/include/hamon/cstring/memmove.hpp
+++ libs/cstring/include/hamon/cstring/memmove.hpp
@@ -7,12 +7,62 @@
 #ifndef HAMON_CSTRING_MEMMOVE_HPP
 #define HAMON_CSTRING_MEMMOVE_HPP
 
+#include <hamon/type_traits/is_constant_evaluated.hpp>
+#include <hamon/config.hpp>
+#include <hamon/assert.hpp>
 #include <cstring>
 
 namespace hamon
 {
 
-using std::memmove;
+namespace ct
+{
+
+/**
+ *	constexpr版のmemmove
+ *	実行時のパフォーマンスはstd::memmoveより劣る可能性がある
+ *	is_constant_evaluatedが使える場合は、実行時にはstd::memmoveを呼ぶ
+ *
+ *	destとsrcが同じ配列の要素を指していない場合constexprにできない。
+ *	その場合はmemcpyを使ってください。
+ */
+template <typename T>
+inline HAMON_CXX14_CONSTEXPR
+T* memmove(T* dest, T const* src, std::size_t count) HAMON_NOEXCEPT
+{
+#if defined(HAMON_HAS_CXX20_IS_CONSTANT_EVALUATED)
+	if (!hamon::is_constant_evaluated())
+	{
+		return static_cast<T*>(std::memmove(dest, src, count));
+	}
+#endif
+
+	HAMON_ASSERT((count % sizeof(T)) == 0u);
+	count /= sizeof(T);
+
+	if (dest < src)
+	{
+		for (std::size_t i = 0; i < count; ++i)
+		{
+			dest[i] = src[i];
+		}
+	}
+	else
+	{
+		for (std::size_t i = count; i > 0; --i)
+		{
+			dest[i - 1] = src[i - 1];
+		}
+	}
+	return dest;
+}
+
+}	// namespace ct
+
+inline void* memmove(void* dest, void const* src, std::size_t count) HAMON_NOEXCEPT
+{
+	return std::memmove(dest, src, count);
+}
 
 }	// namespace hamon
 
--- libs/cstring/test/src/unit_test_cstring_memmove.cpp
+++ libs/cstring/test/src/unit_test_cstring_memmove.cpp
@@ -6,19 +6,137 @@
 
 #include <hamon/cstring/memmove.hpp>
 #include <gtest/gtest.h>
+#include "constexpr_test.hpp"
+
+namespace hamon_cstring_test
+{
+
+namespace memmove_test
+{
+
+#define VERIFY(...)	if (!(__VA_ARGS__)) { return false; }
+
+inline HAMON_CXX14_CONSTEXPR bool constexpr_test()
+{
+	{
+		int a1[] = { 0, 1, 2, 3, 4, 5, };
+
+		auto p = hamon::ct::memmove(&a1[0], &a1[2], sizeof(int) * 3);
+
+		VERIFY(2 == a1[0]);
+		VERIFY(3 == a1[1]);
+		VERIFY(4 == a1[2]);
+		VERIFY(3 == a1[3]);
+		VERIFY(4 == a1[4]);
+		VERIFY(5 == a1[5]);
+
+		VERIFY(p == &a1[0]);
+	}
+	{
+		char a1[] = { 0, 1, 2, 3, 4, 5, };
+
+		auto p = hamon::ct::memmove(&a1[2], &a1[0], sizeof(char) * 3);
+
+		VERIFY(0 == a1[0]);
+		VERIFY(1 == a1[1]);
+		VERIFY(0 == a1[2]);
+		VERIFY(1 == a1[3]);
+		VERIFY(2 == a1[4]);
+		VERIFY(5 == a1[5]);
+
+		VERIFY(p == &a1[2]);
+	}
+	{
+		short a1[] = { 0, 1, 2, 3, 4, 5, };
+
+		auto p = hamon::ct::memmove(&a1[1], &a1[3], sizeof(short) * 3);
+
+		VERIFY(0 == a1[0]);
+		VERIFY(3 == a1[1]);
+		VERIFY(4 == a1[2]);
+		VERIFY(5 == a1[3]);
+		VERIFY(4 == a1[4]);
+		VERIFY(5 == a1[5]);
+
+		VERIFY(p == &a1[1]);
+	}
+
+	return true;
+}
+
+#undef VERIFY
 
 GTEST_TEST(CStringTest, MemMoveTest)
 {
-	int a1[] = { 0, 1, 2, 3, 4, 5, };
+	HAMON_CXX14_CONSTEXPR_EXPECT_TRUE(constexpr_test());
 
-	auto p = hamon::memmove(&a1[0], &a1[2], sizeof(int) * 3);
+	{
+		float f = 1532.625f;	// 0x44bf9400
+		unsigned char c[4];
+		auto p = hamon::memmove(c, &f, sizeof(f));
+		EXPECT_EQ(0x00, c[0]);
+		EXPECT_EQ(0x94, c[1]);
+		EXPECT_EQ(0xbf, c[2]);
+		EXPECT_EQ(0x44, c[3]);
 
-	EXPECT_EQ(2, a1[0]);
-	EXPECT_EQ(3, a1[1]);
-	EXPECT_EQ(4, a1[2]);
-	EXPECT_EQ(3, a1[3]);
-	EXPECT_EQ(4, a1[4]);
-	EXPECT_EQ(5, a1[5]);
+		EXPECT_EQ(p, c);
+	}
+	{
+		const char a1[] = { 1,2,3 };
+		char a2[3]{};
 
-	EXPECT_EQ(p, a1);
+		auto p = hamon::memmove(a2, a1, sizeof(a1));
+
+		EXPECT_EQ(1, a2[0]);
+		EXPECT_EQ(2, a2[1]);
+		EXPECT_EQ(3, a2[2]);
+
+		EXPECT_EQ(p, a2);
+	}
+	{
+		char a1[] = { 0, 1, 2, 3, 4, 5, };
+
+		auto p = hamon::memmove(&a1[0], &a1[2], sizeof(char) * 3);
+
+		EXPECT_EQ(2, a1[0]);
+		EXPECT_EQ(3, a1[1]);
+		EXPECT_EQ(4, a1[2]);
+		EXPECT_EQ(3, a1[3]);
+		EXPECT_EQ(4, a1[4]);
+		EXPECT_EQ(5, a1[5]);
+
+		EXPECT_EQ(p, &a1[0]);
+	}
+	{
+		int a1[] = { 0, 1, 2, 3, 4, 5, };
+
+		auto p = hamon::memmove(&a1[2], &a1[0], sizeof(int) * 4);
+
+		EXPECT_EQ(0, a1[0]);
+		EXPECT_EQ(1, a1[1]);
+		EXPECT_EQ(0, a1[2]);
+		EXPECT_EQ(1, a1[3]);
+		EXPECT_EQ(2, a1[4]);
+		EXPECT_EQ(3, a1[5]);
+
+		EXPECT_EQ(p, &a1[2]);
+	}
+	{
+		short a1[] = { 0, 1, 2, 3, 4, 5, };
+
+		auto p = hamon::ct::memmove(&a1[1], &a1[3], sizeof(short) * 3);
+
+		EXPECT_EQ(0, a1[0]);
+		EXPECT_EQ(3, a1[1]);
+		EXPECT_EQ(4, a1[2]);
+		EXPECT_EQ(5, a1[3]);
+		EXPECT_EQ(4, a1[4]);
+		EXPECT_EQ(5, a1[5]);
+
+		EXPECT_EQ(p, &a1[1]);
+	}
 }
+
+}	// namespace memmove_test
+
+}	// namespace hamon_cstring_test
