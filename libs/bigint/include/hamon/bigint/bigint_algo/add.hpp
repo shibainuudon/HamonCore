3e502341f14e05d749efd153a2f85acf9811b9aa
shibainuudon <shibainuudon@gmail.com>
2026-02-20 17:10:52 +0900

    [BigInt]bigint_algo::add の実装を共通化

--- libs/bigint/include/hamon/bigint/bigint_algo/add.hpp
+++ libs/bigint/include/hamon/bigint/bigint_algo/add.hpp
@@ -11,11 +11,11 @@
 #include <hamon/bigint/bigint_algo/detail/hi.hpp>
 #include <hamon/bigint/bigint_algo/detail/lo.hpp>
 #include <hamon/bigint/bigint_algo/detail/actual_size.hpp>
+#include <hamon/bigint/bigint_algo/detail/resize.hpp>
 #include <hamon/bigint/bigint_algo/normalize.hpp>
 #include <hamon/array.hpp>
 #include <hamon/algorithm/max.hpp>
 #include <hamon/cstddef/size_t.hpp>
-#include <hamon/utility/make_index_sequence.hpp>
 #include <hamon/inplace_vector.hpp>
 #include <hamon/vector.hpp>
 #include <hamon/config.hpp>
@@ -57,29 +57,73 @@ namespace bigint_algo
 namespace add_detail
 {
 
-template <typename T>
-inline HAMON_CXX14_CONSTEXPR T
-add_impl(T* dst, T const* p1, hamon::size_t n1, T const* p2, hamon::size_t n2)
+template <typename VectorType, typename T>
+inline HAMON_CXX14_CONSTEXPR bool
+add_impl(VectorType& lhs, T const* p2, hamon::size_t n2)
 {
+	detail::resize(lhs, hamon::max(lhs.size(), n2) + 1);
+
+	auto const p1 = lhs.data();
+	auto const n1 = lhs.size();
+
 	T carry = 0;
 	hamon::size_t i = 0;
+
 	for (; i < n2; ++i)
 	{
 		auto const x = detail::addc(p1[i], p2[i], carry);
-		dst[i] = detail::lo(x);
-		carry  = detail::hi(x);
+		p1[i] = detail::lo(x);
+		carry = detail::hi(x);
 	}
 	for (; carry != 0 && i < n1; ++i)
 	{
 		auto const x = detail::addc(p1[i], T{0}, carry);
-		dst[i] = detail::lo(x);
-		carry  = detail::hi(x);
+		p1[i] = detail::lo(x);
+		carry = detail::hi(x);
 	}
+
+	bigint_algo::normalize(lhs);
+	return carry != 0;
+}
+
+template <typename VectorType, typename T>
+inline HAMON_CXX14_CONSTEXPR bool
+add_impl(VectorType& out, T const* p1, hamon::size_t n1, T const* p2, hamon::size_t n2)
+{
+	detail::resize(out, hamon::max(n1, n2) + 1);
+
+	auto const p3 = out.data();
+	auto const n3 = out.size();
+
+	T carry = 0;
+	hamon::size_t i = 0;
+
+	for (; i < n2; ++i)
+	{
+		auto const x = detail::addc(p1[i], p2[i], carry);
+		p3[i] = detail::lo(x);
+		carry = detail::hi(x);
+	}
+	for (; carry != 0 && i < n1; ++i)
+	{
+		auto const x = detail::addc(p1[i], T{0}, carry);
+		p3[i] = detail::lo(x);
+		carry = detail::hi(x);
+	}
+
 	for (; i < n1; ++i)
 	{
-		dst[i] = p1[i];
+		p3[i] = p1[i];
 	}
-	return carry;
+	for (; carry != 0 && i < n3; ++i)
+	{
+		auto const x = detail::addc(p3[i], T{0}, carry);
+		p3[i] = detail::lo(x);
+		carry = detail::hi(x);
+	}
+
+	bigint_algo::normalize(out);
+	return carry != 0;
 }
 
 }	// namespace add_detail
@@ -88,119 +132,56 @@ template <typename T>
 inline HAMON_CXX14_CONSTEXPR bool
 add(hamon::vector<T>& lhs, hamon::vector<T> const& rhs)
 {
-	lhs.resize(hamon::max(lhs.size(), rhs.size()));
-	T const carry = add_detail::add_impl(
-		lhs.data(), lhs.data(), lhs.size(), rhs.data(), rhs.size());
-	if (carry != 0)
-	{
-		lhs.push_back(carry);
-	}
-	return false;
+	return add_detail::add_impl(lhs, rhs.data(), rhs.size());
 }
 
 template <typename T>
 inline HAMON_CXX14_CONSTEXPR bool
 add(hamon::vector<T>& lhs, T rhs)
 {
-	T const carry = add_detail::add_impl(
-		lhs.data(), lhs.data(), lhs.size(), &rhs, 1);
-	if (carry != 0)
-	{
-		lhs.push_back(carry);
-	}
-	return false;
+	return add_detail::add_impl(lhs, &rhs, 1);
 }
 
 template <typename T>
 inline HAMON_CXX14_CONSTEXPR bool
 add(hamon::vector<T>& out, hamon::vector<T> const& lhs, T rhs)
 {
-	out.resize(lhs.size());
-	T const carry = add_detail::add_impl(
-		out.data(), lhs.data(), lhs.size(), &rhs, 1);
-	if (carry != 0)
-	{
-		out.push_back(carry);
-	}
-	return false;
+	return add_detail::add_impl(out, lhs.data(), lhs.size(), &rhs, 1);
 }
 
 template <typename T, hamon::size_t N>
 inline HAMON_CXX14_CONSTEXPR bool
 add(hamon::inplace_vector<T, N>& lhs, hamon::inplace_vector<T, N> const& rhs)
 {
-	lhs.resize(hamon::max(lhs.size(), rhs.size()));
-	T const carry = add_detail::add_impl(
-		lhs.data(), lhs.data(), lhs.size(), rhs.data(), rhs.size());
-	if (carry != 0)
-	{
-		if (lhs.size() == N)
-		{
-			bigint_algo::normalize(lhs);
-			return true;
-		}
-		lhs.push_back(carry);
-	}
-	return false;
+	return add_detail::add_impl(lhs, rhs.data(), rhs.size());
 }
 
 template <typename T, hamon::size_t N>
 inline HAMON_CXX14_CONSTEXPR bool
 add(hamon::inplace_vector<T, N>& lhs, T rhs)
 {
-	T const carry = add_detail::add_impl(
-		lhs.data(), lhs.data(), lhs.size(), &rhs, 1);
-	if (carry != 0)
-	{
-		if (lhs.size() == N)
-		{
-			bigint_algo::normalize(lhs);
-			return true;
-		}
-		lhs.push_back(carry);
-	}
-	return false;
+	return add_detail::add_impl(lhs, &rhs, 1);
 }
 
 template <typename T, hamon::size_t N>
 inline HAMON_CXX14_CONSTEXPR bool
 add(hamon::inplace_vector<T, N>& out, hamon::inplace_vector<T, N> const& lhs, T rhs)
 {
-	out.resize(lhs.size());
-	T const carry = add_detail::add_impl(
-		out.data(), lhs.data(), lhs.size(), &rhs, 1);
-	if (carry != 0)
-	{
-		if (out.size() == N)
-		{
-			bigint_algo::normalize(out);
-			return true;
-		}
-		out.push_back(carry);
-	}
-	return false;
+	return add_detail::add_impl(out, lhs.data(), lhs.size(), &rhs, 1);
 }
 
 template <typename T, hamon::size_t N>
 inline HAMON_CXX14_CONSTEXPR bool
 add(hamon::array<T, N>& lhs, hamon::array<T, N> const& rhs)
 {
-	T const carry = add_detail::add_impl(
-		lhs.data(),
-		lhs.data(), lhs.size(),
-		rhs.data(), detail::actual_size(rhs));
-	return carry != 0;
+	return add_detail::add_impl(lhs, rhs.data(), detail::actual_size(rhs));
 }
 
 template <typename T, hamon::size_t N>
 inline HAMON_CXX14_CONSTEXPR bool
 add(hamon::array<T, N>& lhs, T rhs)
 {
-	T const carry = add_detail::add_impl(
-		lhs.data(),
-		lhs.data(), lhs.size(),
-		&rhs, 1);
-	return carry != 0;
+	return add_detail::add_impl(lhs, &rhs, 1);
 }
 
 template <typename T, hamon::size_t N>
@@ -208,11 +189,7 @@ inline HAMON_CXX14_CONSTEXPR bool
 add(hamon::array<T, N>& out, hamon::array<T, N> const& lhs, T rhs)
 {
 	detail::zero(out);
-	T const carry = add_detail::add_impl(
-		out.data(),
-		lhs.data(), lhs.size(),
-		&rhs, 1);
-	return carry != 0;
+	return add_detail::add_impl(out, lhs.data(), lhs.size(), &rhs, 1);
 }
 
 }	// namespace bigint_algo
