ff75ed3b2efd16dd50bd390b790b5c7ededfa23b
shibainuudon <shibainuudon@gmail.com>
2026-03-13 11:34:36 +0900

    [BigInt]bigint_algo::multiply_addを最適化

---
+++ libs/bigint/include/hamon/bigint/bigint_algo/detail/mulc.hpp
@@ -0,0 +1,85 @@
+﻿/**
+ *	@file	mulc.hpp
+ *
+ *	@brief	mulc 関数の定義
+ */
+
+#ifndef HAMON_BIGINT_BIGINT_ALGO_DETAIL_MULC_HPP
+#define HAMON_BIGINT_BIGINT_ALGO_DETAIL_MULC_HPP
+
+#include <hamon/bigint/bigint_algo/detail/mul.hpp>
+#include <hamon/bigint/bigint_algo/add.hpp>
+#include <hamon/array.hpp>
+#include <hamon/cstdint.hpp>
+#include <hamon/config.hpp>
+
+namespace hamon
+{
+namespace bigint_algo
+{
+namespace detail
+{
+
+// x * y + carry
+// 戻り値で計算結果の下位ビットを返し、carryに上位ビットを格納する
+inline HAMON_CXX14_CONSTEXPR hamon::uint8_t
+mulc(hamon::uint8_t x, hamon::uint8_t y, hamon::uint8_t* carry)
+{
+	auto t = static_cast<hamon::uint16_t>(
+		static_cast<hamon::uint16_t>(x) *
+		static_cast<hamon::uint16_t>(y) +
+		static_cast<hamon::uint16_t>(*carry));
+	*carry = static_cast<hamon::uint8_t>(t >> 8);
+	return static_cast<hamon::uint8_t>(t);
+}
+
+inline HAMON_CXX14_CONSTEXPR hamon::uint16_t
+mulc(hamon::uint16_t x, hamon::uint16_t y, hamon::uint16_t* carry)
+{
+	auto t = static_cast<hamon::uint32_t>(
+		static_cast<hamon::uint32_t>(x) *
+		static_cast<hamon::uint32_t>(y) +
+		static_cast<hamon::uint32_t>(*carry));
+	*carry = static_cast<hamon::uint16_t>(t >> 16);
+	return static_cast<hamon::uint16_t>(t);
+}
+
+inline HAMON_CXX14_CONSTEXPR hamon::uint32_t
+mulc(hamon::uint32_t x, hamon::uint32_t y, hamon::uint32_t* carry)
+{
+	auto t = static_cast<hamon::uint64_t>(
+		static_cast<hamon::uint64_t>(x) *
+		static_cast<hamon::uint64_t>(y) +
+		static_cast<hamon::uint64_t>(*carry));
+	*carry = static_cast<hamon::uint32_t>(t >> 32);
+	return static_cast<hamon::uint32_t>(t);
+}
+
+#if defined(__SIZEOF_INT128__)
+inline HAMON_CXX14_CONSTEXPR hamon::uint64_t
+mulc(hamon::uint64_t x, hamon::uint64_t y, hamon::uint64_t* carry)
+{
+	auto t = static_cast<__uint128_t>(
+		static_cast<__uint128_t>(x) *
+		static_cast<__uint128_t>(y) +
+		static_cast<__uint128_t>(*carry));
+	*carry = static_cast<hamon::uint64_t>(t >> 64);
+	return static_cast<hamon::uint64_t>(t);
+}
+#endif
+
+template <typename T>
+inline HAMON_CXX14_CONSTEXPR T
+mulc(T x, T y, T* carry)
+{
+	auto r1 = detail::mul(x, y);
+	auto r2 = detail::addc(detail::lo(r1), T{0}, *carry);
+	*carry = detail::hi(r1) + detail::hi(r2);
+	return detail::lo(r2);
+}
+
+}	// namespace detail
+}	// namespace bigint_algo
+}	// namespace hamon
+
+#endif // HAMON_BIGINT_BIGINT_ALGO_DETAIL_MULC_HPP
--- libs/bigint/include/hamon/bigint/bigint_algo/multiply_add.hpp
+++ libs/bigint/include/hamon/bigint/bigint_algo/multiply_add.hpp
@@ -7,10 +7,7 @@
 #ifndef HAMON_BIGINT_BIGINT_ALGO_MULTIPLY_ADD_HPP
 #define HAMON_BIGINT_BIGINT_ALGO_MULTIPLY_ADD_HPP
 
-#include <hamon/bigint/bigint_algo/detail/actual_size.hpp>
-#include <hamon/bigint/bigint_algo/detail/hi.hpp>
-#include <hamon/bigint/bigint_algo/detail/lo.hpp>
-#include <hamon/bigint/bigint_algo/detail/mul_addc.hpp>
+#include <hamon/bigint/bigint_algo/detail/mulc.hpp>
 #include <hamon/bigint/bigint_algo/detail/resize.hpp>
 #include <hamon/bigint/bigint_algo/normalize.hpp>
 #include <hamon/cstddef/size_t.hpp>
@@ -26,27 +23,6 @@ namespace hamon
 namespace bigint_algo
 {
 
-namespace multiply_add_detail
-{
-
-template <typename T>
-inline HAMON_CXX14_CONSTEXPR bool
-multiply_add_impl(T* p1, hamon::size_t n1, T y, T z)
-{
-	T carry = z;
-	hamon::size_t i = 0;
-	for (; i < n1; ++i)
-	{
-		auto const r = detail::mul_addc(p1[i], y, T{0}, carry);
-		p1[i] = detail::lo(r);
-		carry = detail::hi(r);
-	}
-
-	return carry != 0;
-}
-
-}	// namespace multiply_add_detail
-
 // x = x * y + z
 template <typename VectorType, typename T2,
 	typename T1 = hamon::ranges::range_value_t<VectorType>,
@@ -58,11 +34,17 @@ template <typename VectorType, typename T2,
 inline HAMON_CXX14_CONSTEXPR bool
 multiply_add(VectorType& x, T2 y, T2 z)
 {
-	auto n1 = detail::actual_size(x);
-	detail::resize(x, n1 + 1);
-	auto overflow = multiply_add_detail::multiply_add_impl(x.data(), x.size(), y, z);
+	detail::resize(x, x.size() + 1);
+	auto p = x.data();
+	auto n = x.size();
+	while (n != 0)
+	{
+		*p = detail::mulc(*p, y, &z);
+		++p;
+		--n;
+	}
 	bigint_algo::normalize(x);
-	return overflow;
+	return z != 0;
 }
 
 }	// namespace bigint_algo
