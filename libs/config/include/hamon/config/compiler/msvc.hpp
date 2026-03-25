57fa56f487b8ba8056fd4e42ba8f0f3294f74d20
shibainuudon <shibainuudon@gmail.com>
2026-03-25 17:50:18 +0900

    [Config]msvc.hppを更新
    * Visual Studio 2026 Version: 18.4.2

--- libs/config/include/hamon/config/compiler/msvc.hpp
+++ libs/config/include/hamon/config/compiler/msvc.hpp
@@ -1344,6 +1344,10 @@
 #if (_MSC_FULL_VER >= 195035727)
 #endif
 
+// Visual Studio 2026 Version: 18.4.2
+#if (_MSC_FULL_VER >= 195035728)
+#endif
+
 #if (HAMON_CXX_STANDARD >= 17)
 #  if defined(__cpp_constexpr) && (__cpp_constexpr >= 201603)
 #    define HAMON_HAS_CXX17_CONSTEXPR
