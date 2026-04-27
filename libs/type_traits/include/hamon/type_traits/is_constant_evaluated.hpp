ef05ebe4047e5cb25d4005708ef727e90b06b4e7
shibainuudon <shibainuudon@gmail.com>
2026-04-27 17:59:53 +0900

    [TypeTraits]C++20以前であってもis_constant_evaluatedを定義した

--- libs/type_traits/include/hamon/type_traits/is_constant_evaluated.hpp
+++ libs/type_traits/include/hamon/type_traits/is_constant_evaluated.hpp
@@ -7,6 +7,7 @@
 #ifndef HAMON_TYPE_TRAITS_IS_CONSTANT_EVALUATED_HPP
 #define HAMON_TYPE_TRAITS_IS_CONSTANT_EVALUATED_HPP
 
+#include <hamon/config.hpp>
 #include <type_traits>
 
 namespace hamon
@@ -15,6 +16,14 @@ namespace hamon
 #if defined(__cpp_lib_is_constant_evaluated) && (__cpp_lib_is_constant_evaluated >= 201811)
 
 using std::is_constant_evaluated;
+
+#else
+
+HAMON_NODISCARD HAMON_CXX11_CONSTEXPR bool
+is_constant_evaluated() HAMON_NOEXCEPT
+{
+	return __builtin_is_constant_evaluated();
+}
 
 #endif
 
