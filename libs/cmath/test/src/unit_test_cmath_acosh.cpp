9c98a6ccd7bd30fbed5c2331124367c571a54a4a
shibainuudon <shibainuudon@gmail.com>
2026-05-07 10:06:31 +0900

    [CMath]acoshのテストを修正

--- libs/cmath/test/src/unit_test_cmath_acosh.cpp
+++ libs/cmath/test/src/unit_test_cmath_acosh.cpp
@@ -21,32 +21,11 @@ namespace acosh_test
 {
 
 static_assert(hamon::is_same<float,       decltype(hamon::acosh(0.0f))>::value, "");
-static_assert(hamon::is_same<float,       decltype(hamon::acoshf(0.0f))>::value, "");
 static_assert(hamon::is_same<double,      decltype(hamon::acosh(0.0 ))>::value, "");
 static_assert(hamon::is_same<double,      decltype(hamon::acosh(0   ))>::value, "");
 static_assert(hamon::is_same<long double, decltype(hamon::acosh(0.0l))>::value, "");
+static_assert(hamon::is_same<float,       decltype(hamon::acoshf(0.0f))>::value, "");
 static_assert(hamon::is_same<long double, decltype(hamon::acoshl(0.0l))>::value, "");
-
-template <typename T>
-double get_error();
-
-template <>
-inline HAMON_CXX11_CONSTEXPR double get_error<float>()
-{
-	return 1e-6;
-}
-
-template <>
-inline HAMON_CXX11_CONSTEXPR double get_error<double>()
-{
-	return 1e-12;
-}
-
-template <>
-inline HAMON_CXX11_CONSTEXPR double get_error<long double>()
-{
-	return 1e-12;
-}
 
 template <typename T>
 void AcoshTestFloat()
@@ -55,14 +34,17 @@ void AcoshTestFloat()
 	HAMON_CXX11_CONSTEXPR auto inf = hamon::numeric_limits<T>::infinity();
 	HAMON_CXX11_CONSTEXPR auto eps = hamon::numeric_limits<T>::epsilon();
 
-	HAMON_CXX11_CONSTEXPR double error = get_error<T>();
+	HAMON_CXX11_CONSTEXPR double error = hamon::is_same<T, float>::value ? 1e-6 : 1e-12;
 
-	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(0.000000000000000000000, (double)hamon::acosh(T(1.0)), error);
-	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(0.962423650119206894995, (double)hamon::acosh(T(1.5)), error);
-	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(1.316957896924816708625, (double)hamon::acosh(T(2.0)), error);
-	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(1.566799236972411078664, (double)hamon::acosh(T(2.5)), error);
-	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(1.762747174039086050465, (double)hamon::acosh(T(3.0)), error);
-	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(1.924847300238413789991, (double)hamon::acosh(T(3.5)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(0.0000000000000000000000000000000000000000000000000, (double)hamon::acosh(T(1.0)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(0.9624236501192068949955178268487368462703686687713, (double)hamon::acosh(T(1.5)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(1.3169578969248167086250463473079684440269819714675, (double)hamon::acosh(T(2.0)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(1.5667992369724110786640568625804834938620823510927, (double)hamon::acosh(T(2.5)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(1.7627471740390860504652186499595846180563206565233, (double)hamon::acosh(T(3.0)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(1.9248473002384137899910356536974736925407373375426, (double)hamon::acosh(T(3.5)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(2.0634370688955605467272811726201318714565914498834, (double)hamon::acosh(T(4.0)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(2.1846437916051087266762781330721277714640033555892, (double)hamon::acosh(T(4.5)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(2.2924316695611776878007873113480154316218682400157, (double)hamon::acosh(T(5.0)), error);
 
 	// if the argument is less than 1, FE_INVALID is raised an NaN is returned
 	HAMON_CXX11_CONSTEXPR_EXPECT_TRUE(hamon::isnan(hamon::acosh(T(+1.0) - eps)));
@@ -84,11 +66,13 @@ void AcoshTestFloat()
 template <typename T>
 void AcoshTestSignedInt(void)
 {
-	HAMON_CXX11_CONSTEXPR double error = 0.000000000001;
+	HAMON_CXX11_CONSTEXPR double error = 1e-12;
 
-	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(0.000000000000000000000, (double)hamon::acosh(T(1)), error);
-	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(1.316957896924816708625, (double)hamon::acosh(T(2)), error);
-	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(1.762747174039086050465, (double)hamon::acosh(T(3)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(0.0000000000000000000000000000000000000000000000000, (double)hamon::acosh(T(1)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(1.3169578969248167086250463473079684440269819714675, (double)hamon::acosh(T(2)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(1.7627471740390860504652186499595846180563206565233, (double)hamon::acosh(T(3)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(2.0634370688955605467272811726201318714565914498834, (double)hamon::acosh(T(4)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(2.2924316695611776878007873113480154316218682400157, (double)hamon::acosh(T(5)), error);
 	HAMON_CXX11_CONSTEXPR_EXPECT_TRUE(hamon::isnan(hamon::acosh(T( 0))));
 	HAMON_CXX11_CONSTEXPR_EXPECT_TRUE(hamon::isnan(hamon::acosh(T(-1))));
 }
@@ -96,11 +80,13 @@ void AcoshTestSignedInt(void)
 template <typename T>
 void AcoshTestUnsignedInt(void)
 {
-	HAMON_CXX11_CONSTEXPR double error = 0.000000000001;
+	HAMON_CXX11_CONSTEXPR double error = 1e-12;
 
-	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(0.000000000000000000000, (double)hamon::acosh(T(1)), error);
-	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(1.316957896924816708625, (double)hamon::acosh(T(2)), error);
-	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(1.762747174039086050465, (double)hamon::acosh(T(3)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(0.0000000000000000000000000000000000000000000000000, (double)hamon::acosh(T(1)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(1.3169578969248167086250463473079684440269819714675, (double)hamon::acosh(T(2)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(1.7627471740390860504652186499595846180563206565233, (double)hamon::acosh(T(3)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(2.0634370688955605467272811726201318714565914498834, (double)hamon::acosh(T(4)), error);
+	HAMON_CXX11_CONSTEXPR_EXPECT_NEAR(2.2924316695611776878007873113480154316218682400157, (double)hamon::acosh(T(5)), error);
 	HAMON_CXX11_CONSTEXPR_EXPECT_TRUE(hamon::isnan(hamon::acosh(T( 0))));
 }
 
