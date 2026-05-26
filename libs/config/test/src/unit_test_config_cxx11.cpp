00623fc6bfa8353eefc79131ddd5c309f7a649d4
shibainuudon <shibainuudon@gmail.com>
2026-05-26 14:27:46 +0900

    [Config]gccでの警告を抑制

--- libs/config/test/src/unit_test_config_cxx11.cpp
+++ libs/config/test/src/unit_test_config_cxx11.cpp
@@ -541,6 +541,9 @@ HAMON_WARNING_DISABLE_MSVC(5311)	// '演算子文字列-リテラル識別子' �
 #if defined(HAMON_CLANG_VERSION) && (HAMON_CLANG_VERSION >= 170000)
 HAMON_WARNING_DISABLE_CLANG("-Wdeprecated-literal-operator")
 #endif
+#if defined(HAMON_GCC_VERSION) && (HAMON_GCC_VERSION >= 150000)
+HAMON_WARNING_DISABLE_GCC("-Wdeprecated-literal-operator")
+#endif
 
 int operator"" _ki(unsigned long long)
 {
