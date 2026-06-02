6f54a6712c2f124c0a3aec5605f0e7e1b7649e4a
shibainuudon <shibainuudon@gmail.com>
2026-06-02 13:03:01 +0900

    [FlatMap]gcc16での警告を抑制

--- libs/flat_map/test/src/flat_multimap/unit_test_flat_multimap_incomplete_type.cpp
+++ libs/flat_map/test/src/flat_multimap/unit_test_flat_multimap_incomplete_type.cpp
@@ -13,6 +13,11 @@ namespace hamon_flat_multimap_test
 namespace incomplete_type_test
 {
 
+HAMON_WARNING_PUSH()
+#if defined(HAMON_GCC_VERSION) && (HAMON_GCC_VERSION >= 160000)
+HAMON_WARNING_DISABLE_GCC("-Wsfinae-incomplete")
+#endif
+
 // Check that std::flat_multimap and its iterators can be instantiated with an incomplete
 // type.
 
@@ -24,6 +29,8 @@ struct A
 	Map::iterator it;
 	Map::const_iterator cit;
 };
+
+HAMON_WARNING_POP()
 
 // Implement the operator< required in order to instantiate flat_multimap<A, X>
 constexpr bool operator<(A const& L, A const& R) { return L.data < R.data; }
