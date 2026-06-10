8e5c7fae549466ab00ca8d6c6d03396a073bd750
shibainuudon <shibainuudon@gmail.com>
2026-06-10 16:23:06 +0900

    [Config]HAMON_HAS_CXX26_REMOVE_UNDEFINED_BEHAVIOR_FROM_LEXING のテストを追加

---
+++ libs/config/test/src/unit_test_config_cxx26.cpp
@@ -0,0 +1,31 @@
+﻿/**
+ *	@file	unit_test_config_cxx26.cpp
+ *
+ *	@brief	
+ */
+
+#include <hamon/config.hpp>
+#include <gtest/gtest.h>
+
+namespace hamon_config_cxx26_test
+{
+
+#if defined(HAMON_HAS_CXX26_REMOVE_UNDEFINED_BEHAVIOR_FROM_LEXING)
+namespace remove_undefined_behavior_from_lexing_test
+{
+
+// UB : universal character name accross spliced lines
+int \u\
+0\
+3\
+9\
+1 = 0;
+
+#define CONCAT(x, y) x ## y
+int CONCAT(\, u0393) = 0; // UB: universal character name formed by macro expansion
+#undef CONCAT
+
+}	// namespace remove_undefined_behavior_from_lexing_test
+#endif
+
+}	// namespace hamon_config_cxx26_test
