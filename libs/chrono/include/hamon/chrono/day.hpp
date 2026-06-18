838a75fbb68cdc884354d7258e45eab72f8df08b
shibainuudon <shibainuudon@gmail.com>
2026-06-18 13:22:51 +0900

    [Chrono]stdを使うとき、dayのリテラルが定義されていなかったので修正

--- libs/chrono/include/hamon/chrono/day.hpp
+++ libs/chrono/include/hamon/chrono/day.hpp
@@ -245,6 +245,13 @@ from_stream(
 #endif
 
 }	// namespace chrono
+}	// namespace hamon
+
+#endif
+
+#include <hamon/config.hpp>
+
+namespace hamon {
 
 inline namespace literals {
 inline namespace chrono_literals {
@@ -267,7 +274,5 @@ using namespace literals::chrono_literals;
 }
 
 }	// namespace hamon
-
-#endif
 
 #endif // HAMON_CHRONO_DAY_HPP
