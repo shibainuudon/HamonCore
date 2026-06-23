a0ea4c45584f96c4ff2469dd986abe60f3a5b819
shibainuudon <shibainuudon@gmail.com>
2026-06-23 11:55:59 +0900

    [Detail]HAMON_AUTO_CAST マクロを追加

---
+++ libs/detail/include/hamon/detail/auto_cast.hpp
@@ -0,0 +1,15 @@
+﻿/**
+ *	@file	auto_cast.hpp
+ *
+ *	@brief	HAMON_AUTO_CAST の定義
+ */
+
+#ifndef HAMON_DETAIL_AUTO_CAST_HPP
+#define HAMON_DETAIL_AUTO_CAST_HPP
+
+#include <hamon/type_traits/decay.hpp>
+
+// C++23 で追加された、auto(x) 形式のキャスト
+#define HAMON_AUTO_CAST(expr) static_cast<hamon::decay_t<decltype((expr))>>(expr)
+
+#endif // HAMON_DETAIL_AUTO_CAST_HPP
