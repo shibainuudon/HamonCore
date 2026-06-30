9dfe6ad9ec607130b00f2ea45e7ac535fde946c7
shibainuudon <shibainuudon@gmail.com>
2026-06-30 15:31:02 +0900

    [Ranges]empty_viewのテストを修正
    * テンプレートパラメータ制約のテストを追加
    * コンセプトのテストを追加
    * 継承したメンバ関数のテストを追加

--- libs/ranges/test/src/factories/unit_test_ranges_empty_view.cpp
+++ libs/ranges/test/src/factories/unit_test_ranges_empty_view.cpp
@@ -5,48 +5,168 @@
  */
 
 #include <hamon/ranges/factories/empty_view.hpp>
-#include <hamon/ranges/concepts/view.hpp>
-#include <hamon/ranges/concepts/borrowed_range.hpp>
+#include <hamon/ranges/concepts.hpp>
 #include <hamon/ranges/begin.hpp>
 #include <hamon/ranges/end.hpp>
 #include <hamon/ranges/data.hpp>
 #include <hamon/ranges/size.hpp>
 #include <hamon/ranges/empty.hpp>
+#include <hamon/ranges/cbegin.hpp>
+#include <hamon/ranges/cend.hpp>
+#include <hamon/type_traits/bool_constant.hpp>
 #include <hamon/type_traits/is_same.hpp>
+#include <hamon/type_traits/void_t.hpp>
 #include <hamon/config.hpp>
 #include <gtest/gtest.h>
 #include "constexpr_test.hpp"
+#include "range_test_helper.hpp"
 
 namespace hamon_ranges_test
 {
 namespace empty_view_test
 {
 
+struct Copyable
+{
+	Copyable(Copyable const&);
+};
+
+struct Movable
+{
+	Movable(Movable const&) = delete;
+	Movable(Movable &&);
+};
+
+struct NonMovable
+{
+	NonMovable(NonMovable const&) = delete;
+	NonMovable(NonMovable &&)     = delete;
+};
+
+template <typename T, typename = void>
+struct CanInstantiateEmptyView
+	: hamon::false_type {};
+
+template <typename T>
+struct CanInstantiateEmptyView<T, hamon::void_t<hamon::ranges::empty_view<T>>>
+	: hamon::true_type {};
+
+static_assert( CanInstantiateEmptyView<int>::value, "");
+static_assert( CanInstantiateEmptyView<float>::value, "");
+static_assert( CanInstantiateEmptyView<Copyable>::value, "");
+static_assert( CanInstantiateEmptyView<Movable>::value, "");
+static_assert( CanInstantiateEmptyView<NonMovable>::value, "");
+static_assert(!CanInstantiateEmptyView<void>::value, "");
+
 #define VERIFY(...)	if (!(__VA_ARGS__)) { return false; }
 
 template <typename T>
 HAMON_CXX14_CONSTEXPR bool test()
 {
-	static_assert(hamon::ranges::view_t<hamon::ranges::empty_view<T>>::value, "");
-	static_assert(hamon::ranges::borrowed_range_t<hamon::ranges::empty_view<T>>::value, "");
+	using EV = hamon::ranges::empty_view<T>;
+	using CEV = EV const;
 
-	hamon::ranges::empty_view<T> e{};
+	static_assert( hamon::ranges::range_t<EV>::value, "");
+	static_assert( hamon::ranges::borrowed_range_t<EV>::value, "");
+	static_assert( hamon::ranges::sized_range_t<EV>::value, "");
+	static_assert( hamon::ranges::approximately_sized_range_t<EV>::value, "");
+	static_assert( hamon::ranges::output_range_t<EV, T>::value, "");
+	static_assert( hamon::ranges::input_range_t<EV>::value, "");
+	static_assert( hamon::ranges::forward_range_t<EV>::value, "");
+	static_assert( hamon::ranges::bidirectional_range_t<EV>::value, "");
+	static_assert( hamon::ranges::random_access_range_t<EV>::value, "");
+	static_assert( hamon::ranges::contiguous_range_t<EV>::value, "");
+	static_assert( hamon::ranges::common_range_t<EV>::value, "");
+	static_assert( hamon::ranges::viewable_range_t<EV>::value, "");
+	static_assert( hamon::ranges::view_t<EV>::value, "");
+	static_assert(!hamon::ranges::constant_range_t<EV>::value, "");
 
-	VERIFY(e.begin() == nullptr);
-	VERIFY(e.end()   == nullptr);
-	VERIFY(e.data()  == nullptr);
-	VERIFY(e.size()  == 0);
-	VERIFY(e.empty());
+	static_assert( hamon::ranges::range_t<CEV>::value, "");
+	static_assert( hamon::ranges::borrowed_range_t<CEV>::value, "");
+	static_assert( hamon::ranges::sized_range_t<CEV>::value, "");
+	static_assert( hamon::ranges::approximately_sized_range_t<CEV>::value, "");
+	static_assert( hamon::ranges::output_range_t<CEV, T>::value, "");
+	static_assert( hamon::ranges::input_range_t<CEV>::value, "");
+	static_assert( hamon::ranges::forward_range_t<CEV>::value, "");
+	static_assert( hamon::ranges::bidirectional_range_t<CEV>::value, "");
+	static_assert( hamon::ranges::random_access_range_t<CEV>::value, "");
+	static_assert( hamon::ranges::contiguous_range_t<CEV>::value, "");
+	static_assert( hamon::ranges::common_range_t<CEV>::value, "");
+	static_assert( hamon::ranges::viewable_range_t<CEV>::value, "");
+	static_assert(!hamon::ranges::view_t<CEV>::value, "");
+	static_assert(!hamon::ranges::constant_range_t<CEV>::value, "");
 
-	VERIFY(hamon::ranges::begin(e) == nullptr);
-	VERIFY(hamon::ranges::end(e)   == nullptr);
-	VERIFY(hamon::ranges::data(e)  == nullptr);
-	VERIFY(hamon::ranges::size(e)  == 0);
-	VERIFY(hamon::ranges::empty(e));
+	static_assert( has_begin<EV>::value, "");
+	static_assert( has_end<EV>::value, "");
+	static_assert( has_empty<EV>::value, "");
+	static_assert( has_cbegin<EV>::value, "");
+	static_assert( has_cend<EV>::value, "");
+	static_assert( has_operator_bool<EV>::value, "");
+	static_assert( has_data<EV>::value, "");
+	static_assert( has_size<EV>::value, "");
+	static_assert( has_front<EV>::value, "");
+	static_assert( has_back<EV>::value, "");
+	static_assert( has_subscript<EV>::value, "");
+
+	static_assert( has_begin<CEV>::value, "");
+	static_assert( has_end<CEV>::value, "");
+	static_assert( has_empty<CEV>::value, "");
+	static_assert( has_cbegin<CEV>::value, "");
+	static_assert( has_cend<CEV>::value, "");
+	static_assert( has_operator_bool<CEV>::value, "");
+	static_assert( has_data<CEV>::value, "");
+	static_assert( has_size<CEV>::value, "");
+	static_assert( has_front<CEV>::value, "");
+	static_assert( has_back<CEV>::value, "");
+	static_assert( has_subscript<CEV>::value, "");
+
+	EV ev{};
+
+	VERIFY(ev.begin() == nullptr);
+	VERIFY(ev.end()   == nullptr);
+	VERIFY(ev.data()  == nullptr);
+	VERIFY(ev.size()  == 0);
+	VERIFY(ev.empty());
+	VERIFY(ev.cbegin() == nullptr);
+	VERIFY(ev.cend()   == nullptr);
+	VERIFY((bool)ev    == false);
+//	VERIFY(ev.front()  == T{});
+//	VERIFY(ev.back()   == T{});
+//	VERIFY(ev[0]       == T{});
+
+	VERIFY(hamon::ranges::begin(ev) == nullptr);
+	VERIFY(hamon::ranges::end(ev)   == nullptr);
+	VERIFY(hamon::ranges::data(ev)  == nullptr);
+	VERIFY(hamon::ranges::size(ev)  == 0);
+	VERIFY(hamon::ranges::empty(ev));
+	VERIFY(hamon::ranges::cbegin(ev) == nullptr);
+	VERIFY(hamon::ranges::cend(ev)   == nullptr);
+
+	CEV cev{};
+
+	VERIFY(cev.begin() == nullptr);
+	VERIFY(cev.end()   == nullptr);
+	VERIFY(cev.data()  == nullptr);
+	VERIFY(cev.size()  == 0);
+	VERIFY(cev.empty());
+	VERIFY(cev.cbegin() == nullptr);
+	VERIFY(cev.cend()   == nullptr);
+	VERIFY((bool)cev    == false);
+//	VERIFY(cev.front()  == T{});
+//	VERIFY(cev.back()   == T{});
+//	VERIFY(cev[0]       == T{});
+
+	VERIFY(hamon::ranges::begin(cev) == nullptr);
+	VERIFY(hamon::ranges::end(cev)   == nullptr);
+	VERIFY(hamon::ranges::data(cev)  == nullptr);
+	VERIFY(hamon::ranges::size(cev)  == 0);
+	VERIFY(hamon::ranges::empty(cev));
+	VERIFY(hamon::ranges::cbegin(cev) == nullptr);
+	VERIFY(hamon::ranges::cend(cev)   == nullptr);
 
 #if defined(HAMON_HAS_CXX14_VARIABLE_TEMPLATES)
 	auto e2 = hamon::views::empty<T>;
-	static_assert(hamon::is_same<decltype(e2), hamon::ranges::empty_view<T>>::value, "");
+	static_assert(hamon::is_same<decltype(e2), EV>::value, "");
 #endif
 
 	return true;
---
+++ libs/ranges/test/src/range_test_helper.hpp
@@ -0,0 +1,120 @@
+﻿/**
+ *	@file	range_test_helper.hpp
+ *
+ *	@brief
+ */
+
+#ifndef HAMON_RANGE_TEST_HELPER_HPP
+#define HAMON_RANGE_TEST_HELPER_HPP
+
+#include <hamon/ranges/range_difference_t.hpp>
+#include <hamon/type_traits/bool_constant.hpp>
+#include <hamon/type_traits/void_t.hpp>
+#include <hamon/utility/declval.hpp>
+
+namespace hamon_ranges_test
+{
+
+// begin()
+template <typename T, typename = void>
+struct has_begin
+	: public hamon::false_type {};
+
+template <typename T>
+struct has_begin<T, hamon::void_t<decltype(hamon::declval<T&>().begin())>>
+	: public hamon::true_type {};
+
+// end()
+template <typename T, typename = void>
+struct has_end
+	: public hamon::false_type {};
+
+template <typename T>
+struct has_end<T, hamon::void_t<decltype(hamon::declval<T&>().end())>>
+	: public hamon::true_type {};
+
+// empty()
+template <typename T, typename = void>
+struct has_empty
+	: public hamon::false_type {};
+
+template <typename T>
+struct has_empty<T, hamon::void_t<decltype(hamon::declval<T&>().empty())>>
+	: public hamon::true_type {};
+
+// cbegin()
+template <typename T, typename = void>
+struct has_cbegin
+	: public hamon::false_type {};
+
+template <typename T>
+struct has_cbegin<T, hamon::void_t<decltype(hamon::declval<T&>().cbegin())>>
+	: public hamon::true_type {};
+
+// cend()
+template <typename T, typename = void>
+struct has_cend
+	: public hamon::false_type {};
+
+template <typename T>
+struct has_cend<T, hamon::void_t<decltype(hamon::declval<T&>().cend())>>
+	: public hamon::true_type {};
+
+// operator bool()
+template <typename T, typename = void>
+struct has_operator_bool
+	: public hamon::false_type {};
+
+template <typename T>
+struct has_operator_bool<T, hamon::void_t<decltype(hamon::declval<T&>().operator bool())>>
+	: public hamon::true_type {};
+
+// data()
+template <typename T, typename = void>
+struct has_data
+	: public hamon::false_type {};
+
+template <typename T>
+struct has_data<T, hamon::void_t<decltype(hamon::declval<T&>().data())>>
+	: public hamon::true_type {};
+
+// size()
+template <typename T, typename = void>
+struct has_size
+	: public hamon::false_type {};
+
+template <typename T>
+struct has_size<T, hamon::void_t<decltype(hamon::declval<T&>().size())>>
+	: public hamon::true_type {};
+
+// front()
+template <typename T, typename = void>
+struct has_front
+	: public hamon::false_type {};
+
+template <typename T>
+struct has_front<T, hamon::void_t<decltype(hamon::declval<T&>().front())>>
+	: public hamon::true_type {};
+
+// back()
+template <typename T, typename = void>
+struct has_back
+	: public hamon::false_type {};
+
+template <typename T>
+struct has_back<T, hamon::void_t<decltype(hamon::declval<T&>().back())>>
+	: public hamon::true_type {};
+
+// operator[]
+template <typename T, typename = void>
+struct has_subscript
+	: public hamon::false_type {};
+
+template <typename T>
+struct has_subscript<T, hamon::void_t<decltype(hamon::declval<T&>()[hamon::declval<hamon::ranges::range_difference_t<T>>()])>>
+	: public hamon::true_type {};
+
+
+}	// namespace hamon_ranges_test
+
+#endif // HAMON_RANGE_TEST_HELPER_HPP
