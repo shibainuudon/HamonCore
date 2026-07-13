06e34a20f63a2cb583a8afb4899b952b4735b90a
shibainuudon <shibainuudon@gmail.com>
2026-07-13 10:47:03 +0900

    [Iterator]detail::has_member_reference コンセプトの定義を修正

--- libs/iterator/include/hamon/iterator/detail/has_member_reference.hpp
+++ libs/iterator/include/hamon/iterator/detail/has_member_reference.hpp
@@ -20,23 +20,21 @@ namespace detail
 #if defined(HAMON_HAS_CXX20_CONCEPTS)
 
 template <typename T>
-concept has_member_reference = requires { typename T::reference; };
-
-template <typename T>
-using has_member_reference_t = hamon::bool_constant<has_member_reference<T>>;
+HAMON_CONCEPT_OR_BOOL has_member_reference = requires { typename T::reference; };
 
 #else
 
 template <typename, typename = void>
-struct has_member_reference
+struct has_member_reference_impl
 	: public hamon::false_type {};
  
 template <typename T>
-struct has_member_reference<T, hamon::void_t<typename T::reference>>
+struct has_member_reference_impl<T, hamon::void_t<typename T::reference>>
 	: public hamon::true_type {};
 
 template <typename T>
-using has_member_reference_t = has_member_reference<T>;
+HAMON_CONCEPT_OR_BOOL has_member_reference =
+	detail::has_member_reference_impl<T>::value;
 
 #endif
 
--- libs/iterator/include/hamon/iterator/detail/iterator_traits_base.hpp
+++ libs/iterator/include/hamon/iterator/detail/iterator_traits_base.hpp
@@ -132,7 +132,7 @@ template <typename I>
 using with_nested_types = hamon::bool_constant<
 	hamon::detail::has_member_difference_type<I> &&
 	hamon::detail::has_member_value_type_t<I>::value &&
-	hamon::detail::has_member_reference_t<I>::value &&
+	hamon::detail::has_member_reference<I> &&
 	hamon::detail::has_member_iterator_category<I>
 >;
 
