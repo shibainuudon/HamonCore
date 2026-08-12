400d9f03ed32b1b1c61f7443e7ab3ecfafd259eb
shibainuudon <shibainuudon@gmail.com>
2026-08-12 17:41:39 +0900

    [TypeTraits]std::is_member_object_pointer を使わずに常に自前実装を使うようにした

--- libs/type_traits/include/hamon/type_traits/is_member_object_pointer.hpp
+++ libs/type_traits/include/hamon/type_traits/is_member_object_pointer.hpp
@@ -8,11 +8,14 @@
 #define HAMON_TYPE_TRAITS_IS_MEMBER_OBJECT_POINTER_HPP
 
 #include <hamon/type_traits/bool_constant.hpp>
+#include <hamon/type_traits/is_member_function_pointer.hpp>
+#include <hamon/type_traits/is_member_pointer.hpp>
 #include <hamon/config.hpp>
-#include <type_traits>
 
 namespace hamon
 {
+
+// 21.3.5.2 Primary type categories[meta.unary.cat]
 
 /**
  *	@brief	型Tがデータメンバへのポインタか調べる
@@ -28,17 +31,14 @@ namespace hamon
 template <typename T>
 struct is_member_object_pointer
 	: public hamon::bool_constant<
-		std::is_member_object_pointer<T>::value
+		hamon::is_member_pointer<T>::value &&
+		!hamon::is_member_function_pointer<T>::value
 	>
 {};
 
-#if defined(HAMON_HAS_CXX14_VARIABLE_TEMPLATES)
-
 template <typename T>
-HAMON_INLINE_VAR HAMON_CONSTEXPR
+HAMON_INLINE_VAR HAMON_CXX11_CONSTEXPR
 bool is_member_object_pointer_v = is_member_object_pointer<T>::value;
-
-#endif
 
 }	// namespace hamon
 
