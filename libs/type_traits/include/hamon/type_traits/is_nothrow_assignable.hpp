1bd4334d02135ca09fc31630eff4124543406bab
shibainuudon <shibainuudon@gmail.com>
2026-08-15 18:41:21 +0900

    [TypeTraits]std::is_nothrow_assignable を使わずに常に自前実装を使うようにした

--- libs/type_traits/include/hamon/type_traits/is_nothrow_assignable.hpp
+++ libs/type_traits/include/hamon/type_traits/is_nothrow_assignable.hpp
@@ -9,10 +9,11 @@
 
 #include <hamon/type_traits/bool_constant.hpp>
 #include <hamon/config.hpp>
-#include <type_traits>
 
 namespace hamon
 {
+
+// 21.3.5.4 Type properties[meta.unary.prop]
 
 /**
  *	@brief	型Tが型Uから代入可能で、かつ代入操作が例外を投げないかを調べる。
@@ -30,17 +31,17 @@ namespace hamon
 template <typename T, typename U>
 struct is_nothrow_assignable
 	: public hamon::bool_constant<
-		std::is_nothrow_assignable<T, U>::value
+#if HAMON_HAS_BUILTIN(__is_nothrow_assignable) || defined(HAMON_MSVC)
+		__is_nothrow_assignable(T, U)
+#else
+		// TODO
+#endif
 	>
 {};
 
-#if defined(HAMON_HAS_CXX14_VARIABLE_TEMPLATES)
-
 template <typename T, typename U>
-HAMON_INLINE_VAR HAMON_CONSTEXPR
+HAMON_INLINE_VAR HAMON_CXX11_CONSTEXPR
 bool is_nothrow_assignable_v = is_nothrow_assignable<T, U>::value;
-
-#endif
 
 }	// namespace hamon
 
