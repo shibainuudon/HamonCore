49e1543024950504407b969a47d75b063308a904
shibainuudon <shibainuudon@gmail.com>
2026-08-19 14:58:08 +0900

    [TypeTraits]std::is_nothrow_convertible を使わずに常に自前実装を使うようにした

---
+++ libs/type_traits/include/hamon/type_traits/detail/is_nothrow_convertible_impl.hpp
@@ -0,0 +1,52 @@
+﻿/**
+ *	@file	is_nothrow_convertible_impl.hpp
+ *
+ *	@brief	is_nothrow_convertible_impl の定義
+ */
+
+#ifndef HAMON_TYPE_TRAITS_DETAIL_IS_NOTHROW_CONVERTIBLE_IMPL_HPP
+#define HAMON_TYPE_TRAITS_DETAIL_IS_NOTHROW_CONVERTIBLE_IMPL_HPP
+
+#include <hamon/type_traits/bool_constant.hpp>
+#include <hamon/type_traits/is_convertible.hpp>
+#include <hamon/type_traits/is_void.hpp>
+#include <hamon/utility/declval.hpp>
+
+namespace hamon
+{
+namespace detail
+{
+
+HAMON_WARNING_PUSH()
+HAMON_WARNING_DISABLE_MSVC(4244)	// '引数': '_Ty1' から 'To' への変換です。データが失われる可能性があります。
+HAMON_WARNING_DISABLE_CLANG("-Wimplicit-int-conversion")
+HAMON_WARNING_DISABLE_CLANG("-Wimplicit-int-float-conversion")
+
+template <typename To>
+static void is_nothrow_convertible_impl_helper(To) noexcept;
+
+template <typename From, typename To,
+	bool = hamon::is_convertible_v<From, To>,
+	bool = hamon::is_void_v<From> && hamon::is_void_v<To>
+>
+struct is_nothrow_convertible_impl
+	: public hamon::false_type
+{};
+
+template <typename From, typename To>
+struct is_nothrow_convertible_impl<From, To, true, true>
+	: public hamon::true_type
+{};
+
+template <typename From, typename To>
+struct is_nothrow_convertible_impl<From, To, true, false>
+	: public hamon::bool_constant<
+		noexcept(is_nothrow_convertible_impl_helper<To>(hamon::declval<From>()))>
+{};
+
+HAMON_WARNING_POP()
+
+}	// namespace detail
+}	// namespace hamon
+
+#endif // HAMON_TYPE_TRAITS_DETAIL_IS_NOTHROW_CONVERTIBLE_IMPL_HPP
--- libs/type_traits/include/hamon/type_traits/is_nothrow_convertible.hpp
+++ libs/type_traits/include/hamon/type_traits/is_nothrow_convertible.hpp
@@ -7,30 +7,14 @@
 #ifndef HAMON_TYPE_TRAITS_IS_NOTHROW_CONVERTIBLE_HPP
 #define HAMON_TYPE_TRAITS_IS_NOTHROW_CONVERTIBLE_HPP
 
-#include <hamon/config.hpp>
-#include <type_traits>
-
-#if defined(__cpp_lib_is_nothrow_convertible) && (__cpp_lib_is_nothrow_convertible >= 201806) && \
-	defined(__cpp_lib_integral_constant_callable) && (__cpp_lib_integral_constant_callable >= 201304)
-
-namespace hamon
-{
-
-using std::is_nothrow_convertible;
-
-}	// namespace hamon
-
-#else
-
-#include <hamon/type_traits/conjunction.hpp>
-#include <hamon/type_traits/disjunction.hpp>
 #include <hamon/type_traits/bool_constant.hpp>
-#include <hamon/type_traits/is_void.hpp>
-#include <hamon/type_traits/is_convertible.hpp>
-#include <hamon/utility/declval.hpp>
+#include <hamon/type_traits/detail/is_nothrow_convertible_impl.hpp>
+#include <hamon/config.hpp>
 
 namespace hamon
 {
+
+// 21.3.7 Relationships between types[meta.rel]
 
 /**
  *	@brief		型Fromから型Toに例外を投げずに暗黙的に変換可能か調べる。
@@ -47,58 +31,19 @@ namespace hamon
  *	explicitなコンストラクタによる明示的な型変換は、暗黙的に変換可能であるとは見なされない。
  */
 template <typename From, typename To>
-struct is_nothrow_convertible;
-
-namespace detail
-{
-
-template <typename T>
-static void test_noexcept(T) noexcept;
-
-HAMON_WARNING_PUSH()
-HAMON_WARNING_DISABLE_CLANG("-Wimplicit-int-conversion")
-HAMON_WARNING_DISABLE_CLANG("-Wimplicit-int-float-conversion")
-
-template <typename From, typename To>
-static hamon::bool_constant<noexcept(test_noexcept<To>(hamon::declval<From>()))>
-is_nothrow_convertible_test();
-
-HAMON_WARNING_POP()
-
-template <typename From, typename To>
-struct is_nothrow_convertible_helper : decltype(is_nothrow_convertible_test<From, To>())
-{};
-
-}	// namespace detail
-
-template <typename From, typename To>
-struct is_nothrow_convertible :
-	hamon::disjunction<
-		hamon::conjunction<
-			hamon::is_void<To>,
-			hamon::is_void<From>
-		>,
-		hamon::conjunction<
-			hamon::is_convertible<From, To>,
-			detail::is_nothrow_convertible_helper<From, To>
-		>
+struct is_nothrow_convertible
+	: public hamon::bool_constant<
+#if HAMON_HAS_BUILTIN(__is_nothrow_convertible)
+		__is_nothrow_convertible(From, To)
+#else
+		hamon::detail::is_nothrow_convertible_impl<From, To>::value
+#endif
 	>
 {};
 
-}	// namespace hamon
-
-#endif
-
-namespace hamon
-{
-
-#if defined(HAMON_HAS_CXX14_VARIABLE_TEMPLATES)
-
 template <typename From, typename To>
-HAMON_INLINE_VAR HAMON_CONSTEXPR
+HAMON_INLINE_VAR HAMON_CXX11_CONSTEXPR
 bool is_nothrow_convertible_v = is_nothrow_convertible<From, To>::value;
-
-#endif
 
 }	// namespace hamon
 
