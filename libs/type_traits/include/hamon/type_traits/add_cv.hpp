3ff842b9de0964249b623736ae8b3ba03d0b63c8
shibainuudon <shibainuudon@gmail.com>
2026-08-19 15:25:09 +0900

    [TypeTraits]std::add_cv を使わずに常に自前実装を使うようにした

--- libs/type_traits/include/hamon/type_traits/add_cv.hpp
+++ libs/type_traits/include/hamon/type_traits/add_cv.hpp
@@ -7,10 +7,13 @@
 #ifndef HAMON_TYPE_TRAITS_ADD_CV_HPP
 #define HAMON_TYPE_TRAITS_ADD_CV_HPP
 
-#include <type_traits>
+#include <hamon/type_traits/add_const.hpp>
+#include <hamon/type_traits/add_volatile.hpp>
 
 namespace hamon
 {
+
+// 21.3.8.2 Const-volatile modifications[meta.trans.cv]
 
 /**
  *	@brief		型をconst-volatile修飾する
@@ -20,7 +23,11 @@ namespace hamon
  *	add_cvは、型Tにconst修飾およびvolatile修飾両方を付加した型を、メンバ型typeとして定義する。
  *	型Tが参照、関数、すでに最上位がconst-volatile修飾された型である場合は、型Tをそのままメンバ型typeとして定義する。
  */
-using std::add_cv;
+template <typename T>
+struct add_cv
+{
+	using type = hamon::add_const_t<hamon::add_volatile_t<T>>;
+};
 
 /**
  *	@brief	add_cvのエイリアステンプレート
