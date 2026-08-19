c1d805da3698b0ad8a8f9b8ed6a57bf202e33a0a
shibainuudon <shibainuudon@gmail.com>
2026-08-19 16:08:55 +0900

    [TypeTraits]std::decay を使わずに常に自前実装を使うようにした

--- libs/type_traits/include/hamon/type_traits/decay.hpp
+++ libs/type_traits/include/hamon/type_traits/decay.hpp
@@ -7,10 +7,18 @@
 #ifndef HAMON_TYPE_TRAITS_DECAY_HPP
 #define HAMON_TYPE_TRAITS_DECAY_HPP
 
-#include <type_traits>
+#include <hamon/type_traits/add_pointer.hpp>
+#include <hamon/type_traits/conditional.hpp>
+#include <hamon/type_traits/is_array.hpp>
+#include <hamon/type_traits/is_function.hpp>
+#include <hamon/type_traits/remove_cv.hpp>
+#include <hamon/type_traits/remove_extent.hpp>
+#include <hamon/type_traits/remove_reference.hpp>
 
 namespace hamon
 {
+
+// 21.3.8.7 Other transformations[meta.trans.other]
 
 /**
  *	@brief	配列と関数ポインタに関して、関数テンプレートと同様に推論された型を取得する。
@@ -27,7 +35,23 @@ namespace hamon
  *	型Uが関数型R(Args...)であれば、R(*)(Args...)型をメンバ型typeとして定義する。
  *	それ以外の場合は、remove_cv<U>::typeした型を、メンバ型typeとして定義する。
  */
-using std::decay;
+template <typename T>
+struct decay
+{
+private:
+	using U = hamon::remove_reference_t<T>;
+
+public:
+	using type = hamon::conditional_t<
+		hamon::is_array_v<U>,
+		hamon::remove_extent_t<U>*,
+		hamon::conditional_t<
+			hamon::is_function_v<U>,
+			hamon::add_pointer_t<U>,
+			hamon::remove_cv_t<U>
+		>
+	>;
+};
 
 template <typename T>
 using decay_t = typename decay<T>::type;
