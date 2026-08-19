784b577451a803ad7114b6c2c9d9bd1e294f683c
shibainuudon <shibainuudon@gmail.com>
2026-08-19 15:24:27 +0900

    [TypeTraits]std::add_const を使わずに常に自前実装を使うようにした

--- libs/type_traits/include/hamon/type_traits/add_const.hpp
+++ libs/type_traits/include/hamon/type_traits/add_const.hpp
@@ -7,10 +7,12 @@
 #ifndef HAMON_TYPE_TRAITS_ADD_CONST_HPP
 #define HAMON_TYPE_TRAITS_ADD_CONST_HPP
 
-#include <type_traits>
+#include <hamon/config.hpp>
 
 namespace hamon
 {
+
+// 21.3.8.2 Const-volatile modifications[meta.trans.cv]
 
 /**
  *	@brief		型をconst修飾する
@@ -20,7 +22,14 @@ namespace hamon
  *	add_constは、型Tにconst修飾を付加した型を、メンバ型typeとして定義する。
  *	型Tが参照、関数、すでに最上位がconst修飾された型である場合は、型Tをそのままメンバ型typeとして定義する。
  */
-using std::add_const;
+template <typename T>
+struct add_const
+{
+HAMON_WARNING_PUSH()
+HAMON_WARNING_DISABLE_MSVC(4180)	// 関数型へ適用された修飾子は無効なため、無視されます。
+	using type = T const;
+HAMON_WARNING_POP()
+};
 
 /**
  *	@brief	add_constのエイリアステンプレート
