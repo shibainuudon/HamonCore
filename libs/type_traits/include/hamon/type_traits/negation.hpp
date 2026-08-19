b488a6ccad5964ae5042dd25245778d3db083e6f
shibainuudon <shibainuudon@gmail.com>
2026-08-19 16:43:21 +0900

    [TypeTraits]std::negation を使わずに常に自前実装を使うようにした

--- libs/type_traits/include/hamon/type_traits/negation.hpp
+++ libs/type_traits/include/hamon/type_traits/negation.hpp
@@ -7,23 +7,13 @@
 #ifndef HAMON_TYPE_TRAITS_NEGATION_HPP
 #define HAMON_TYPE_TRAITS_NEGATION_HPP
 
-#include <type_traits>
-
-#if defined(__cpp_lib_logical_traits) && (__cpp_lib_logical_traits >= 201510)
-
-namespace hamon
-{
-
-using std::negation;
-
-}	// namespace hamon
-
-#else
-
 #include <hamon/type_traits/bool_constant.hpp>
+#include <hamon/config.hpp>
 
 namespace hamon
 {
+
+// 21.3.9 Logical operator traits[meta.logical]
 
 /**
  *	@brief		コンパイル時の論理否定
@@ -35,22 +25,9 @@ namespace hamon
 template <typename B>
 struct negation : public hamon::bool_constant<!B::value> {};
 
-}	// namespace hamon
-
-#endif
-
-#include <hamon/config.hpp>
-
-namespace hamon
-{
-
-#if defined(HAMON_HAS_CXX14_VARIABLE_TEMPLATES)
-
 template <typename B>
-HAMON_INLINE_VAR HAMON_CONSTEXPR
+HAMON_INLINE_VAR HAMON_CXX11_CONSTEXPR
 bool negation_v = negation<B>::value;
-
-#endif
 
 }	// namespace hamon
 
