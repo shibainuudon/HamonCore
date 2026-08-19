1fa19ed909e5015752d47ad3438c276827394152
shibainuudon <shibainuudon@gmail.com>
2026-08-19 15:22:33 +0900

    [TypeTraits]std::remove_const を使わずに常に自前実装を使うようにした

--- libs/type_traits/include/hamon/type_traits/remove_const.hpp
+++ libs/type_traits/include/hamon/type_traits/remove_const.hpp
@@ -7,10 +7,10 @@
 #ifndef HAMON_TYPE_TRAITS_REMOVE_CONST_HPP
 #define HAMON_TYPE_TRAITS_REMOVE_CONST_HPP
 
-#include <type_traits>
-
 namespace hamon
 {
+
+// 21.3.8.2 Const-volatile modifications[meta.trans.cv]
 
 /**
  *	@brief		型のconst修飾を除去する。
@@ -19,7 +19,17 @@ namespace hamon
  *
  *	remove_constは、型Tに含まれる最上位のconst修飾を除去した型を、メンバ型typeとして定義する。
  */
-using std::remove_const;
+template <typename T>
+struct remove_const
+{
+	using type = T;
+};
+
+template <typename T>
+struct remove_const<T const>
+{
+	using type = T;
+};
 
 template <typename T>
 using remove_const_t = typename remove_const<T>::type;
