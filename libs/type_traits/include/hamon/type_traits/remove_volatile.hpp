f25f54b805220393fdda51a868da990defeb347d
shibainuudon <shibainuudon@gmail.com>
2026-08-19 15:23:10 +0900

    [TypeTraits]std::remove_volatile を使わずに常に自前実装を使うようにした

--- libs/type_traits/include/hamon/type_traits/remove_volatile.hpp
+++ libs/type_traits/include/hamon/type_traits/remove_volatile.hpp
@@ -7,10 +7,10 @@
 #ifndef HAMON_TYPE_TRAITS_REMOVE_VOLATILE_HPP
 #define HAMON_TYPE_TRAITS_REMOVE_VOLATILE_HPP
 
-#include <type_traits>
-
 namespace hamon
 {
+
+// 21.3.8.2 Const-volatile modifications[meta.trans.cv]
 
 /**
  *	@brief		型のvolatile修飾を除去する。
@@ -19,7 +19,17 @@ namespace hamon
  *
  *	remove_volatileは、型Tに含まれる最上位のvolatile修飾を除去した型を、メンバ型typeとして定義する。
  */
-using std::remove_volatile;
+template <typename T>
+struct remove_volatile
+{
+	using type = T;
+};
+
+template <typename T>
+struct remove_volatile<T volatile>
+{
+	using type = T;
+};
 
 template <typename T>
 using remove_volatile_t = typename remove_volatile<T>::type;
