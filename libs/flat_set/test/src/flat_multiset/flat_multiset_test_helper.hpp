c2362fcc408fdb9a3d7f0b6f0384f9b145ebe956
shibainuudon <shibainuudon@gmail.com>
2026-09-01 17:52:34 +0900

    [FlatSet]libstdc++でのコンパイルエラーを修正

--- libs/flat_set/test/src/flat_multiset/flat_multiset_test_helper.hpp
+++ libs/flat_set/test/src/flat_multiset/flat_multiset_test_helper.hpp
@@ -292,10 +292,10 @@ template <typename T>
 struct TestAllocator2
 {
 	using value_type = T;
-	using is_always_equal = hamon::false_type;
-	using propagate_on_container_copy_assignment = hamon::true_type;
-	using propagate_on_container_move_assignment = hamon::true_type;
-	using propagate_on_container_swap = hamon::true_type;
+	using is_always_equal = std::false_type;
+	using propagate_on_container_copy_assignment = std::true_type;
+	using propagate_on_container_move_assignment = std::true_type;
+	using propagate_on_container_swap = std::true_type;
 
 	int id;
 
@@ -340,7 +340,7 @@ template <typename T>
 struct TestAllocator3
 {
 	using value_type = T;
-	using is_always_equal = hamon::false_type;
+	using is_always_equal = std::false_type;
 
 	int id;
 
@@ -393,9 +393,9 @@ template <typename T>
 struct TestAllocator4
 {
 	using value_type = T;
-	using is_always_equal = hamon::false_type;
-	using propagate_on_container_copy_assignment = hamon::true_type;
-	using propagate_on_container_move_assignment = hamon::false_type;
+	using is_always_equal = std::false_type;
+	using propagate_on_container_copy_assignment = std::true_type;
+	using propagate_on_container_move_assignment = std::false_type;
 
 	int id;
 
@@ -441,9 +441,9 @@ template <typename T>
 struct TestAllocator5
 {
 	using value_type = T;
-	using is_always_equal = hamon::false_type;
-	using propagate_on_container_copy_assignment = hamon::false_type;
-	using propagate_on_container_move_assignment = hamon::true_type;
+	using is_always_equal = std::false_type;
+	using propagate_on_container_copy_assignment = std::false_type;
+	using propagate_on_container_move_assignment = std::true_type;
 
 	int id;
 
@@ -488,8 +488,8 @@ template <typename T>
 struct TestAllocator6
 {
 	using value_type = T;
-	using is_always_equal = hamon::false_type;
-	using propagate_on_container_swap = hamon::false_type;
+	using is_always_equal = std::false_type;
+	using propagate_on_container_swap = std::false_type;
 
 	int id;
 
