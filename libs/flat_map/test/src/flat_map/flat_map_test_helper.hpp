1ecc398832a336583376a1550eefe49003f2fbd1
shibainuudon <shibainuudon@gmail.com>
2026-09-01 17:51:47 +0900

    [FlatMap]libstdc++でのコンパイルエラーを修正

--- libs/flat_map/test/src/flat_map/flat_map_test_helper.hpp
+++ libs/flat_map/test/src/flat_map/flat_map_test_helper.hpp
@@ -321,10 +321,10 @@ template <typename T>
 struct TestAllocator2
 {
 	using value_type = T;
-	using is_always_equal = hamon::false_type;
-	using propagate_on_container_copy_assignment = hamon::true_type;
-	using propagate_on_container_move_assignment = hamon::true_type;
-	using propagate_on_container_swap = hamon::true_type;
+	using is_always_equal = std::false_type;
+	using propagate_on_container_copy_assignment = std::true_type;
+	using propagate_on_container_move_assignment = std::true_type;
+	using propagate_on_container_swap = std::true_type;
 
 	int id;
 
@@ -369,7 +369,7 @@ template <typename T>
 struct TestAllocator3
 {
 	using value_type = T;
-	using is_always_equal = hamon::false_type;
+	using is_always_equal = std::false_type;
 
 	int id;
 
@@ -422,9 +422,9 @@ template <typename T>
 struct TestAllocator4
 {
 	using value_type = T;
-	using is_always_equal = hamon::false_type;
-	using propagate_on_container_copy_assignment = hamon::true_type;
-	using propagate_on_container_move_assignment = hamon::false_type;
+	using is_always_equal = std::false_type;
+	using propagate_on_container_copy_assignment = std::true_type;
+	using propagate_on_container_move_assignment = std::false_type;
 
 	int id;
 
@@ -470,9 +470,9 @@ template <typename T>
 struct TestAllocator5
 {
 	using value_type = T;
-	using is_always_equal = hamon::false_type;
-	using propagate_on_container_copy_assignment = hamon::false_type;
-	using propagate_on_container_move_assignment = hamon::true_type;
+	using is_always_equal = std::false_type;
+	using propagate_on_container_copy_assignment = std::false_type;
+	using propagate_on_container_move_assignment = std::true_type;
 
 	int id;
 
@@ -517,8 +517,8 @@ template <typename T>
 struct TestAllocator6
 {
 	using value_type = T;
-	using is_always_equal = hamon::false_type;
-	using propagate_on_container_swap = hamon::false_type;
+	using is_always_equal = std::false_type;
+	using propagate_on_container_swap = std::false_type;
 
 	int id;
 
