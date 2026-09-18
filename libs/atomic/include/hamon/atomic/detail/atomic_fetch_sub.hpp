1eb7550b336a8d1ec68571a2ca86b28ff793d7d9
shibainuudon <shibainuudon@gmail.com>
2026-09-18 10:45:14 +0900

    fix atomic_fetch_add/sub

--- libs/atomic/include/hamon/atomic/detail/atomic_fetch_sub.hpp
+++ libs/atomic/include/hamon/atomic/detail/atomic_fetch_sub.hpp
@@ -14,14 +14,11 @@
 #include <hamon/atomic/detail/to_gcc_memory_order.hpp>
 #include <hamon/concepts/arithmetic.hpp>
 #include <hamon/concepts/detail/constraint.hpp>
+#include <hamon/concepts/floating_point.hpp>
 #include <hamon/concepts/integral.hpp>
 #include <hamon/detail/overload_priority.hpp>
 #include <hamon/memory/addressof.hpp>
-#include <hamon/type_traits/bool_constant.hpp>
-#include <hamon/type_traits/enable_if.hpp>
 #include <hamon/type_traits/is_constant_evaluated.hpp>
-#include <hamon/type_traits/void_t.hpp>
-#include <hamon/utility/declval.hpp>
 #include <hamon/utility/exchange.hpp>
 #include <hamon/config.hpp>
 
@@ -30,23 +27,22 @@ namespace hamon
 namespace detail
 {
 
-template <typename T, typename = void>
-struct has_builtin_atomic_fetch_sub
-	: public hamon::false_type
-{};
+// Note:
+// gcc の __atomic_fetch_sub は浮動小数点数に対応していない(gcc-16 現在)
+// SFINAEでテストしたかったが、引数の型違いの場合はハードエラーになってしまうようだ。
+// (関数自体が無い場合はOK) 
 
-template <typename T>
-struct has_builtin_atomic_fetch_sub<T, hamon::void_t<decltype(__atomic_fetch_sub(hamon::declval<T*>(), hamon::declval<T>(), 0))>>
-	: public hamon::true_type
-{};
-
-template <typename T, typename = hamon::enable_if_t<has_builtin_atomic_fetch_sub<T>::value>>
+// floating_point
+#if HAMON_HAS_BUILTIN(__atomic_fetch_sub) && !defined(HAMON_GCC)
+template <HAMON_CONSTRAINT(hamon::floating_point, T)>
 HAMON_CXX14_CONSTEXPR T
 atomic_fetch_sub_impl(T* ptr, T val, hamon::memory_order order, hamon::detail::overload_priority<2>)
 {
 	return __atomic_fetch_sub(ptr, val, hamon::detail::to_gcc_memory_order(order));
 }
+#endif
 
+// integral
 #if defined(HAMON_MSVC)
 template <HAMON_CONSTRAINT(hamon::integral, T)>
 HAMON_CXX14_CONSTEXPR T
@@ -55,8 +51,16 @@ atomic_fetch_sub_impl(T* ptr, T val, hamon::memory_order order, hamon::detail::o
 	(void)order;
 	return hamon::detail::interlocked_exchange_add(ptr, static_cast<T>(~val + 1));
 }
+#elif HAMON_HAS_BUILTIN(__atomic_fetch_sub)
+template <HAMON_CONSTRAINT(hamon::integral, T)>
+HAMON_CXX14_CONSTEXPR T
+atomic_fetch_sub_impl(T* ptr, T val, hamon::memory_order order, hamon::detail::overload_priority<1>)
+{
+	return __atomic_fetch_sub(ptr, val, hamon::detail::to_gcc_memory_order(order));
+}
 #endif
 
+// ビルトイン関数等が対応していない場合のジェネリックな実装
 template <typename T>
 HAMON_CXX14_CONSTEXPR T
 atomic_fetch_sub_impl(T* ptr, T val, hamon::memory_order order, hamon::detail::overload_priority<0>)
